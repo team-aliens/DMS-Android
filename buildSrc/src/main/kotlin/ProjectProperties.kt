bda73f0411866df1b7e5f48ab8b7b8898318a77e
임세현 <80076029+limsaehyun@users.noreply.github.com>
2022-09-28 08:26:49 +0900

    Merge: Migration Kotlin DSL (#6)
    * ⚙️ :: gitignore
    
    * 📝 :: create design system module
    
    * ⚙️ :: migrate kotlin dsl
    
    * ⚰️ :: unused testcase
    
    * ⚙️ :: gradle setup
    
    * ♻️ :: kt lint rule에 따라 코드 정리

---
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -0,0 +1,21 @@
+import org.gradle.api.JavaVersion
+
+object ProjectProperties {
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+
+    const val APPLICATION_ID = "com.example.dms_android"
+
+    const val GRADLE_ANDROID = "7.2.1"
+    const val GRADLE_KOTLIN = "1.6.10"
+
+    val JAVA_VERSION = JavaVersion.VERSION_1_8
+    const val KOTLIN_VERSION = "1.6.10"
+    const val KOTLINX_COROUTINES = "1.6.0"
+
+    const val BUILD_GRADLE = "4.2.1"
+    const val COMPILE_SDK_VERSION = 32
+    const val BUILD_TOOLS_VERSION = "32.0.0"
+    const val MIN_SDK_VERSION = 26
+    const val TARGET_SDK_VERSION = 32
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DMS_Android"
+
+include (":presentation")
+include (":data")
+include (":domain")
+include (":design-system")
