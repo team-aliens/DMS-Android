fdba5d7718e98da5354613dac2be60bcca4f171f
임세현 <80076029+limsaehyun@users.noreply.github.com>
2022-09-30 09:56:22 +0900

    🔀 :: Design System Theme (#17)
    * ⚰️ :: unused file
    
    * 📑 :: font (NotoSansKR)
    
    * ⚰️ :: 기존 theme 의존성 제거
    
    * ⚡️ :: dormClickable
    
    * 📑 :: DormTypography
    
    * 📑 :: DormTheme
    
    * 📑 :: DormIcons
    
    * 📑 :: DormPadding
    
    * 📑 :: DormColors

---
+++ design-system/src/main/java/com/example/design_system/modifier/clickable.kt
@@ -0,0 +1,29 @@
+package com.example.design_system.modifier
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+
+@Stable
+internal fun Modifier.dormClickable(
+    rippleEnabled: Boolean = true,
+    rippleColor: Color? = null,
+    onClick: (() -> Unit)?,
+) = if(onClick != null) {
+    composed {
+        clickable(
+            onClick = onClick,
+            indication = rememberRipple(
+                color = rippleColor ?: Color.Unspecified,
+            ).takeIf {
+                rippleEnabled
+            },
+            interactionSource = remember { MutableInteractionSource() },
+        )
+    }
+} else this
