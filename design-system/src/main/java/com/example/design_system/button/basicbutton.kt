29aff68e69a3a824f1816620ec0edb0b2f38ad28
limsaehyun <sh00710000@gmail.com>
2022-10-06 11:57:36 +0900

    fix: basic button은 외부에서 접근 불가

--- design-system/src/main/java/com/example/design_system/button/basicbutton.kt
+++ design-system/src/main/java/com/example/design_system/button/basicbutton.kt
@@ -1,5 +1,6 @@
-package com.example.design_system.component
+package com.example.design_system.button
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -10,9 +11,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
@@ -20,15 +23,16 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.design_system.modifier.dormClickable
 import com.example.design_system.typography.ButtonText
+import com.example.design_system.utils.runIf
 
 @Composable
-fun BasicButton(
+private fun BasicButton(
     modifier: Modifier = Modifier,
     shape: Shape = RectangleShape,
     backgroundColor: Color,
     disbledColor: Color,
     rippleColor: Color,
-    rippleEnabled: Boolean = true,
+    enabled: Boolean = true,
     onClick: () -> Unit,
     content: @Composable (isPressed: Boolean) -> Unit
 ) {
@@ -38,15 +42,19 @@ fun BasicButton(
     val isPressed = interactionSource.collectIsPressedAsState()
 
     val btnColor =
-        if (!rippleEnabled) disbledColor else if (isPressed.value) rippleColor else backgroundColor
+        if (!enabled) disbledColor else if (isPressed.value) rippleColor else backgroundColor
 
     Box(
         modifier = modifier
-            .dormClickable(
-                rippleEnabled = rippleEnabled,
-                rippleColor = rippleColor,
-                onClick = onClick,
-            )
+            .runIf(enabled) {
+                composed {
+                    dormClickable(
+                        rippleEnabled = enabled,
+                        rippleColor = rippleColor,
+                        onClick = onClick,
+                    )
+                }
+            }
             .background(
                 color = btnColor,
                 shape = shape,
@@ -58,7 +66,7 @@ fun BasicButton(
 }
 
 @Composable
-fun BasicContainedButton(
+private fun BasicContainedButton(
     modifier: Modifier = Modifier,
     text: String,
     textColor: Color,
@@ -66,7 +74,7 @@ fun BasicContainedButton(
     backgroundColor: Color,
     disbledColor: Color,
     rippleColor: Color,
-    rippleEnabled: Boolean = true,
+    enabled: Boolean = true,
     onClick: () -> Unit,
 ) {
 
@@ -76,7 +84,7 @@ fun BasicContainedButton(
         backgroundColor = backgroundColor,
         disbledColor = disbledColor,
         rippleColor = rippleColor,
-        rippleEnabled = rippleEnabled,
+        enabled = enabled,
         onClick = onClick,
     ) {
         ButtonText(
@@ -87,7 +95,7 @@ fun BasicContainedButton(
 }
 
 @Composable
-fun BasicOutlineButton(
+private fun BasicOutlineButton(
     modifier: Modifier = Modifier,
     text: String,
     textColor: Color,
@@ -95,32 +103,41 @@ fun BasicOutlineButton(
     backgroundColor: Color,
     disbledColor: Color,
     rippleColor: Color,
-    rippleEnabled: Boolean = true,
+    enabled: Boolean = true,
     onClick: () -> Unit,
 ) {
+
+    val outlineBackgroundColor by animateColorAsState(
+        if(enabled) backgroundColor else disbledColor
+    )
+
+    val outlineTextColor by animateColorAsState(
+        if(enabled) textColor else disbledColor
+    )
 
     BasicButton(
         modifier = modifier
             .border(
                 width = 1.dp,
-                color = backgroundColor,
+                color = outlineBackgroundColor,
+                shape = shape,
             ),
         shape = shape,
         backgroundColor = Color.Transparent,
-        disbledColor = disbledColor,
+        disbledColor = Color.Transparent,
         rippleColor = rippleColor,
-        rippleEnabled = rippleEnabled,
+        enabled = enabled,
         onClick = onClick,
     ) {
         ButtonText(
             text = text,
-            color = textColor,
+            color = outlineTextColor,
         )
     }
 }
 
 @Composable
-fun BasicContainedRoundButton(
+internal fun BasicContainedRoundButton(
     modifier: Modifier = Modifier,
     text: String,
     textColor: Color,
@@ -128,7 +145,7 @@ fun BasicContainedRoundButton(
     backgroundColor: Color,
     disbledColor: Color,
     rippleColor: Color,
-    rippleEnabled: Boolean = true,
+    enabled: Boolean = true,
     onClick: () -> Unit,
 ) {
     BasicContainedButton(
@@ -139,8 +156,33 @@ fun BasicContainedRoundButton(
         backgroundColor = backgroundColor,
         disbledColor = disbledColor,
         rippleColor = rippleColor,
-        rippleEnabled = rippleEnabled,
-        onClick = onClick
+        enabled = enabled,
+        onClick = onClick,
+    )
+}
+
+@Composable
+internal fun BasicOutlineRoundButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    textColor: Color,
+    round: Dp = 0.dp,
+    backgroundColor: Color,
+    disbledColor: Color,
+    rippleColor: Color,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    BasicOutlineButton(
+        modifier = modifier,
+        text = text,
+        textColor = textColor,
+        shape = RoundedCornerShape(round),
+        backgroundColor = backgroundColor,
+        disbledColor = disbledColor,
+        rippleColor = rippleColor,
+        enabled = enabled,
+        onClick = onClick,
     )
 }
 
@@ -148,7 +190,7 @@ fun BasicContainedRoundButton(
 private val BasicLargeButtonHeight = 48.dp
 
 @Composable
-fun BasicContainedRoundLargeButton(
+internal fun BasicContainedRoundLargeButton(
     modifier: Modifier = Modifier,
     text: String,
     textColor: Color,
@@ -156,20 +198,47 @@ fun BasicContainedRoundLargeButton(
     backgroundColor: Color,
     disbledColor: Color,
     rippleColor: Color,
-    rippleEnabled: Boolean = true,
+    enabled: Boolean = true,
     onClick: () -> Unit,
 ) {
-    BasicContainedButton(
+    BasicContainedRoundButton(
         modifier = modifier
             .fillMaxWidth()
             .height(BasicLargeButtonHeight),
         text = text,
         textColor = textColor,
-        shape = RoundedCornerShape(round),
+        round = round,
         backgroundColor = backgroundColor,
         disbledColor = disbledColor,
         rippleColor = rippleColor,
-        rippleEnabled = rippleEnabled,
+        enabled = enabled,
+        onClick = onClick,
+    )
+}
+
+@Composable
+internal fun BasicOutlineRoundLargeButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    textColor: Color,
+    round: Dp = 0.dp,
+    backgroundColor: Color,
+    disbledColor: Color,
+    rippleColor: Color,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    BasicOutlineRoundButton(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(BasicLargeButtonHeight),
+        text = text,
+        textColor = textColor,
+        round = round,
+        backgroundColor = backgroundColor,
+        disbledColor = disbledColor,
+        rippleColor = rippleColor,
+        enabled = enabled,
         onClick = onClick,
     )
 }
