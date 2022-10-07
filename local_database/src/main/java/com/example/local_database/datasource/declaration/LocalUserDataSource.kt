fab89fbea8c9b1c43fc0e06715890a8896b18000
유현명 <aahspringaa4@naver.com>
2022-10-07 14:02:12 +0900

    ⚡️ :: LocalUserDataSource

---
+++ local_database/src/main/java/com/example/local_database/datasource/declaration/LocalUserDataSource.kt
@@ -0,0 +1,11 @@
+package com.example.local_database.datasource.declaration
+
+import com.example.local_database.param.UserVisibleParam
+import com.example.local_domain.entity.UserVisibleLocalEntity
+
+interface LocalUserDataSource {
+
+    suspend fun setUserVisibleInform(userVisibleParam: UserVisibleParam)
+    suspend fun fetchUserVisibleInform(): UserVisibleParam
+    suspend fun clearUserVisible()
+}
