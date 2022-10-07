de7369591141540cbcf43f61a7c83340c9a1f8f5
유현명 <aahspringaa4@naver.com>
2022-10-07 14:03:00 +0900

    ⚡️ :: Local UserVisible DataClass

---
+++ local_domain/src/main/java/com/example/local_domain/entity/UserVisibleLocalEntity.kt
@@ -0,0 +1,8 @@
+package com.example.local_domain.entity
+
+data class UserVisibleLocalEntity(
+    val surveyBoolean: Boolean,
+    val noticeBoolean: Boolean,
+    val myPageBoolean: Boolean,
+    val recentRoomBoolean: Boolean,
+)
