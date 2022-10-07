7bfce3a0bf94633a9a11ecf4f40675a16812098e
유현명 <aahspringaa4@naver.com>
2022-10-07 13:57:37 +0900

    ⚡️ :: UserApi Interface

---
+++ auth_data/src/main/java/com/example/auth_data/remote/api/UserApi.kt
@@ -0,0 +1,14 @@
+package com.example.auth_data.remote.api
+
+import com.example.auth_data.remote.request.SignInRequest
+import com.example.auth_data.remote.response.SignInResponse
+import com.example.auth_data.remote.url.DmsUrl
+import retrofit2.http.POST
+
+interface UserApi {
+
+    @POST (DmsUrl.login)
+    suspend fun postLogin(
+        signInRequest: SignInRequest,
+    ): SignInResponse
+}
