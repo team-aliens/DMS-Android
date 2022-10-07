a9469929e25ee1275690341bbcdb5c3ba35cbce7
유현명 <aahspringaa4@naver.com>
2022-10-07 14:02:27 +0900

    ⚡️ :: LocalUserDataSource Implementation

---
+++ local_database/src/main/java/com/example/local_database/datasource/implementation/LocalUserDataSourceImpl.kt
@@ -0,0 +1,30 @@
+package com.example.local_database.datasource.implementation
+
+import com.example.local_database.datasource.declaration.LocalUserDataSource
+import com.example.local_database.param.UserVisibleParam
+import com.example.local_database.storage.declaration.UserDataStorage
+import com.example.local_domain.entity.UserVisibleLocalEntity
+import javax.inject.Inject
+
+class LocalUserDataSourceImpl @Inject constructor(
+    private val userDataStorage: UserDataStorage
+): LocalUserDataSource {
+    override suspend fun setUserVisibleInform(userVisibleParam: UserVisibleParam) {
+        userDataStorage.setUserVisible(userVisibleParam)
+    }
+
+    override suspend fun fetchUserVisibleInform(): UserVisibleParam {
+        userDataStorage.apply {
+            return UserVisibleParam(
+                fetchSurveyBoolean(),
+                fetchNoticeBoolean(),
+                fetchMyPageBoolean(),
+                fetchRecentRoomBoolean(),
+            )
+        }
+    }
+
+    override suspend fun clearUserVisible() {
+        TODO("Not yet implemented")
+    }
+}
