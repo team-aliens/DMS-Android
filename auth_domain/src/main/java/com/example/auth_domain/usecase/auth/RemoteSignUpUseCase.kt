40aa37e198b17d553ae216aa5aaaea8ee2056bf7
유현명 <aahspringaa4@naver.com>
2022-10-17 09:05:26 +0900

    ⚡️ :: Auth UseCase

--- auth_domain/src/main/java/com/example/auth_domain/usecase/auth/RemoteSignUpUseCase.kt
+++ auth_domain/src/main/java/com/example/auth_domain/usecase/auth/RemoteSignUpUseCase.kt
@@ -1,8 +1,8 @@
-package com.example.auth_domain.usecase
+package com.example.auth_domain.usecase.auth
 
-import com.example.auth_domain.param.LoginParam
 import com.example.auth_domain.param.RegisterParam
 import com.example.auth_domain.repository.UserRepository
+import com.example.auth_domain.usecase.UseCase
 import javax.inject.Inject
 
 class RemoteSignUpUseCase @Inject constructor(
