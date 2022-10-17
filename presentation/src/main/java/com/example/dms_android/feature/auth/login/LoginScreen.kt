4a38efb561f93c3176665cb0c5f2611054f7ad00
유현명 <aahspringaa4@naver.com>
2022-10-17 19:54:24 +0900

    ♻️ :: Review Refactor

--- presentation/src/main/java/com/example/dms_android/feature/auth/login/LoginScreen.kt
+++ presentation/src/main/java/com/example/dms_android/feature/auth/login/LoginScreen.kt
@@ -2,27 +2,38 @@ package com.example.dms_android.feature.auth.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.design_system.button.DormButtonColor
 import com.example.dms_android.R
 import com.example.design_system.color.DormColor
 import com.example.design_system.textfield.DormTextField
-import com.example.design_system.typography.NotoSansFamily
 import com.example.design_system.button.DormTextCheckBox
 import com.example.design_system.button.DormContainedLargeButton
+import com.example.design_system.typography.Body4
 import com.example.design_system.typography.Body6
+import com.example.design_system.typography.Title5
 
 @Composable
 fun LoginScreen() {
@@ -41,17 +52,18 @@ fun LoginScreen() {
 
 @Composable
 fun MainTitle() {
-    val mainTitle = "DMS For Android"
-    val subTitle = "더 편한 기숙사 생활을 위해"
     Box(
         contentAlignment = Alignment.TopStart
     ) {
-        Column() {
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Image(
                 modifier = Modifier
                     .padding(
-                        start = 15.dp,
-                        top = 40.dp,
+                        top = 56.dp,
                         bottom = 7.dp
                     )
                     .height(85.dp)
@@ -59,29 +71,12 @@ fun MainTitle() {
                 painter = painterResource(id = R.drawable.ic_information_toast),
                 contentDescription = "MainLogo",
             )
-            Text(
+            Title5(text = "DMS For Android")
+            Spacer(
                 modifier = Modifier
-                    .padding(
-                        top = 10.dp,
-                        start = 15.dp
-                    ),
-                text = mainTitle,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                style = TextStyle(color = DormColor.Gray900),
+                    .height(1.dp)
             )
-            Text(
-                modifier = Modifier
-                    .padding(
-                        top = 10.dp,
-                        start = 15.dp
-                    ),
-                text = subTitle,
-                fontSize = 16.sp,
-                fontFamily = NotoSansFamily,
-                fontWeight = FontWeight.Normal,
-                style = TextStyle(color = DormColor.Gray600),
-            )
+            Body4(text = "더 편한 기숙사 생활을 위해")
         }
     }
 }
@@ -97,7 +92,11 @@ fun TextField() {
     ) {
         Column(
             modifier = Modifier
-                .padding(16.dp),
+                .padding(
+                    start = 16.dp,
+                    top = 6.dp,
+                    end = 16.dp
+                ),
         ) {
             Spacer(
                 modifier = Modifier
