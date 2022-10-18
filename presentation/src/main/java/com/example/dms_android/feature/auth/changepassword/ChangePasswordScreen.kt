e548d028dd94f555e8a0804e03b415058a7b7910
유현명 <aahspringaa4@naver.com>
2022-10-19 00:08:17 +0900

    ⚡️ :: ChangePasswordScreen

--- presentation/src/main/java/com/example/dms_android/feature/auth/changepassword/ChangePasswordScreen.kt
+++ presentation/src/main/java/com/example/dms_android/feature/auth/changepassword/ChangePasswordScreen.kt
@@ -1,5 +1,7 @@
 package com.example.dms_android.feature.auth.changepassword
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.SnackbarDuration
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,19 +21,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.design_system.button.DormButtonColor
 import com.example.design_system.button.DormContainedLargeButton
 import com.example.design_system.color.DormColor
 import com.example.design_system.textfield.DormTextField
 import com.example.design_system.typography.Body4
 import com.example.dms_android.R
+import com.example.dms_android.util.EventFlow
+import com.example.dms_android.util.observeWithLifecycle
+import com.example.dms_android.viewmodel.changepw.ChangePasswordViewModel
 
 @Composable
-fun ChangePasswordScreen() {
+fun ChangePasswordScreen(
+    changePasswordViewModel: ChangePasswordViewModel = hiltViewModel()
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,7 +48,83 @@ fun ChangePasswordScreen() {
     ) {
         MainValue()
         PasswordTextField()
-        ScanNewPasswordButton()
+        ScanNewPasswordButton(changePasswordViewModel)
+    }
+}
+
+@Composable
+private fun HandleViewEffect(
+    effect: EventFlow<ChangePasswordEvent>,
+    scaffoldState: ScaffoldState
+) {
+
+    val badRequestComment = stringResource(id = R.string.ChangePasswordBadRequest)
+    val unAuthorizedComment = stringResource(id = R.string.ChangePasswordUnAuthorized)
+    val notFoundComment = stringResource(id = R.string.ChangePasswordNotFound)
+    val tooManyRequestComment = stringResource(id = R.string.TooManyRequest)
+    val serverException = stringResource(id = R.string.ServerException)
+    val unKnownException = stringResource(id = R.string.UnKnownException)
+
+    effect.observeWithLifecycle(action = {
+        when (it) {
+            is ChangePasswordEvent.ChangePasswordSuccess -> {
+                TODO("FeatureNavigator")
+            }
+
+            is ChangePasswordEvent.BadRequestException -> {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = badRequestComment,
+                    duration = SnackbarDuration.Short
+                )
+            }
+
+            is ChangePasswordEvent.UnAuthorizedException -> {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = unAuthorizedComment,
+                    duration = SnackbarDuration.Short
+                )
+            }
+
+            is ChangePasswordEvent.NotFoundException -> {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = notFoundComment,
+                    duration = SnackbarDuration.Short
+                )
+            }
+
+            is ChangePasswordEvent.TooManyRequestException -> {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = tooManyRequestComment,
+                    duration = SnackbarDuration.Short
+                )
+            }
+
+            is ChangePasswordEvent.InternalServerException -> {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = serverException,
+                    duration = SnackbarDuration.Short
+                )
+            }
+
+            is ChangePasswordEvent.UnKnownException -> {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = unKnownException,
+                    duration = SnackbarDuration.Short
+                )
+            }
+
+            else -> {}
+        }
+    })
+    BackPressHandle()
+}
+
+@Composable
+private fun BackPressHandle() {
+    val backHandlingEnabled by remember { mutableStateOf(true) }
+    val activity = (LocalContext.current as? Activity)
+    BackHandler(backHandlingEnabled) {
+        activity?.finish()
     }
 }
 
@@ -123,7 +210,9 @@ fun PasswordTextField() {
 }
 
 @Composable
-fun ScanNewPasswordButton() {
+fun ScanNewPasswordButton(
+    changePasswordViewModel: ChangePasswordViewModel
+) {
     Box(
         contentAlignment = Alignment.BottomCenter,
         modifier = Modifier
@@ -138,7 +227,9 @@ fun ScanNewPasswordButton() {
             text = stringResource(id = R.string.Check),
             color = DormButtonColor.Blue,
         ) {
-            TODO("ViewModel Function")
+            TODO("두개의 TextField에 Null 검사 후 눌리게 구현해야함.")
+
+            changePasswordViewModel.changePassword()
         }
     }
 }
