0aa7f52804e8d24ff7c0e65c65f50c143d784ab1
hou <dragon02229@naver.com>
2022-10-24 17:18:08 +0900

    ⚡♻️ :: Manager 관련 요소 삭제 및 id,email check 추가
    schools는 todo로만 object맹들어놓음

--- auth_data/src/main/java/com/example/auth_data/remote/url/DmsUrl.kt
+++ auth_data/src/main/java/com/example/auth_data/remote/url/DmsUrl.kt
@@ -4,7 +4,6 @@ object DmsUrl {
 
     const val auth = "auth"
     const val students = "students"
-    const val managers = "managers"
 
     object User {
         const val login = auth
@@ -16,10 +15,11 @@ object DmsUrl {
 
     object Students {
         const val register = "$students/signup"
+        const val duplicateCheckId = "$students/account-id/duplication"
+        const val duplicateCheckEmail ="$students/email/duplication"
     }
 
-    object Managers {
-        const val findId = "$managers/account-id"
-        const val changePassword = "$managers/password/initialization"
+    object Schools {
+        //TODO: SChool
     }
 }
