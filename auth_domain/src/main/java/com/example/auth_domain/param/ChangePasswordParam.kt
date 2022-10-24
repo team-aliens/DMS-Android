244dcffc9dd3f2e1180ab1846cc80982f23422f5
hou <dragon02229@naver.com>
2022-10-24 17:19:28 +0900

    ⚰️ :: Managers 관련된 로직 전부 삭제

--- auth_domain/src/main/java/com/example/auth_domain/param/ChangePasswordParam.kt
+++
@@ -1,8 +0,0 @@
-package com.example.auth_domain.param
-
-data class ChangePasswordParam(
-    val accountId: String,
-    val email: String,
-    val authCode: String,
-    val newPassword: String,
-)
