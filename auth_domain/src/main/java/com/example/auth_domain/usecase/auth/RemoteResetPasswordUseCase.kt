e5559fd2b65be1c885059cc1668429f820649404
hou <dragon02229@naver.com>
2022-10-24 23:31:11 +0900

    ♻️ :: UserRepository -> StudentsRepository 변경

--- auth_domain/src/main/java/com/example/auth_domain/usecase/auth/RemoteResetPasswordUseCase.kt
+++ auth_domain/src/main/java/com/example/auth_domain/usecase/auth/RemoteResetPasswordUseCase.kt
@@ -1,14 +1,14 @@
 package com.example.auth_domain.usecase.auth
 
 import com.example.auth_domain.param.ResetPasswordParam
-import com.example.auth_domain.repository.UserRepository
+import com.example.auth_domain.repository.StudentsRepository
 import com.example.auth_domain.usecase.UseCase
 import javax.inject.Inject
 
 class RemoteResetPasswordUseCase @Inject constructor(
-    private val userRepository: UserRepository
+    private val studentsRepository: StudentsRepository
 ) : UseCase<ResetPasswordParam, Unit>() {
     override suspend fun execute(data: ResetPasswordParam) {
-        userRepository.resetPassword(data)
+        studentsRepository.resetPassword(data)
     }
 }
--- auth_domain/src/main/java/com/example/auth_domain/usecase/auth/duplicateCheckEmailUseCase.kt
+++ auth_domain/src/main/java/com/example/auth_domain/usecase/auth/duplicateCheckEmailUseCase.kt
@@ -1,13 +1,13 @@
 package com.example.auth_domain.usecase.auth
 
-import com.example.auth_domain.repository.UserRepository
+import com.example.auth_domain.repository.StudentsRepository
 import com.example.auth_domain.usecase.UseCase
 import javax.inject.Inject
 
 class duplicateCheckEmailUseCase @Inject constructor(
-    private val userRepository: UserRepository
+    private val studentsRepository: StudentsRepository
 ) : UseCase<String, Unit>() {
     override suspend fun execute(data: String) {
-        userRepository.duplicateCheckEmail(data)
+        studentsRepository.duplicateCheckEmail(data)
     }
 }
