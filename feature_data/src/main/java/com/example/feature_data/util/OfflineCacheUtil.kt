589a8009af30ac0bcc29ee516e7a30a2fefce60e
유현명 <aahspringaa4@naver.com>
2022-10-25 12:18:09 +0900

    ⚡️ :: ErrorCatching & Deal with Remote Local

---
+++ feature_data/src/main/java/com/example/feature_data/util/OfflineCacheUtil.kt
@@ -0,0 +1,40 @@
+package com.example.feature_data.util
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class OfflineCacheUtil<T> {
+
+    private lateinit var fetchLocalData: suspend () -> T
+    private lateinit var fetchRemoteData: suspend () -> T
+    private var isNeedRefresh: suspend (localData: T, remoteData: T) -> Boolean =
+        { localData, remoteData -> localData != remoteData }
+    private lateinit var refreshLocalData: suspend (remoteData: T) -> Unit
+
+    fun remoteData(fetchRemoteData: suspend () -> T): OfflineCacheUtil<T> =
+        this.apply { this.fetchRemoteData = fetchRemoteData }
+
+    fun compareData(isNeedRefresh: suspend (localData: T, remoteData: T) -> Boolean): OfflineCacheUtil<T> =
+        this.apply { this.isNeedRefresh = isNeedRefresh }
+
+    fun doOnNeedRefresh(refreshLocalData: suspend (remoteData: T) -> Unit): OfflineCacheUtil<T> =
+        this.apply { this.refreshLocalData = refreshLocalData }
+
+    fun createFlow(): Flow<T> =
+        flow {
+            try {
+                val localData = fetchLocalData()
+                emit(localData)
+
+                val remoteData = fetchRemoteData()
+                if (isNeedRefresh(localData, remoteData)) {
+                    refreshLocalData(remoteData)
+                    emit(remoteData)
+                }
+            } catch (e: NullPointerException) {
+                val remoteData = fetchRemoteData()
+                refreshLocalData(remoteData)
+                emit(remoteData)
+            }
+        }
+}
