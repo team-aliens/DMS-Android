41a29c7568c3396f7bc6bac806c7b82200c0b772
hou <dragon02229@naver.com>
2022-10-29 21:40:09 +0900

    ⚰️ :: pr 단위 축소를 위해 viewmodel들 제거

--- presentation/src/main/java/com/example/dms_android/viewmodel/auth/register/DuplicateCheckEmailViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dms_android.viewmodel.auth.register
-
-import com.example.dms_android.base.BaseViewModel
-import javax.inject.Inject
-
-class DuplicateCheckEmailViewModel @Inject constructor(
-    private val duplicateCheckEmailViewModel: DuplicateCheckEmailViewModel
-): BaseViewModel<> {
-}
