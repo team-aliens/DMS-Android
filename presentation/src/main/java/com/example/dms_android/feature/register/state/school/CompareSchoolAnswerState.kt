2d12039adc684e9ac8e916174549a7528ee99232
hou <dragon02229@naver.com>
2022-10-30 11:23:35 +0900

    ⚡ :: 학교 확인 질문 답변 검사 Event, State 작성

---
+++ presentation/src/main/java/com/example/dms_android/feature/register/state/school/CompareSchoolAnswerState.kt
@@ -0,0 +1,15 @@
+package com.example.dms_android.feature.register.state.school
+
+import com.example.dms_android.base.MviState
+
+data class CompareSchoolAnswerState(
+    val schoolAnswer: String,
+) : MviState {
+
+    companion object {
+        fun initial() =
+            CompareSchoolAnswerState(
+                schoolAnswer = "",
+            )
+    }
+}
