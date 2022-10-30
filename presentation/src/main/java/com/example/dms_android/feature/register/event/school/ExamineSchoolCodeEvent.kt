eb9658524cec24a35e406a2e2d12c33ff4efdc0b
hou <dragon02229@naver.com>
2022-10-30 12:15:02 +0900

    ♻️ :: codeSuccess 수정 및 줄붙힘

--- presentation/src/main/java/com/example/dms_android/feature/register/event/school/ExamineSchoolCodeEvent.kt
+++ presentation/src/main/java/com/example/dms_android/feature/register/event/school/ExamineSchoolCodeEvent.kt
@@ -1,21 +1,17 @@
 package com.example.dms_android.feature.register.event.school
 
 import com.example.dms_android.base.MviEvent
+import java.util.UUID
 
 
 sealed class ExamineSchoolCodeEvent: MviEvent {
 
     data class InputSchoolCode(val schoolCode: String) : ExamineSchoolCodeEvent()
 
-    object ExamineSchoolCodeSuccess : ExamineSchoolCodeEvent()
-
+    data class ExamineSchoolCodeSuccess(val schoolId: UUID) : ExamineSchoolCodeEvent()
     object BadRequestException : ExamineSchoolCodeEvent()
-
     object UnAuthorizedException : ExamineSchoolCodeEvent()
-
     object TooManyRequestException : ExamineSchoolCodeEvent()
-
     object InternalServerException : ExamineSchoolCodeEvent()
-
     object UnknownException : ExamineSchoolCodeEvent()
 }
