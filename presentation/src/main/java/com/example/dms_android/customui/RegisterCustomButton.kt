af61942e16da43a0cbe7de04aaee69d8630fd879
hou <dragon02229@naver.com>
2022-11-07 19:15:08 +0900

    ⚡ :: 회원가입 커스텀 버튼 구현

---
+++ presentation/src/main/java/com/example/dms_android/customui/RegisterCustomButton.kt
@@ -0,0 +1,46 @@
+package com.example.dms_android.customui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.getColorOrThrow
+import androidx.core.content.res.getDrawableOrThrow
+import com.example.dms_android.R
+import com.example.dms_android.databinding.RegisterCustomButtonBinding
+
+
+@SuppressLint("ResourceType")
+class RegisterCustomButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private var binding: RegisterCustomButtonBinding
+
+    init {
+        binding = RegisterCustomButtonBinding.inflate(LayoutInflater.from(context), this, true)
+
+        attrs?.run {
+            val typedArr = context.obtainStyledAttributes(attrs, R.styleable.RegisterCustomButton)
+            setMainText(typedArr.getString(R.styleable.RegisterCustomButton_first_text) ?: "값없음")
+            setCustomColorText(typedArr.getColorOrThrow(R.styleable.RegisterCustomButton_custom_color_text))
+            if (!isInEditMode) {
+                typedArr.recycle()
+            }
+        }
+    }
+
+    private fun setMainText(text_string: String) {
+        binding.tvMain.text = text_string
+    }
+
+    private fun setCustomColorText(@ColorRes text_int: Int) {
+        binding.tvMain.setTextColor(text_int)
+    }
+}
