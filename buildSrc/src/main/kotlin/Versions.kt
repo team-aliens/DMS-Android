c8d7336496001840972e2b62698b69d7e683fce8
유현명 <aahspringaa4@naver.com>
2022-11-08 12:23:04 +0900

    ⚙️ :: Room Version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val APP_COMPAT = "1.3.0"
     const val ACTIVITY_KTX = "1.2.3"
     const val FRAGMENT_KTX = "1.3.4"
-    const val ROOM = "2.5.0-alpha02"
+    const val ROOM = "2.4.0"
     const val DYNAMIC_TOAST = "3.3.1"
     const val DATASTORE = "1.0.0"
 
