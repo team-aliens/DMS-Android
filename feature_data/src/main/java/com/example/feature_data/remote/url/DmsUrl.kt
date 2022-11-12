d8b5abb303004a23f5661ccdfc02a39009996be7
유현명 <aahspringaa4@naver.com>
2022-11-13 00:49:06 +0900

    ⚡️ :: Dms Url

--- feature_data/src/main/java/com/example/feature_data/remote/url/DmsUrl.kt
+++ feature_data/src/main/java/com/example/feature_data/remote/url/DmsUrl.kt
@@ -3,11 +3,16 @@ package com.example.feature_data.remote.url
 object DmsUrl {
 
     const val meal = "meals"
-
     const val notices = "notices"
+    const val students = "/students"
+    const val points = "points"
 
     object Notice {
         const val NewNoticeBoolean = "$notices/status"
         const val NoticeDetail = "$notices/{notice-id}"
+    }
+
+    object MyPage {
+        const val MyPage = "${students}/profile"
     }
 }
