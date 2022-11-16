72973689a6f0d84c7e4293e528df5cb336db8a28
유현명 <aahspringaa4@naver.com>
2022-11-16 11:40:13 +0900

    ⚡️ :: BASE_URL

--- di/src/main/java/com/example/di/NetWorkModule.kt
+++ di/src/main/java/com/example/di/NetWorkModule.kt
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 @Module
 @InstallIn(SingletonComponent::class)
 object NetWorkModule {
-    private const val BASE_URL = "BASE_URL"
+    private const val BASE_URL = "http://3.39.162.197:8080"
 
     @Provides
     fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
