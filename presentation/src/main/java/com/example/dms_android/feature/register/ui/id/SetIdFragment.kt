64246d304bc5a7b23ecd8ec20cc91f8fd888169e
hou <dragon02229@naver.com>
2022-11-20 17:15:07 +0900

    ⚡ :: Implement SetIdFragment

---
+++ presentation/src/main/java/com/example/dms_android/feature/register/ui/id/SetIdFragment.kt
@@ -0,0 +1,120 @@
+package com.example.dms_android.feature.register.ui.id
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.fragment.app.viewModels
+import com.example.dms_android.databinding.FragmentSetIdBinding
+import com.example.dms_android.R
+import com.example.dms_android.base.BaseFragment
+import com.example.dms_android.feature.register.event.id.SetIdEvent
+import com.example.dms_android.util.invisible
+import com.example.dms_android.util.repeatOnStarted
+import com.example.dms_android.util.visible
+import com.example.dms_android.viewmodel.auth.register.id.SetIdViewModel
+
+class SetIdFragment : BaseFragment<FragmentSetIdBinding>(
+    R.layout.fragment_set_id
+) {
+    private val vm: SetIdViewModel by viewModels()
+
+    private var a = false
+    private var b = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        repeatOnStarted {
+            vm.examineGradeEvent.collect { event -> handleEvent(event) }
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun handleEvent(event: SetIdEvent) {
+        when (event) {
+            is SetIdEvent.ExamineGradeSuccess -> {
+                binding.clName.visible()
+                binding.tvRealName.text = "${vm.name.value}님이 맞습니까?"
+            }
+
+            is SetIdEvent.DuplicateIdSuccess -> {
+                b = true
+                binding.etId.setBackgroundResource(R.drawable.register_et_background)
+                binding.tvError.invisible()
+            }
+
+            is SetIdEvent.ExamineConflict -> {
+                showShortToast(R.string.CheckGrade.toString())
+                binding.etGrade.setBackgroundResource(R.drawable.register_et_error_background)
+                binding.etClass.setBackgroundResource(R.drawable.register_et_error_background)
+                binding.etNumber.setBackgroundResource(R.drawable.register_et_error_background)
+            }
+
+            is SetIdEvent.ExamineGradeBadRequest -> {
+                showShortToast(R.string.LoginBadRequest.toString())
+            }
+
+            is SetIdEvent.ExamineGradeNotFound -> {
+                showShortToast(R.string.CheckGrade.toString())
+                binding.etGrade.setBackgroundResource(R.drawable.register_et_error_background)
+                binding.etClass.setBackgroundResource(R.drawable.register_et_error_background)
+                binding.etNumber.setBackgroundResource(R.drawable.register_et_error_background)
+            }
+
+            is SetIdEvent.DuplicateIdConflict -> {
+                binding.etId.setBackgroundResource(R.drawable.register_et_error_background)
+                binding.tvError.visible()
+            }
+
+            is SetIdEvent.ErrorMessage -> showShortToast(event.message)
+        }
+    }
+
+    override fun initView() {
+        binding.btnVerificationCode.isClickable = false
+
+        if (binding.etClass.length() > 1 && binding.etNumber.length() > 1 && binding.etGrade.length() > 1) {
+            vm.grade.value = binding.etGrade.toString().toInt()
+            vm.classRoom.value = binding.etClass.toString().toInt()
+            vm.number.value = binding.etNumber.toString().toInt()
+
+            vm.examineGrade()
+        }
+
+        binding.btnOKName.setOnClickListener {
+            binding.clName.isGone
+            a = true
+        }
+
+        if (a) {
+            binding.etClass.isClickable = false
+            binding.etClass.isFocusable = false
+            binding.etGrade.isClickable = false
+            binding.etGrade.isFocusable = false
+            binding.etNumber.isClickable = false
+            binding.etNumber.isFocusable = false
+        }
+
+        if (a && b) {
+            binding.btnVerificationCode.setBackgroundResource(R.drawable.register_custom_active_btn_background)
+            binding.btnVerificationCode.isClickable = true
+        } else {
+            binding.btnVerificationCode.setBackgroundResource(R.drawable.register_custom_btn_background)
+            binding.btnVerificationCode.isClickable = false
+        }
+
+        binding.btnVerificationCode.setOnClickListener {
+            //TODO: 다음화면으로 이동하는 코드 작성 필요
+        }
+
+        binding.ivBack.setOnClickListener {
+            //TODO: 뒤로가는 코드 작성
+        }
+    }
+}
