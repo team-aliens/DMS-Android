debe5d87075fd4f46893711c87770af8e5e62da4
hou <dragon02229@naver.com>
2022-11-22 09:09:32 +0900

    ⚡ :: add event & wrap event

--- presentation/src/main/java/com/example/dms_android/feature/register/event/email/RegisterEmailEvent.kt
+++ presentation/src/main/java/com/example/dms_android/feature/register/event/email/RegisterEmailEvent.kt
@@ -1,10 +1,15 @@
 package com.example.dms_android.feature.register.event.email
 
 import com.example.dms_android.base.MviEvent
+import com.example.dms_android.feature.register.event.SignUpEvent
 
 sealed class RegisterEmailEvent : MviEvent {
     object SendEmailSuccess : RegisterEmailEvent()
     object CheckEmailSuccess : RegisterEmailEvent()
+    object BadRequestException : RegisterEmailEvent()
+    object CheckEmailNotFound : RegisterEmailEvent()
     object CheckEmailUnauthorized : RegisterEmailEvent()
-    data class ErrorMessage(val message: String) : RegisterEmailEvent()
+    object TooManyRequestsException : RegisterEmailEvent()
+    object InternalServerException : RegisterEmailEvent()
+    object UnKnownException : RegisterEmailEvent()
 }
