7bda142a07c35c0f0fb091a3d8c6dd31a3c901db
hou <dragon02229@naver.com>
2022-12-05 13:37:40 +0900

    ⚡ :: HomeFragment

--- presentation/src/main/java/com/example/dms_android/feature/MainActivity.kt
+++ presentation/src/main/java/com/example/dms_android/feature/MainActivity.kt
@@ -5,6 +5,7 @@ import com.example.dms_android.base.BaseActivity
 import com.example.dms_android.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import com.example.dms_android.R
+import com.example.dms_android.feature.register.ui.home.HomeFragment
 import com.example.dms_android.feature.register.ui.school.ConfirmSchoolFragment
 import com.example.dms_android.feature.register.ui.school.SchoolCertificationFragment
 
@@ -18,7 +19,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 
     override fun initView() {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container, SchoolCertificationFragment())
+            .replace(R.id.container, HomeFragment())
             .commit()
     }
 
