01cb179517f5bc3e120879b1611c0b843533b93a
hou <dragon02229@naver.com>
2022-12-14 18:22:40 +0900

    ⚡ :: writing out signUpViewModel

--- presentation/src/main/java/com/example/dms_android/viewmodel/auth/register/SignUpViewModel.kt
+++ presentation/src/main/java/com/example/dms_android/viewmodel/auth/register/SignUpViewModel.kt
@@ -11,6 +11,7 @@ import com.example.auth_domain.param.RegisterParam
 import com.example.auth_domain.usecase.students.RemoteSignUpUseCase
 import com.example.dms_android.base.BaseViewModel
 import com.example.dms_android.feature.register.event.SignUpEvent
+import com.example.dms_android.feature.register.event.email.RegisterEmailEvent
 import com.example.dms_android.feature.register.state.SignUpState
 import com.example.dms_android.util.MutableEventFlow
 import com.example.dms_android.util.asEventFlow
@@ -22,14 +23,6 @@ import javax.inject.Inject
 class SignUpViewModel @Inject constructor(
     private val remoteSignUpUseCase: RemoteSignUpUseCase
 ) : BaseViewModel<SignUpState, SignUpEvent>() {
-
-    fun setSchoolCode(schoolCode: String) {
-        sendEvent(SignUpEvent.InputSchoolCode(schoolCode))
-    }
-
-    fun setSchoolAnswer(schoolAnswer: String) {
-        sendEvent(SignUpEvent.InputSchoolAnswer(schoolAnswer))
-    }
 
     private val parameter =
         RegisterParam(
@@ -53,18 +46,24 @@ class SignUpViewModel @Inject constructor(
             kotlin.runCatching {
                 remoteSignUpUseCase.execute(parameter)
             }.onSuccess {
-                SignUpEvent.SignUpSuccess
+                event(SignUpEvent.SignUpSuccess)
             }.onFailure {
                 when (it) {
-                    is BadRequestException -> SignUpEvent.BadRequestException
-                    is UnauthorizedException -> SignUpEvent.UnAuthorizedException
-                    is NotFoundException -> SignUpEvent.NotFoundException
-                    is TooManyRequestException -> SignUpEvent.TooManyRequestsException
-                    is ConflictException -> SignUpEvent.ConflictException
-                    is ServerException -> SignUpEvent.InternalServerException
-                    else -> SignUpEvent.UnKnownException
+                    is BadRequestException -> event(SignUpEvent.BadRequestException)
+                    is UnauthorizedException -> event(SignUpEvent.UnAuthorizedException)
+                    is NotFoundException -> event(SignUpEvent.NotFoundException)
+                    is TooManyRequestException -> event(SignUpEvent.TooManyRequestsException)
+                    is ConflictException -> event(SignUpEvent.ConflictException)
+                    is ServerException -> event(SignUpEvent.InternalServerException)
+                    else -> event(SignUpEvent.UnKnownException)
                 }
             }
+        }
+    }
+
+    private fun event(event: SignUpEvent) {
+        viewModelScope.launch {
+            _signUpEvent.emit(event)
         }
     }
 
@@ -73,48 +72,6 @@ class SignUpViewModel @Inject constructor(
 
 
     override fun reduceEvent(oldState: SignUpState, event: SignUpEvent) {
-        when (event) {
-            is SignUpEvent.InputAccountId -> {
-                setState(oldState.copy(accountId = event.accountId))
-            }
-
-            is SignUpEvent.InputPassword -> {
-                setState(oldState.copy(password = event.password))
-            }
-
-            is SignUpEvent.InputAuthCode -> {
-                setState(oldState.copy(authCode = event.authCode))
-            }
-
-            is SignUpEvent.InputClassRoom -> {
-                setState(oldState.copy(classRoom = event.classRoom))
-            }
-
-            is SignUpEvent.InputEmail -> {
-                setState(oldState.copy(email = event.email))
-            }
-
-            is SignUpEvent.InputGrade -> {
-                setState(oldState.copy(grade = event.grade))
-            }
-
-            is SignUpEvent.InputNumber -> {
-                setState(oldState.copy(number = event.number))
-            }
-
-            is SignUpEvent.InputSchoolCode -> {
-                setState(oldState.copy(schoolCode = event.schoolCode))
-            }
-
-            is SignUpEvent.InputProfileImageUrl -> {
-                setState(oldState.copy(profileImageUrl = event.profileImageUrl))
-            }
-
-            is SignUpEvent.InputSchoolAnswer -> {
-                setState(oldState.copy(schoolAnswer = event.schoolAnswer))
-            }
-
-            else -> {}
-        }
+        //TODO
     }
 }
