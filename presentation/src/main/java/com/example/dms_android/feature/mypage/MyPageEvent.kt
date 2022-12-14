18c150375fed87652803ed2bfe0244ec3acb9a1b
유현명 <aahspringaa4@naver.com>
2022-12-14 16:41:18 +0900

    ⚡️ :: MyPage Event & State

---
+++ presentation/src/main/java/com/example/dms_android/feature/mypage/MyPageEvent.kt
@@ -0,0 +1,14 @@
+package com.example.dms_android.feature.mypage
+
+import com.example.dms_android.base.MviEvent
+import com.example.feature_domain.enums.PointType
+
+sealed class MyPageEvent: MviEvent {
+    
+    data class ChangeType(val type: PointType): MyPageEvent()
+    object UnAuthorizedException: MyPageEvent()
+    object ForbiddenException: MyPageEvent()
+    object TooManyException: MyPageEvent()
+    object InternalServerException: MyPageEvent()
+    object UnknownException: MyPageEvent()
+}
