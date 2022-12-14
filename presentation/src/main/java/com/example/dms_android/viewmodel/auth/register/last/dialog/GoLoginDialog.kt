19aa4a7961c643f544fe8d33dc2726e223c74258
hou <dragon02229@naver.com>
2022-12-14 17:21:49 +0900

    ⚡ :: Implement GoLoginDialog class

---
+++ presentation/src/main/java/com/example/dms_android/viewmodel/auth/register/last/dialog/GoLoginDialog.kt
@@ -0,0 +1,37 @@
+package com.example.dms_android.viewmodel.auth.register.last.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.Window
+import androidx.databinding.DataBindingUtil
+import com.example.dms_android.R
+import com.example.dms_android.databinding.DialogSignUpBinding
+
+class GoLoginDialog(
+    private val context: Context,
+    private val onYesClick: () -> Unit = {},
+) {
+
+    fun callDialog() {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val binding: DialogSignUpBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.dialog_sign_up,
+            null,
+            false
+        )
+        dialog.setContentView(binding.root)
+
+        binding.tvGoLogin.setOnClickListener {
+            onYesClick()
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+}
