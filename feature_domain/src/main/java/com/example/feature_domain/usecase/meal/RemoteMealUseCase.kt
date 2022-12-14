7214d4c8801132c0c1e5d16f8c72f66ab31a8328
유현명 <aahspringaa4@naver.com>
2022-12-14 16:51:30 +0900

    🐛 :: CI Error

--- feature_domain/src/main/java/com/example/feature_domain/usecase/meal/RemoteMealUseCase.kt
+++ feature_domain/src/main/java/com/example/feature_domain/usecase/meal/RemoteMealUseCase.kt
@@ -4,11 +4,12 @@ import com.example.feature_domain.entity.MealEntity
 import com.example.feature_domain.repository.MealRepository
 import com.example.feature_domain.usecase.UseCase
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 import javax.inject.Inject
 
 class RemoteMealUseCase @Inject constructor(
     private val mealRepository: MealRepository
-) : UseCase<String, Flow<MealEntity>>() {
-    override suspend fun execute(data: String) =
+) : UseCase<LocalDate, Flow<MealEntity>>() {
+    override suspend fun execute(data: LocalDate) =
         mealRepository.fetchMealValue(data)
 }
