0f6f0bcd8bc7edb8830ebcb3edf1ed93271429cb
유현명 <aahspringaa4@naver.com>
2022-12-27 22:43:39 +0900

    ⚡️ :: MyPageState DataClase

--- presentation/src/main/java/com/example/dms_android/feature/mypage/MyPageState.kt
+++ presentation/src/main/java/com/example/dms_android/feature/mypage/MyPageState.kt
@@ -4,12 +4,25 @@ import com.example.dms_android.base.MviState
 import com.example.domain.enums.PointType
 
 data class MyPageState(
-    val type: PointType
+    val type: PointType,
+    var totalPoint: Int,
+    val myPageEntity: MyPageEntity = MyPageEntity(),
 ) : MviState {
     companion object {
         fun initial() =
             MyPageState(
-                type = PointType.ALL
+                type = PointType.ALL,
+                totalPoint = 0
             )
     }
 }
+
+data class MyPageEntity(
+    var schoolName: String = "",
+    var name: String = "",
+    var gcn: String = "",
+    var profileImageUrl: String = "",
+    var bonusPoint: Int = 0,
+    var minusPoint: Int = 0,
+    var phrase: String = "",
+)
