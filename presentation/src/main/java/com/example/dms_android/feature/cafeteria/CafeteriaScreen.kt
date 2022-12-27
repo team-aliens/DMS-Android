74bd1406fc325b58bd93f03550a9c22d3d66c24e
hou <dragon02229@naver.com>
2022-12-28 00:09:12 +0900

    ⚡ :: CafeteriaScreen Logo

--- presentation/src/main/java/com/example/dms_android/feature/cafeteria/CafeteriaScreen.kt
+++ presentation/src/main/java/com/example/dms_android/feature/cafeteria/CafeteriaScreen.kt
@@ -53,7 +53,7 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun CafeteriaScreen(
-    navController: NavController
+    navController: NavController,
 ) {
 
     val pagerState = rememberPagerState(3)
@@ -88,9 +88,10 @@ fun TopBar(
     ) {
         Image(
             modifier = Modifier
-                .size(60.dp)
-                .padding(start = 13.dp, top = 10.dp),
-            painter = painterResource(id = R.drawable.temporarylogo),
+                .height(34.dp)
+                .width(97.dp)
+                .padding(start = 13.dp, top = 12.dp),
+            painter = painterResource(id = R.drawable.ic_logo),
             contentDescription = stringResource(id = R.string.MainLogo),
         )
     }
