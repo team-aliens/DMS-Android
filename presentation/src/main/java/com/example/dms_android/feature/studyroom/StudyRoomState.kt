613fa5f6c1d6315ff47d7d64b10afc6a59b9b501
limsaehyun <sh00710000@gmail.com>
2022-12-28 09:13:13 +0900

    ♻️ :: StudyRoomState 개선

--- presentation/src/main/java/com/example/dms_android/feature/studyroom/StudyRoomState.kt
+++ presentation/src/main/java/com/example/dms_android/feature/studyroom/StudyRoomState.kt
@@ -1,54 +1,48 @@
 package com.example.dms_android.feature.studyroom
 
 import com.example.dms_android.base.MviState
+import com.example.domain.entity.studyroom.StudyRoomDetailEntity
 
 data class StudyRoomState(
-    var a: String,
-    var studyRoomDetailEntity: StudyRoomDetailEntity = StudyRoomDetailEntity(),
-    var seats: List<Seat> = listOf<Seat>()
-): MviState {
+    val roomDetail: StudyRoomDetailEntity,
+) : MviState {
     companion object {
         fun initial() =
             StudyRoomState(
-                a = ""
+                roomDetail = StudyRoomDetailEntity(
+                    floor = 0,
+                    name = "",
+                    totalAvailableSeat = 0,
+                    inUseHeadCount = 0,
+                    availableSex = "",
+                    availableGrade = 0,
+                    eastDescription = "",
+                    westDescription = "",
+                    southDescription = "",
+                    northDescription = "",
+                    totalWidthSize = 2,
+                    totalHeightSize = 2,
+                    studyRoomSex = "",
+                    seats = listOf(
+                        StudyRoomDetailEntity.Seat(
+                            id = "",
+                            widthLocation = 1,
+                            heightLocation = 1,
+                            number = 0,
+                            type = StudyRoomDetailEntity.Type(
+                                id = "",
+                                name = "",
+                                color = "#FFFFFF",
+                            ),
+                            status = "",
+                            isMine = false,
+                            student = StudyRoomDetailEntity.Student(
+                                id = "",
+                                name = ""
+                            )
+                        )
+                    )
+                )
             )
     }
-}
-
-data class Seat(
-    var id: String = "",
-    var widthLocation: Int = 0,
-    var heightLocation: Int = 0,
-    var number: Int? = 0,
-    var type: StudyRoomDetailEntity.Type = StudyRoomDetailEntity.Type(),
-    var status: String = "",
-    var isMine: Boolean? = false,
-    var student: StudyRoomDetailEntity.Student = StudyRoomDetailEntity.Student(),
-)
-
-data class StudyRoomDetailEntity(
-    var floor: Int = 0,
-    var name: String = "",
-    var totalAvailableSeat: Int = 0,
-    var inUseHeadCount: Int = 0,
-    var availableSex: String = "",
-    var availableGrade: Int = 0,
-    var eastDescription: String = "",
-    var westDescription: String = "",
-    var southDescription: String = "",
-    var northDescription: String = "",
-    var totalWidthSize: Int = 0,
-    var totalHeightSize: Int = 0,
-) {
-
-    data class Type(
-        var id: String = "",
-        var name: String = "",
-        var color: String = "",
-    )
-
-    data class Student(
-        var id: String = "",
-        var name: String = "",
-    )
 }
--- presentation/src/main/java/com/example/dms_android/feature/studyroom/StudyRoomViewModel.kt
+++ presentation/src/main/java/com/example/dms_android/feature/studyroom/StudyRoomViewModel.kt
@@ -1,13 +1,9 @@
 package com.example.dms_android.feature.studyroom
 
-import android.util.Log
-import androidx.lifecycle.Transformations.map
 import androidx.lifecycle.viewModelScope
 import com.example.dms_android.base.BaseViewModel
-import com.example.dms_android.feature.register.state.home.MealList
 import com.example.dms_android.util.MutableEventFlow
 import com.example.dms_android.util.asEventFlow
-import com.example.dms_android.viewmodel.home.MealViewModel
 import com.example.domain.entity.studyroom.StudyRoomDetailEntity
 import com.example.domain.entity.studyroom.StudyRoomListEntity
 import com.example.domain.exception.BadRequestException
@@ -18,12 +14,9 @@ import com.example.domain.exception.UnauthorizedException
 import com.example.domain.usecase.studyroom.RemoteFetchStudyRoomDetailUseCase
 import com.example.domain.usecase.studyroom.RemoteFetchStudyRoomListUseCase
 import com.example.domain.usecase.studyroom.RemoteFetchStudyRoomTypeUseCase
-import com.example.local_domain.entity.meal.MealEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import java.time.LocalDate
 import javax.inject.Inject
-import kotlin.math.floor
 
 @HiltViewModel
 class StudyRoomViewModel @Inject constructor(
@@ -66,7 +59,11 @@ class StudyRoomViewModel @Inject constructor(
             kotlin.runCatching {
                 studyRoomDetailUseCase.execute(roomId)
             }.onSuccess {
-                event2(Event.FetchStudyDetail(it))
+                setState(
+                    state = state.value.copy(
+                        roomDetail = it
+                    )
+                )
             }.onFailure {
                 when (it) {
                     is BadRequestException -> event2(Event.BadRequestException)
