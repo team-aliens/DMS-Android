de13ed6902c3a306f8d7f74dcf267ad285b98b67
유현명 <aahspringaa4@naver.com>
2023-01-11 12:01:15 +0900

    ♻️ :: UseCase Parameter return Value Refacotring

--- domain/src/main/java/com/example/domain/usecase/studyroom/RemoteFetchStudyRoomTypeUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/studyroom/RemoteFetchStudyRoomTypeUseCase.kt
@@ -1,15 +1,13 @@
 package com.example.domain.usecase.studyroom
 
-import com.example.domain.entity.studyroom.StudyRoomListEntity
-import com.example.domain.entity.studyroom.StudyRoomTypeEntity
+import com.example.domain.entity.studyroom.SeatTypeEntity
 import com.example.domain.repository.StudyRoomRepository
 import com.example.domain.usecase.UseCase
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class RemoteFetchStudyRoomTypeUseCase @Inject constructor(
     private val studyRoomRepository: StudyRoomRepository,
-) : UseCase<Unit, StudyRoomTypeEntity>() {
-    override suspend fun execute(data: Unit): StudyRoomTypeEntity =
+) : UseCase<Unit, SeatTypeEntity>() {
+    override suspend fun execute(data: Unit): SeatTypeEntity =
         studyRoomRepository.fetchStudyRoomType()
 }
