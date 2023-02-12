4f2d8d1f6820ca5c0cd892664e99ae7cc38b50db
Tmdhoon2 <hks026naver@gmail.com>
2023-02-12 22:24:38 +0900

    ⚡️ :: Show last application item

--- presentation/src/main/java/team/aliens/dms_android/feature/apply/ApplicationMainScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/apply/ApplicationMainScreen.kt
@@ -3,20 +3,35 @@ package team.aliens.dms_android.feature.apply
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import team.aliens.design_system.button.DormButtonColor
 import team.aliens.design_system.button.DormContainedLargeButton
 import team.aliens.design_system.color.DormColor
 import team.aliens.design_system.typography.Body1
 import team.aliens.design_system.typography.Body5
-import team.aliens.design_system.typography.SubTitle2
+import team.aliens.design_system.typography.DormTypography
+import team.aliens.design_system.typography.NotoSansFamily
 import team.aliens.dms_android.feature.navigator.NavigationRoute
 import team.aliens.presentation.R
+
+@Preview(
+    showSystemUi = true,
+)
+@Composable
+fun Preview() {
+    ApplicationMainScreen(
+        navController = rememberNavController(),
+    )
+}
 
 @Composable
 fun ApplicationMainScreen(
@@ -60,6 +75,7 @@ fun ApplicationCard(
     content: String,
     buttonText: String,
     buttonOnClick: () -> Unit,
+    lastApplied: String = "",
 ) {
     Column(
         modifier = Modifier
@@ -71,7 +87,37 @@ fun ApplicationCard(
             .padding(20.dp),
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
-        SubTitle2(text = title)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(0.74f),
+                text = title,
+                style = DormTypography.subtitle2,
+                color = DormColor.Gray900,
+            )
+            if (lastApplied.isNotBlank()) {
+                Box(
+                    modifier = Modifier
+                        .size(
+                            width = 92.dp,
+                            height = 34.dp,
+                        )
+                        .background(
+                            color = DormColor.Lighten200,
+                            shape = RoundedCornerShape(100)
+                        ),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = lastApplied,
+                        fontFamily = NotoSansFamily,
+                        fontWeight = FontWeight.Bold,
+                        color = DormColor.DormPrimary,
+                    )
+                }
+            }
+        }
         Body5(text = content)
         DormContainedLargeButton(
             modifier = Modifier.height(40.dp),
