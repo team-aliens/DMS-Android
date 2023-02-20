a518c226fdf2c515f79f1570bb0e20ed6ba21be5
정승훈 <hks026naver@gmail.com>
2023-02-20 11:43:52 +0900

    ⚡️ :: implement day of week enum class

---
+++ presentation/src/main/java/team/aliens/dms_android/util/DayOfWeek.kt
@@ -0,0 +1,13 @@
+package team.aliens.dms_android.util
+
+enum class DayOfWeek(
+    val week: String,
+) {
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일"),
+    SATURDAY("토요일"),
+    SUNDAY("일요일")
+}
