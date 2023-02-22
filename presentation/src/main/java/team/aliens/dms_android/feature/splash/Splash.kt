2e39c85e53837266db2d71f704bb43ba66a522eb
junsuPark <krpjs0508@gmail.com>
2023-02-22 21:14:37 +0900

    ♻️ :: refactor Splash

--- presentation/src/main/java/team/aliens/dms_android/feature/splash/Splash.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/splash/Splash.kt
@@ -3,7 +3,9 @@ package team.aliens.dms_android.feature.splash
 import android.content.Context
 import android.content.Intent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
@@ -11,62 +13,63 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
-import team.aliens.design_system.color.DormColor
-import team.aliens.design_system.typography.SubTitle1
 import team.aliens.dms_android.feature.MainActivity
 import team.aliens.presentation.R
 
-@Composable
-fun Splash() {
-    val viewModel: SplashViewModel = hiltViewModel()
-    viewModel.autoLogin()
-    Splash(viewModel)
-}
-
-
+// todo 스플래시 스크린을 없애는 방법 검토 필요!@!
 @Composable
 fun Splash(
-    splashViewModel: SplashViewModel,
+    splashViewModel: SplashViewModel = hiltViewModel(),
 ) {
+
+    splashViewModel.autoLogin()
+
     val coroutineScope = rememberCoroutineScope()
+
     val context = LocalContext.current
+
     LaunchedEffect(Unit) {
         coroutineScope.launch {
             splashViewModel.eventFlow.collect {
-                when (it) {
-                    SplashViewModel.Event.AutoLoginSuccess -> {
-                        startMainActivity(context, "main")
+                moveToMainActivity(
+                    context,
+                    when (it) {
+                        SplashViewModel.Event.AutoLoginSuccess -> {
+                            "main"
+                        }
+                        SplashViewModel.Event.NeedLogin -> {
+                            "login"
+                        }
                     }
-                    SplashViewModel.Event.NeedLogin -> startMainActivity(context, "login")
-                }
-            }
-        }
-    }
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Box(contentAlignment = Alignment.TopCenter) {
-            Image(
-                modifier = Modifier.size(180.dp),
-                painter = painterResource(id = R.drawable.ic_logo),
-                contentDescription = null,
-            )
-            Column {
-                Spacer(Modifier.size(142.dp))
-                SubTitle1(
-                    text = stringResource(id = R.string.Dms),
-                    color = DormColor.Gray100,
                 )
             }
         }
     }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            modifier = Modifier.size(180.dp),
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = null,
+        )
+    }
 }
 
-fun startMainActivity(context: Context, route: String) {
-    val intent = Intent(context, MainActivity::class.java).apply {
-        putExtra("route", route)
-    }
+private fun moveToMainActivity(context: Context, route: String) {
+
+    val intent = Intent(
+        context,
+        MainActivity::class.java,
+    )
+
+    intent.putExtra("route", route)
+
     context.startActivity(intent)
+    (context as SplashActivity).finish()
 }
