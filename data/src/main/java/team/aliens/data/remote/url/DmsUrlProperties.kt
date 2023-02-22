51d8d3c40cb4bd2ae548afb5f4eac2998db60c44
junsuPark <krpjs0508@gmail.com>
2023-02-22 13:07:16 +0900

    ⚡️ :: implement DmsUrlProperties

---
+++ data/src/main/java/team/aliens/data/remote/url/DmsUrlProperties.kt
@@ -0,0 +1,17 @@
+package team.aliens.data.remote.url
+
+object DmsUrlProperties {
+
+    object ContentType {
+        const val APPLICATION_JSON = "application/json"
+    }
+
+    object Header {
+        const val AUTHORIZATION = "Authorization"
+        const val REFRESH_TOKEN = "refresh-token"
+    }
+
+    object Prefix {
+        const val BEARER = "Bearer "
+    }
+}
