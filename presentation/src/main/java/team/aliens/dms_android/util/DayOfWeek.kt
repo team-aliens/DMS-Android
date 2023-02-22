5f0d3f1ca38ed58469a577c9a08233db0ba6c50f
Tmdhoon2 <hks026naver@gmail.com>
2023-02-22 17:21:15 +0900

    ♻️ :: refactor DayOfWeek

--- presentation/src/main/java/team/aliens/dms_android/util/DayOfWeek.kt
+++ presentation/src/main/java/team/aliens/dms_android/util/DayOfWeek.kt
@@ -3,11 +3,11 @@ package team.aliens.dms_android.util
 enum class DayOfWeek(
     val week: String,
 ) {
-    MONDAY("월요일"),
-    TUESDAY("화요일"),
-    WEDNESDAY("수요일"),
-    THURSDAY("목요일"),
-    FRIDAY("금요일"),
-    SATURDAY("토요일"),
-    SUNDAY("일요일")
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일")
 }
