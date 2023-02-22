2984301aaa10c8e8729240f8a077990663da0809
junsuPark <krpjs0508@gmail.com>
2023-02-22 16:21:32 +0900

    📝 :: add document annotations to Qualifiers, for injecting OkHttpClient

--- data/src/main/java/team/aliens/data/intercepter/OkHttpClientQualifier.kt
+++ data/src/main/java/team/aliens/data/intercepter/OkHttpClientQualifier.kt
@@ -2,6 +2,10 @@ package team.aliens.data.intercepter
 
 import javax.inject.Qualifier
 
+/**
+ * @author junsuPark
+ * Annotation, should inject global OkHttpClient, calling common APIs.
+ */
 @Qualifier
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
@@ -9,6 +13,10 @@ annotation class DefaultOkHttpClient(
     val value: String = "defaultOkHttpClient",
 )
 
+/**
+ * @author junsuPark
+ * Annotation, should inject OkHttpClient only for reissuing tokens, calling unique reissuing API.
+ */
 @Qualifier
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
