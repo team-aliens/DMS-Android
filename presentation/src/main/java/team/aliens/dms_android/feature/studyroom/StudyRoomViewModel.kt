4dd9c6e67b12bed8982045e4f62644435388ab0d
junsuPark <krpjs0508@gmail.com>
2023-02-23 20:44:22 +0900

    ♻️ :: refactor StudyRoomViewModel

--- presentation/src/main/java/team/aliens/dms_android/feature/studyroom/StudyRoomViewModel.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/studyroom/StudyRoomViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import team.aliens.dms_android.base.BaseViewModel
+import team.aliens.dms_android.feature.studyroom.StudyRoomViewModel.Event
 import team.aliens.dms_android.util.MutableEventFlow
 import team.aliens.dms_android.util.asEventFlow
 import team.aliens.domain.entity.studyroom.StudyRoomDetailEntity
@@ -22,6 +23,10 @@ class StudyRoomViewModel @Inject constructor(
     private val studyRoomApplyTimeUseCase: RemoteFetchApplySeatUseCase,
 ) : BaseViewModel<StudyRoomState, StudyRoomEvent>() {
 
+    init {
+        fetchApplyTime()
+    }
+
     private val _studyRoomEffect = MutableEventFlow<Event>()
     val studyRoomEffect = _studyRoomEffect.asEventFlow()
 
@@ -37,148 +42,172 @@ class StudyRoomViewModel @Inject constructor(
     override val initialState: StudyRoomState
         get() = StudyRoomState.getDefaultInstance()
 
-    fun fetchStudyRoomList() {
+
+    internal fun fetchStudyRoomList() {
         viewModelScope.launch {
-            kotlin.runCatching {
+
+            val result = kotlin.runCatching {
                 studyRoomListUseCase.execute(Unit)
-            }.onSuccess {
-                event(Event.FetchStudyRoomList(it))
-            }.onFailure {
-                when (it) {
-                    is BadRequestException -> event(Event.BadRequestException)
-                    is UnauthorizedException -> event(Event.UnAuthorizedTokenException)
-                    is ForbiddenException -> event(Event.CannotConnectException)
-                    is TooManyRequestException -> event(Event.TooManyRequestException)
-                    is NullPointerException -> event(Event.NullPointException)
-                    is ServerException -> event(Event.InternalServerException)
-                    else -> event(Event.UnknownException)
-                }
             }
+
+            emitStudyRoomEvent(
+                if (result.isSuccess) {
+
+                    val resultEntity = result.getOrThrow()
+
+                    Event.FetchStudyRoomList(resultEntity)
+                } else {
+                    getEventFromThrowable(
+                        result.exceptionOrNull(),
+                    )
+                },
+            )
         }
     }
 
-    fun fetchStudyRoomDetail(roomId: String) {
-        viewModelScope.launch {
-            kotlin.runCatching {
-                studyRoomDetailUseCase.execute(roomId)
-            }.onSuccess {
-                setState(state = state.value.copy(roomDetail = it))
-            }.onFailure {
-                when (it) {
-                    is BadRequestException -> event2(Event.BadRequestException)
-                    is UnauthorizedException -> event2(Event.UnAuthorizedTokenException)
-                    is ForbiddenException -> event2(Event.CannotConnectException)
-                    is TooManyRequestException -> event2(Event.TooManyRequestException)
-                    is NullPointerException -> event2(Event.NullPointException)
-                    is ServerException -> event2(Event.InternalServerException)
-                    else -> event2(Event.UnknownException)
-                }
-            }
-        }
-    }
-
-    fun applyStudyRoomSeat(
-        currentSeat: String,
+    private fun emitStudyRoomEvent(
+        event: Event,
     ) {
-        viewModelScope.launch {
-            kotlin.runCatching {
-                studyApplySeatUseCase.execute(currentSeat)
-            }.onSuccess {
-                event3(Event.ApplyStudyRoom)
-            }.onFailure {
-                when (it) {
-                    is BadRequestException -> event3(Event.BadRequestException)
-                    is UnauthorizedException -> event3(Event.UnAuthorizedTokenException)
-                    is ForbiddenException -> event3(Event.CannotConnectException)
-                    is TooManyRequestException -> event3(Event.TooManyRequestException)
-                    is ConflictException -> event3(Event.ConflictException)
-                    is NullPointerException -> event3(Event.NullPointException)
-                    is ServerException -> event3(Event.InternalServerException)
-                    else -> event3(Event.UnknownException)
-                }
-            }
-        }
-    }
-
-    fun cancelStudyRoomSeat() {
-        viewModelScope.launch {
-            kotlin.runCatching {
-                studyCancelApplySeat.execute(Unit)
-            }.onSuccess {
-                event3(Event.CancelStudyRoom)
-            }.onFailure {
-                when (it) {
-                    is BadRequestException -> event3(Event.BadRequestException)
-                    is UnauthorizedException -> event3(Event.UnAuthorizedTokenException)
-                    is ForbiddenException -> event3(Event.CannotConnectException)
-                    is TooManyRequestException -> event3(Event.TooManyRequestException)
-                    is NullPointerException -> event3(Event.NullPointException)
-                    is ServerException -> event3(Event.InternalServerException)
-                    else -> event3(Event.UnknownException)
-                }
-            }
-        }
-    }
-
-    fun fetchApplyTime() {
-        viewModelScope.launch {
-            kotlin.runCatching {
-                studyRoomApplyTimeUseCase.execute(Unit)
-            }.onSuccess {
-                event4(Event.RoomApplyTime)
-                setState(state = state.value.copy(
-                    startAt = it.startAt.toHour(),
-                    endAt = it.endAt.toHour(),
-                ))
-            }.onFailure {
-                when (it) {
-                    is BadRequestException -> event4(Event.BadRequestException)
-                    is UnauthorizedException -> event4(Event.UnAuthorizedTokenException)
-                    is TooManyRequestException -> event4(Event.TooManyRequestException)
-                    is NullPointerException -> event4(Event.NullPointException)
-                    is TooManyRequestException -> event4(Event.TooManyRequestException)
-                    is ServerException -> event4(Event.InternalServerException)
-                    else -> event4(Event.UnknownException)
-                }
-            }
-        }
-    }
-
-    private fun String.toHour(): String {
-        return this.substring(0, 5)
-    }
-
-    fun updateCurrentSeat(
-        seatId: String,
-    ) {
-        setState(state = state.value.copy(
-            currentSeat = seatId,
-        ))
-    }
-
-    private fun event(event: Event) {
         viewModelScope.launch {
             _studyRoomEffect.emit(event)
         }
     }
 
-    private fun event2(event: Event) {
+
+    fun fetchStudyRoomDetail(
+        studyRoomId: String,
+    ) {
+        viewModelScope.launch {
+
+            val result = kotlin.runCatching {
+                studyRoomDetailUseCase.execute(studyRoomId)
+            }
+
+            if (result.isSuccess) {
+
+                val resultEntity = result.getOrThrow()
+
+                setState(
+                    state.value.copy(
+                        roomDetail = resultEntity,
+                    ),
+                )
+            } else {
+                emitStudyRoomDetailEvent(
+                    getEventFromThrowable(
+                        result.exceptionOrNull(),
+                    )
+                )
+            }
+        }
+    }
+
+    private fun emitStudyRoomDetailEvent(
+        event: Event,
+    ) {
         viewModelScope.launch {
             _studyRoomDetailEffect.emit(event)
         }
     }
 
-    private fun event3(event: Event) {
+
+    fun applyStudyRoomSeat(
+        currentSeat: String,
+    ) {
+        viewModelScope.launch {
+
+            val result = kotlin.runCatching {
+                studyApplySeatUseCase.execute(currentSeat)
+            }
+
+            emitStudyRoomApplyEvent(
+                if (result.isSuccess) {
+                    Event.ApplyStudyRoom
+                } else {
+                    getEventFromThrowable(
+                        result.exceptionOrNull(),
+                    )
+                },
+            )
+        }
+    }
+
+    fun cancelStudyRoomSeat() {
+        viewModelScope.launch {
+
+            val result = kotlin.runCatching {
+                studyCancelApplySeat.execute(Unit)
+            }
+
+            emitStudyRoomApplyEvent(
+                if (result.isSuccess) {
+                    Event.CancelStudyRoom
+                } else {
+                    getEventFromThrowable(
+                        result.exceptionOrNull(),
+                    )
+                },
+            )
+        }
+    }
+
+    private fun emitStudyRoomApplyEvent(
+        event: Event,
+    ) {
         viewModelScope.launch {
             _studyRoomApplyEffect.emit(event)
         }
     }
 
-    private fun event4(event: Event) {
+
+    fun fetchApplyTime() {
+        viewModelScope.launch {
+
+            val result = kotlin.runCatching {
+                studyRoomApplyTimeUseCase.execute(Unit)
+            }
+
+            if (result.isSuccess) {
+
+                emitStudyRoomTimeEvent(Event.RoomApplyTime)
+
+                val resultEntity = result.getOrThrow()
+
+                setState(
+                    state = state.value.copy(
+                        startAt = resultEntity.startAt.extractHourFromDate(),
+                        endAt = resultEntity.endAt.extractHourFromDate(),
+                    ),
+                )
+            } else {
+                emitStudyRoomApplyEvent(
+                    getEventFromThrowable(
+                        result.exceptionOrNull(),
+                    ),
+                )
+            }
+        }
+    }
+
+    private fun emitStudyRoomTimeEvent(
+        event: Event,
+    ) {
         viewModelScope.launch {
             _studyRoomTimeEffect.emit(event)
         }
     }
+
+
+    fun updateCurrentSeat(
+        seatId: String,
+    ) {
+        setState(
+            state = state.value.copy(
+                currentSeat = seatId,
+            ),
+        )
+    }
+
 
     //TODO BaseViewModel 에서 처리 필요. StudentRoomEvent
     sealed class Event {
@@ -198,7 +227,24 @@ class StudyRoomViewModel @Inject constructor(
         object UnknownException : Event()
     }
 
-    override fun reduceEvent(oldState: StudyRoomState, event: StudyRoomEvent) {
-        TODO("Not yet implemented")
+    override fun reduceEvent(oldState: StudyRoomState, event: StudyRoomEvent) {}
+}
+
+private fun String.extractHourFromDate(): String {
+    return this.substring(0, 5)
+}
+
+// 추후 BaseViewModel로 Event 위임을 위해 클래스의 외부 함수로 구현
+private fun getEventFromThrowable(
+    throwable: Throwable?,
+): Event {
+    return when (throwable) {
+        is BadRequestException -> Event.BadRequestException
+        is UnauthorizedException -> Event.UnAuthorizedTokenException
+        is ForbiddenException -> Event.CannotConnectException
+        is TooManyRequestException -> Event.TooManyRequestException
+        is NullPointerException -> Event.NullPointException
+        is ServerException -> Event.InternalServerException
+        else -> Event.UnknownException
     }
 }
