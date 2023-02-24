c31b1a7506146d43559ed48e83507127d11b4def
Tmdhoon2 <hks026naver@gmail.com>
2023-02-24 14:48:36 +0900

    ♻️ :: refactor RemainApi @Query -> @Path

--- data/src/main/java/team/aliens/data/remote/api/RemainApi.kt
+++ data/src/main/java/team/aliens/data/remote/api/RemainApi.kt
@@ -2,6 +2,7 @@ package team.aliens.data.remote.api
 
 import retrofit2.http.GET
 import retrofit2.http.PUT
+import retrofit2.http.Path
 import retrofit2.http.Query
 import team.aliens.data.remote.response.remain.FetchAvailableRemainTimeResponse
 import team.aliens.data.remote.response.remain.FetchCurrentRemainOptionResponse
@@ -13,7 +14,7 @@ interface RemainApi {
 
     @PUT(DmsUrl.Remain.updateRemainOption)
     suspend fun updateRemainOption(
-        @Query("remain-option-id") remainOptionId: UUID,
+        @Path("remain-option-id") remainOptionId: UUID,
     )
 
     @GET(DmsUrl.Remain.fetchCurrentRemainOption)
