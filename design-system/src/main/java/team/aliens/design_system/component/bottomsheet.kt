7a3726fd40d17b2fc18c9991b0777dbc20b8cb8a
junsuPark <krpjs0508@gmail.com>
2023-02-25 00:37:58 +0900

    ⚡️ :: implement DormBottomSheetDialogWithButtonLayout

--- design-system/src/main/java/team/aliens/design_system/component/bottomsheet.kt
+++ design-system/src/main/java/team/aliens/design_system/component/bottomsheet.kt
@@ -1,19 +1,23 @@
 package team.aliens.design_system.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import team.aliens.design_system.color.DormColor
+import team.aliens.design_system.typography.Body1
+import team.aliens.design_system.typography.Body2
 
 private val BottomSheetShape = RoundedCornerShape(
     topStart = 16.dp,
@@ -39,7 +43,6 @@ fun DormBottomSheetDialog(
             )
         },
         sheetBackgroundColor = Color.Transparent,
-        scrimColor = DormColor.Gray500,
     ) {
         content()
     }
@@ -98,3 +101,79 @@ private fun DormBottomSheetHandle(
         }
     }
 }
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DormBottomSheetDialogWithButtonLayout(
+    sheetState: ModalBottomSheetState,
+    sheetContent: @Composable () -> Unit,
+    btnText: String,
+    content: @Composable () -> Unit,
+) {
+
+    val scope = rememberCoroutineScope()
+
+    ModalBottomSheetLayout(
+        modifier = Modifier
+            .fillMaxSize(),
+        sheetState = sheetState,
+        sheetContent = {
+            DormBottomSheetContent(
+                useHandle = false,
+                sheetContent = {
+                    Column {
+
+                        sheetContent()
+
+                        Divider(
+                            modifier = Modifier
+                                .padding(
+                                    horizontal = 10.dp,
+                                )
+                                .fillMaxWidth(),
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(56.dp)
+                                .clickable {
+                                    scope.launch {
+                                        sheetState.hide()
+                                    }
+                                },
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Body2(
+                                text = btnText,
+                            )
+                        }
+                    }
+                },
+            )
+        },
+        sheetBackgroundColor = Color.Transparent,
+        content = content,
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Preview()
+@Composable
+private fun DormCancellableBottomSheetDialogPreview() {
+
+    val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Expanded)
+
+    DormBottomSheetDialogWithButtonLayout(
+        sheetState = sheetState,
+        sheetContent = {
+            Body1(text = "HIHI")
+
+        },
+        btnText = "취소"
+    ) {
+
+    }
+}
+
