c5c16b1202a7d309f69c97be285a3525a1b75a14
leesin0222 <dragon02229@naver.com>
2023-02-26 00:41:07 +0900

    ♻ :: Improve SchoolConfirm Logic

--- presentation/src/main/java/team/aliens/dms_android/feature/register/ui/school/ConfirmSchoolFragment.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/register/ui/school/ConfirmSchoolFragment.kt
@@ -2,22 +2,20 @@ package team.aliens.dms_android.feature.register.ui.school
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import team.aliens.dms_android.base.BaseFragment
 import team.aliens.dms_android.feature.MainActivity
 import team.aliens.dms_android.feature.RegisterActivity
-import team.aliens.dms_android.feature.register.event.school.ConfirmSchoolEvent
+import team.aliens.dms_android.feature.register.event.school.*
 import team.aliens.dms_android.feature.register.ui.email.EnterEmailFragment
-import team.aliens.dms_android.util.invisible
-import team.aliens.dms_android.util.repeatOnStarted
-import team.aliens.dms_android.util.visible
+import team.aliens.dms_android.util.repeatOnFragmentStarted
 import team.aliens.dms_android.viewmodel.auth.register.school.ConfirmSchoolViewModel
 import team.aliens.domain.entity.user.SchoolConfirmQuestionEntity
 import team.aliens.presentation.R
@@ -42,10 +40,10 @@ class ConfirmSchoolFragment :
         inputData = args?.get("schoolId") as String
         schoolCode = args.get("schoolCode") as String
 
-        val uuid = UUID.fromString(inputData.toString())
+        val uuid = UUID.fromString(inputData)
         confirmSchoolViewModel.schoolId = uuid
 
-        repeatOnStarted {
+        repeatOnFragmentStarted {
             confirmSchoolViewModel.confirmSchoolEvent.collect { event -> handleEvent(event) }
         }
 
@@ -56,7 +54,9 @@ class ConfirmSchoolFragment :
 
     private fun handleEvent(event: ConfirmSchoolEvent) {
         when (event) {
-            is ConfirmSchoolEvent.CompareSchoolAnswerSuccess -> {
+            is FetchSchoolQuestion -> setSchoolQuestionValue(event.schoolConfirmQuestionEntity)
+            is CompareSchoolAnswerSuccess -> {
+                //Fixme : 추후PR에서 개선 필요
                 val registerActive = activity as RegisterActivity
 
                 val bundle = Bundle()
@@ -71,46 +71,22 @@ class ConfirmSchoolFragment :
                     .replace(R.id.containerRegister, fragment).commit()
             }
 
-            is ConfirmSchoolEvent.CompareSchoolBadRequestException -> {
-                binding.tvError.text = getString(R.string.BadRequest)
-                binding.tvError.setTextColor(ContextCompat.getColor(requireContext(),
-                    R.color.error))
-                binding.btnConfirm.setBackgroundResource(R.drawable.register_custom_btn_background)
-                binding.etReply.setBackgroundResource(R.drawable.register_et_error_background)
-                binding.tvError.visible()
-            }
-
-            is ConfirmSchoolEvent.CompareSchoolNotFoundException -> {
-                binding.tvError.text = getString(R.string.CompareSchoolNotFound)
-                binding.tvError.setTextColor(ContextCompat.getColor(requireContext(),
-                    R.color.error))
-                binding.btnConfirm.setBackgroundResource(R.drawable.register_custom_btn_background)
-                binding.etReply.setBackgroundResource(R.drawable.register_et_error_background)
-                binding.tvError.visible()
-            }
-
-            is ConfirmSchoolEvent.CompareSchoolUnauthorizedException -> {
+            MissMatchCompareSchool -> {
                 binding.tvError.text = getString(R.string.InconsistentSchoolReply)
                 binding.tvError.setTextColor(ContextCompat.getColor(requireContext(),
                     R.color.error))
                 binding.btnConfirm.setBackgroundResource(R.drawable.register_custom_btn_background)
                 binding.etReply.setBackgroundResource(R.drawable.register_et_error_background)
-                binding.tvError.visible()
+                binding.tvError.isVisible = true
             }
-
-            is ConfirmSchoolEvent.SchoolQuestionInternalServerException -> showShortToast(getString(
-                R.string.ServerException))
-
-            is ConfirmSchoolEvent.SchoolQuestionTooManyRequestException -> showShortToast(getString(
-                R.string.TooManyRequest))
-
-            is ConfirmSchoolEvent.CompareSchoolInternalServerException -> showShortToast(getString(R.string.ServerException))
-            is ConfirmSchoolEvent.CompareSchoolTooManyRequestException -> showShortToast(getString(R.string.TooManyRequest))
-
-            is ConfirmSchoolEvent.SchoolQuestionBadRequestException -> showShortToast(getString(R.string.BadRequest))
-            is ConfirmSchoolEvent.SchoolQuestionNotFoundException -> showShortToast(getString(R.string.CompareSchoolNotFound))
-            is ConfirmSchoolEvent.FetchSchoolQuestion -> setSchoolQuestionValue(event.schoolConfirmQuestionEntity)
-            is ConfirmSchoolEvent.UnknownException -> showShortToast(getString(R.string.UnKnownException))
+            NotFoundCompareSchool -> {
+                binding.tvError.text = getString(R.string.CompareSchoolNotFound)
+                binding.tvError.setTextColor(ContextCompat.getColor(requireContext(),
+                    R.color.error))
+                binding.btnConfirm.setBackgroundResource(R.drawable.register_custom_btn_background)
+                binding.etReply.setBackgroundResource(R.drawable.register_et_error_background)
+                binding.tvError.isVisible = true
+            }
         }
     }
 
@@ -123,25 +99,16 @@ class ConfirmSchoolFragment :
             startActivity(intent)
         }
 
-        binding.etReply.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                answer = p0.toString()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                if (p0!!.isNotEmpty()) {
-                    binding.tvError.invisible()
-                    binding.btnConfirm.setBackgroundResource(R.drawable.register_custom_active_btn_background)
-                    binding.etReply.setBackgroundResource(R.drawable.register_et_background)
-                    binding.btnConfirm.isClickable = true
-                    binding.btnConfirm.isEnabled = true
-                }
-            }
-        })
+       with(binding.etReply){
+           addTextChangedListener {
+               answer = it.toString()
+               binding.tvError.isVisible = false
+               binding.btnConfirm.setBackgroundResource(R.drawable.register_custom_active_btn_background)
+               binding.etReply.setBackgroundResource(R.drawable.register_et_background)
+               binding.btnConfirm.isClickable = true
+               binding.btnConfirm.isEnabled = true
+           }
+       }
 
         binding.btnConfirm.setOnClickListener {
             confirmSchoolViewModel.compareSchoolAnswer(answer)
