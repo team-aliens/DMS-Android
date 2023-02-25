3035f6c8d75ac548670c117004323eb47c10204d
leesin0222 <dragon02229@naver.com>
2023-02-25 22:28:58 +0900

    ♻ :: refactore ExamineSchoolCode Event, ViewModel

--- presentation/src/main/java/team/aliens/dms_android/viewmodel/auth/register/school/ExamineSchoolCodeViewModel.kt
+++ presentation/src/main/java/team/aliens/dms_android/viewmodel/auth/register/school/ExamineSchoolCodeViewModel.kt
@@ -5,11 +5,10 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import team.aliens.dms_android.feature.register.event.school.ExamineSchoolCodeEvent
+import team.aliens.dms_android.feature.register.event.school.ExamineSchoolCodeSuccess
+import team.aliens.dms_android.feature.register.event.school.MissMatchSchoolCode
 import team.aliens.dms_android.util.MutableEventFlow
 import team.aliens.dms_android.util.asEventFlow
-import team.aliens.domain.exception.BadRequestException
-import team.aliens.domain.exception.ServerException
-import team.aliens.domain.exception.TooManyRequestException
 import team.aliens.domain.exception.UnauthorizedException
 import team.aliens.domain.usecase.schools.RemoteSchoolCodeUseCase
 import java.util.*
@@ -25,20 +24,14 @@ class ExamineSchoolCodeViewModel @Inject constructor(
 
     var schoolId: UUID = UUID(0, 0)
 
-    fun examineSchoolCode(schoolCode: String) {
+    internal fun examineSchoolCode(schoolCode: String) {
         viewModelScope.launch {
             kotlin.runCatching {
                 schoolId = remoteSchoolCodeUseCase.execute(schoolCode).schoolId
             }.onSuccess {
-                event(ExamineSchoolCodeEvent.ExamineSchoolCodeSuccess)
+                event(ExamineSchoolCodeSuccess)
             }.onFailure {
-                when (it) {
-                    is BadRequestException -> event(ExamineSchoolCodeEvent.BadRequestException)
-                    is UnauthorizedException -> event(ExamineSchoolCodeEvent.UnAuthorizedException)
-                    is TooManyRequestException -> event(ExamineSchoolCodeEvent.TooManyRequestException)
-                    is ServerException -> event(ExamineSchoolCodeEvent.InternalServerException)
-                    else -> event(ExamineSchoolCodeEvent.UnknownException)
-                }
+                if (it is UnauthorizedException) event(MissMatchSchoolCode)
             }
         }
     }
