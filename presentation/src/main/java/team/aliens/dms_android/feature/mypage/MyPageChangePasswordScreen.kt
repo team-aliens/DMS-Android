61ebc2b82dfa14f47d03077998f9ccfaeac8422d
정승훈 <hks026naver@gmail.com>
2023-02-26 02:38:51 +0900

    ♻️ :: refactor change my page password screen view

--- presentation/src/main/java/team/aliens/dms_android/feature/mypage/MyPageChangePasswordScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/mypage/MyPageChangePasswordScreen.kt
@@ -14,16 +14,18 @@ import team.aliens.design_system.button.DormButtonColor
 import team.aliens.design_system.button.DormContainedLargeButton
 import team.aliens.design_system.color.DormColor
 import team.aliens.design_system.textfield.DormTextField
-import team.aliens.design_system.typography.Body4
+import team.aliens.design_system.typography.Body2
 import team.aliens.design_system.typography.OverLine
 import team.aliens.dms_android.util.TopBar
 import team.aliens.presentation.R
 
 @Composable
 fun MyPageChangePasswordScreen() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(DormColor.Gray200)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DormColor.Gray200),
+    ) {
         TopBar(title = stringResource(id = R.string.ChangePassword))
         ChangePwValue()
     }
@@ -33,53 +35,61 @@ fun MyPageChangePasswordScreen() {
 fun ChangePwValue() {
 
     var passwordValue by remember { mutableStateOf("") }
-    var rePasswordValue by remember { mutableStateOf("") }
+    var repeatPasswordValue by remember { mutableStateOf("") }
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(DormColor.Gray100)) {
-        Column(modifier = Modifier
+    Column(
+        modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp)
-            .padding(top = 47.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.temporarylogo),
-                contentDescription = null,
-                modifier = Modifier.size(49.dp),
+            .background(color = DormColor.Gray100)
+            .padding(horizontal = 16.dp),
+    ) {
+        Spacer(
+            modifier = Modifier.height(
+                46.dp,
             )
-            Spacer(modifier = Modifier.height(7.dp))
-            Body4(text = stringResource(R.string.ChangePassword), color = DormColor.Gray600)
-            Spacer(modifier = Modifier.height(6.dp))
-            OverLine(
-                text = stringResource(id = R.string.PwWarning),
-                color = DormColor.Gray500,
-            )
-            Spacer(modifier = Modifier.height(60.dp))
-            DormTextField(
-                value = passwordValue,
-                onValueChange = { passwordValue = it },
-                hint = stringResource(R.string.EnterPassword),
-                isPassword = true,
-            )
-            Spacer(modifier = Modifier.height(37.dp))
-            DormTextField(
-                value = rePasswordValue,
-                onValueChange = { rePasswordValue = it },
-                hint = stringResource(R.string.ReEnterPassword),
-                //error = stringResource(R.string.CheckPassword),
-                isPassword = true,
-            )
-            Box(
-                contentAlignment = Alignment.BottomCenter,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(bottom = 60.dp),
+        )
+        Image(
+            modifier = Modifier
+                .size(
+                    width = 96.dp,
+                    height = 34.dp,
+                ),
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Body2(text = stringResource(R.string.ChangePassword), color = DormColor.Gray600)
+        Spacer(modifier = Modifier.height(6.dp))
+        OverLine(
+            text = stringResource(id = R.string.PwWarning),
+            color = DormColor.Gray500,
+        )
+        Spacer(modifier = Modifier.height(60.dp))
+        DormTextField(
+            value = passwordValue,
+            onValueChange = { passwordValue = it },
+            hint = stringResource(R.string.EnterPassword),
+            isPassword = true,
+        )
+        Spacer(modifier = Modifier.height(36.dp))
+        DormTextField(
+            value = repeatPasswordValue,
+            onValueChange = { repeatPasswordValue = it },
+            hint = stringResource(R.string.ReEnterPassword),
+            isPassword = true,
+        )
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 60.dp),
+        ) {
+            DormContainedLargeButton(
+                text = stringResource(R.string.Check),
+                color = DormButtonColor.Blue,
+                enabled = (passwordValue.isNotBlank() && repeatPasswordValue.isNotBlank()),
             ) {
-                DormContainedLargeButton(
-                    text = stringResource(R.string.Next),
-                    color = DormButtonColor.Blue,
-                    enabled = false,
-                ) {}
+
             }
         }
     }
