af0ed62571d3c84829fd175dda54d3a3a29e2b5b
junsuPark <krpjs0508@gmail.com>
2023-02-26 15:02:13 +0900

    ⚡️ :: add background color at ConfirmImageScreen

--- presentation/src/main/java/team/aliens/dms_android/feature/image/ConfirmImageScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/image/ConfirmImageScreen.kt
@@ -2,6 +2,7 @@ package team.aliens.dms_android.feature.image
 
 import android.content.Context
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.*
@@ -20,6 +21,7 @@ import coil.compose.rememberAsyncImagePainter
 import kotlinx.coroutines.launch
 import team.aliens.design_system.button.DormButtonColor
 import team.aliens.design_system.button.DormContainedLargeButton
+import team.aliens.design_system.color.DormColor
 import team.aliens.design_system.modifier.dormClickable
 import team.aliens.design_system.toast.rememberToast
 import team.aliens.dms_android.util.SelectImageType
@@ -118,6 +120,7 @@ internal fun ConfirmImageScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .background(DormColor.Gray200)
                 .padding(
                     horizontal = 16.dp,
                 ),
