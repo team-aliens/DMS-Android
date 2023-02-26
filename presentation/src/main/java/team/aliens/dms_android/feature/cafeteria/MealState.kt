c86d033a502c7c38009dc1c1842e5f911941e64a
junsuPark <krpjs0508@gmail.com>
2023-02-26 10:39:49 +0900

    ♻️ :: refactor MealViewModel

--- presentation/src/main/java/team/aliens/dms_android/feature/cafeteria/MealState.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/cafeteria/MealState.kt
@@ -7,19 +7,17 @@ data class MealState(
     val meal: String,
     var mealList: MealList = MealList(),
     var today: LocalDate = LocalDate.now(),
-    var a: Int = 0,
-    var b: Int = 0,
-    var noticeBoolean: Boolean = false,
+    var hasNewNotice: Boolean = false,
 ) : MviState {
     companion object {
-        fun initial() = MealState(
+        fun getDefaultInstance() = MealState(
             meal = "",
         )
     }
 }
 
 data class MealList(
-    var breakfast: List<String> = listOf(""),
-    var lunch: List<String> = listOf(""),
-    var dinner: List<String> = listOf(""),
+    var breakfast: Pair<List<String>, String> = listOf("") to "",
+    var lunch: Pair<List<String>, String> = listOf("") to "",
+    var dinner: Pair<List<String>, String> = listOf("") to "",
 )
