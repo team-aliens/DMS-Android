e3e160a4a5142791e0d216e3efe94798d887067b
정승훈 <hks026naver@gmail.com>
2023-02-27 15:33:44 +0900

    ♻️ :: change remain application forbiddenexception string resource

--- presentation/src/main/java/team/aliens/dms_android/feature/remain/RemainApplicationScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/remain/RemainApplicationScreen.kt
@@ -292,7 +292,7 @@ private fun getStringFromEvent(
         when (event) {
             is Event.BadRequestException -> R.string.BadRequest
             is Event.UnauthorizedException -> R.string.UnAuthorized
-            is Event.ForbiddenException -> R.string.Forbidden
+            is Event.ForbiddenException -> R.string.ForbiddenApplyRemain
             is Event.TooManyRequestException -> R.string.TooManyRequest
             is Event.ServerException -> R.string.ServerException
             else -> R.string.UnKnownException
