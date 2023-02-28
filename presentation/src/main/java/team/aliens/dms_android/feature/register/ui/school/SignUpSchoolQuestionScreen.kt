a7afd5fa4272b92c220e1e4082a3f1ecff4e7c89
Tmdhoon2 <hks026naver@gmail.com>
2023-02-28 22:50:06 +0900

    ⚡️ :: implement login text

--- presentation/src/main/java/team/aliens/dms_android/feature/register/ui/school/SignUpSchoolQuestionScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/register/ui/school/SignUpSchoolQuestionScreen.kt
@@ -2,7 +2,9 @@ package team.aliens.dms_android.feature.register.ui.school
 
 import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -11,10 +13,15 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import team.aliens.design_system.button.DormButtonColor
 import team.aliens.design_system.button.DormContainedLargeButton
+import team.aliens.design_system.color.DormColor
+import team.aliens.design_system.modifier.dormClickable
 import team.aliens.design_system.textfield.DormTextField
 import team.aliens.design_system.toast.rememberToast
 import team.aliens.design_system.typography.Body2
+import team.aliens.design_system.typography.ButtonText
+import team.aliens.design_system.typography.Caption
 import team.aliens.dms_android.component.AppLogo
+import team.aliens.dms_android.feature.navigator.NavigationRoute
 import team.aliens.dms_android.feature.register.event.school.*
 import team.aliens.dms_android.viewmodel.auth.register.school.ConfirmSchoolViewModel
 import team.aliens.presentation.R
@@ -97,6 +104,29 @@ fun SignUpSchoolQuestionScreen(
             confirmSchoolViewModel.compareSchoolAnswer(
                 schoolId = schoolId,
                 answer = schoolAnswer,
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Caption(
+                text = stringResource(id = R.string.AlreadyAccount),
+                color = DormColor.Gray500,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            ButtonText(
+                modifier = Modifier
+                    .padding(top = 1.dp)
+                    .dormClickable(
+                        rippleEnabled = false,
+                    ) {
+                        navController.navigate(NavigationRoute.Login)
+                    },
+                text = stringResource(id = R.string.Login),
+                color = DormColor.Gray900,
             )
         }
     }
