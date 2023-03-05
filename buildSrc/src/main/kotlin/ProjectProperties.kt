e1edd3936a105c09ce060557b7c704fbd652f8c3
junsuPark <krpjs0508@gmail.com>
2023-03-05 09:22:42 +0900

    ⚡️ :: upgrade project and application version

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object ProjectProperties {
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "1.2"
 
     const val APPLICATION_ID = "team.aliens.dms_android"
 
