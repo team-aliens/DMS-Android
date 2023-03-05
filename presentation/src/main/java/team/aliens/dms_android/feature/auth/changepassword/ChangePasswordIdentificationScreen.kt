c98fa6af2b80302ece987f6947ab577c884175bd
Tmdhoon2 <hks026naver@gmail.com>
2023-03-05 19:19:08 +0900

    ♻️ :: change keyboard type

--- presentation/src/main/java/team/aliens/dms_android/feature/auth/changepassword/ChangePasswordIdentificationScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/auth/changepassword/ChangePasswordIdentificationScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -192,6 +193,7 @@ fun IdentificationScreen(
                             onValueChange = onEmailChange,
                             hint = stringResource(id = R.string.EnterEmailAddress),
                             error = isEmailError,
+                            keyboardType = KeyboardType.Email,
                             errorDescription = context.getString(R.string.NotValidEmailFormat)
                         )
                     }
