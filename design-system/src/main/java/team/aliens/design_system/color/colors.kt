7337a6b98951ada7ba113c083605fb239a28d081
junsuPark <krpjs0508@gmail.com>
2023-03-05 10:30:08 +0900

    ♻️ :: add design system colors

--- design-system/src/main/java/team/aliens/design_system/color/colors.kt
+++ design-system/src/main/java/team/aliens/design_system/color/colors.kt
@@ -53,6 +53,9 @@ object DormColor {
     val Gray900 = Color(color = 0xFF000000)
 
     @Stable
+    val Gray1000 = Color(color = 0xFF000000)
+
+    @Stable
     val ViewHide = Color(color = 0xFFD6D6D6)
 
     @Stable
--- design-system/src/main/java/team/aliens/design_system/theme/theme.kt
+++ design-system/src/main/java/team/aliens/design_system/theme/theme.kt
@@ -11,24 +11,31 @@ import team.aliens.design_system.color.DormColor
 @SuppressLint("ConflictingOnColor")
 private val LightColorPalette = lightColors(
     primary = DormColor.DormPrimary,
-    background = DormColor.Gray200,
-    surface = DormColor.Gray100,
+    onPrimary = DormColor.Gray100,
+    secondary = DormColor.Lighten200, // primary container
+    onSecondary = DormColor.Darken200, // on primary container
     error = DormColor.Error,
     onError = DormColor.Gray100,
-    onPrimary = DormColor.Gray100,
-    onBackground = DormColor.Gray900,
+    background = DormColor.Gray200,
+    onBackground = DormColor.Gray1000,
+    surface = DormColor.Gray100,
     onSurface = DormColor.Gray900,
+    primaryVariant = DormColor.Gray500, // icon
+    secondaryVariant = DormColor.Gray300, // line
 )
 
 private val DarkColorPalette = darkColors(
     primary = DormColor.DormPrimary,
-    background = DormColor.Gray800,
-    surface = DormColor.Gray800,
-    error = DormColor.Error,
-    onError = DormColor.Gray100,
     onPrimary = DormColor.Gray100,
+    secondary = DormColor.Lighten100, // primary container
+    onSecondary = DormColor.Darken100, // on primary container
+    onError = DormColor.Gray100,
+    background = DormColor.Gray900,
     onBackground = DormColor.Gray100,
-    onSurface = DormColor.Gray100,
+    surface = DormColor.Gray800,
+    onSurface = DormColor.Gray200,
+    primaryVariant = DormColor.Gray500, // icon
+    secondaryVariant = DormColor.Gray700, // line
 )
 
 @Composable
