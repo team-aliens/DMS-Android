5a203b8ff7724a1219f7151751db60668b21c209
limsaehyun <sh00710000@gmail.com>
2023-03-07 00:10:38 +0900

    ⚡️ :: ProvideRemoteMealUseCase

---
+++ presentation/src/main/java/team/aliens/dms_android/widget/UseCaseModule.kt
@@ -0,0 +1,22 @@
+package team.aliens.dms_android.widget
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import team.aliens.domain.repository.MealRepository
+import team.aliens.domain.usecase.meal.RemoteMealUseCase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Singleton
+    @Provides
+    fun bindRemoteMealUseCase(
+        mealRepository: MealRepository
+    ): RemoteMealUseCase = RemoteMealUseCase(
+        mealRepository = mealRepository,
+    )
+}
