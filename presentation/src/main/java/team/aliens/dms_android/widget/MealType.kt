94c1fb99b642ba56b7888cb650e8e146a61ef5cb
limsaehyun <sh00710000@gmail.com>
2023-03-07 00:12:09 +0900

    📑 :: 급식 시간대를 분류하기 위한 MealType 작업

---
+++ presentation/src/main/java/team/aliens/dms_android/widget/MealType.kt
@@ -0,0 +1,43 @@
+package team.aliens.dms_android.widget
+
+import team.aliens.design_system.icon.DormIcon
+import java.time.LocalDateTime
+
+enum class MealType(
+    val icon: DormIcon,
+    val title: String,
+) {
+    Breakfast(
+        icon = DormIcon.BlueBreakfast,
+        title = "아침",
+    ),
+    Lunch(
+        icon = DormIcon.BlueLunch,
+        title = "점심",
+    ),
+    Dinner(
+        icon = DormIcon.BlueDinner,
+        title = "저녁",
+    ),
+    ;
+
+    companion object {
+        fun getCurrentMealType(localDateTime: LocalDateTime): MealType {
+            val currentHour = localDateTime.hour
+
+            val morningStart = 0
+            val lunchStart = 8
+            val eveningStart = 13
+
+            return when (currentHour) {
+                in morningStart until lunchStart -> Breakfast
+                in lunchStart until eveningStart -> Lunch
+                else -> Breakfast
+            }
+        }
+
+        fun toMealType(str: String): MealType {
+            return values().firstOrNull { it.title == str } ?: Breakfast
+        }
+    }
+}
