41ec1aac1f4cab2258ae75b1f651755777374089
junsuPark <krpjs0508@gmail.com>
2023-03-08 16:35:00 +0900

    ⚡️ :: implement EditProfileResponse param

---
+++ domain/src/main/java/team/aliens/domain/_param/user/profile/EditProfileResponse.kt
@@ -0,0 +1,9 @@
+package team.aliens.domain._param.user.profile
+
+/**
+ * @author junsuPark
+ * [profileImageUrl] an url expresses user's profile image url
+ */
+data class EditProfileResponse(
+    val profileImageUrl: String,
+)
