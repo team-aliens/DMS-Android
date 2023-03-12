a248ba33a6690a22f5c2f102250084a9605eb399
junsuPark <krpjs0508@gmail.com>
2023-03-12 21:18:55 +0900

    ⚡️ :: implement remains entity

---
+++ domain/src/main/java/team/aliens/domain/_param/remains/querycurrentappliedremainoption/QueryCurrentAppliedRemainOptionResponse.kt
@@ -0,0 +1,10 @@
+package team.aliens.domain._param.remains.querycurrentappliedremainoption
+
+/**
+ * @author junsuPark
+ * A response returned when querying current applied remain option
+ * @property title the title of the remain option
+ */
+data class QueryCurrentAppliedRemainOptionResponse(
+    val title: String,
+)
