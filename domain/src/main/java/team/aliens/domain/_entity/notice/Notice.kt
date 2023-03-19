50d6522c90f502c99d1480effe5c53eca598aed6
junsuPark <krpjs0508@gmail.com>
2023-03-19 21:45:11 +0900

    ⚡️ :: implement entities

---
+++ domain/src/main/java/team/aliens/domain/_entity/notice/Notice.kt
@@ -0,0 +1,14 @@
+package team.aliens.domain._entity.notice
+
+/**
+ * @author junsuPark
+ * A notice entity contains title, content, and created at
+ * @property title notice's title
+ * @property content notice's property
+ * @property createdAt when notice was created at
+ */
+data class Notice(
+    val title: String,
+    val content: String,
+    val createdAt: String,
+)
