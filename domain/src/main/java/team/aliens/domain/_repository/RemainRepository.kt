251698d92ee97e5dfca5743f9e7aeaacc532beb2
junsuPark <krpjs0508@gmail.com>
2023-03-23 20:28:52 +0900

    Revert "♻️ :: refactor RemainsRepository"
    This reverts commit bd6b771abe0bf89cb4531ece1ea87bf482a4a749.

--- domain/src/main/java/team/aliens/domain/_repository/RemainRepository.kt
+++ domain/src/main/java/team/aliens/domain/_repository/RemainRepository.kt
@@ -5,15 +5,15 @@ import team.aliens.domain._model.remains.FetchRemainsApplicationTimeOutput
 import team.aliens.domain._model.remains.FetchRemainsOptionsOutput
 import java.util.*
 
-interface RemainsRepository {
+interface RemainRepository {
 
-    suspend fun updateRemainsOption(
+    suspend fun updateRemainOption(
         remainOptionId: UUID,
     )
 
-    suspend fun fetchCurrentAppliedRemainsOption(): FetchCurrentAppliedRemainsOptionOutput
+    suspend fun fetchCurrentAppliedRemainOption(): FetchCurrentAppliedRemainsOptionOutput
 
-    suspend fun fetchRemainsApplicationTime(): FetchRemainsApplicationTimeOutput
+    suspend fun fetchRemainApplicationTime(): FetchRemainsApplicationTimeOutput
 
-    suspend fun fetchRemainsOptions(): FetchRemainsOptionsOutput
+    suspend fun fetchRemainOptions(): FetchRemainsOptionsOutput
 }
