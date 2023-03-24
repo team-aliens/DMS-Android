237760cb4edc2635d086389ead78d29996ccbbd3
junsuPark <krpjs0508@gmail.com>
2023-03-24 12:55:12 +0900

    ⚡️ :: HttpProperty 구현

---
+++ remote/src/main/java/team/aliens/remote/common/HttpProperty.kt
@@ -0,0 +1,38 @@
+package team.aliens.remote.common
+
+object HttpProperty {
+
+    object Header {
+        const val RefreshToken = "refresh-token"
+    }
+
+    object PathVariable {
+        const val SchoolId = "school-id"
+        const val NoticeId = "notice-id"
+        const val StudyRoomId = "study-room-id"
+        const val SeatId = "seat-id"
+        const val RemainOptionId = "remain-option-id"
+    }
+
+    object QueryString {
+        const val Name = "name"
+        const val AccountId = "account_id"
+        const val SchoolId = "school_id"
+        const val Email = "email"
+        const val Password = "password"
+        const val AuthCode = "auth_code"
+        const val Answer = "answer"
+        const val Type = "type"
+        const val Grade = "grade"
+        const val ClassRoom = "class_room"
+        const val Number = "number"
+        const val SchoolCode = "school_code"
+        const val File = "file"
+        const val FileName = "file_name"
+        const val Date = "date"
+        const val Order = "order"
+        const val Page = "page"
+        const val Size = "size"
+        const val TimeSlot = "time_slot"
+    }
+}
