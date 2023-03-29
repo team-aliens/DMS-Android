aca85b3906c3e93efe70d22d97776c2e0a094583
junsuPark <krpjs0508@gmail.com>
2023-03-29 21:27:41 +0900

    ⚡️ :: ServiceModule 구현

---
+++ di/src/main/java/team/aliens/_di/remote/ServiceModule.kt
@@ -0,0 +1,94 @@
+package team.aliens._di.remote
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import team.aliens.remote.service.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthService(
+        retrofit: Retrofit,
+    ): AuthService {
+        return retrofit.create(AuthService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFileService(
+        retrofit: Retrofit,
+    ): FileService {
+        return retrofit.create(FileService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMealService(
+        retrofit: Retrofit,
+    ): MealService {
+        return retrofit.create(MealService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoticeService(
+        retrofit: Retrofit,
+    ): NoticeService {
+        return retrofit.create(NoticeService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providePointService(
+        retrofit: Retrofit,
+    ): PointService {
+        return retrofit.create(PointService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemainsService(
+        retrofit: Retrofit,
+    ): RemainsService {
+        return retrofit.create(RemainsService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSchoolService(
+        retrofit: Retrofit,
+    ): SchoolService {
+        return retrofit.create(SchoolService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideStudentService(
+        retrofit: Retrofit,
+    ): StudentService {
+        return retrofit.create(StudentService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideStudyRoomService(
+        retrofit: Retrofit,
+    ): StudyRoomService {
+        return retrofit.create(StudyRoomService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserService(
+        retrofit: Retrofit,
+    ): UserService {
+        return retrofit.create(UserService::class.java)
+    }
+}
