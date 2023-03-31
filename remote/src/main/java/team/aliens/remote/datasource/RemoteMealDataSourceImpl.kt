e398c334e900d9d20f296b88687a8ba90f55e6c9
junsuPark <krpjs0508@gmail.com>
2023-03-31 22:36:52 +0900

    ⚡️ :: RemoteMealDataSourceImpl 구현

--- remote/src/main/java/team/aliens/remote/datasource/RemoteMealDataSourceImpl.kt
+++ remote/src/main/java/team/aliens/remote/datasource/RemoteMealDataSourceImpl.kt
@@ -2,12 +2,22 @@ package team.aliens.remote.datasource
 
 import team.aliens.data._datasource.remote.RemoteMealDataSource
 import team.aliens.domain._model.meal.FetchMealsOutput
+import team.aliens.remote.model.meal.toDomain
+import team.aliens.remote.service.MealService
+import team.aliens.remote.util.sendHttpRequest
+import javax.inject.Inject
 
-class RemoteMealDataSourceImpl : RemoteMealDataSource {
+class RemoteMealDataSourceImpl @Inject constructor(
+    private val mealService: MealService,
+) : RemoteMealDataSource {
 
     override suspend fun fetchMeals(
         date: String,
     ): FetchMealsOutput {
-        TODO("Not yet implemented")
+        return sendHttpRequest {
+            mealService.fetchMeals(
+                date = date,
+            )
+        }.toDomain()
     }
 }
