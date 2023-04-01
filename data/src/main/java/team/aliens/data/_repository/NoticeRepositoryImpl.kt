c12d0ad0b61fb792987188333128055523d490ac
junsuPark <krpjs0508@gmail.com>
2023-04-01 14:19:18 +0900

    ⚡️ :: NoticeRepositoryImpl 구현

--- data/src/main/java/team/aliens/data/_repository/NoticeRepositoryImpl.kt
+++ data/src/main/java/team/aliens/data/_repository/NoticeRepositoryImpl.kt
@@ -1,29 +1,35 @@
 package team.aliens.data._repository
 
+import team.aliens.data._datasource.remote.RemoteNoticeDataSource
 import team.aliens.domain._model._common.Order
 import team.aliens.domain._model.notice.FetchNoticeDetailsOutput
 import team.aliens.domain._model.notice.FetchNoticesOutput
 import team.aliens.domain._model.notice.FetchWhetherNewNoticesExistOutput
 import team.aliens.domain._repository.NoticeRepository
 import java.util.*
+import javax.inject.Inject
 
-class NoticeRepositoryImpl(
-    // private val remoteNoticeDataSource: RemoteNoticeDataSource,
+class NoticeRepositoryImpl @Inject constructor(
+    private val remoteNoticeDataSource: RemoteNoticeDataSource,
 ) : NoticeRepository {
 
     override suspend fun fetchWhetherNewNoticesExist(): FetchWhetherNewNoticesExistOutput {
-        TODO("Not yet implemented")
+        return remoteNoticeDataSource.fetchWhetherNewNoticesExist()
     }
 
     override suspend fun fetchNotices(
         order: Order,
     ): FetchNoticesOutput {
-        TODO("Not yet implemented")
+        return remoteNoticeDataSource.fetchNotices(
+            order = order,
+        )
     }
 
     override suspend fun fetchNoticeDetails(
         noticeId: UUID,
     ): FetchNoticeDetailsOutput {
-        TODO("Not yet implemented")
+        return remoteNoticeDataSource.fetchNoticeDetails(
+            noticeId = noticeId,
+        )
     }
 }
