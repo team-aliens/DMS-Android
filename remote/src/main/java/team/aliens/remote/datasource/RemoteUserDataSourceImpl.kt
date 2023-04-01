11e0ab7f2a1b54b29af9e7133ceb220ec33b7c8b
junsuPark <krpjs0508@gmail.com>
2023-04-01 10:26:01 +0900

    ⚡️ :: RemoteUserDataSource 구현

--- remote/src/main/java/team/aliens/remote/datasource/RemoteUserDataSourceImpl.kt
+++ remote/src/main/java/team/aliens/remote/datasource/RemoteUserDataSourceImpl.kt
@@ -2,18 +2,32 @@ package team.aliens.remote.datasource
 
 import team.aliens.data._datasource.remote.RemoteUserDataSource
 import team.aliens.domain._model.user.EditPasswordInput
+import team.aliens.remote.model.user.toData
+import team.aliens.remote.service.UserService
+import team.aliens.remote.util.sendHttpRequest
+import javax.inject.Inject
 
-class RemoteUserDataSourceImpl : RemoteUserDataSource {
+class RemoteUserDataSourceImpl @Inject constructor(
+    private val userService: UserService,
+) : RemoteUserDataSource {
 
     override suspend fun editPassword(
         input: EditPasswordInput,
     ) {
-        TODO("Not yet implemented")
+        return sendHttpRequest {
+            userService.editPassword(
+                request = input.toData()
+            )
+        }
     }
 
     override suspend fun comparePassword(
         password: String,
     ) {
-        TODO("Not yet implemented")
+        return sendHttpRequest {
+            userService.comparePassword(
+                password = password,
+            )
+        }
     }
 }
