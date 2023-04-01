34a5d85459718279b6f5b1b9b4dd1172425ba952
junsuPark <krpjs0508@gmail.com>
2023-04-01 14:21:56 +0900

    ⚡️ :: RemainsRepositoryImpl 구현

--- data/src/main/java/team/aliens/data/_repository/RemainsRepositoryImpl.kt
+++ data/src/main/java/team/aliens/data/_repository/RemainsRepositoryImpl.kt
@@ -1,30 +1,34 @@
 package team.aliens.data._repository
 
+import team.aliens.data._datasource.remote.RemoteRemainsDataSource
 import team.aliens.domain._model.remains.FetchCurrentAppliedRemainsOptionOutput
 import team.aliens.domain._model.remains.FetchRemainsApplicationTimeOutput
 import team.aliens.domain._model.remains.FetchRemainsOptionsOutput
 import team.aliens.domain._repository.RemainsRepository
 import java.util.*
+import javax.inject.Inject
 
-class RemainsRepositoryImpl(
-    // private val remoteRemainsDataSource: RemoteRemainsDataSource,
+class RemainsRepositoryImpl @Inject constructor(
+    private val remoteRemainsDataSource: RemoteRemainsDataSource,
 ) : RemainsRepository {
 
     override suspend fun updateRemainsOption(
         remainsOptionId: UUID,
     ) {
-        TODO("Not yet implemented")
+        return remoteRemainsDataSource.updateRemainsOption(
+            remainsOptionId = remainsOptionId,
+        )
     }
 
     override suspend fun fetchCurrentAppliedRemainsOption(): FetchCurrentAppliedRemainsOptionOutput {
-        TODO("Not yet implemented")
+        return remoteRemainsDataSource.fetchCurrentAppliedRemainsOption()
     }
 
     override suspend fun fetchRemainsApplicationTime(): FetchRemainsApplicationTimeOutput {
-        TODO("Not yet implemented")
+        return remoteRemainsDataSource.fetchRemainsApplicationTime()
     }
 
     override suspend fun fetchRemainsOptions(): FetchRemainsOptionsOutput {
-        TODO("Not yet implemented")
+        return remoteRemainsDataSource.fetchRemainsOptions()
     }
 }
