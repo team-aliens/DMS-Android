4b21db7b58d56ea4eb03391cd2c0848429bbd73f
junsuPark <krpjs0508@gmail.com>
2023-04-01 23:51:46 +0900

    ⚡️ :: SerializationModule 구현

---
+++ di/src/main/java/team/aliens/_di/serialization/SerializationModule.kt
@@ -0,0 +1,21 @@
+package team.aliens._di.serialization
+/*
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SerializationModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+}
+*/
