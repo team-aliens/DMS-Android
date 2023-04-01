3c311367c57060d7d3d0d0118b928de20704a22f
junsuPark <krpjs0508@gmail.com>
2023-04-01 14:23:49 +0900

    ⚡️ :: SchoolRepositoryImpl 구현

--- data/src/main/java/team/aliens/data/_repository/SchoolRepositoryImpl.kt
+++ data/src/main/java/team/aliens/data/_repository/SchoolRepositoryImpl.kt
@@ -1,40 +1,49 @@
 package team.aliens.data._repository
 
+import team.aliens.data._datasource.remote.RemoteSchoolDataSource
 import team.aliens.domain._model.school.ExamineSchoolVerificationCodeOutput
 import team.aliens.domain._model.school.FetchAvailableFeaturesOutput
 import team.aliens.domain._model.school.FetchSchoolVerificationQuestionOutput
 import team.aliens.domain._model.school.FetchSchoolsOutput
 import team.aliens.domain._repository.SchoolRepository
 import java.util.*
+import javax.inject.Inject
 
-class SchoolRepositoryImpl(
-    // private val remoteSchoolDataSource: RemoteSchoolDataSource,
+class SchoolRepositoryImpl @Inject constructor(
+    private val remoteSchoolDataSource: RemoteSchoolDataSource,
 ) : SchoolRepository {
 
     override suspend fun fetchSchools(): FetchSchoolsOutput {
-        TODO("Not yet implemented")
+        return remoteSchoolDataSource.fetchSchools()
     }
 
     override suspend fun fetchSchoolVerificationQuestion(
         schoolId: UUID,
     ): FetchSchoolVerificationQuestionOutput {
-        TODO("Not yet implemented")
+        return remoteSchoolDataSource.fetchSchoolVerificationQuestion(
+            schoolId = schoolId,
+        )
     }
 
     override suspend fun examineSchoolVerificationQuestion(
         schoolId: UUID,
         answer: String,
     ) {
-        TODO("Not yet implemented")
+        return remoteSchoolDataSource.examineSchoolVerificationQuestion(
+            schoolId = schoolId,
+            answer = answer,
+        )
     }
 
     override suspend fun examineSchoolVerificationCode(
         schoolCode: String,
     ): ExamineSchoolVerificationCodeOutput {
-        TODO("Not yet implemented")
+        return remoteSchoolDataSource.examineSchoolVerificationCode(
+            schoolCode = schoolCode,
+        )
     }
 
     override suspend fun fetchAvailableFeatures(): FetchAvailableFeaturesOutput {
-        TODO("Not yet implemented")
+        return remoteSchoolDataSource.fetchAvailableFeatures()
     }
 }
