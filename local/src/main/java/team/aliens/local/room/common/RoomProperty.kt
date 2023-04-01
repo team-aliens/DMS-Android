56b91f140aeb4319dbf6f21f893a251ca94bf5d9
junsuPark <krpjs0508@gmail.com>
2023-04-01 16:32:38 +0900

    ⚡️ :: MealEntity 구현

---
+++ local/src/main/java/team/aliens/local/room/common/RoomProperty.kt
@@ -0,0 +1,18 @@
+package team.aliens.local.room.common
+
+internal object RoomProperty {
+
+    object TableName {
+        const val Meal = "tbl_meals"
+    }
+
+    object ColumnName {
+
+        object Meal {
+            const val Date = "date"
+            const val Breakfast = "breakfast"
+            const val Lunch = "lunch"
+            const val Dinner = "dinner"
+        }
+    }
+}
---
+++ local/src/main/java/team/aliens/local/room/entity/MealEntity.kt
@@ -0,0 +1,30 @@
+package team.aliens.local.room.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import team.aliens.local.room.common.RoomProperty.ColumnName
+import team.aliens.local.room.common.RoomProperty.TableName
+
+@Entity(
+    tableName = TableName.Meal,
+)
+internal data class MealEntity(
+
+    @PrimaryKey
+    @ColumnInfo(
+        name = ColumnName.Meal.Date,
+    ) val date: String,
+
+    @ColumnInfo(
+        name = ColumnName.Meal.Breakfast,
+    ) val breakfast: List<String>,
+
+    @ColumnInfo(
+        name = ColumnName.Meal.Lunch,
+    ) val lunch: List<String>,
+
+    @ColumnInfo(
+        name = ColumnName.Meal.Dinner,
+    ) val dinner: List<String>,
+)
