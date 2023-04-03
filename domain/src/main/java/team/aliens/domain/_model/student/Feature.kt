9e77c9cacc7e6a1da319940833f899288cd2b591
junsuPark <krpjs0508@gmail.com>
2023-04-03 22:22:14 +0900

    ⚡️ :: AuthDataStorage및 구현체 구현

--- domain/src/main/java/team/aliens/domain/_model/student/Feature.kt
+++ domain/src/main/java/team/aliens/domain/_model/student/Feature.kt
@@ -2,10 +2,16 @@ package team.aliens.domain._model.student
 
 /**
  * A data class of app feature
- * @property serviceName feature's name
- * @property enabled a boolean value whether feature is enabled
+ * @property mealService boolean value of meal service enabled
+ * @property noticeService boolean value of notice service enabled
+ * @property pointService boolean value of point service enabled
+ * @property studyRoomService boolean value of study room service enabled
+ * @property remainsService boolean value of remains service enabled
  */
 data class Feature(
-    val serviceName: String,
-    val enabled: Boolean,
+    val mealService: Boolean,
+    val noticeService: Boolean,
+    val pointService: Boolean,
+    val studyRoomService: Boolean,
+    val remainsService: Boolean,
 )
---
+++ local/src/main/java/team/aliens/local/datastore/storage/AuthDataStorage.kt
@@ -0,0 +1,45 @@
+package team.aliens.local.datastore.storage
+
+import team.aliens.domain._model.auth.Token
+import team.aliens.domain._model.student.Feature
+
+interface AuthDataStorage {
+
+    suspend fun findToken(): Token
+
+    suspend fun findAccessToken(): String
+
+    suspend fun findAccessTokenExpiredAt(): String
+
+    suspend fun findRefreshToken(): String
+
+    suspend fun findRefreshTokenExpiredAt(): String
+
+    suspend fun saveToken(
+        token: Token,
+    )
+
+    suspend fun clearToken()
+
+    suspend fun findAutoSignInOption(): Boolean
+
+    suspend fun updateAutoSignInOption(
+        autoSignIn: Boolean,
+    )
+
+    suspend fun findFeature(): Feature
+
+    suspend fun findMealFeatureEnabled(): Boolean
+
+    suspend fun findNoticeFeatureEnabled(): Boolean
+
+    suspend fun findPointServiceEnabled(): Boolean
+
+    suspend fun findStudyRoomServiceEnabled(): Boolean
+
+    suspend fun findRemainsServiceEnabled(): Boolean
+
+    suspend fun saveFeature(
+        feature: Feature,
+    )
+}
