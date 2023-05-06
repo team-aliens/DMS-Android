7af31d99dc696064d3620bd69075d7b4f3ec011b
chlgkdms <haeun3882@gmail.com>
2023-05-06 21:52:22 +0900

    ♻️ :: PointListScreen 줄 바꿈

--- presentation/src/main/java/team/aliens/dms_android/feature/pointlist/PointHistoryScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/pointlist/PointHistoryScreen.kt
@@ -49,7 +49,7 @@ fun PointHistoryScreen(
     var selectedType by remember {
         mutableStateOf(PointType.ALL)
     }
-    
+
     LaunchedEffect(Unit) {
         myPageViewModel.pointTypeEvent.collect {
             if (it is MyPageViewModel.Event.PointTypeEvent) {
