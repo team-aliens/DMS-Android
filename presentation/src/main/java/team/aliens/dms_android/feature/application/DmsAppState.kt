569fff89f650d54d2f6414077289dbe005c9a619
정승훈 <hks026naver@gmail.com>
2023-05-15 21:42:18 +0900

    ♻️ :: navcontroller 접근 제한 제거

--- presentation/src/main/java/team/aliens/dms_android/feature/application/DmsAppState.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/application/DmsAppState.kt
@@ -1,9 +1,9 @@
 package team.aliens.dms_android.feature.application
 
 import androidx.compose.material.ScaffoldState
-import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -11,57 +11,49 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import team.aliens.design_system.toast.ToastType
 
 @Composable
 internal fun rememberDmsAppState(
     scaffoldState: ScaffoldState = rememberScaffoldState(),
     navController: NavController = rememberNavController(),
     coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    bottomSheetState: MutableState<Boolean> = mutableStateOf(false),
 ) = remember {
     DmsAppState(
-        snackbarHostState = scaffoldState.snackbarHostState,
+        scaffoldState = scaffoldState,
         navController = navController,
         coroutineScope = coroutineScope,
     )
 }
 
 internal class DmsAppState(
-    private val snackbarHostState: SnackbarHostState,
-    private val navController: NavController,
-    private val coroutineScope: CoroutineScope,
+    val scaffoldState: ScaffoldState,
+    val navController: NavController,
+    coroutineScope: CoroutineScope,
 ) {
-
-    @Stable
-    val dialogState
-        @Composable get() = remember { mutableStateOf(false) }
 
     @Stable
     val currentDestination = navController.currentDestination?.route
 
-    internal fun showToast(
-        message: String,
-        toastType: ToastType,
-    ) {
+    /*init{
         coroutineScope.launch {
-            snackbarHostState.showSnackbar(
+            scaffoldState.snackbarHostState.showSnackbar(
                 message = message,
                 actionLabel = toastType.toString(),
             )
         }
     }
 
-    internal fun navigateToRoute(
+    fun showToast(
+        message: String,
+        toastType: ToastType,
+    ) {
+
+    }*/
+
+    fun navigateToRoute(
         route: String,
     ) {
         navController.navigate(route)
-    }
-
-    @Composable
-    internal fun setDialogState(
-        dialogState: Boolean,
-    ) {
-        this.dialogState.value = dialogState
     }
 }
