0aac45282f9aeddab8d121b43ff30986cd5bd7e5
junsuPark <krpjs0508@gmail.com>
2023-05-16 12:39:59 +0900

    ⚡️ :: MainViewModel UseCase 로직 변경

--- presentation/src/main/java/team/aliens/dms_android/feature/MainViewModel.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/MainViewModel.kt
@@ -1,35 +1,37 @@
 package team.aliens.dms_android.feature
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import team.aliens.domain.usecase.auth.ReissueTokenUseCase
+import team.aliens.domain.usecase.auth.AutoSignInUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 internal class MainViewModel @Inject constructor(
-    private val reissueTokenUseCase: ReissueTokenUseCase,
+    private val autoSignInUseCase: AutoSignInUseCase,
 ) : ViewModel() {
     init {
         autoSignIn()
     }
 
-    private val _isSignInSuccess = MutableLiveData<Boolean>()
-    val isSignInSuccess: LiveData<Boolean>
-        get() = _isSignInSuccess
+    private val _isSignInSuccess = MutableStateFlow(false)
+    val isSignInSuccess: StateFlow<Boolean>
+        get() = _isSignInSuccess.asStateFlow()
 
     private fun autoSignIn() {
         viewModelScope.launch(Dispatchers.IO) {
+
             kotlin.runCatching {
-                reissueTokenUseCase()
+                autoSignInUseCase()
             }.onSuccess {
-                _isSignInSuccess.postValue(true)
+                _isSignInSuccess.value = true
             }.onFailure {
-                _isSignInSuccess.postValue(false)
+                _isSignInSuccess.value = false
             }
         }
     }
