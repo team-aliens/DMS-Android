bc2826a37f09b87ea6029cc20005af8b9c28bd38
junsuPark <krpjs0508@gmail.com>
2023-05-16 12:39:04 +0900

    ♻️ :: DmsAppState currentDestination 로직 변경

--- presentation/src/main/java/team/aliens/dms_android/feature/application/DmsAppState.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/application/DmsAppState.kt
@@ -8,7 +8,9 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import team.aliens.dms_android.feature.navigator.DmsRoute
@@ -34,7 +36,8 @@ internal class DmsAppState(
 ) {
 
     @Stable
-    val currentDestination = navController.currentDestination?.route
+    val currentDestination: NavDestination?
+        @Composable get() = navController.currentBackStackEntryAsState().value?.destination
 
     /*init{
         coroutineScope.launch {
