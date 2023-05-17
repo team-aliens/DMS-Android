21e626bf6910669c49923ab204dcd7e2ef3299b2
junsuPark <krpjs0508@gmail.com>
2023-05-17 21:43:55 +0900

    ♻️ :: MainActivity DI 제거

--- presentation/src/main/java/team/aliens/dms_android/feature/MainActivity.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/MainActivity.kt
@@ -14,10 +14,9 @@ import team.aliens.dms_android.common.LocalAvailableFeatures
 import team.aliens.dms_android.constans.Extra
 import team.aliens.dms_android.feature.navigator.DmsApp
 import team.aliens.dms_android.feature.navigator.DmsRoute
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity @Inject constructor() : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
