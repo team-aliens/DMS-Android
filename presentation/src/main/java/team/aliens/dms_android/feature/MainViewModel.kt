eb67ae0cf189034a6e74959b5e656640a4c2ec95
junsuPark <krpjs0508@gmail.com>
2023-05-17 21:36:08 +0900

    ⚡️ :: MainViewModel MviViewModel 상속하도록 변경

--- presentation/src/main/java/team/aliens/dms_android/feature/MainViewModel.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/MainViewModel.kt
@@ -1,38 +1,65 @@
 package team.aliens.dms_android.feature
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import team.aliens.dms_android._base.MviViewModel
+import team.aliens.dms_android._base.UiEvent
+import team.aliens.dms_android._base.UiState
+import team.aliens.domain._model._common.toModel
+import team.aliens.domain._model.student.Feature
 import team.aliens.domain.usecase.auth.AutoSignInUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 internal class MainViewModel @Inject constructor(
     private val autoSignInUseCase: AutoSignInUseCase,
-) : ViewModel() {
+) : MviViewModel<MainUiState, MainUiEvent>(
+    initialState = MainUiState.initial(),
+) {
     init {
         autoSignIn()
     }
 
-    private val _isSignInSuccess = MutableStateFlow(false)
-    val isSignInSuccess: StateFlow<Boolean>
-        get() = _isSignInSuccess.asStateFlow()
-
     private fun autoSignIn() {
         viewModelScope.launch(Dispatchers.IO) {
-
             kotlin.runCatching {
                 autoSignInUseCase()
             }.onSuccess {
-                _isSignInSuccess.value = true
+                setState(
+                    newState = uiState.value.copy(
+                        autoSignInSuccess = true,
+                        feature = it.features.toModel(),
+                    ),
+                )
             }.onFailure {
-                _isSignInSuccess.value = false
+                setState(
+                    newState = uiState.value.copy(
+                        autoSignInSuccess = false,
+                    ),
+                )
             }
         }
     }
 }
+
+internal data class MainUiState(
+    val autoSignInSuccess: Boolean,
+    val feature: Feature,
+) : UiState {
+    companion object {
+        fun initial() = MainUiState(
+            autoSignInSuccess = false,
+            feature = Feature(
+                mealService = true,
+                noticeService = true,
+                pointService = true,
+                studyRoomService = true,
+                remainsService = true,
+            ),
+        )
+    }
+}
+
+internal sealed class MainUiEvent : UiEvent
