1736b80b22c6d5b1a61f5294b389b52a4bee06bb
junsuPark <krpjs0508@gmail.com>
2023-05-20 17:33:35 +0900

    ♻️ :: SignInScreen 재구성

--- presentation/src/main/java/team/aliens/dms_android/feature/auth/signin/SignInScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/auth/signin/SignInScreen.kt
@@ -1,40 +1,41 @@
 package team.aliens.dms_android.feature.auth.signin
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import team.aliens.design_system.button.DormButtonColor
 import team.aliens.design_system.button.DormContainedLargeButton
 import team.aliens.design_system.button.DormTextCheckBox
-import team.aliens.design_system.extension.Space
-import team.aliens.design_system.modifier.dormClickable
 import team.aliens.design_system.textfield.DormTextField
 import team.aliens.design_system.theme.DormTheme
 import team.aliens.design_system.typography.Body2
 import team.aliens.design_system.typography.Caption
-import team.aliens.dms_android.common.LocalAvailableFeatures
 import team.aliens.dms_android.component.AppLogo
-import team.aliens.dms_android.constans.Extra
-import team.aliens.dms_android.feature.auth.signin.SignInViewModel.Event
+import team.aliens.dms_android.feature.application.DmsAppState
 import team.aliens.dms_android.feature.navigator.DmsRoute
-import team.aliens.domain.model.student.Feature
 import team.aliens.presentation.R
 
+/*
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SignInScreen(
@@ -261,4 +262,198 @@ fun SignInScreen(
             Space(space = 57.dp)
         }
     }
+}
+*/
+
+@Composable
+internal fun SignInScreen(
+    appState: DmsAppState,
+    signInViewModel: SignInViewModel = hiltViewModel(),
+) {
+    val uiState = signInViewModel.uiState.collectAsStateWithLifecycle()
+    val signInButtonEnabled = uiState.value.signInButtonEnabled
+    val navController = appState.navController
+
+    val onIdChange = { newId: String ->
+        signInViewModel.onEvent(SignInUiEvent.UpdateId(newId))
+    }
+    val onPasswordChange = { newPassword: String ->
+        signInViewModel.onEvent(SignInUiEvent.UpdatePassword(newPassword))
+    }
+    val onAutoSignInOptionChanged = { newAutoSignInOption: Boolean ->
+        signInViewModel.onEvent(SignInUiEvent.UpdateAutoSignInOption(newAutoSignInOption))
+    }
+
+    val onSignUpClicked = {
+        navController.navigate(DmsRoute.SignUp.route)
+    }
+    val onFindIdClicked = {
+        navController.navigate(DmsRoute.Auth.FindId)
+    }
+    val onResetPasswordClicked = {
+        navController.navigate(DmsRoute.Auth.ResetPassword)
+    }
+
+    val onSignInButtonClicked = {
+        signInViewModel.onEvent(SignInUiEvent.SignIn)
+    }
+
+    LaunchedEffect(Unit) {
+        signInViewModel.signInSuccess.collect {
+            if (it) navController.navigate(DmsRoute.Home.route)
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Spacer(Modifier.height(92.dp))
+        Banner()
+        Spacer(Modifier.height(60.dp))
+        UserInformationInputs(
+            idValue = uiState.value.id,
+            passwordValue = uiState.value.password,
+            autoSignInValue = uiState.value.autoSignIn,
+            onIdChange = onIdChange, // 사용자의 행위에 대한 자동 콜백 = 능동형
+            onPasswordChange = onPasswordChange,
+            onAutoSignInOptionChanged = onAutoSignInOptionChanged, // 사용자의 행위 = 수동형(~ed)
+        )
+        Spacer(Modifier.height(12.dp))
+        AuthActions(
+            onSignUpClicked = onSignUpClicked,
+            onFindIdClicked = onFindIdClicked,
+            onResetPasswordClicked = onResetPasswordClicked,
+        )
+        Spacer(Modifier.weight(1f))
+        DormContainedLargeButton(
+            modifier = Modifier.padding(
+                horizontal = 16.dp,
+            ),
+            text = stringResource(R.string.sign_in),
+            color = DormButtonColor.Blue,
+            enabled = uiState.value.signInButtonEnabled,
+            onClick = onSignInButtonClicked,
+        )
+        Spacer(Modifier.height(57.dp))
+    }
+}
+
+@Composable
+private fun Banner() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 16.dp,
+            ),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        AppLogo()
+        Spacer(Modifier.height(8.dp))
+        Body2(
+            text = stringResource(R.string.app_description),
+        )
+    }
+}
+
+@Composable
+private fun UserInformationInputs(
+    idValue: String,
+    passwordValue: String,
+    autoSignInValue: Boolean,
+    onIdChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onAutoSignInOptionChanged: (Boolean) -> Unit,
+) {
+    val focusManager = LocalFocusManager.current
+
+    // 아이디
+    DormTextField(
+        modifier = Modifier.padding(
+            horizontal = 16.dp
+        ),
+        value = idValue,
+        onValueChange = onIdChange,
+        hint = stringResource(R.string.id),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Next)
+            },
+        ),
+    )
+    Spacer(Modifier.height(32.dp))
+    // 비밀번호
+    DormTextField(
+        modifier = Modifier.padding(
+            horizontal = 16.dp
+        ),
+        value = passwordValue,
+        onValueChange = onPasswordChange,
+        hint = stringResource(R.string.password),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusManager.clearFocus()
+            },
+        ),
+    )
+    Spacer(Modifier.height(28.dp))
+    DormTextCheckBox(
+        modifier = Modifier.padding(
+            start = 12.dp,
+        ),
+        text = stringResource(R.string.sign_in_auto_sign_in),
+        checked = autoSignInValue,
+        onCheckedChange = onAutoSignInOptionChanged,
+    )
+    Spacer(Modifier.height(28.dp))
+}
+
+@Composable
+private fun AuthActions(
+    onSignUpClicked: () -> Unit,
+    onFindIdClicked: () -> Unit,
+    onResetPasswordClicked: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(CenterHorizontally)
+            .padding(
+                horizontal = 10.dp,
+            ),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Caption(
+            text = stringResource(R.string.do_sign_up),
+            onClick = onSignUpClicked,
+            color = DormTheme.colors.primaryVariant,
+        )
+        AuthActionDivider()
+        Caption(
+            text = stringResource(R.string.sign_in_find_id),
+            onClick = onFindIdClicked,
+            color = DormTheme.colors.primaryVariant,
+        )
+        AuthActionDivider()
+        Caption(
+            text = stringResource(R.string.change_password),
+            onClick = onResetPasswordClicked,
+            color = DormTheme.colors.primaryVariant,
+        )
+    }
+}
+
+@Composable
+private fun AuthActionDivider() {
+    Caption(
+        text = "|",
+        color = DormTheme.colors.primaryVariant,
+    )
 }
