e6fc8f5874ad0a6e119c7ea954ad084d4296dc5c
정승훈 <hks026naver@gmail.com>
2023-05-23 22:11:01 +0900

    ♻️ :: text 간격 수정

--- presentation/src/main/java/team/aliens/dms_android/component/FloatingNotice.kt
+++ presentation/src/main/java/team/aliens/dms_android/component/FloatingNotice.kt
@@ -2,7 +2,13 @@ package team.aliens.dms_android.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -34,14 +40,16 @@ fun FloatingNotice(
             )
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 16.dp,
+                ),
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Image(
-                modifier = Modifier
-                    .padding(start = 15.dp)
-                    .size(26.dp),
+                modifier = Modifier.size(26.dp),
                 painter = painterResource(id = R.drawable.coloricnotice),
                 contentDescription = "NoticeIcon",
             )
