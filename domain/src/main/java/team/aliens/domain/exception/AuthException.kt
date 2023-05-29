65b618cea8df8b1fbdd05ac5c496eba3e90668d2
junsuPark <krpjs0508@gmail.com>
2023-05-29 11:56:42 +0900

    ⚡️ :: AuthException 구현

---
+++ domain/src/main/java/team/aliens/domain/exception/AuthException.kt
@@ -0,0 +1,18 @@
+package team.aliens.domain.exception
+
+sealed class AuthException(
+    message: String,
+    val code: Int,
+) : RuntimeException(
+    message,
+) {
+    object PasswordMismatch : AuthException(
+        message = "Password mismatch",
+        code = 401,
+    )
+
+    object UserNotFound : AuthException(
+        message = "User not found",
+        code = 404,
+    )
+}
