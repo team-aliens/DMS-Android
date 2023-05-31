d19241365dbc5bc9de7ab678530915edd344c124
junsuPark <krpjs0508@gmail.com>
2023-05-31 17:10:30 +0900

    ⚡️ :: DateUtil 구현

--- data/src/main/java/team/aliens/data/util/DateUtil.kt
+++ data/src/main/java/team/aliens/data/util/DateUtil.kt
@@ -1,14 +1,14 @@
-package team.aliens.remote.util
+package team.aliens.data.util
 
 import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
 
 @SuppressLint("SimpleDateFormat")
 internal val tokenDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
 
 internal fun String.toDate(
-    formatter: SimpleDateFormat,
+    formatter: SimpleDateFormat = tokenDateFormat,
 ): Date {
     return formatter.parse(this)!!
 }
