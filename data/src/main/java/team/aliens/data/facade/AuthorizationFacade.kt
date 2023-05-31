3e43da38e8a8c9e2763e68670012c8293b443cfe
junsuPark <krpjs0508@gmail.com>
2023-05-31 17:39:07 +0900

    ⚡️ :: AuthorizationFacade 재구성

--- data/src/main/java/team/aliens/data/facade/AuthorizationFacade.kt
+++ data/src/main/java/team/aliens/data/facade/AuthorizationFacade.kt
@@ -1,38 +1,61 @@
 package team.aliens.data.facade
 
+import java.util.Date
+import kotlinx.coroutines.runBlocking
 import team.aliens.data.datasource.local.LocalAuthDataSource
+import team.aliens.data.manager.TokenReissueManager
+import team.aliens.data.util.toDate
 import team.aliens.domain.model.auth.Token
 import javax.inject.Inject
 
 class AuthorizationFacade @Inject constructor(
     private val localAuthDataSource: LocalAuthDataSource,
+    private val tokenReissueManager: TokenReissueManager,
 ) {
+    val accessTokenAvailable: Boolean
+        get() {
+            val currentTime = Date()
+            val accessTokenExpiredAt = this.accessTokenExpiredAt().toDate()
 
-    suspend fun accessToken(): String {
-        return localAuthDataSource.findAccessToken()
-    }
+            return currentTime.before(accessTokenExpiredAt)
+        }
 
-    suspend fun accessTokenExpiredAt(): String {
-        return localAuthDataSource.findAccessTokenExpiredAt()
-    }
+    fun reissueAndSaveToken() {
+        val refreshToken = this.refreshToken()
+        val reissuedToken = tokenReissueManager.reissueToken(refreshToken)
 
-    suspend fun refreshToken(): String {
-        return localAuthDataSource.findRefreshToken()
-    }
-
-    suspend fun refreshTokenExpiredAt(): String {
-        return localAuthDataSource.findRefreshTokenExpiredAt()
-    }
-
-    suspend fun saveToken(
-        token: Token,
-    ) {
-        localAuthDataSource.saveToken(
-            token = token,
+        this.saveToken(
+            token = reissuedToken,
         )
     }
 
+    fun accessToken(): String {
+        return runBlocking { localAuthDataSource.findAccessToken() }
+    }
+
+    fun accessTokenExpiredAt(): String {
+        return runBlocking { localAuthDataSource.findAccessTokenExpiredAt() }
+    }
+
+    fun refreshToken(): String {
+        return runBlocking { localAuthDataSource.findRefreshToken() }
+    }
+
+    fun refreshTokenExpiredAt(): String {
+        return runBlocking { localAuthDataSource.findRefreshTokenExpiredAt() }
+    }
+
+    fun saveToken(token: Token) {
+        runBlocking {
+            localAuthDataSource.saveToken(
+                token = token,
+            )
+        }
+    }
+
     suspend fun clearToken() {
-        localAuthDataSource.clearToken()
+        runBlocking {
+            localAuthDataSource.clearToken()
+        }
     }
 }
