f71be79453c9f6b90cb3c95636d1f03f2416dff4
junsuPark <krpjs0508@gmail.com>
2023-06-02 07:43:09 +0900

    ⚡️ :: RemainsOptionResponse에 remain option id 추가

--- domain/src/main/java/team/aliens/domain/model/remains/FetchCurrentAppliedRemainsOptionOutput.kt
+++ domain/src/main/java/team/aliens/domain/model/remains/FetchCurrentAppliedRemainsOptionOutput.kt
@@ -1,9 +1,13 @@
 package team.aliens.domain.model.remains
 
+import java.util.UUID
+
 /**
  * A response returned when fetching current applied remains option
+ * @property appliedRemainsOptionId the unique id of the remains option
  * @property title the title of the remains option
  */
 data class FetchCurrentAppliedRemainsOptionOutput(
+    val appliedRemainsOptionId: UUID,
     val title: String,
 )
--- remote/src/main/java/team/aliens/remote/model/remains/FetchCurrentAppliedRemainsOptionResponse.kt
+++ remote/src/main/java/team/aliens/remote/model/remains/FetchCurrentAppliedRemainsOptionResponse.kt
@@ -1,14 +1,17 @@
 package team.aliens.remote.model.remains
 
 import com.google.gson.annotations.SerializedName
+import java.util.UUID
 import team.aliens.domain.model.remains.FetchCurrentAppliedRemainsOptionOutput
 
 data class FetchCurrentAppliedRemainsOptionResponse(
+    @SerializedName("remain_option_id") val id: UUID,
     @SerializedName("title") val title: String,
 )
 
 internal fun FetchCurrentAppliedRemainsOptionResponse.toDomain(): FetchCurrentAppliedRemainsOptionOutput {
     return FetchCurrentAppliedRemainsOptionOutput(
+        id = this.id,
         title = this.title,
     )
 }
