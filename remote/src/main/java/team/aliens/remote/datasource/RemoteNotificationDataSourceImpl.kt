74c0182af63cff389f0d8614692a735118f1e8c6
junsuPark <krpjs0508@gmail.com>
2023-06-04 10:05:32 +0900

    ⚡️ :: NotificationDataSourceImpl 구현

---
+++ remote/src/main/java/team/aliens/remote/datasource/RemoteNotificationDataSourceImpl.kt
@@ -0,0 +1,36 @@
+package team.aliens.remote.datasource
+
+import team.aliens.data.datasource.remote.RemoteNotificationDataSource
+import team.aliens.domain.model.notification.RegisterDeviceNotificationTokenInput
+import team.aliens.domain.model.notification.SubscribeNotificationTopicInput
+import team.aliens.domain.model.notification.UnsubscribeNotificationTopicInput
+import team.aliens.remote.apiservice.NotificationApiService
+import javax.inject.Inject
+
+class RemoteNotificationDataSourceImpl @Inject constructor(
+    private val notificationApiService: NotificationApiService,
+) : RemoteNotificationDataSource {
+    override suspend fun registerDeviceNotificationToken(
+        input: RegisterDeviceNotificationTokenInput,
+    ) {
+        notificationApiService.registerDeviceNotificationToken(
+            token = input.token,
+        )
+    }
+
+    override suspend fun subscribeNotificationTopic(
+        input: SubscribeNotificationTopicInput,
+    ) {
+        notificationApiService.subscribeNotificationTopic(
+            topic = input.topic.name,
+        )
+    }
+
+    override suspend fun unsubscribeNotificationTopic(
+        input: UnsubscribeNotificationTopicInput,
+    ) {
+        notificationApiService.unsubscribeNotificationTopic(
+            topic = input.topic.name,
+        )
+    }
+}
