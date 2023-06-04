e1732ee60bce5d6c6558365d415b91185754e246
junsuPark <krpjs0508@gmail.com>
2023-06-04 10:11:25 +0900

    ⚡️ :: SubscribeNotificationTopicUseCase 구현

---
+++ domain/src/main/java/team/aliens/domain/usecase/notification/SubscribeNotificationTopicUseCase.kt
@@ -0,0 +1,17 @@
+package team.aliens.domain.usecase.notification
+
+import team.aliens.domain.model.notification.SubscribeNotificationTopicInput
+import team.aliens.domain.repository.NotificationRepository
+import javax.inject.Inject
+
+class SubscribeNotificationTopicUseCase @Inject constructor(
+    private val notificationRepository: NotificationRepository,
+) {
+    suspend operator fun invoke(
+        subscribeNotificationTopicInput: SubscribeNotificationTopicInput,
+    ) {
+        notificationRepository.subscribeNotificationTopic(
+            input = subscribeNotificationTopicInput,
+        )
+    }
+}
