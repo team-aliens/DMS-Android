1366f8ed1b1ab0a6ca16c350a5a830a01419cb00
junsuPark <krpjs0508@gmail.com>
2023-06-13 16:54:07 +0900

    ⚡️ :: ProjectProperties 호환 버전 하향

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -1,10 +1,10 @@
 object ProjectProperties {
     const val ApplicationId = "team.aliens.dms_android"
 
-    const val VersionCode = 110
-    const val VersionName = "v1.1.0"
+    const val VersionCode = 111_14
+    const val VersionName = "v1.1.1 alpha-4"
 
     const val CompileSdkVersion = 33
-    const val MinSdkVersion = 26
-    const val TargetSdkVersion = 33
+    const val MinSdkVersion = 23
+    const val TargetSdkVersion = 30
 }
