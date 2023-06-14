824c83b883ef1280d66429ebf445566e34a0e74c
chlgkdms <haeun3882@gmail.com>
2023-06-15 08:29:49 +0900

    ⚡️ :: Student exception 구현

---
+++ domain/src/main/java/team/aliens/domain/exception/StudentException.kt
@@ -0,0 +1,44 @@
+package team.aliens.domain.exception
+
+sealed class StudentException(
+    message: String,
+    val code: Int,
+) : RuntimeException(
+    message = message,
+) {
+
+    object EmailUnauthorized : StudentException(
+        message = "Email unauthorized",
+        code = 401,
+    )
+
+    object StudentInfoMismatch : StudentException(
+        message = "Student Information mismatch",
+        code = 401,
+    )
+
+    object AuthCodeUnAuthorized : StudentException(
+        message = "Auth code unauthorized",
+        code = 401,
+    )
+
+    object StudentNotFound : StudentException(
+        message = "Student not found",
+        code = 404,
+    )
+
+    object AccountIdAlreadyExists : StudentException(
+        message = "Account Id already exists",
+        code = 409,
+    )
+
+    object StudentAlreadyExists : StudentException(
+        message = "Student already exists",
+        code = 409,
+    )
+
+    object AlreadySignedUp : StudentException(
+        message = "Already signed up",
+        code = 409,
+    )
+}
