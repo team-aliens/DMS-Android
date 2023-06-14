b7ec8e6841e3c1b6075a47f9d8df71823c4975ac
chlgkdms <haeun3882@gmail.com>
2023-06-15 08:30:16 +0900

    ⚡️ :: Notice exception 구현

---
+++ domain/src/main/java/team/aliens/domain/exception/NoticeException.kt
@@ -0,0 +1,14 @@
+package team.aliens.domain.exception
+
+sealed class NoticeException(
+    message: String,
+    val code: Int,
+) : RuntimeException(
+    message = message,
+) {
+
+    object NoticesNotFound : NoticeException(
+        message = "Notices not found",
+        code = 404,
+    )
+}
