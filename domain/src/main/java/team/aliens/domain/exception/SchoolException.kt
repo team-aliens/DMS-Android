0367be8cc3ea582546fd870697386947b0fd3217
chlgkdms <haeun3882@gmail.com>
2023-06-15 08:30:03 +0900

    ⚡️ :: School exception 구현

---
+++ domain/src/main/java/team/aliens/domain/exception/SchoolException.kt
@@ -0,0 +1,24 @@
+package team.aliens.domain.exception
+
+sealed class SchoolException(
+    message: String,
+    val code: Int,
+) : RuntimeException(
+    message = message,
+) {
+
+    object QuestionAnswerMismatch : SchoolException(
+        message = "Question and Answer mismatch",
+        code = 401,
+    )
+
+    object SchoolCodeMismatch : SchoolException(
+        message = "School code mismatch",
+        code = 401,
+    )
+
+    object SchoolNotFound : SchoolException(
+        message = "School not found",
+        code = 404,
+    )
+}
