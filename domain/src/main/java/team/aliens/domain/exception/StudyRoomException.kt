95d0cafe3a0e0fc38df00d95c23580e3373ea3ed
chlgkdms <haeun3882@gmail.com>
2023-06-15 08:29:24 +0900

    ⚡️ :: StudyRoom exception 구현

---
+++ domain/src/main/java/team/aliens/domain/exception/StudyRoomException.kt
@@ -0,0 +1,34 @@
+package team.aliens.domain.exception
+
+sealed class StudyRoomException(
+    message: String,
+    val code: Int,
+) : RuntimeException(
+    message = message,
+) {
+
+    object GenderGradeMismatch: StudyRoomException(
+        message = "Gender and Grade mismatch",
+        code = 401,
+    )
+
+    object NotApplicationTime: StudyRoomException(
+        message = "Not application time",
+        code = 403,
+    )
+
+    object SeatNotFound: StudyRoomException(
+        message = "Seat not found",
+        code = 404,
+    )
+
+    object AvailableTimeNotFound: StudyRoomException(
+        message = "Available time not found",
+        code = 404,
+    )
+
+    object SomeoneAlreadyApplied: StudyRoomException(
+        message = "Someone already applied",
+        code = 409,
+    )
+}
