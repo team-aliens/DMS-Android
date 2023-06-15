ce214a0170175189fb6f433671229e0c2320a091
chlgkdms <haeun3882@gmail.com>
2023-06-15 19:53:18 +0900

    ⚡️ :: Remain exception 구현

---
+++ domain/src/main/java/team/aliens/domain/exception/RemainException.kt
@@ -0,0 +1,23 @@
+package team.aliens.domain.exception
+
+sealed class RemainException(
+    message: String,
+    val code: Int,
+) : RuntimeException(
+    message = message,
+) {
+    object NotRemainApplyTime: RemainException(
+        message = "Not remain apply time",
+        code = 403,
+    )
+
+    object DoNotRemainApply: RemainException(
+        message = "Do not Remain apply",
+        code = 404,
+    )
+
+    object NoRemainApplicationPeriodSet: RemainException(
+        message = "No remain application period set",
+        code = 404,
+    )
+}
