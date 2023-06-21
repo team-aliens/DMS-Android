81029b0e7a2878f37b9b84a4f4291e42faf275e2
junsuPark <krpjs0508@gmail.com>
2023-06-21 22:58:54 +0900

    ♻️ :: DmsAppState 수정

--- presentation/src/main/java/team/aliens/dms_android/DmsAppState.kt
+++ presentation/src/main/java/team/aliens/dms_android/DmsAppState.kt
@@ -4,13 +4,12 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import team.aliens.design_system.toast.ToastState
 import team.aliens.design_system.toast.rememberToastState
+import team.aliens.dms_android.common.rememberAvailableFeatures
 import team.aliens.dms_android.feature.DmsRoute
 import team.aliens.domain.model.student.Feature
 
@@ -19,9 +18,8 @@ internal fun rememberDmsAppState(
     scaffoldState: ScaffoldState = rememberScaffoldState(),
     navController: NavHostController = rememberNavController(),
     toastState: ToastState = rememberToastState(),
-    availableFeatures: Feature = Feature.falseInitialized(),
+    availableFeatures: Feature = rememberAvailableFeatures(),
     darkTheme: Boolean = isSystemInDarkTheme(),
-    bottomSheetState: MutableState<Boolean> = mutableStateOf(false),
 ) = remember {
     DmsAppState(
         scaffoldState = scaffoldState,
@@ -40,20 +38,3 @@ internal class DmsAppState(
     val availableFeatures: Feature,
     val darkTheme: Boolean,
 )
-
-internal fun NavHostController.navigateToSignIn() {
-    this.navigate(DmsRoute.Auth.route) {
-        popUpTo(this@navigateToSignIn.graph.id) {
-            inclusive = true
-        }
-    }
-}
-
-internal fun NavHostController.navigateToHome() {
-    this.navigate(DmsRoute.Home.route) {
-        launchSingleTop = true
-        popUpTo(this@navigateToHome.graph.startDestinationId) {
-            saveState = true
-        }
-    }
-}
