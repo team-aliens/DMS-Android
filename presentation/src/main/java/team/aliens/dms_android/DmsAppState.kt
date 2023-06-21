a0a750a7ceba0f1c109cad93fc27a93ad53f7fd5
junsuPark <krpjs0508@gmail.com>
2023-06-21 17:39:44 +0900

    ♻️ :: DmsAppState 파라미터 변경

--- presentation/src/main/java/team/aliens/dms_android/DmsAppState.kt
+++ presentation/src/main/java/team/aliens/dms_android/DmsAppState.kt
@@ -5,35 +5,30 @@ import androidx.compose.material.ScaffoldState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import team.aliens.design_system.toast.ToastState
+import team.aliens.design_system.toast.rememberToastState
 import team.aliens.dms_android.feature.DmsRoute
-import team.aliens.dms_android.util.manager.ToastManager
 import team.aliens.domain.model.student.Feature
 
 @Composable
 internal fun rememberDmsAppState(
     scaffoldState: ScaffoldState = rememberScaffoldState(),
     navController: NavHostController = rememberNavController(),
-    toastManager: ToastManager = ToastManager,
+    toastState: ToastState = rememberToastState(),
     availableFeatures: Feature = Feature.falseInitialized(),
     darkTheme: Boolean = isSystemInDarkTheme(),
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
     bottomSheetState: MutableState<Boolean> = mutableStateOf(false),
 ) = remember {
     DmsAppState(
         scaffoldState = scaffoldState,
         navController = navController,
-        toastManager = toastManager,
+        toastState = toastState,
         darkTheme = darkTheme,
         availableFeatures = availableFeatures,
-        coroutineScope = coroutineScope,
     )
 }
 
@@ -41,34 +36,10 @@ internal fun rememberDmsAppState(
 internal class DmsAppState(
     val scaffoldState: ScaffoldState,
     val navController: NavHostController,
-    val toastManager: ToastManager,
+    val toastState: ToastState,
     val availableFeatures: Feature,
     val darkTheme: Boolean,
-    coroutineScope: CoroutineScope,
-) {
-
-    init {
-        coroutineScope.launch {
-            toastManager.message.collect { toastMessage ->
-                if (toastMessage.isNotEmpty()) {
-                    scaffoldState.snackbarHostState.showSnackbar(
-                        message = toastMessage[0].message,
-                        actionLabel = toastMessage[0].toastType.toString(),
-                    )
-                }
-            }
-        }
-    }
-
-    @Stable
-    val currentDestination = navController.currentDestination?.route
-
-    fun navigateToRoute(
-        route: String,
-    ) {
-        navController.navigate(route)
-    }
-}
+)
 
 internal fun NavHostController.navigateToSignIn() {
     this.navigate(DmsRoute.Auth.route) {
