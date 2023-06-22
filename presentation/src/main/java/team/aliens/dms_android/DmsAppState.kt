61dde16c2e6285599a79f92402a1dfc53cc013ff
junsuPark <krpjs0508@gmail.com>
2023-06-22 19:23:13 +0900

    ♻️ :: AppState 불필요 프로퍼티 제거

--- presentation/src/main/java/team/aliens/dms_android/DmsAppState.kt
+++ presentation/src/main/java/team/aliens/dms_android/DmsAppState.kt
@@ -1,8 +1,5 @@
 package team.aliens.dms_android
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
@@ -14,26 +11,20 @@ import team.aliens.domain.model.student.Features
 
 @Composable
 internal fun rememberDmsAppState(
-    scaffoldState: ScaffoldState = rememberScaffoldState(),
     navController: NavHostController = rememberNavController(),
     toastState: ToastState = rememberToastState(),
     availableFeatures: Features = rememberAvailableFeatures(),
-    darkTheme: Boolean = isSystemInDarkTheme(),
 ) = remember {
     DmsAppState(
-        scaffoldState = scaffoldState,
         navController = navController,
         toastState = toastState,
-        darkTheme = darkTheme,
         availableFeatures = availableFeatures,
     )
 }
 
 // TODO dialog State 관리 로직 추가하기
 internal class DmsAppState(
-    val scaffoldState: ScaffoldState,
     val navController: NavHostController,
     val toastState: ToastState,
     val availableFeatures: Features,
-    val darkTheme: Boolean,
 )
