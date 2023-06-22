2096354accc86f5559688880408bfbd8ffeb6754
junsuPark <krpjs0508@gmail.com>
2023-06-22 19:23:41 +0900

    ♻️ :: MainViewModel 함수명 변경

--- presentation/src/main/java/team/aliens/dms_android/MainViewModel.kt
+++ presentation/src/main/java/team/aliens/dms_android/MainViewModel.kt
@@ -4,19 +4,19 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.runBlocking
 import team.aliens.domain.model.student.Features
-import team.aliens.domain.usecase.auth.CheckRefreshTokenAvailableUseCase
+import team.aliens.domain.usecase.auth.CheckAutoSignInAvailableUseCase
 import team.aliens.domain.usecase.school.FetchLocalAvailableFeaturesUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 internal class MainViewModel @Inject constructor(
-    private val checkRefreshTokenAvailableUseCase: CheckRefreshTokenAvailableUseCase,
+    private val checkAutoSignInAvailableUseCase: CheckAutoSignInAvailableUseCase,
     private val fetchLocalAvailableFeaturesUseCase: FetchLocalAvailableFeaturesUseCase,
 ) : ViewModel() {
-    fun checkRefreshTokenAvailable(): Boolean {
+    fun checkAutoSignInAvailable(): Boolean {
         return runBlocking {
             kotlin.runCatching {
-                checkRefreshTokenAvailableUseCase()
+                checkAutoSignInAvailableUseCase()
             }
         }.getOrThrow()
     }
