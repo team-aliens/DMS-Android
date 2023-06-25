7d3baf35c84b0c0f4d0e63fc1a5fc03bd183ab3b
junsuPark <krpjs0508@gmail.com>
2023-06-26 08:43:09 +0900

    ⚡️ :: BottomNavigationItem 수정

--- presentation/src/main/java/team/aliens/dms_android/feature/main/home/HomeBottomNavigationItem.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/main/home/HomeBottomNavigationItem.kt
@@ -1,33 +1,35 @@
 package team.aliens.dms_android.feature.main.home
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import team.aliens.presentation.R
 
 internal sealed class HomeBottomNavigationItem(
     val route: String,
-    val iconResId: Int,
-    val titleResId: Int,
+    @DrawableRes val iconResId: Int,
+    @StringRes val titleResId: Int,
 ) {
     object Meal : HomeBottomNavigationItem(
         route = "meal",
         iconResId = R.drawable.ic_home,
-        titleResId = R.string.Home,
+        titleResId = R.string.bottom_nav_home,
     )
 
     object Application : HomeBottomNavigationItem(
         route = "application",
         iconResId = R.drawable.ic_application,
-        titleResId = R.string.Application,
+        titleResId = R.string.bottom_nav_application,
     )
 
     object Notice : HomeBottomNavigationItem(
         route = "notice",
         iconResId = R.drawable.ic_notice,
-        titleResId = R.string.Notice,
+        titleResId = R.string.bottom_nav_notices,
     )
 
     object MyPage : HomeBottomNavigationItem(
         route = "mypage",
         iconResId = R.drawable.ic_mypage,
-        titleResId = R.string.MyPage,
+        titleResId = R.string.bottom_nav_my_page,
     )
 }
