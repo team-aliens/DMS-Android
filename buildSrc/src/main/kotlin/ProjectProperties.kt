467f87b7540cdd94d0efec4418b3ccb4374e9e2f
junsuPark <krpjs0508@gmail.com>
2023-06-27 18:25:13 +0900

    ⚡️ :: project 버전 업그레이드

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -1,8 +1,8 @@
 object ProjectProperties {
     const val ApplicationId = "team.aliens.dms_android"
 
-    const val VersionCode = 111_15
-    const val VersionName = "v1.1.1 alpha-5"
+    const val VersionCode = 111_16
+    const val VersionName = "v1.1.1 alpha-6"
 
     const val CompileSdkVersion = 33
     const val MinSdkVersion = 23
