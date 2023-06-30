20e9b67e9d08042e2cce2e3b92cdc99268eb671e
정승훈 <hks026naver@gmail.com>
2023-06-30 22:56:19 +0900

    ♻️ :: SignUpNavigation 재구성

--- presentation/src/main/java/team/aliens/dms_android/feature/signup/SignUpNavigation.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/signup/SignUpNavigation.kt
@@ -18,6 +18,7 @@ import team.aliens.dms_android.feature.signup.ui.email.SendVerificationEmailScre
 import team.aliens.dms_android.feature.signup.ui.email.VerifyEmailScreen
 import team.aliens.dms_android.feature.signup.ui.id.SetIdScreen
 import team.aliens.dms_android.feature.signup.ui.last.SetProfileImageScreen
+import team.aliens.dms_android.feature.signup.ui.last.TermsScreen
 import team.aliens.dms_android.feature.signup.ui.password.SetPasswordScreen
 import team.aliens.dms_android.feature.signup.ui.school.EnterSchoolVerificationCodeScreen
 import team.aliens.dms_android.feature.signup.ui.school.EnterSchoolVerificationQuestionScreen
@@ -82,14 +83,26 @@ fun NavGraphBuilder.signUpNavigation(
 // todo sign up 작업 후 마저 구현하기
 internal object SignUpNavigation {
     const val route = "signUp"
+    const val verifySchool = this.route + "/verifySchool"
+    const val verifyEmail = this.route + "/verifyEmail"
+    const val setUserInformation = this.route + "/setUserInformation"
 
-    const val EnterSchoolVerificationCode = this.route + "/enterSchoolVerificationCode"
-    const val EnterSchoolVerificationQuestion = this.route + "/enterSchoolVerificationQuestion"
-    const val SetEmail = this.route + "/setEmail"
-    const val VerifyEmail = this.route + "/verifyEmail"
-    const val SetId = this.route + "/setId"
-    const val SetPassword = this.route + "/setPassword"
-    const val SetProfile = this.route + "/setProfile"
+    object VerifySchool {
+        const val EnterSchoolVerificationCode = "$verifySchool/enterSchoolVerificationCode"
+        const val EnterSchoolVerificationQuestion = "$verifySchool/enterSchoolVerificationQuestion"
+    }
+
+    object VerifyEmail {
+        const val SetEmail = "$verifyEmail/setEmail"
+        const val VerifyEmail = "$verifyEmail/verifyEmail"
+    }
+
+    object SetUserInformation {
+        const val SetId = "$setUserInformation/setId"
+        const val SetPassword = "$setUserInformation/setPassword"
+        const val SetProfile = "$setUserInformation/setProfile"
+    }
+
     const val Terms = this.route + "/terms"
 }
 
@@ -97,46 +110,92 @@ internal fun NavGraphBuilder.signUpNavigation(
     navController: NavController,
 ) {
     navigation(
-        startDestination = SignUpNavigation.EnterSchoolVerificationCode,
+        startDestination = SignUpNavigation.verifySchool,
         route = SignUpNavigation.route,
     ) {
-        composable(SignUpNavigation.EnterSchoolVerificationCode) {
+        verifySchoolNavigation(
+            navController = navController,
+        )
+
+        verifyEmailNavigation(
+            navController = navController,
+        )
+
+        setUserInformationNavigation(
+            navController = navController,
+        )
+
+        composable(SignUpNavigation.Terms) {
+            TermsScreen(
+                navController = navController,
+            )
+        }
+    }
+}
+
+private fun NavGraphBuilder.verifySchoolNavigation(
+    navController: NavController,
+) {
+    navigation(
+        startDestination = SignUpNavigation.VerifySchool.EnterSchoolVerificationCode,
+        route = SignUpNavigation.verifySchool,
+    ) {
+        composable(SignUpNavigation.VerifySchool.EnterSchoolVerificationCode) {
             EnterSchoolVerificationCodeScreen(
                 navController = navController,
             )
         }
-        composable(SignUpNavigation.EnterSchoolVerificationQuestion) {
+
+        composable(SignUpNavigation.VerifySchool.EnterSchoolVerificationQuestion) {
             EnterSchoolVerificationQuestionScreen(
                 navController = navController,
             )
         }
-        composable(SignUpNavigation.SetEmail) {
+    }
+}
+
+private fun NavGraphBuilder.verifyEmailNavigation(
+    navController: NavController,
+) {
+    navigation(
+        startDestination = SignUpNavigation.VerifyEmail.SetEmail,
+        route = SignUpNavigation.verifyEmail,
+    ) {
+        composable(SignUpNavigation.VerifyEmail.SetEmail) {
             SendVerificationEmailScreen(
                 navController = navController,
             )
         }
-        composable(SignUpNavigation.VerifyEmail){
+
+        composable(SignUpNavigation.VerifyEmail.VerifyEmail) {
             VerifyEmailScreen(
                 navController = navController,
             )
         }
-        composable(SignUpNavigation.SetId) {
+    }
+}
+
+private fun NavGraphBuilder.setUserInformationNavigation(
+    navController: NavController,
+) {
+    navigation(
+        startDestination = SignUpNavigation.SetUserInformation.SetId,
+        route = SignUpNavigation.setUserInformation,
+    ) {
+        composable(SignUpNavigation.SetUserInformation.SetId) {
             SetIdScreen(
                 navController = navController,
             )
         }
-        composable(SignUpNavigation.SetPassword) {
+
+        composable(SignUpNavigation.SetUserInformation.SetPassword) {
             SetPasswordScreen(
                 navController = navController,
             )
         }
-        composable(SignUpNavigation.SetProfile) {
+
+        composable(SignUpNavigation.SetUserInformation.SetProfile) {
             SetProfileImageScreen(
-                navController = navController,
-            )
-        }
-        composable(SignUpNavigation.Terms) {
-            TermsScreen(
                 navController = navController,
             )
         }
