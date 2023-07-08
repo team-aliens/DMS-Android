ee5a0b9fe6175a7a33b865bbb140a5174761d816
Tmdhoon2 <hks026naver@gmail.com>
2023-07-08 21:43:37 +0900

    ♻️ :: SignUpNavigation 주석 제거

--- presentation/src/main/java/team/aliens/dms_android/feature/signup/SignUpNavigation.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/signup/SignUpNavigation.kt
@@ -1,15 +1,5 @@
 package team.aliens.dms_android.feature.signup
 
-/*import team.aliens.dms_android.feature.DmsRoute
-import team.aliens.dms_android.feature.signup.ui.email.SignUpEmailVerificationCodeScreen
-import team.aliens.dms_android.feature.signup.ui.email.SignUpSendVerificationEmailScreen
-import team.aliens.dms_android.feature.signup.ui.id.SignUpSetIdScreen
-import team.aliens.dms_android.feature.signup.ui.last.SignUpPolicyScreen
-import team.aliens.dms_android.feature.signup.ui.last.SignUpSetProfileImageScreen
-import team.aliens.dms_android.feature.signup.ui.password.SignUpSetPasswordScreen
-import team.aliens.dms_android.feature.signup.ui.school.SignUpSchoolVerificationQuestionScreen
-import team.aliens.dms_android.feature.signup.ui.school.SignUpVerifySchoolScreen
-import androidx.navigation.NavController*/
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -24,63 +14,6 @@ import team.aliens.dms_android.feature.signup.ui.school.EnterSchoolVerificationC
 import team.aliens.dms_android.feature.signup.ui.school.EnterSchoolVerificationQuestionScreen
 import team.aliens.dms_android.util.composableActivityViewModel
 
-/*
-fun NavGraphBuilder.signUpNavigation(
-    navController: NavController,
-) {
-    navigation(
-        startDestination = DmsRoute.SignUp.VerifySchool,
-        route = DmsRoute.SignUp.route,
-    ) {
-        composable(DmsRoute.SignUp.VerifySchool) {
-            SignUpVerifySchoolScreen(
-                navController = navController,
-            )
-        }
-
-        composable(DmsRoute.SignUp.SchoolVerificationQuestion) {
-            SignUpSchoolVerificationQuestionScreen(
-                navController = navController,
-            )
-        }
-
-        composable(DmsRoute.SignUp.SendVerificationEmail) {
-            SignUpSendVerificationEmailScreen(
-                navController = navController,
-            )
-        }
-
-        composable(DmsRoute.SignUp.CheckEmailVerificationCode) {
-            SignUpEmailVerificationCodeScreen(
-                navController = navController,
-            )
-        }
-
-        composable(DmsRoute.SignUp.SetId) {
-            SignUpSetIdScreen(
-                navController = navController,
-            )
-        }
-
-        composable(DmsRoute.SignUp.SetPassword) {
-            SignUpSetPasswordScreen(
-                navController = navController,
-            )
-        }
-
-        composable(DmsRoute.SignUp.SetProfileImage) {
-            SignUpSetProfileImageScreen(
-                navController = navController,
-            )
-        }
-
-        composable(DmsRoute.SignUp.Policy) {
-            SignUpPolicyScreen(
-                navController = navController,
-            )
-        }
-    }
-}*/
 // todo sign up 작업 후 마저 구현하기
 internal object SignUpNavigation {
     const val route = "signUp"
