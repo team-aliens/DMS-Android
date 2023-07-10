935eea829a648d5b94ca5fbc39bb6c6beccf1f64
junsuPark <krpjs0508@gmail.com>
2023-07-10 10:10:42 +0900

    notification box screen 구현

--- presentation/src/main/java/team/aliens/dms_android/feature/main/notificationbox/NotificationBoxScreen.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/main/notificationbox/NotificationBoxScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,7 +20,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import team.aliens.design_system.icon.DormIcon
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import team.aliens.design_system.layout.VerticallyFadedLazyColumn
 import team.aliens.design_system.modifier.dormClickable
 import team.aliens.design_system.modifier.dormShadow
@@ -26,13 +29,18 @@ import team.aliens.design_system.theme.DormTheme
 import team.aliens.design_system.typography.Body3
 import team.aliens.design_system.typography.Caption
 import team.aliens.dms_android.util.TopBar
+import team.aliens.domain.model.notification.Notification
+import team.aliens.domain.model.notification.NotificationTopic
 import team.aliens.presentation.R
 
 @Composable
 internal fun NotificationBoxScreen(
     modifier: Modifier = Modifier,
+    notificationBoxViewModel: NotificationBoxViewModel = hiltViewModel(),
     onPrevious: () -> Unit,
 ) {
+    val uiState by notificationBoxViewModel.stateFlow.collectAsStateWithLifecycle()
+
     Column(
         modifier = modifier
             .background(DormTheme.colors.background)
@@ -44,7 +52,8 @@ internal fun NotificationBoxScreen(
         )
         Notifications(
             modifier = Modifier.fillMaxWidth(),
-            notifications = listOf(),
+            newNotifications = uiState.newNotifications,
+            priorNotifications = uiState.priorNotifications,
         )
     }
 }
@@ -53,54 +62,34 @@ internal fun NotificationBoxScreen(
 @Composable
 private fun Notifications(
     modifier: Modifier = Modifier,
-    notifications: List<Any>, // todo
+    newNotifications: List<Notification>,
+    priorNotifications: List<Notification>,
 ) {
+    println(newNotifications)
+    println(priorNotifications)
     VerticallyFadedLazyColumn(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(12.dp),
-        contentPadding = PaddingValues(top = 16.dp),
     ) {
-        item {
-            Notification(
-                icon = DormIcon.MyPage.drawableId,
-                isNew = true,
-            )
+        if (newNotifications.isNotEmpty()) {
+            items(newNotifications) { newNotification ->
+                Notification(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    notification = newNotification,
+                )
+            }
         }
-        item{
-            Notification(
-                icon = DormIcon.MyPage.drawableId,
-                isNew = true,
-            )
-        }
-        item {
-            Notification(
-                icon = DormIcon.MyPage.drawableId,
-                isNew = false,
-            )
-        }
-        item {
-            Notification(
-                icon = DormIcon.MyPage.drawableId,
-                isNew = false,
-            )
-        }
-        item {
-            Notification(
-                icon = DormIcon.MyPage.drawableId,
-                isNew = false,
-            )
-        }
-        item {
-            Notification(
-                icon = DormIcon.MyPage.drawableId,
-                isNew = false,
-            )
-        }
-        item {
-            Notification(
-                icon = DormIcon.MyPage.drawableId,
-                isNew = false,
-            )
+        if (priorNotifications.isNotEmpty()) {
+            items(priorNotifications) { priorNotification ->
+                Notification(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    notification = priorNotification,
+                )
+            }
         }
     }
 }
@@ -108,16 +97,14 @@ private fun Notifications(
 @Composable
 private fun Notification(
     modifier: Modifier = Modifier,
-    icon: Int,
-    isNew: Boolean,
+    notification: Notification,
 ) {
     Row(
         modifier = modifier
-            .padding(horizontal = 16.dp)
             .dormShadow(DormTheme.colors.primaryVariant)
             .fillMaxWidth()
             .background(
-                color = if (isNew) {
+                color = if (notification.read) {
                     DormTheme.colors.secondary
                 } else {
                     DormTheme.colors.surface
@@ -136,9 +123,13 @@ private fun Notification(
         verticalAlignment = Alignment.Top,
     ) {
         Icon(
-            painter = painterResource(icon),
+            painter = painterResource(
+                when (notification.topic) {
+                    NotificationTopic.NOTICE -> R.drawable.ic_notice
+                },
+            ),
             contentDescription = null,
-            tint = if (isNew) {
+            tint = if (notification.read) {
                 DormTheme.colors.primary
             } else {
                 DormTheme.colors.onBackground
@@ -151,16 +142,16 @@ private fun Notification(
             verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             Body3(
-                text = "TEXTTEXT",
+                text = notification.title,
                 color = Color.Black,
             )
             Caption(
-                text = "CAPTIONCAPTION",
+                text = notification.content,
                 color = Color.Black,
             )
         }
         Caption(
-            text = "2년 전",
+            text = notification.createdAt,
             color = Color.Black,
         )
     }
