8640230de4c349a89deed56b907f099610b6293c
chlgkdms <haeun3882@gmail.com>
2023-07-11 18:38:15 +0900

    ♻️ :: find id view model mvi 적용

--- presentation/src/main/java/team/aliens/dms_android/feature/auth/findid/FindIdViewModel.kt
+++ presentation/src/main/java/team/aliens/dms_android/feature/auth/findid/FindIdViewModel.kt
@@ -1,51 +1,78 @@
 package team.aliens.dms_android.feature.auth.findid
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.util.UUID
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import team.aliens.dms_android.util.MutableEventFlow
-import team.aliens.dms_android.util.asEventFlow
+import team.aliens.dms_android.base.BaseMviViewModel
+import team.aliens.dms_android.base.MviIntent
+import team.aliens.dms_android.base.MviSideEffect
+import team.aliens.dms_android.base.MviState
+import team.aliens.domain.exception.AuthException
+import team.aliens.domain.exception.RemoteException
+import team.aliens.domain.model.school.FetchSchoolsOutput
 import team.aliens.domain.model.student.FindIdInput
 import team.aliens.domain.usecase.school.FetchSchoolsUseCase
 import team.aliens.domain.usecase.student.FindIdUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class FindIdViewModel @Inject constructor(
+internal class FindIdViewModel @Inject constructor(
     private val findIdUseCase: FindIdUseCase,
     private val fetchSchoolsUseCase: FetchSchoolsUseCase,
-) : ViewModel() {
-
+) : BaseMviViewModel<FindIdIntent, FindIdState, FindIdSideEffect>(
+    initialState = FindIdState.initial(),
+) {
     init {
         fetchSchools()
     }
 
-    private val _findIdEvent = MutableEventFlow<FindIdEvent>()
-    internal val findIdEvent = _findIdEvent.asEventFlow()
+    private val nameEntered: Boolean
+        get() = stateFlow.value.name.isNotBlank()
+    private val gradeEntered: Boolean
+        get() = stateFlow.value.grade.isNotBlank()
+    private val classRoomEntered: Boolean
+        get() = stateFlow.value.classRoom.isNotBlank()
+    private val numberEntered: Boolean
+        get() = stateFlow.value.number.isNotBlank()
 
-    lateinit var email: String
 
-    internal fun findId(schoolId: UUID, name: String, grade: Int, classRoom: Int, number: Int) {
+    override fun processIntent(intent: FindIdIntent) {
+        when (intent) {
+            is FindIdIntent.FindId -> this.findId()
+            is FindIdIntent.FetchSchools -> this.fetchSchools()
+            is FindIdIntent.UpdateGrade -> this.setGrade(newGrade = intent.newGrade)
+            is FindIdIntent.UpdateClassRoom -> this.setClassRoom(newClassRoom = intent.newClassRoom)
+            is FindIdIntent.UpdateNumber -> this.setNumber(newNumber = intent.newNumber)
+            is FindIdIntent.UpdateName -> this.setName(newName = intent.newName)
+        }
+    }
+
+    private fun findId() {
+        setFindIdButtonState(false)
+
         viewModelScope.launch {
             kotlin.runCatching {
-                email = findIdUseCase(
+                val currentUserInformation = this@FindIdViewModel.stateFlow.value
+                findIdUseCase(
                     FindIdInput(
-                        schoolId = schoolId,
-                        studentName = name,
-                        grade = grade,
-                        classRoom = classRoom,
-                        number = number,
+                        schoolId = currentUserInformation.schools?.id!!,
+                        studentName = currentUserInformation.name,
+                        grade = currentUserInformation.grade.toInt(),
+                        classRoom = currentUserInformation.classRoom.toInt(),
+                        number = currentUserInformation.number.toInt(),
                     ),
-                ).email
-            }.onSuccess {
-                event(SuccessFindId)
-            }.onFailure {
-                event(
-                    getEventFromThrowable(it),
                 )
+            }.onSuccess {
+                postSideEffect(
+                    sideEffect = FindIdSideEffect.FindIdSuccess(email = it.email)
+                )
+            }.onFailure {
+                when (it) {
+                    RemoteException.BadRequest -> postSideEffect(FindIdSideEffect.BadRequest)
+                    AuthException.UserNotFound -> postSideEffect(FindIdSideEffect.NotFound)
+                }
             }
         }
     }
@@ -55,23 +82,112 @@ class FindIdViewModel @Inject constructor(
             kotlin.runCatching {
                 fetchSchoolsUseCase()
             }.onSuccess {
-                event(FetchSchools(it))
+
             }.onFailure {
 
             }
         }
     }
 
+    private fun setGrade(newGrade: String) {
+        reduce(
+            newState = currentState.copy(
+                gradeError = false,
+                grade = newGrade,
+            ),
+        )
 
-    fun event(event: FindIdEvent) {
-        viewModelScope.launch {
-            _findIdEvent.emit(event)
-        }
+        setFindIdButtonState()
+    }
+
+    private fun setClassRoom(newClassRoom: String) {
+        reduce(
+            newState = currentState.copy(
+                classRoomError = false,
+                classRoom = newClassRoom,
+            ),
+        )
+
+        setFindIdButtonState()
+    }
+
+    private fun setNumber(newNumber: String) {
+        reduce(
+            newState = currentState.copy(
+                numberError = false,
+                number = newNumber,
+            ),
+        )
+
+        setFindIdButtonState()
+    }
+
+    private fun setName(newName: String) {
+        reduce(
+            newState = currentState.copy(
+                nameError = false,
+                name = newName,
+            ),
+        )
+
+        setFindIdButtonState()
+    }
+
+    private fun setFindIdButtonState(
+        enabled: Boolean = nameEntered && gradeEntered && classRoomEntered && numberEntered
+    ) {
+        reduce(
+            newState = currentState.copy(
+                findIdButtonEnabled = enabled,
+            ),
+        )
     }
 }
 
-private fun getEventFromThrowable(throwable: Throwable): FindIdEvent {
-    return when (throwable) {
-        else -> FindIdUnknownException
+
+internal sealed class FindIdIntent : MviIntent {
+    object FindId : FindIdIntent()
+    object FetchSchools : FindIdIntent()
+    class UpdateGrade(val newGrade: String) : FindIdIntent()
+    class UpdateClassRoom(val newClassRoom: String) : FindIdIntent()
+    class UpdateNumber(val newNumber: String) : FindIdIntent()
+    class UpdateName(val newName: String) : FindIdIntent()
+
+}
+
+internal data class FindIdState(
+    val grade: String,
+    val classRoom: String,
+    val number: String,
+    val name: String,
+    val gradeError: Boolean,
+    val classRoomError: Boolean,
+    val numberError: Boolean,
+    val nameError: Boolean,
+    val findIdButtonEnabled: Boolean,
+    val schools: FetchSchoolsOutput.SchoolInformation?,
+    val selectedSchool: FetchSchoolsOutput.SchoolInformation?,
+) : MviState {
+    companion object {
+        fun initial() = FindIdState(
+            grade = "",
+            classRoom = "",
+            number = "",
+            name = "",
+            gradeError = false,
+            classRoomError = false,
+            numberError = false,
+            nameError = false,
+            findIdButtonEnabled = false,
+            schools = null,
+            selectedSchool = null,
+        )
     }
+}
+
+internal sealed class FindIdSideEffect : MviSideEffect {
+    class FindIdSuccess(val email: String) : FindIdSideEffect()
+    object BadRequest : FindIdSideEffect()
+    object NotFound : FindIdSideEffect()
+
 }
