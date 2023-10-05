addc091113a1ad061ac60c1e8937ad72b605e95c
junsuPark <krpjs0508@gmail.com>
2023-10-05 13:16:52 +0900

    ♻️ :: app 기본 Activity 구현

---
+++ DMS/src/main/java/team/aliens/dms_android/app/MainActivityViewModel.kt
@@ -0,0 +1,5 @@
+package team.aliens.dms_android.app
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel()
---
+++ DMS/src/main/java/team/aliens/dms_android/app/splash/SplashActivity.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms_android.app.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
