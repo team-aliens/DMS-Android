77739cf36cb15aff5f8b2b9ac9f18339bc6ebd90
junsuPark <krpjs0508@gmail.com>
2023-10-05 16:07:43 +0900

    ⚡️:: ksp 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.google.ksp) apply false
 }
