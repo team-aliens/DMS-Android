a85a1fc1cc0f3be3782cc4276d92c35e23a0932b
junsuPark <krpjs0508@gmail.com>
2023-10-05 23:05:43 +0900

    ♻️ :: project gradle 리팩토링

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
-@file:Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+// TODO: Remove once KTIJ-19369 is fixed
+@file:Suppress("DSL_SCOPE_VIOLATION")
 
 plugins {
     alias(libs.plugins.android.application) apply false
