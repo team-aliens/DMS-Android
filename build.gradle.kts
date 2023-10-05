cdf34324c9c43b44b5052abf13d78b59fc5b4f72
junsuPark <krpjs0508@gmail.com>
2023-10-05 15:52:08 +0900

    ♻️ :: 프로젝트 레벨 그래들 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,7 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath(Plugins.Gradle.Android)
-        classpath(Plugins.Gradle.Kotlin)
-        classpath(Plugins.Gradle.Hilt)
-        classpath(Plugins.Gradle.Firebase)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.google.services) apply false
 }
