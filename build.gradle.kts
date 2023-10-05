b0d698d381f878187ec27a370a2420935814f951
junsuPark <krpjs0508@gmail.com>
2023-10-05 20:30:17 +0900

    ♻️ :: gradle version catalog lint 억제

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+@file:Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.google.services) apply false
-    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.ksp) apply false
 }
