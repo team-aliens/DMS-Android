a8d65253f996ec08dea660316348fce4de5723f7
junsuPark <krpjs0508@gmail.com>
2023-10-07 08:03:25 +0900

    ️⚡️ :: BadRequestException 추가

--- core/network/src/main/java/team/aliens/dms_android/core/network/exception/BadRequestException.kt
+++ core/network/src/main/java/team/aliens/dms_android/core/network/exception/BadRequestException.kt
@@ -1,3 +1,6 @@
 package team.aliens.dms_android.core.network.exception
 
-class BadRequestException : NetworkException("Bad request")
+class BadRequestException : NetworkException(
+    code = 400,
+    message = "Bad request",
+)
