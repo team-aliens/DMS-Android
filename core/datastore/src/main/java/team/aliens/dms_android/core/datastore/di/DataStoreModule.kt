c49519a0858d20c97ca85ee46537553bc397c8e5
junsuPark <krpjs0508@gmail.com>
2023-10-06 21:22:14 +0900

    ️⚡️ :: DataStoreModule 구현

---
+++ core/datastore/src/main/java/team/aliens/dms_android/core/datastore/di/DataStoreModule.kt
@@ -0,0 +1,23 @@
+package team.aliens.dms_android.core.datastore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import team.aliens.dms_android.core.datastore.dataStore
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun providePreferenceDataStore(
+        @ApplicationContext context: Context,
+    ): DataStore<Preferences> = context.dataStore
+}
