0602cf734ea474271861c118e4f3672c5b3030da
junsuPark <krpjs0508@gmail.com>
2023-10-07 08:02:58 +0900

    ️⚡️ :: NetworkException 파라미터 추가

--- core/network/src/main/java/team/aliens/dms_android/core/network/exception/NetworkException.kt
+++ core/network/src/main/java/team/aliens/dms_android/core/network/exception/NetworkException.kt
@@ -1,5 +1,7 @@
 package team.aliens.dms_android.core.network.exception
 
-sealed class RemoteException(
+sealed class NetworkException(
+    // TODO: consider default value
+    val code: Int,
     message: String?,
 ) : RuntimeException(message)
