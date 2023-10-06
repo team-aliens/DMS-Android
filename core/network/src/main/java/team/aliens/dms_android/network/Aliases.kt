53653a8e53b8b5d89c87b6d8b2c48d03c5d53fd2
junsuPark <krpjs0508@gmail.com>
2023-10-06 22:27:32 +0900

    ️⚡️ :: HttpClient Alias 추가

---
+++ core/network/src/main/java/team/aliens/dms_android/network/Aliases.kt
@@ -0,0 +1,9 @@
+package team.aliens.dms_android.network
+
+import okhttp3.OkHttpClient
+
+typealias DefaultHttpClient = OkHttpClient
+
+typealias GlobalHttpClient = OkHttpClient
+
+typealias TokenReissueHttpClient = OkHttpClient
