10b5e101d15eb1998f42516653fd410fe886f168
junsuPark <krpjs0508@gmail.com>
2023-10-06 19:14:51 +0900

    ♻️ :: Hilt 적용

--- app/src/main/java/team/aliens/dms_android/app/MainActivity.kt
+++ app/src/main/java/team/aliens/dms_android/app/MainActivity.kt
@@ -2,7 +2,9 @@ package team.aliens.dms_android.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
