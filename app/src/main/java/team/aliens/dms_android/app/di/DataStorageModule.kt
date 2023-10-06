679d740a727d869f8c6331f855d0570f6b57944f
junsuPark <krpjs0508@gmail.com>
2023-10-06 19:08:24 +0900

    ♻️ :: DataStorageModule 추가

---
+++ app/src/main/java/team/aliens/dms_android/app/di/DataStorageModule.kt
@@ -0,0 +1,24 @@
+package team.aliens.dms_android.app.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import team.aliens.local.datastore.storage.AuthDataStorage
+import team.aliens.local.datastore.storage.AuthDataStorageImpl
+import team.aliens.local.datastore.storage.SchoolDataStorage
+import team.aliens.local.datastore.storage.SchoolDataStorageImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataStorageModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthDataStorage(impl: AuthDataStorageImpl): AuthDataStorage
+
+    @Binds
+    @Singleton
+    abstract fun bindStudentDataStorage(impl: SchoolDataStorageImpl): SchoolDataStorage
+}
