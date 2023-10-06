f3ccb283707c1f299f931f62fb4f43469521bfce
junsuPark <krpjs0508@gmail.com>
2023-10-06 13:18:03 +0900

    ♻️ :: NetworkDataSourceModule 이동

---
+++ di/src/main/java/team/aliens/di/datasource/RemoteDataSourceModule.kt
@@ -0,0 +1,81 @@
+package team.aliens.di.datasource
+/*
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import team.aliens.data.datasource.remote.*
+import team.aliens.remote.datasource.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RemoteDataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteAuthDataSource(
+        impl: RemoteAuthDataSourceImpl,
+    ): RemoteAuthDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteFileDataSource(
+        impl: RemoteFileDataSourceImpl,
+    ): RemoteFileDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteMealDataSource(
+        impl: RemoteMealDataSourceImpl,
+    ): RemoteMealDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteNoticeDataSource(
+        impl: RemoteNoticeDataSourceImpl,
+    ): RemoteNoticeDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemotePointDataSource(
+        impl: RemotePointDataSourceImpl,
+    ): RemotePointDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteRemainDataSource(
+        impl: RemoteRemainsDataSourceImpl,
+    ): RemoteRemainsDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteSchoolDataSource(
+        impl: RemoteSchoolDataSourceImpl,
+    ): RemoteSchoolDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteStudentDataSource(
+        impl: RemoteStudentDataSourceImpl,
+    ): RemoteStudentDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteStudyRoomDataSource(
+        impl: RemoteStudyRoomDataSourceImpl,
+    ): RemoteStudyRoomDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteUserDataSource(
+        impl: RemoteUserDataSourceImpl,
+    ): RemoteUserDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteNotificationDataSource(
+        impl: RemoteNotificationDataSourceImpl,
+    ): RemoteNotificationDataSource
+}
+*/
