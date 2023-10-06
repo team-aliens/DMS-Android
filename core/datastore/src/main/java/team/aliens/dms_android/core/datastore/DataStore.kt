7589bc5132da36c79ec316c52baaa39d3d123ba0
junsuPark <krpjs0508@gmail.com>
2023-10-06 21:28:11 +0900

    ♻️ :: Typealias 적용

--- core/datastore/src/main/java/team/aliens/dms_android/core/datastore/DataStore.kt
+++ core/datastore/src/main/java/team/aliens/dms_android/core/datastore/DataStore.kt
@@ -1,8 +1,6 @@
 package team.aliens.dms_android.core.datastore
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 
-internal val Context.dataStore: DataStore<Preferences> by preferencesDataStore("dms-datastore")
+internal val Context.dataStore: PreferencesDataStore by preferencesDataStore("dms-datastore")
--- core/datastore/src/main/java/team/aliens/dms_android/core/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/team/aliens/dms_android/core/datastore/di/DataStoreModule.kt
@@ -1,13 +1,12 @@
 package team.aliens.dms_android.core.datastore.di
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import team.aliens.dms_android.core.datastore.PreferencesDataStore
 import team.aliens.dms_android.core.datastore.dataStore
 import javax.inject.Singleton
 
@@ -19,5 +18,5 @@ internal object DataStoreModule {
     @Singleton
     fun providePreferenceDataStore(
         @ApplicationContext context: Context,
-    ): DataStore<Preferences> = context.dataStore
+    ): PreferencesDataStore = context.dataStore
 }
