8341dcb5295896c463caff997342092de967cd99
junsuPark <krpjs0508@gmail.com>
2023-10-06 21:21:36 +0900

    ️⚡️ :: datastore 구현

---
+++ core/datastore/src/main/java/team/aliens/dms_android/core/datastore/DataStore.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms_android.core.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+internal val Context.dataStore: DataStore<Preferences> by preferencesDataStore("dms-datastore")
