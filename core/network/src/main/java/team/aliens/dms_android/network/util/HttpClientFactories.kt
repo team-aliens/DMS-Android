be782e3337fe663297abf67f254c47952031236f
junsuPark <krpjs0508@gmail.com>
2023-10-06 23:24:52 +0900

    ️⚡️ :: HttpClientFactories 구현

---
+++ core/network/src/main/java/team/aliens/dms_android/network/util/HttpClientFactories.kt
@@ -0,0 +1,27 @@
+package team.aliens.dms_android.network.util
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+fun Retrofit(
+    vararg clients: OkHttpClient,
+    baseUrl: String,
+    gsonConverter: Boolean = false,
+): Retrofit = Retrofit.Builder().baseUrl(baseUrl).apply {
+    if (clients.isNotEmpty()) {
+        clients.forEach(this::client)
+    }
+    if (gsonConverter) {
+        addConverterFactory(GsonConverterFactory.create())
+    }
+}.build()
+
+fun OkHttpClient(
+    vararg interceptors: Interceptor,
+): OkHttpClient = OkHttpClient().newBuilder().apply {
+    if (interceptors.isNotEmpty()) {
+        interceptors.forEach(this::addInterceptor)
+    }
+}.build()
