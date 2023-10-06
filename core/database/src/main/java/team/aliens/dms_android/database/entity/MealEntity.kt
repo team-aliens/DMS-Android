a6e36faa3f9ec1f7693f8b5c20a746898345ecbf
junsuPark <krpjs0508@gmail.com>
2023-10-06 21:50:45 +0900

    ♻️ :: Entity 접근제한자 수정

--- core/database/src/main/java/team/aliens/dms_android/database/entity/MealEntity.kt
+++ core/database/src/main/java/team/aliens/dms_android/database/entity/MealEntity.kt
@@ -4,8 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+// TODO: 접근제한자 internal로 수정 필요
 @Entity(tableName = "tbl_meals")
-internal data class MealEntity(
+data class MealEntity(
     @PrimaryKey @ColumnInfo("date") val date: String,
     @ColumnInfo(name = "breakfast") val breakfast: List<String>,
     @ColumnInfo(name = "lunch") val lunch: List<String>,
--- core/database/src/main/java/team/aliens/dms_android/database/entity/NoticeEntity.kt
+++ core/database/src/main/java/team/aliens/dms_android/database/entity/NoticeEntity.kt
@@ -5,8 +5,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.UUID
 
+// TODO: 접근제한자 internal로 수정 필요
 @Entity(tableName = "tbl_notices")
-internal data class NoticeEntity(
+data class NoticeEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: UUID,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "content") val content: String?,
