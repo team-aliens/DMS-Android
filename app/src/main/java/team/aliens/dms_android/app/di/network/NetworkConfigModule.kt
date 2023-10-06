e14c564b774b7df2ccbfb8c78a5dc7a8061a335f
junsuPark <krpjs0508@gmail.com>
2023-10-06 18:29:30 +0900

    ️⚡️ :: NetworkConfigModule 구현

---
+++ app/src/main/java/team/aliens/dms_android/app/di/network/NetworkConfigModule.kt
@@ -0,0 +1,23 @@
+package team.aliens.dms_android.app.di.network
+
+import team.aliens.dms_android.remote.BuildConfig as RemoteBuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import team.aliens.remote.annotation.BaseUrl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkConfigModule {
+
+    @Provides
+    @Singleton
+    @BaseUrl
+    fun provideBaseUrl(): String = RemoteBuildConfig.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideTokenReissueUrl(@BaseUrl baseUrl: String): String = "$baseUrl/auth/reissue"
+}
