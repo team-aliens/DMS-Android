dc1b66b4a24abc7785f9f23ba50219eb3318bc78
junsuPark <krpjs0508@gmail.com>
2023-10-07 10:22:45 +0900

    ️⚡️ :: CannotStoreTokenExpirationException 구현

---
+++ core/jwt/src/main/java/team/aliens/dms_android/core/jwt/exception/CannotStoreTokenExpirationException.kt
@@ -0,0 +1,12 @@
+package team.aliens.dms_android.core.jwt.exception
+
+import team.aliens.dms_android.core.datastore.exception.TransformFailureException
+
+sealed class CannotStoreTokenExpirationException(message: String?) :
+    TransformFailureException(message)
+
+class CannotStoreAccessTokenExpirationException :
+    CannotStoreTokenExpirationException("Cannot store access token expiration")
+
+class CannotStoreRefreshTokenExpirationException :
+    CannotStoreTokenExpirationException("Cannot store refresh token expiration")
