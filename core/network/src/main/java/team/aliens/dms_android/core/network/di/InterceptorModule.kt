973caab772a2a1730d0d382d372367becf9a0289
junsuPark <krpjs0508@gmail.com>
2023-10-07 10:56:43 +0900

    ️⚡️ :: InterceptorModule 구현

---
+++ core/network/src/main/java/team/aliens/dms_android/core/network/di/InterceptorModule.kt
@@ -0,0 +1,38 @@
+package team.aliens.dms_android.core.network.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object InterceptorModule {
+
+    // TODO: remove in future
+    @Provides
+    @Singleton
+    @Debug
+    fun provideDebug(): Boolean = BuildConfig.DEBUG
+
+    @Provides
+    @Singleton
+    @DefaultHttpLoggingLevel
+    fun provideHttpLoggingLevel(
+        // TODO: implement :core:project and inject debug
+        @Debug debug: Boolean,
+    ): HttpLoggingInterceptor.Level = if (debug) {
+        HttpLoggingInterceptor.Level.BODY
+    } else {
+        HttpLoggingInterceptor.Level.BASIC
+    }
+
+    @Provides
+    @Singleton
+    @DefaultHttpLoggingInterceptor
+    fun provideDefaultHttpLoggingInterceptor(
+        @DefaultHttpLoggingLevel level: HttpLoggingInterceptor.Level,
+    ): HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(level)
+}
