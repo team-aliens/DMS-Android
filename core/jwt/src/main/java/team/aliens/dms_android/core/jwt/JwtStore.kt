dc0c0ad5dc24011298287a72f9993ca7c230d7b1
junsuPark <krpjs0508@gmail.com>
2023-10-07 11:30:35 +0900

    ️⚡️ :: JwtStore 구현

--- core/jwt/src/main/java/team/aliens/dms_android/core/jwt/JwtStore.kt
+++ core/jwt/src/main/java/team/aliens/dms_android/core/jwt/JwtStore.kt
@@ -1,3 +1,12 @@
 package team.aliens.dms_android.core.jwt
 
-abstract class JwtStore
+abstract class JwtStore {
+    abstract fun loadAccessToken(): AccessToken
+    abstract suspend fun storeAccessToken(token: AccessToken)
+    abstract fun loadAccessTokenExpiration(): AccessTokenExpiration
+    abstract suspend fun storeAccessTokenExpiration(expiration: AccessTokenExpiration)
+    abstract fun loadRefreshToken(): RefreshToken
+    abstract suspend fun storeRefreshToken(token: RefreshToken)
+    abstract fun loadRefreshTokenExpiration(): RefreshTokenExpiration
+    abstract suspend fun storeRefreshTokenExpiration(expiration: RefreshTokenExpiration)
+}
--- core/jwt/src/main/java/team/aliens/dms_android/core/jwt/JwtStoreImpl.kt
+++ core/jwt/src/main/java/team/aliens/dms_android/core/jwt/JwtStoreImpl.kt
@@ -1,3 +1,105 @@
 package team.aliens.dms_android.core.jwt
 
-internal class JwtStoreImpl : JwtStore()
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import team.aliens.dms_android.core.datastore.PreferencesDataStore
+import team.aliens.dms_android.core.datastore.util.transform
+import team.aliens.dms_android.core.jwt.exception.AccessTokenExpirationNotFoundException
+import team.aliens.dms_android.core.jwt.exception.AccessTokenNotFoundException
+import team.aliens.dms_android.core.jwt.exception.CannotStoreAccessTokenException
+import team.aliens.dms_android.core.jwt.exception.CannotStoreAccessTokenExpirationException
+import team.aliens.dms_android.core.jwt.exception.CannotStoreRefreshTokenException
+import team.aliens.dms_android.core.jwt.exception.CannotStoreRefreshTokenExpirationException
+import team.aliens.dms_android.core.jwt.exception.RefreshTokenExpirationNotFoundException
+import team.aliens.dms_android.core.jwt.exception.RefreshTokenNotFoundException
+import team.aliens.dms_android.core.jwt.util.toEpochMillis
+import team.aliens.dms_android.core.jwt.util.toLocalDateTime
+
+internal class JwtStoreImpl(
+    private val preferencesDataStore: PreferencesDataStore,
+) : JwtStore() {
+    override fun loadAccessToken(): AccessToken = runBlocking {
+        preferencesDataStore.data.map { preferences ->
+            preferences[ACCESS_TOKEN] ?: throw AccessTokenNotFoundException()
+        }.first()
+    }
+
+    override suspend fun storeAccessToken(token: AccessToken) {
+        transform(
+            job = {
+                preferencesDataStore.edit { preferences ->
+                    preferences[ACCESS_TOKEN] = token
+                }
+            },
+            onFailure = { throw CannotStoreAccessTokenException() },
+        )
+    }
+
+    override fun loadAccessTokenExpiration(): AccessTokenExpiration = runBlocking {
+        preferencesDataStore.data.map { preferences ->
+            val longValue = preferences[ACCESS_TOKEN_EXPIRATION]
+                ?: throw AccessTokenExpirationNotFoundException()
+
+            return@map longValue.toLocalDateTime()
+        }.first()
+    }
+
+    override suspend fun storeAccessTokenExpiration(expiration: AccessTokenExpiration) {
+        transform(
+            job = {
+                preferencesDataStore.edit { preferences ->
+                    preferences[ACCESS_TOKEN_EXPIRATION] = expiration.toEpochMillis()
+                }
+            },
+            onFailure = { throw CannotStoreAccessTokenExpirationException() },
+        )
+    }
+
+    override fun loadRefreshToken(): RefreshToken = runBlocking {
+        preferencesDataStore.data.map { preferences ->
+            preferences[REFRESH_TOKEN] ?: throw RefreshTokenNotFoundException()
+        }.first()
+    }
+
+    override suspend fun storeRefreshToken(token: RefreshToken) {
+        transform(
+            job = {
+                preferencesDataStore.edit { preferences ->
+                    preferences[REFRESH_TOKEN] = token
+                }
+            },
+            onFailure = { throw CannotStoreRefreshTokenException() },
+        )
+    }
+
+    override fun loadRefreshTokenExpiration(): RefreshTokenExpiration = runBlocking {
+        preferencesDataStore.data.map { preferences ->
+            val longValue = preferences[REFRESH_TOKEN_EXPIRATION]
+                ?: throw RefreshTokenExpirationNotFoundException()
+
+            return@map longValue.toLocalDateTime()
+        }.first()
+    }
+
+    override suspend fun storeRefreshTokenExpiration(expiration: RefreshTokenExpiration) {
+        transform(
+            job = {
+                preferencesDataStore.edit { preferences ->
+                    preferences[REFRESH_TOKEN_EXPIRATION] = expiration.toEpochMillis()
+                }
+            },
+            onFailure = { throw CannotStoreRefreshTokenExpirationException() },
+        )
+    }
+
+    private companion object {
+        val ACCESS_TOKEN = stringPreferencesKey("access-token")
+        val ACCESS_TOKEN_EXPIRATION = longPreferencesKey("access-token-expiration")
+        val REFRESH_TOKEN = stringPreferencesKey("refresh-token")
+        val REFRESH_TOKEN_EXPIRATION = longPreferencesKey("refresh-token-expiration")
+    }
+}
