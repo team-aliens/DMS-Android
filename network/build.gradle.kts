a8c97031d126008ef81ff64e9bafd11a2d2ec56f
junsuPark <krpjs0508@gmail.com>
2023-10-07 20:17:39 +0900

    ️♻️ :: network gradle 패키지명 변경

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "team.aliens.dms_android.remote"
+    namespace = "team.aliens.dms_android.network"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
