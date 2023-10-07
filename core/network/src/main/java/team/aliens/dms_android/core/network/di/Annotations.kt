89923147de08f1ab1145f915e9a5e1eb24f34369
junsuPark <krpjs0508@gmail.com>
2023-10-07 11:03:18 +0900

    ♻️ :: nw Annotations 제거

--- core/network/src/main/java/team/aliens/dms_android/core/network/di/Annotations.kt
+++ core/network/src/main/java/team/aliens/dms_android/core/network/di/Annotations.kt
@@ -1,11 +1,3 @@
 // TODO: move file to JWT module
 
 package team.aliens.dms_android.core.network.di
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.BINARY)
-annotation class RequiresAccessToken
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.BINARY)
-annotation class RequiresRefreshToken
