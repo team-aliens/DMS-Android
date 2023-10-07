7593d46ed5f53e1b338f92b998afb7c227f0e7e5
junsuPark <krpjs0508@gmail.com>
2023-10-07 10:57:00 +0900

    ️⚡️ :: JwtModule 구현

---
+++ core/jwt/src/main/java/team/aliens/dms_android/core/jwt/di/JwtModule.kt
@@ -0,0 +1,18 @@
+package team.aliens.dms_android.core.jwt.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import team.aliens.dms_android.core.jwt.JwtStore
+import team.aliens.dms_android.core.jwt.JwtStoreImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class JwtModule {
+
+    @Binds
+    @Singleton
+    fun bindJwtStore(impl: JwtStoreImpl): JwtStore = JwtStoreImpl()
+}
