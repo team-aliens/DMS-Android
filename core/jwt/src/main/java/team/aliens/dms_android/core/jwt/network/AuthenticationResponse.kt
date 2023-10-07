955ff85c5ac11f8e1ee6177f605e057472edc43e
junsuPark <krpjs0508@gmail.com>
2023-10-07 10:42:06 +0900

    ️⚡️ :: AuthenticationResponse 구현

---
+++ core/jwt/src/main/java/team/aliens/dms_android/core/jwt/network/AuthenticationResponse.kt
@@ -0,0 +1,19 @@
+package team.aliens.dms_android.core.jwt.network
+
+import com.google.gson.annotations.SerializedName
+
+internal data class AuthenticationResponse(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("access_token_expired_at") val accessTokenExpiration: String,
+    @SerializedName("refresh_token") val refreshToken: String,
+    @SerializedName("refresh_token_expired_at") val refreshTokenExpiration: String,
+    @SerializedName("features") val features: FeaturesResponse,
+) {
+    data class FeaturesResponse(
+        @SerializedName("meal_service") val mealService: Boolean,
+        @SerializedName("notice_service") val noticeService: Boolean,
+        @SerializedName("point_service") val pointService: Boolean,
+        @SerializedName("study_room_service") val studyRoomService: Boolean,
+        @SerializedName("remain_service") val remainsService: Boolean,
+    )
+}
