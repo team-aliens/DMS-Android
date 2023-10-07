c5507f5fc5e96198a6bf6506c990e9549f5b1b5b
junsuPark <krpjs0508@gmail.com>
2023-10-07 09:15:18 +0900

    ️⚡️ :: UndefinedServerSideException 추가

---
+++ core/network/src/main/java/team/aliens/dms_android/core/network/exception/UndefinedServerSideException.kt
@@ -0,0 +1,6 @@
+package team.aliens.dms_android.core.network.exception
+
+class UndefinedServerSideException : NetworkException(
+    code = -500,
+    message = "Undefined server side exception",
+)
