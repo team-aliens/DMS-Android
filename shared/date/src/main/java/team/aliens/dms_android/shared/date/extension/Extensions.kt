168c4511063ad5d953b4e73430a03a0ef73ccfca
junsuPark <krpjs0508@gmail.com>
2023-10-08 16:12:35 +0900

    ⚡️ :: Date Extensions 수정

--- shared/date/src/main/java/team/aliens/dms_android/shared/date/extension/Extensions.kt
+++ shared/date/src/main/java/team/aliens/dms_android/shared/date/extension/Extensions.kt
@@ -5,12 +5,7 @@ import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneOffset
 
-fun LocalDateTime.toEpochMillis(zone: ZoneOffset = ZoneOffset.UTC): Long =
-    this.toInstant(zone).toEpochMilli()
-
 fun Long.toLocalDateTime(zone: ZoneOffset = ZoneOffset.UTC): LocalDateTime =
     LocalDateTime.ofInstant(Instant.ofEpochMilli(this), zone)
-
-fun LocalDate.toEpochDay(zone: ZoneOffset = ZoneOffset.UTC): Long = this.toEpochDay()
 
 fun Long.toLocalDate(): LocalDate = LocalDate.ofEpochDay(this)
