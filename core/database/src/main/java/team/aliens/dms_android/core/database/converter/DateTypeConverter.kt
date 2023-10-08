577c694960a94011f982250fced6ec2182fd7082
junsuPark <krpjs0508@gmail.com>
2023-10-08 16:13:04 +0900

    ⚡️ :: DateTypeConverter 구현

---
+++ core/database/src/main/java/team/aliens/dms_android/core/database/converter/DateTypeConverter.kt
@@ -0,0 +1,28 @@
+package team.aliens.dms_android.core.database.converter
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZoneOffset
+import team.aliens.dms_android.shared.date.extension.toLocalDate
+import team.aliens.dms_android.shared.date.extension.toLocalDateTime
+import javax.inject.Inject
+
+@ProvidedTypeConverter
+internal class DateTypeConverter @Inject constructor(
+    private val zoneOffset: ZoneOffset,
+) {
+
+    @TypeConverter
+    fun longToLocalDate(value: Long): LocalDate = value.toLocalDate()
+
+    @TypeConverter
+    fun localDateToLong(value: LocalDate): Long = value.toEpochDay()
+
+    @TypeConverter
+    fun longToLocalDateTime(value: Long): LocalDateTime = value.toLocalDateTime(zoneOffset)
+
+    @TypeConverter
+    fun localDateTimeToLong(value: LocalDateTime): Long = value.toEpochSecond(zoneOffset)
+}
