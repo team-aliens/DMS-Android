abfbdce91b9f5158f81ac190d02ee22bcf04b7c6
junsuPark <krpjs0508@gmail.com>
2023-10-10 08:56:41 +0900

    ♻️ :: di domain 경로 수정

--- di/src/main/java/team/aliens/di/repository/RepositoryModule.kt
+++ di/src/main/java/team/aliens/di/repository/RepositoryModule.kt
@@ -4,7 +4,7 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import team.aliens.data.repository.*
-import team.aliens.domain.repository.*
+import team.aliens.dms_android.domain.repository.*
 import javax.inject.Singleton
 
 @Module
