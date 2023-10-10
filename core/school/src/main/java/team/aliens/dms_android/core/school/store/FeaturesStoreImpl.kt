b391243a98b5b39bde570a539667bb457b6d8698
junsuPark <krpjs0508@gmail.com>
2023-10-10 22:01:46 +0900

    ⚡️ :: FeaturesStoreImpl 파라미터 추가

--- core/school/src/main/java/team/aliens/dms_android/core/school/store/FeaturesStoreImpl.kt
+++ core/school/src/main/java/team/aliens/dms_android/core/school/store/FeaturesStoreImpl.kt
@@ -1,5 +1,10 @@
 package team.aliens.dms_android.core.school.store
 
+import team.aliens.dms_android.core.school.datastore.FeaturesDataStoreDataSource
+import team.aliens.dms_android.core.school.network.FeaturesManager
 import javax.inject.Inject
 
-internal class FeaturesStoreImpl @Inject constructor() : FeaturesStore()
+internal class FeaturesStoreImpl @Inject constructor(
+    val featuresDataStoreDataSource: FeaturesDataStoreDataSource,
+    val featuresManager: FeaturesManager,
+) : FeaturesStore()
