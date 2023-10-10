daac8284f6430c591a66cea6122daf8683dfb1c1
junsuPark <krpjs0508@gmail.com>
2023-10-10 13:58:22 +0900

    ♻️ :: 주석 제거

--- core/network/src/main/java/team/aliens/dms_android/core/network/exception/NetworkException.kt
+++ core/network/src/main/java/team/aliens/dms_android/core/network/exception/NetworkException.kt
@@ -1,7 +1,6 @@
 package team.aliens.dms_android.core.network.exception
 
 sealed class NetworkException(
-    // TODO: consider default value
     val code: Int = NO_CODE,
     message: String?,
 ) : RuntimeException(message)
