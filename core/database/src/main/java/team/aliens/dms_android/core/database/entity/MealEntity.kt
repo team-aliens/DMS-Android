1a7929d284eda29ad5798954cce141b8319926c3
junsuPark <krpjs0508@gmail.com>
2023-10-10 14:01:16 +0900

    ♻️ :: DB Entity 주석 제거

--- core/database/src/main/java/team/aliens/dms_android/core/database/entity/MealEntity.kt
+++ core/database/src/main/java/team/aliens/dms_android/core/database/entity/MealEntity.kt
@@ -4,7 +4,6 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-// TODO: 접근제한자 internal로 수정 필요
 @Entity(tableName = "tbl_meals")
 data class MealEntity(
     @PrimaryKey @ColumnInfo("date") val date: String,
