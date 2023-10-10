f8a8744826fbf50aa4164dcf7809a6a7f3637918
junsuPark <krpjs0508@gmail.com>
2023-10-10 21:57:53 +0900

    ⚡️ :: School DataSourceModule 구현

--- core/school/src/main/java/team/aliens/dms_android/core/school/di/DataSourceModule.kt
+++ core/school/src/main/java/team/aliens/dms_android/core/school/di/DataSourceModule.kt
@@ -4,14 +4,15 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import team.aliens.dms_android.core.school.store.FeaturesStore
 import team.aliens.dms_android.core.school.store.FeaturesStoreImpl
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal abstract class SchoolModule {
+internal abstract class DataSourceModule {
 
     @Binds
     @Singleton
-    abstract fun bindFeaturesStore(): FeaturesStoreImpl
+    abstract fun bindFeaturesStore(impl: FeaturesStoreImpl): FeaturesStore
 }
