f6872709b2c774d8dc4754a1691739283d886546
junsuPark <krpjs0508@gmail.com>
2023-10-10 21:22:28 +0900

    ♻️ :: JwtDataStoreDataSourceImpl 위치 수정

--- core/jwt/src/main/java/team/aliens/dms_android/core/jwt/datastore/JwtDataStoreDataSource.kt
+++ core/jwt/src/main/java/team/aliens/dms_android/core/jwt/datastore/JwtDataStoreDataSource.kt
@@ -6,7 +6,6 @@ import team.aliens.dms_android.core.jwt.RefreshToken
 import team.aliens.dms_android.core.jwt.RefreshTokenExpiration
 
 internal abstract class JwtDataStoreDataSource {
-
     abstract fun loadAccessToken(): AccessToken
     abstract suspend fun storeAccessToken(token: AccessToken)
     abstract fun loadAccessTokenExpiration(): AccessTokenExpiration
