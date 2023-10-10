91d73e1e2a1923e5a431ec84750b7e992fe6deeb
junsuPark <krpjs0508@gmail.com>
2023-10-10 22:54:02 +0900

    ♻️ :: DmsNavHost 애너테이션 수정

--- app/src/main/java/team/aliens/dms_android/app/DmsNavHost.kt
+++ app/src/main/java/team/aliens/dms_android/app/DmsNavHost.kt
@@ -2,37 +2,14 @@ package team.aliens.dms_android.app
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.DestinationsNavHost
 
-@RootNavGraph(start = true)
-@Destination
 @Composable
 fun DmsNavHost(
     modifier: Modifier = Modifier,
-) {/*
-    NavHost(
-        modifier = modifier,
-        navController = navController,
-        startDestination = DmsDestinations.MAIN.route,
-    ) {
-
-    }*//*
+) {
     DestinationsNavHost(
         modifier = modifier,
-        navGraph = NavGraphs
-    )*/
-    // DirectionDe
-}
-
-enum class DmsDestinations(
-    val route: String,
-) {
-    MAIN(route = "main"),
-    SIGN_IN(route = "sign-in"),
-    SIGN_UP(route = "sign-up"),
-    FIND_ID(route = "find-id"),
-    ;
+        navGraph = NavGraphs.root,
+    )
 }
