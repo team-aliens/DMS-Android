038f64670cdf3be7642134bdabd7228080acd86a
junsuPark <krpjs0508@gmail.com>
2023-10-10 09:23:41 +0900

    ♻️ :: design-system 패키지명 변경

---
+++ design-system/src/main/java/team/aliens/dms_android/design_system/color/colors.kt
@@ -0,0 +1,68 @@
+@file:Suppress("unused")
+
+package team.aliens.dms_android.design_system.color
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+import team.aliens.dms_android.design_system.annotation.DormDeprecated
+
+@DormDeprecated
+object DormColor {
+
+    @Stable
+    internal val Unspecified = Color.Unspecified
+
+    @Stable
+    val DormPrimary = Color(color = 0xFF3D8AFF)
+
+    @Stable
+    val Lighten200 = Color(color = 0xFFf5f9ff)
+
+    @Stable
+    val Lighten100 = Color(color = 0xFFb1d0ff)
+
+    @Stable
+    val Darken100 = Color(color = 0xFF1070ff)
+
+    @Stable
+    val Darken200 = Color(color = 0xFF005de8)
+
+    @Stable
+    val Gray100 = Color(color = 0xFFFFFFFF)
+
+    @Stable
+    val Gray200 = Color(color = 0xFFf9f9f9)
+
+    @Stable
+    val Gray300 = Color(color = 0xFFeeeeee)
+
+    @Stable
+    val Gray400 = Color(color = 0xFFdddddd)
+
+    @Stable
+    val Gray500 = Color(color = 0xFF999999)
+
+    @Stable
+    val Gray600 = Color(color = 0xFF555555)
+
+    @Stable
+    val Gray700 = Color(color = 0xFF343434)
+
+    @Stable
+    val Gray800 = Color(color = 0xFF202020)
+
+    @Stable
+    val Gray900 = Color(color = 0xFF121212)
+
+    @Stable
+    val Gray1000 = Color(color = 0xFF000000)
+
+    @Stable
+    val ViewHide = Color(color = 0xFFD6D6D6)
+
+    @Stable
+    val Error = Color(color = 0xFFFF4646)
+
+    @Stable
+    val LightenError = Color(color = 0x14FF4646)
+}
