d09f2a33efea7afec1cd686485f7c59a13663391
junsuPark <krpjs0508@gmail.com>
2023-10-10 13:19:05 +0900

    ⚡️️ :: BuildType DI 제공

--- core/project/src/main/java/team/aliens/dms_android/core/project/di/ProjectConfigModule.kt
+++ core/project/src/main/java/team/aliens/dms_android/core/project/di/ProjectConfigModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import team.aliens.dms_android.core.project.BuildConfig
+import team.aliens.dms_android.core.project.BuildType
 import javax.inject.Singleton
 
 @Module
@@ -15,4 +16,8 @@ internal object ProjectConfigModule {
     @Singleton
     @Debug
     fun provideIsDebugEnabled(): Boolean = BuildConfig.DEBUG
+
+    @Provides
+    @Singleton
+    fun provideBuildType(): BuildType = BuildType.valueOf(BuildConfig.BUILD_TYPE)
 }
