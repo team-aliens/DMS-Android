4b4574c2b8d51e8d4199fca3eacd54adac298238
junsuPark <krpjs0508@gmail.com>
2023-10-10 21:27:23 +0900

    ♻️ :: JwtProvider 파일 추가

---
+++ core/jwt/src/main/java/team/aliens/dms_android/core/jwt/JwtProvider.kt
@@ -0,0 +1,13 @@
+package team.aliens.dms_android.core.jwt
+
+import team.aliens.dms_android.core.jwt.store.JwtStore
+import javax.inject.Inject
+
+object JwtProvider {/*
+
+    val cachedAccessToken: AccessToken
+    val cachedRefreshToken: RefreshToken
+
+    @Inject
+    private lateinit var jwtStore: JwtStore*/
+}
