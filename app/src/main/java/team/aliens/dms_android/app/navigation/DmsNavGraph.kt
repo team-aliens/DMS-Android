24d5270065e1c0871d4e304073b40c1b02215260
junsuPark <krpjs0508@gmail.com>
2023-10-11 12:40:59 +0900

    ⚡️ :: DmsNavGraph 구현

--- app/src/main/java/team/aliens/dms_android/app/navigation/DmsNavGraph.kt
+++ app/src/main/java/team/aliens/dms_android/app/navigation/DmsNavGraph.kt
@@ -1,42 +1,26 @@
 package team.aliens.dms_android.app.navigation
 
-import com.ramcosta.composedestinations.dynamic.routedIn
 import com.ramcosta.composedestinations.spec.DestinationSpec
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import com.ramcosta.composedestinations.spec.Route
+import team.aliens.dms_android.app.navigation.authorized.AuthorizedNavGraph
+import team.aliens.dms_android.app.navigation.unauthorized.UnauthorizedNavGraph
 
 object DmsNavGraph {
+    fun root(autoSignIn: Boolean): NavGraphSpec =
+        object : NavGraphSpec {
+            override val route: String = "root"
+            override val startRoute: Route = if (autoSignIn) {
+                AuthorizedNavGraph
+            } else {
+                UnauthorizedNavGraph
+            }
 
-    fun root(autoSignIn: Boolean) = object : NavGraphSpec {
-        override val route: String = "root"
-        override val startRoute: Route = if (autoSignIn) {
-            authorized
-        } else {
-            unauthorized
+            override val destinationsByRoute: Map<String, DestinationSpec<*>> = emptyMap()
+            override val nestedNavGraphs: List<NavGraphSpec> =
+                listOf(
+                    AuthorizedNavGraph,
+                    UnauthorizedNavGraph,
+                )
         }
-        override val destinationsByRoute: Map<String, DestinationSpec<*>> = emptyMap()
-        override val nestedNavGraphs: List<NavGraphSpec> =
-            listOf(
-                authorized,
-                unauthorized,
-            )
-    }
-
-    val authorized = object : NavGraphSpec {
-        override val route: String = "authorized"
-        override val startRoute: Route = TODO()
-        override val destinationsByRoute: Map<String, DestinationSpec<*>> =
-            listOf<DestinationSpec<*>>(/* TODO */)
-                .routedIn(navGraphSpec = this)
-                .associateBy { it.route }
-    }
-
-    val unauthorized = object : NavGraphSpec {
-        override val route: String = "unauthorized"
-        override val startRoute: Route = TODO()
-        override val destinationsByRoute: Map<String, DestinationSpec<*>> =
-            listOf<DestinationSpec<*>>(/* TODO */)
-                .routedIn(navGraphSpec = this)
-                .associateBy { it.route }
-    }
 }
