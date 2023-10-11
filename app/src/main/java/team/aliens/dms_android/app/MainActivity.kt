0ada692e51f97d14aa7a615bc370e6460a076dd4
junsuPark <krpjs0508@gmail.com>
2023-10-12 07:31:26 +0900

    ♻️ :: MainActivity ViewModel 선언

--- app/src/main/java/team/aliens/dms_android/app/MainActivity.kt
+++ app/src/main/java/team/aliens/dms_android/app/MainActivity.kt
@@ -3,6 +3,7 @@ package team.aliens.dms_android.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +14,8 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
