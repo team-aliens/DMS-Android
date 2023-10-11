f2dd33406de1c0f61df8dd69e8a25e34f80d3195
junsuPark <krpjs0508@gmail.com>
2023-10-11 12:51:05 +0900

    ⚡️ :: AuthorizedNavGraph에 MainNavGraph 추가

--- app/src/main/java/team/aliens/dms_android/app/navigation/authorized/AuthorizedNavGraph.kt
+++ app/src/main/java/team/aliens/dms_android/app/navigation/authorized/AuthorizedNavGraph.kt
@@ -4,10 +4,16 @@ import com.ramcosta.composedestinations.dynamic.routedIn
 import com.ramcosta.composedestinations.spec.DestinationSpec
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import com.ramcosta.composedestinations.spec.Route
+import team.aliens.dms_android.app.navigation.authorized.main.MainNavGraph
 
 object AuthorizedNavGraph : NavGraphSpec {
     override val route: String = "authorized"
-    override val startRoute: Route = TODO()
+    override val startRoute: Route = MainNavGraph
+    override val nestedNavGraphs: List<NavGraphSpec> =
+        listOf(
+            MainNavGraph,
+        )
+
     override val destinationsByRoute: Map<String, DestinationSpec<*>> =
         listOf<DestinationSpec<*>>(/* TODO */)
             .routedIn(navGraphSpec = this)
