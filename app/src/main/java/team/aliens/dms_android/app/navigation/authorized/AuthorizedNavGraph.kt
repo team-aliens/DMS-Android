3a3e5b078aa0e06843426103e55593e5a1c11530
junsuPark <krpjs0508@gmail.com>
2023-10-11 12:32:23 +0900

    ⚡️ :: AuthorizedNavGraph 구현

--- app/src/main/java/team/aliens/dms_android/app/navigation/authorized/AuthorizedNavGraph.kt
+++ app/src/main/java/team/aliens/dms_android/app/navigation/authorized/AuthorizedNavGraph.kt
@@ -1,13 +1,15 @@
 package team.aliens.dms_android.app.navigation.authorized
 
-import com.ramcosta.composedestinations.annotation.NavGraph
-import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.dynamic.routedIn
+import com.ramcosta.composedestinations.spec.DestinationSpec
+import com.ramcosta.composedestinations.spec.NavGraphSpec
+import com.ramcosta.composedestinations.spec.Route
 
-// TODO: consider making abstraction
-object AuthorizedDestinations {
-    const val route = "authorized"
+object AuthorizedNavGraph : NavGraphSpec {
+    override val route: String = "authorized"
+    override val startRoute: Route = TODO()
+    override val destinationsByRoute: Map<String, DestinationSpec<*>> =
+        listOf<DestinationSpec<*>>(/* TODO */)
+            .routedIn(navGraphSpec = this)
+            .associateBy { it.route }
 }
-
-@RootNavGraph(start = true)
-@NavGraph(route = AuthorizedDestinations.route)
-annotation class AuthorizedNavGraph
