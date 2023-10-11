70580d32e34b629a0bef191fd8589078346f536a
junsuPark <krpjs0508@gmail.com>
2023-10-11 13:27:54 +0900

    ♻️ :: Entity Extension 구현

--- database/src/main/java/team/aliens/dms_android/database/room/entity/NoticeEntity.kt
+++ database/src/main/java/team/aliens/dms_android/database/room/entity/NoticeEntity.kt
@@ -1,34 +1,7 @@
 package team.aliens.dms_android.database.room.entity
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import team.aliens.dms_android.core.database.entity.NoticeEntity
 import team.aliens.dms_android.domain.model.notice.Notice
-import team.aliens.dms_android.database.room.common.RoomProperty.ColumnName
-import team.aliens.dms_android.database.room.common.RoomProperty.TableName
-import java.util.*
-
-@Entity(
-    tableName = TableName.Notice,
-)
-data class NoticeEntity(
-
-    @PrimaryKey @ColumnInfo(
-        name = ColumnName.Notice.Id,
-    ) val id: UUID,
-
-    @ColumnInfo(
-        name = ColumnName.Notice.Title,
-    ) val title: String,
-
-    @ColumnInfo(
-        name = ColumnName.Notice.Content,
-    ) val content: String?,
-
-    @ColumnInfo(
-        name = ColumnName.Notice.CreatedAt,
-    ) val createdAt: String,
-)
 
 internal fun NoticeEntity.toDomain(): Notice {
     return Notice(
