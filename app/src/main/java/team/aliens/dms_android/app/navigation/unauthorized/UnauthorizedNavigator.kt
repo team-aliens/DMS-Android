50a203e56db5007e0446f84032017433c7b16dc0
junsuPark <krpjs0508@gmail.com>
2023-10-12 20:00:16 +0900

    ♻️ :: SignInNavigator in UnauthorizedNavigator

--- app/src/main/java/team/aliens/dms_android/app/navigation/unauthorized/UnauthorizedNavigator.kt
+++ app/src/main/java/team/aliens/dms_android/app/navigation/unauthorized/UnauthorizedNavigator.kt
@@ -2,7 +2,9 @@ package team.aliens.dms_android.app.navigation.unauthorized
 
 import team.aliens.dms_android.feature.findid.navigator.FindIdNavigator
 import team.aliens.dms_android.feature.resetpassword.navigation.ResetPasswordNavigator
+import team.aliens.dms_android.feature.signin.navigation.SignInNavigator
 
 interface UnauthorizedNavigator :
     FindIdNavigator,
-    ResetPasswordNavigator
+    ResetPasswordNavigator,
+    SignInNavigator
