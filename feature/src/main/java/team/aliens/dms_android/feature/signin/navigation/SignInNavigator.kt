a25d917eaa1f31a4f7a6301cc04a26eb439951bb
junsuPark <krpjs0508@gmail.com>
2023-10-12 19:59:48 +0900

    ♻️ :: SignInNavigator

---
+++ feature/src/main/java/team/aliens/dms_android/feature/signin/navigation/SignInNavigator.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms_android.feature.signin.navigation
+
+interface SignInNavigator {
+    fun openMain()
+    fun openSignUp()
+    fun openFindId()
+    fun openResetPassword()
+}
