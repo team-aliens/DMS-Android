6eb108873598f34b636da0212a9f20e2a8e70bf5
junsuPark <krpjs0508@gmail.com>
2023-10-12 20:14:28 +0900

    ♻️ :: SignUp

---
+++ feature/src/main/java/team/aliens/dms_android/feature/signup/SignUp.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms_android.feature.signup
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun SignUp() {
+
+}
