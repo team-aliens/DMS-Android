18ed5db8b2b3b87602d50aa280ddbdcaee769b68
junsuPark <krpjs0508@gmail.com>
2023-10-12 20:07:13 +0900

    ♻️ :: EnterSchoolVerificationQuestionScreen

--- feature/src/main/java/team/aliens/dms_android/feature/signup/navigation/SignUpNavigator.kt
+++ feature/src/main/java/team/aliens/dms_android/feature/signup/navigation/SignUpNavigator.kt
@@ -2,5 +2,6 @@ package team.aliens.dms_android.feature.signup.navigation
 
 interface SignUpNavigator{
     fun openEnterSchoolVerificationQuestion()
-
+    fun openEnterEmail()
+    fun openSignIn()
 }
