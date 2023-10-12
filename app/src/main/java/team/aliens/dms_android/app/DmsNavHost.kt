f84543f845d1c2486796b34161f15af09005ea1a
junsuPark <krpjs0508@gmail.com>
2023-10-13 08:26:55 +0900

    ♻️ :: app navhost

--- app/src/main/java/team/aliens/dms_android/app/DmsNavHost.kt
+++ app/src/main/java/team/aliens/dms_android/app/DmsNavHost.kt
@@ -6,7 +6,10 @@ import androidx.compose.ui.Modifier
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
+import com.ramcosta.composedestinations.navigation.dependency
+import com.ramcosta.composedestinations.utils.navGraph
 import team.aliens.dms_android.app.navigation.DmsNavGraph
+import team.aliens.dms_android.app.navigation.DmsNavigator
 
 @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class)
 @Composable
@@ -15,10 +18,19 @@ fun DmsNavHost(
     autoSignIn: Boolean,
 ) {
     val engine = rememberAnimatedNavHostEngine()
+    val navController = engine.rememberNavController()
 
     DestinationsNavHost(
         modifier = modifier,
         engine = engine,
         navGraph = DmsNavGraph.root(autoSignIn = autoSignIn),
+        dependenciesContainerBuilder = {
+            dependency(
+                DmsNavigator(
+                    navGraph = navBackStackEntry.navGraph(),
+                    navController = navController,
+                )
+            )
+        },
     )
 }
