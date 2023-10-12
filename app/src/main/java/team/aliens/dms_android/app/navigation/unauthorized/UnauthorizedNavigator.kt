df72b8d9137afe167c440930c4b0c2f24c102f08
junsuPark <krpjs0508@gmail.com>
2023-10-12 19:52:32 +0900

    ♻️ :: ResetPasswordNavigator

--- app/src/main/java/team/aliens/dms_android/app/navigation/unauthorized/UnauthorizedNavigator.kt
+++ app/src/main/java/team/aliens/dms_android/app/navigation/unauthorized/UnauthorizedNavigator.kt
@@ -1,6 +1,8 @@
 package team.aliens.dms_android.app.navigation.unauthorized
 
 import team.aliens.dms_android.feature.findid.navigator.FindIdNavigator
+import team.aliens.dms_android.feature.resetpassword.navigation.ResetPasswordNavigator
 
 interface UnauthorizedNavigator :
-        FindIdNavigator
+    FindIdNavigator,
+    ResetPasswordNavigator
---
+++ feature/src/main/java/team/aliens/dms_android/feature/resetpassword/navigation/ResetPasswordNavigator.kt
@@ -0,0 +1,6 @@
+package team.aliens.dms_android.feature.resetpassword.navigation
+
+interface ResetPasswordNavigator {
+
+
+}
