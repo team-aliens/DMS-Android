45d924409ecb0d77643973783e8fb2a32ee3111e
junsuPark <krpjs0508@gmail.com>
2023-10-12 14:53:35 +0900

    ⚡️ :: EditPasswordNavGraph 추가

---
+++ feature/src/main/java/team/aliens/dms_android/feature/editpassword/EditPasswordNavGraph.kt
@@ -0,0 +1,20 @@
+package team.aliens.dms_android.feature.editpassword
+
+import com.ramcosta.composedestinations.dynamic.routedIn
+import com.ramcosta.composedestinations.spec.DestinationSpec
+import com.ramcosta.composedestinations.spec.NavGraphSpec
+import com.ramcosta.composedestinations.spec.Route
+import team.aliens.dms_android.feature.destinations.ConfirmPasswordScreenDestination
+import team.aliens.dms_android.feature.destinations.EditPasswordSetPasswordScreenDestination
+
+object EditPasswordNavGraph : NavGraphSpec {
+    override val route: String = "edit_password"
+    override val startRoute: Route = ConfirmPasswordScreenDestination routedIn this
+    override val destinationsByRoute: Map<String, DestinationSpec<*>> =
+        listOf(
+            ConfirmPasswordScreenDestination,
+            EditPasswordSetPasswordScreenDestination,
+        )
+            .routedIn(this)
+            .associateBy { it.route }
+}
