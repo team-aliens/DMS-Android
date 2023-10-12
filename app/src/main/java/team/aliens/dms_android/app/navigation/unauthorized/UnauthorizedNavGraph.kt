1345a40f633c7d2b8651535cea7e87298b72a914
junsuPark <krpjs0508@gmail.com>
2023-10-12 13:56:15 +0900

    ♻️ :: UnauthorizedNavGraph sign up graph 추가

--- app/src/main/java/team/aliens/dms_android/app/navigation/unauthorized/UnauthorizedNavGraph.kt
+++ app/src/main/java/team/aliens/dms_android/app/navigation/unauthorized/UnauthorizedNavGraph.kt
@@ -5,14 +5,19 @@ import com.ramcosta.composedestinations.spec.DestinationSpec
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import com.ramcosta.composedestinations.spec.Route
 import team.aliens.dms_android.feature.destinations.SignInScreenDestination
+import team.aliens.dms_android.feature.signup.SignUpNavGraph
 
 object UnauthorizedNavGraph : NavGraphSpec {
     override val route: String = "unauthorized"
     override val startRoute: Route = SignInScreenDestination routedIn this
+    override val nestedNavGraphs: List<NavGraphSpec> =
+        listOf(
+            SignUpNavGraph,
+        )
+
     override val destinationsByRoute: Map<String, DestinationSpec<*>> =
         listOf<DestinationSpec<*>>(
             SignInScreenDestination,
-            SignUpNavGraph,
         )
             .routedIn(navGraphSpec = this)
             .associateBy { it.route }
