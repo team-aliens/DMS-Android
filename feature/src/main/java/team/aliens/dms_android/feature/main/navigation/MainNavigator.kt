3a011432acc12bb30c2bb5ae6900cf129b5abd6b
junsuPark <krpjs0508@gmail.com>
2023-10-12 19:30:24 +0900

    ♻️ :: MainNavigator

--- feature/src/main/java/team/aliens/dms_android/feature/main/navigation/MainNavigator.kt
+++ feature/src/main/java/team/aliens/dms_android/feature/main/navigation/MainNavigator.kt
@@ -1,10 +1,13 @@
 package team.aliens.dms_android.feature.main.navigation
 
+import java.util.UUID
+
 interface MainNavigator {
     fun openHome()
     fun openApplication()
     fun openAnnouncementList()
     fun openMyPage()
+    fun openNavigateToNoticeDetails(noticeId: UUID)
 
     fun switchNavGraphRoot()
 }
