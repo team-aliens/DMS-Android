1b8ed0d57c6763f1c4c3df1e62748e9f7f700a53
junsuPark <krpjs0508@gmail.com>
2023-10-12 19:55:39 +0900

    ♻️ :: ResetPasswordEnterEmailVerificationCodeScreen

--- feature/src/main/java/team/aliens/dms_android/feature/resetpassword/navigation/ResetPasswordNavigator.kt
+++ feature/src/main/java/team/aliens/dms_android/feature/resetpassword/navigation/ResetPasswordNavigator.kt
@@ -2,4 +2,5 @@ package team.aliens.dms_android.feature.resetpassword.navigation
 
 interface ResetPasswordNavigator {
     fun openResetPasswordEnterEmailVerificationCode()
+    fun openResetPasswordSetPassword()
 }
