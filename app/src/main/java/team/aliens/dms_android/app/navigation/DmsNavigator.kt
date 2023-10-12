0721651365c273ac1c538b5acd43df2d2afc8b77
junsuPark <krpjs0508@gmail.com>
2023-10-12 19:05:59 +0900

    ♻️ :: DmsNavigator 추가

---
+++ app/src/main/java/team/aliens/dms_android/app/navigation/DmsNavigator.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms_android.app.navigation
+
+import team.aliens.dms_android.app.navigation.authorized.AuthorizedNavigator
+import team.aliens.dms_android.app.navigation.unauthorized.UnauthorizedNavigator
+
+class DmsNavigator : AuthorizedNavigator, UnauthorizedNavigator {
+
+}
