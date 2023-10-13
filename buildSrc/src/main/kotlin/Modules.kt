eff8ff8d7bc3d73d52680e538ed0ebc05da4e275
junsuPark <krpjs0508@gmail.com>
2023-10-13 22:13:52 +0900

    ♻️ :: Modules 구현

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -2,6 +2,13 @@ sealed class Modules(
     val path: String,
 ) {
     object App : Modules(path = ":app")
+    object Data : Modules(path = ":data")
+    object Database : Modules(path = ":database")
+    object DesignSystem : Modules(path = ":design-system")
+    object Domain : Modules(path = ":domain")
+    object Feature : Modules(path = ":feature")
+    object Network : Modules(path = ":network")
+
     object Core {
         object Database : Modules(path = ":core:database")
         object DataStore : Modules(path = ":core:datastore")
@@ -9,5 +16,9 @@ sealed class Modules(
         object Network : Modules(path = ":core:network")
         object Project : Modules(path = ":core:project")
         object School : Modules(path = ":core:school")
+    }
+
+    object Shared {
+        object Date : Modules(path = ":shared:date")
     }
 }
