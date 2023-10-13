c61df5011f921d651ef38c69021dc0f79b316a5a
junsuPark <krpjs0508@gmail.com>
2023-10-13 14:04:52 +0900

    ♻️ :: core project android test

--- core/project/src/androidTest/java/team/aliens/dms/android/core/project/ExampleInstrumentedTest.kt
+++ core/project/src/androidTest/java/team/aliens/dms/android/core/project/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package team.aliens.dms_android.core.project
+package team.aliens.dms.android.core.project
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
