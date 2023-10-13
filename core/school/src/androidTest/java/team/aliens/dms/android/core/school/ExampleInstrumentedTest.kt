73a981043149546f5201268eab4e9fd99a704c30
junsuPark <krpjs0508@gmail.com>
2023-10-13 14:05:27 +0900

    ♻️ :: core school test

--- core/school/src/androidTest/java/team/aliens/dms/android/core/school/ExampleInstrumentedTest.kt
+++ core/school/src/androidTest/java/team/aliens/dms/android/core/school/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package team.aliens.dms_android.core.school
+package team.aliens.dms.android.core.school
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
