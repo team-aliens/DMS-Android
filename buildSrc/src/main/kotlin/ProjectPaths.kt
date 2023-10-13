d6aef6e4dfae865f617f76feadff956b2120abce
junsuPark <krpjs0508@gmail.com>
2023-10-13 22:22:01 +0900

    ♻️ :: ProjectPaths 네이밍 변경

---
+++ buildSrc/src/main/kotlin/ProjectPaths.kt
@@ -0,0 +1,22 @@
+object ProjectPaths {
+    const val app = ":app"
+    const val data = ":data"
+    const val database = ":database"
+    const val designSystem = ":design-system"
+    const val domain = ":domain"
+    const val feature = ":feature"
+    const val network = ":network"
+
+    object Core {
+        const val database = ":core:database"
+        const val dataStore = ":core:datastore"
+        const val jwt = ":core:jwt"
+        const val network = ":core:network"
+        const val project = ":core:project"
+        const val school = ":core:school"
+    }
+
+    object Shared {
+        const val shared = ":shared:date"
+    }
+}
