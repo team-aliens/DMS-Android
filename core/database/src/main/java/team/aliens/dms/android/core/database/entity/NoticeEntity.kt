fce650c8aa5255d2b07b90bd4ec7600897c91d5f
junsuPark <krpjs0508@gmail.com>
2023-10-14 11:21:23 +0900

    ⚡️ :: NoticeEntity local date 적용

--- core/database/src/main/java/team/aliens/dms/android/core/database/entity/NoticeEntity.kt
+++ core/database/src/main/java/team/aliens/dms/android/core/database/entity/NoticeEntity.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.android.core.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.threeten.bp.LocalDate
 import java.util.UUID
 
 @Entity(tableName = "tbl_notices")
@@ -10,5 +11,5 @@ data class NoticeEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: UUID,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "content") val content: String?,
-    @ColumnInfo(name = "created_at") val createdAt: String,
+    @ColumnInfo(name = "created_at") val createdAt: LocalDate,
 )
