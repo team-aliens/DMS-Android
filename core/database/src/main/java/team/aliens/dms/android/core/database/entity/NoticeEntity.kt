edec4739d16b3639ca1d6e9bc1d7dbd2ece5436f
junsuPark <krpjs0508@gmail.com>
2023-10-14 12:00:26 +0900

    ⚡️ :: NoticeEntity 멤버 수정

--- core/database/src/main/java/team/aliens/dms/android/core/database/entity/NoticeEntity.kt
+++ core/database/src/main/java/team/aliens/dms/android/core/database/entity/NoticeEntity.kt
@@ -3,7 +3,7 @@ package team.aliens.dms.android.core.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
 import java.util.UUID
 
 @Entity(tableName = "tbl_notices")
@@ -11,5 +11,5 @@ data class NoticeEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: UUID,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "content") val content: String?,
-    @ColumnInfo(name = "created_at") val createdAt: LocalDate,
+    @ColumnInfo(name = "created_at") val createdAt: LocalDateTime,
 )
