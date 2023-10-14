fbe9fbdd99d0483844fefdc19c7129cb45d96bce
junsuPark <krpjs0508@gmail.com>
2023-10-14 12:00:58 +0900

    ⚡️ :: mapper 구현

---
+++ database/src/main/java/team/aliens/dms/android/database/model/mapper/MealMapper.kt
@@ -0,0 +1,21 @@
+package team.aliens.dms.android.database.model.mapper
+
+import org.threeten.bp.LocalDate
+import team.aliens.dms.android.core.database.entity.MealEntity
+import team.aliens.dms.android.domain.model.meal.Meal
+
+internal fun MealEntity.toDomain() = Meal(
+    date = this.date.toString(), // TODO: change type
+    breakfast = this.breakfast,
+    lunch = this.lunch,
+    dinner = this.dinner,
+)
+
+internal fun Meal.toData() = MealEntity(
+    date = LocalDate.now(), // TODO: this.date
+    breakfast = this.breakfast,
+    lunch = this.lunch,
+    dinner = this.dinner,
+)
+
+internal fun List<Meal>.toData() = this.map(Meal::toData)
