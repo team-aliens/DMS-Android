032eed6d32d680b1945ff9b130ec3e9881d73bb1
junsuPark <krpjs0508@gmail.com>
2023-10-14 11:24:41 +0900

    ⚡️ :: DatabaseMealDataSourceImpl 구현

---
+++ database/src/main/java/team/aliens/dms/android/database/datasource/DatabaseMealDataSourceImpl.kt
@@ -0,0 +1,22 @@
+package team.aliens.dms.android.database.datasource
+
+import org.threeten.bp.LocalDate
+import team.aliens.dms.android.core.database.dao.MealDao
+import team.aliens.dms.android.core.database.entity.MealEntity
+import javax.inject.Inject
+
+internal class DatabaseMealDataSourceImpl @Inject constructor(
+    private val mealDao: MealDao,
+) : DatabaseMealDataSource() {
+    override suspend fun queryMeal(date: LocalDate): MealEntity {
+        return mealDao.findByDate(date)
+    }
+
+    override suspend fun saveMeal(meal: MealEntity) {
+        mealDao.save(meal)
+    }
+
+    override suspend fun saveMeals(meals: List<MealEntity>) {
+        mealDao.saveAll(meals)
+    }
+}
