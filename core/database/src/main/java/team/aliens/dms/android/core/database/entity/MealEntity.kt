ad31d2ebd63ab27f31065aaeb76c6c5b81af8cc3
junsuPark <krpjs0508@gmail.com>
2023-10-14 11:21:41 +0900

    ⚡️ :: MealEntity local date 적용

--- core/database/src/main/java/team/aliens/dms/android/core/database/entity/MealEntity.kt
+++ core/database/src/main/java/team/aliens/dms/android/core/database/entity/MealEntity.kt
@@ -3,10 +3,11 @@ package team.aliens.dms.android.core.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.threeten.bp.LocalDate
 
 @Entity(tableName = "tbl_meals")
 data class MealEntity(
-    @PrimaryKey @ColumnInfo("date") val date: String,
+    @PrimaryKey @ColumnInfo("date") val date: LocalDate,
     @ColumnInfo(name = "breakfast") val breakfast: List<String>,
     @ColumnInfo(name = "lunch") val lunch: List<String>,
     @ColumnInfo(name = "dinner") val dinner: List<String>,
