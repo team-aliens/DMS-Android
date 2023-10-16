8539677362293351b95aaf1733b42fe9730baaf6
junsuPark <krpjs0508@gmail.com>
2023-10-16 19:26:54 +0900

    ⚡️ :: NoticeApiService 구현

--- network/src/main/java/team/aliens/dms/android/network/notice/apiservice/NoticeApiService.kt
+++ network/src/main/java/team/aliens/dms/android/network/notice/apiservice/NoticeApiService.kt
@@ -4,6 +4,8 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 import team.aliens.dms.android.core.jwt.RequiresAccessToken
+import team.aliens.dms.android.network.notice.model.FetchNoticeDetailsResponse
+import team.aliens.dms.android.network.notice.model.FetchNoticesResponse
 import team.aliens.dms.android.network.notice.model.FetchWhetherNewNoticesExistResponse
 import team.aliens.dms.android.network.notice.model.Order
 import java.util.UUID
