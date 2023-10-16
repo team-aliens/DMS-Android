b27826d7db3fd41afcf67ccc7b5b8f50d8aa967a
junsuPark <krpjs0508@gmail.com>
2023-10-16 23:09:40 +0900

    ⚡️ :: Study Room datasource impl 구현

---
+++ network/src/main/java/team/aliens/dms/android/network/studyroom/datasource/NetworkStudyRoomDataSourceImpl.kt
@@ -0,0 +1,48 @@
+package team.aliens.dms.android.network.studyroom.datasource
+
+import team.aliens.dms.android.core.network.util.sendHttpRequest
+import team.aliens.dms.android.network.studyroom.apiservice.StudyRoomApiService
+import team.aliens.dms.android.network.studyroom.model.FetchAppliedStudyRoomResponse
+import team.aliens.dms.android.network.studyroom.model.FetchAvailableStudyRoomTimesResponse
+import team.aliens.dms.android.network.studyroom.model.FetchSeatTypesResponse
+import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomApplicationTimeResponse
+import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomDetailsResponse
+import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomsResponse
+import java.util.UUID
+import javax.inject.Inject
+
+internal class NetworkStudyRoomDataSourceImpl @Inject constructor(
+    private val studyRoomApiService: StudyRoomApiService,
+) : NetworkStudyRoomDataSource() {
+
+    override suspend fun fetchStudyRoomApplicationTime(): FetchStudyRoomApplicationTimeResponse =
+        sendHttpRequest { studyRoomApiService.fetchStudyRoomApplicationTime() }
+
+    override suspend fun applySeat(
+        seatId: UUID,
+        timeslot: UUID,
+    ) = sendHttpRequest { studyRoomApiService.applySeat(seatId, timeslot) }
+
+    override suspend fun cancelSeat(
+        seatId: UUID,
+        timeslot: UUID,
+    ) = sendHttpRequest { studyRoomApiService.cancelSeat(seatId, timeslot) }
+
+    override suspend fun fetchStudyRooms(timeslot: UUID): FetchStudyRoomsResponse =
+        sendHttpRequest { studyRoomApiService.fetchStudyRooms(timeslot) }
+
+    override suspend fun fetchStudyRoomDetails(
+        studyRoomId: UUID,
+        timeslot: UUID,
+    ): FetchStudyRoomDetailsResponse =
+        sendHttpRequest { studyRoomApiService.fetchStudyRoomDetails(studyRoomId, timeslot) }
+
+    override suspend fun fetchAppliedStudyRoom(): FetchAppliedStudyRoomResponse =
+        sendHttpRequest { studyRoomApiService.fetchAppliedStudyRoom() }
+
+    override suspend fun fetchSeatTypes(studyRoomId: UUID): FetchSeatTypesResponse =
+        sendHttpRequest { studyRoomApiService.fetchSeatTypes(studyRoomId) }
+
+    override suspend fun fetchAvailableStudyRoomTimes(): FetchAvailableStudyRoomTimesResponse =
+        sendHttpRequest { studyRoomApiService.fetchAvailableStudyRoomTimes() }
+}
