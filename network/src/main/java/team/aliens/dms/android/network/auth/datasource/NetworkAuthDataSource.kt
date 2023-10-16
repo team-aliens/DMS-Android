d24680b405f388b344b4ba2c1d7d6c97638e1f55
junsuPark <krpjs0508@gmail.com>
2023-10-16 18:19:44 +0900

    ♻️:: NetworkAuthDataSource 추가

---
+++ network/src/main/java/team/aliens/dms/android/network/auth/datasource/NetworkAuthDataSource.kt
@@ -0,0 +1,22 @@
+package team.aliens.dms.android.network.auth.datasource
+
+import team.aliens.dms.android.core.jwt.network.model.AuthenticationResponse
+import team.aliens.dms.android.network.auth.model.CheckIdExistsResponse
+import team.aliens.dms.android.network.auth.model.EmailVerificationType
+import team.aliens.dms.android.network.auth.model.SendEmailVerificationCodeRequest
+import team.aliens.dms.android.network.auth.model.SignInRequest
+
+abstract class NetworkAuthDataSource {
+
+    abstract suspend fun signIn(request: SignInRequest): AuthenticationResponse
+
+    abstract suspend fun sendEmailVerificationCode(request: SendEmailVerificationCodeRequest)
+
+    abstract suspend fun checkEmailVerificationCode(
+        email: String,
+        authCode: String,
+        type: EmailVerificationType,
+    )
+
+    abstract suspend fun checkIdExists(accountId: String): CheckIdExistsResponse
+}
