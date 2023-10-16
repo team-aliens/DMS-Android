fb2d8337889af0959c4ab4c34d7d635232a4080e
junsuPark <krpjs0508@gmail.com>
2023-10-16 19:49:04 +0900

    ⚡️ :: subs notification top req 구현

---
+++ network/src/main/java/team/aliens/dms/android/network/notification/model/SubscribeNotificationTopicRequest.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms.android.network.notification.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SubscribeNotificationTopicRequest(
+    @SerializedName("device_token") val deviceToken: String,
+    @SerializedName("topic") val topic: NotificationTopic,
+)
