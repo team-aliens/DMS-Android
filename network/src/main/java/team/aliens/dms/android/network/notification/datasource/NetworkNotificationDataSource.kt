694e4d01bab4fc9f55846fa411e36e1df901605b
junsuPark <krpjs0508@gmail.com>
2023-10-16 19:56:37 +0900

    ⚡️ :: noti nw datasource 구현

---
+++ network/src/main/java/team/aliens/dms/android/network/notification/datasource/NetworkNotificationDataSource.kt
@@ -0,0 +1,27 @@
+package team.aliens.dms.android.network.notification.datasource
+
+import team.aliens.dms.android.network.notification.model.BatchUpdateNotificationTopicRequest
+import team.aliens.dms.android.network.notification.model.CancelFcmDeviceTokenRegistrationRequest
+import team.aliens.dms.android.network.notification.model.FetchNotificationTopicStatusRequest
+import team.aliens.dms.android.network.notification.model.FetchNotificationTopicStatusResponse
+import team.aliens.dms.android.network.notification.model.FetchNotificationsResponse
+import team.aliens.dms.android.network.notification.model.RegisterFcmDeviceTokenRequest
+import team.aliens.dms.android.network.notification.model.SubscribeNotificationTopicRequest
+import team.aliens.dms.android.network.notification.model.UnsubscribeNotificationTopicRequest
+
+abstract class NetworkNotificationDataSource {
+
+    abstract suspend fun registerFcmDeviceToken(request: RegisterFcmDeviceTokenRequest)
+
+    abstract suspend fun cancelFcmDeviceTokenRegistration(request: CancelFcmDeviceTokenRegistrationRequest)
+
+    abstract suspend fun subscribeNotificationTopic(request: SubscribeNotificationTopicRequest)
+
+    abstract suspend fun unsubscribeNotificationTopic(request: UnsubscribeNotificationTopicRequest)
+
+    abstract suspend fun batchUpdateNotificationTopic(request: BatchUpdateNotificationTopicRequest)
+
+    abstract suspend fun fetchNotificationTopicStatus(request: FetchNotificationTopicStatusRequest): FetchNotificationTopicStatusResponse
+
+    abstract suspend fun fetchNotifications(): FetchNotificationsResponse
+}
