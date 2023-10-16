eb0eb4f6182e750f75375b95df173b30ffc3bcbe
junsuPark <krpjs0508@gmail.com>
2023-10-17 08:20:30 +0900

    ♻️ :: tokens response extension 파라미터 수정

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/Tokens.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/Tokens.kt
@@ -1,8 +1,17 @@
 package team.aliens.dms.android.core.jwt
 
+import team.aliens.dms.android.core.jwt.network.model.TokensResponse
+
 data class Tokens(
     val accessToken: AccessToken,
     val accessTokenExpiration: AccessTokenExpiration,
     val refreshToken: RefreshToken,
     val refreshTokenExpiration: RefreshTokenExpiration,
+)
+
+internal fun TokensResponse.toModel(): Tokens = Tokens(
+    accessToken = this.accessToken,
+    accessTokenExpiration = this.accessTokenExpiration,
+    refreshToken = this.refreshToken,
+    refreshTokenExpiration = this.refreshTokenExpiration,
 )
