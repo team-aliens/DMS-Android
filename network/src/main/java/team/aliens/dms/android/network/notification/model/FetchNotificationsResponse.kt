0395d6882dd07f212939b0e276ed60f1b7ad44b8
junsuPark <krpjs0508@gmail.com>
2023-10-16 19:53:48 +0900

    ⚡️ :: fetch notifications res 구현

---
+++ network/src/main/java/team/aliens/dms/android/network/notification/model/FetchNotificationsResponse.kt
@@ -0,0 +1,19 @@
+package team.aliens.dms.android.network.notification.model
+
+import com.google.gson.annotations.SerializedName
+import team.aliens.dms.android.domain.model.notification.NotificationTopic
+import java.util.UUID
+
+data class FetchNotificationsResponse(
+    @SerializedName("notifications") val notifications: List<NotificationResponse>,
+) {
+    data class NotificationResponse(
+        @SerializedName("id") val id: UUID,
+        @SerializedName("topic") val topic: NotificationTopic,
+        @SerializedName("link_identifier") val linkId: UUID,
+        @SerializedName("title") val title: String,
+        @SerializedName("content") val content: String,
+        @SerializedName("created_at") val createdAt: String,
+        @SerializedName("is_read") val read: Boolean,
+    )
+}
