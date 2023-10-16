93aac8ddf2173e0146fcedc4268523c980cc376b
junsuPark <krpjs0508@gmail.com>
2023-10-16 23:37:31 +0900

    ♻️ :: JwtStore 구현

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/store/JwtStore.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/store/JwtStore.kt
@@ -1,3 +1,17 @@
 package team.aliens.dms.android.core.jwt.store
 
-abstract class JwtStore
+import team.aliens.dms.android.core.jwt.AccessToken
+import team.aliens.dms.android.core.jwt.AccessTokenExpiration
+import team.aliens.dms.android.core.jwt.RefreshToken
+import team.aliens.dms.android.core.jwt.RefreshTokenExpiration
+
+abstract class JwtStore {
+    abstract fun loadAccessToken(): AccessToken
+    abstract suspend fun storeAccessToken(token: AccessToken)
+    abstract fun loadAccessTokenExpiration(): AccessTokenExpiration
+    abstract suspend fun storeAccessTokenExpiration(expiration: AccessTokenExpiration)
+    abstract fun loadRefreshToken(): RefreshToken
+    abstract suspend fun storeRefreshToken(token: RefreshToken)
+    abstract fun loadRefreshTokenExpiration(): RefreshTokenExpiration
+    abstract suspend fun storeRefreshTokenExpiration(expiration: RefreshTokenExpiration)
+}
