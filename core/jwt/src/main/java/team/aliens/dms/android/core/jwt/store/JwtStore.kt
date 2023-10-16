3b47d1e01a0f145bfcb07f62f1f8d89069acbb50
junsuPark <krpjs0508@gmail.com>
2023-10-16 23:38:23 +0900

    ♻️ :: JwtStore line break 추가

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/store/JwtStore.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/store/JwtStore.kt
@@ -6,12 +6,20 @@ import team.aliens.dms.android.core.jwt.RefreshToken
 import team.aliens.dms.android.core.jwt.RefreshTokenExpiration
 
 abstract class JwtStore {
+
     abstract fun loadAccessToken(): AccessToken
+
     abstract suspend fun storeAccessToken(token: AccessToken)
+
     abstract fun loadAccessTokenExpiration(): AccessTokenExpiration
+
     abstract suspend fun storeAccessTokenExpiration(expiration: AccessTokenExpiration)
+
     abstract fun loadRefreshToken(): RefreshToken
+
     abstract suspend fun storeRefreshToken(token: RefreshToken)
+
     abstract fun loadRefreshTokenExpiration(): RefreshTokenExpiration
+
     abstract suspend fun storeRefreshTokenExpiration(expiration: RefreshTokenExpiration)
 }
