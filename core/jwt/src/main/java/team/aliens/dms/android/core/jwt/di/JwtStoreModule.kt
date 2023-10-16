b610e59eb891f5213ccec4f0d27774bee0aea902
junsuPark <krpjs0508@gmail.com>
2023-10-17 08:15:38 +0900

    ♻️ :: jwt store di 구현

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/di/JwtStoreModule.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/di/JwtStoreModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal abstract class JwtModule {
+internal abstract class JwtStoreModule {
 
     @Binds
     @Singleton
