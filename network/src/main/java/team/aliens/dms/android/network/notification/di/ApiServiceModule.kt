700adfb4d859061e966a0ad22be23d09bb6c4574
junsuPark <krpjs0508@gmail.com>
2023-10-16 19:54:02 +0900

    ⚡️ :: noti api service module 구현

---
+++ network/src/main/java/team/aliens/dms/android/network/notification/di/ApiServiceModule.kt
@@ -0,0 +1,19 @@
+package team.aliens.dms.android.network.notification.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import team.aliens.dms.android.network.notification.apiservice.NotificationApiService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object ApiServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideNotificationApiService(retrofit: Retrofit): NotificationApiService =
+        retrofit.create(NotificationApiService::class.java)
+}
