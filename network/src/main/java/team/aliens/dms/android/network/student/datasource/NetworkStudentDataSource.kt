c4ee5789a30623ca3603dba4fcb2bc16bac28e6b
junsuPark <krpjs0508@gmail.com>
2023-10-16 22:48:27 +0900

    ⚡️ :: student datasource impl 구현

--- network/src/main/java/team/aliens/dms/android/network/student/datasource/NetworkStudentDataSource.kt
+++ network/src/main/java/team/aliens/dms/android/network/student/datasource/NetworkStudentDataSource.kt
@@ -1,9 +1,11 @@
 package team.aliens.dms.android.network.student.datasource
 
 import team.aliens.dms.android.core.jwt.network.model.AuthenticationResponse
+import team.aliens.dms.android.network.student.model.EditProfileRequest
 import team.aliens.dms.android.network.student.model.ExamineStudentNumberResponse
 import team.aliens.dms.android.network.student.model.FetchMyPageResponse
 import team.aliens.dms.android.network.student.model.FindIdResponse
+import team.aliens.dms.android.network.student.model.ResetPasswordRequest
 import team.aliens.dms.android.network.student.model.SignUpRequest
 import java.util.UUID
 
@@ -26,13 +28,7 @@ abstract class NetworkStudentDataSource {
         number: Int,
     ): FindIdResponse
 
-    abstract suspend fun resetPassword(
-        accountId: String,
-        studentName: String,
-        email: String,
-        emailVerificationCode: String,
-        newPassword: String,
-    )
+    abstract suspend fun resetPassword(request: ResetPasswordRequest)
 
     abstract suspend fun checkIdDuplication(accountId: String)
 
@@ -40,7 +36,7 @@ abstract class NetworkStudentDataSource {
 
     abstract suspend fun fetchMyPage(): FetchMyPageResponse
 
-    abstract suspend fun editProfile(profileImageUrl: String)
+    abstract suspend fun editProfile(request: EditProfileRequest)
 
     abstract suspend fun withdraw()
 }
