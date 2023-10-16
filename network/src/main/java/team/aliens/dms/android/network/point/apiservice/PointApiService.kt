4266bb94c7eb571a18fe77e2e0ac64bedc3a3e21
junsuPark <krpjs0508@gmail.com>
2023-10-16 20:13:56 +0900

    ⚡️ :: point api service 파라미터 타입 변경

--- network/src/main/java/team/aliens/dms/android/network/point/apiservice/PointApiService.kt
+++ network/src/main/java/team/aliens/dms/android/network/point/apiservice/PointApiService.kt
@@ -4,13 +4,14 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 import team.aliens.dms.android.core.jwt.RequiresAccessToken
 import team.aliens.dms.android.network.point.model.FetchPointsResponse
+import team.aliens.dms.android.network.point.model.PointType
 
 internal abstract class PointApiService {
 
     @GET("points")
     @RequiresAccessToken
     abstract suspend fun fetchPoints(
-        @Query("type") type: String,
+        @Query("type") type: PointType,
         @Query("page") page: Long?,
         @Query("size") size: Long?,
     ): FetchPointsResponse
