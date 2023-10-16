f98065626acd43e0003d9498c857246adee9ba46
junsuPark <krpjs0508@gmail.com>
2023-10-16 19:49:32 +0900

    ⚡️ :: unsub notification top req 구현

--- network/src/main/java/team/aliens/dms/android/network/notification/model/UnsubscribeNotificationTopicRequest.kt
+++ network/src/main/java/team/aliens/dms/android/network/notification/model/UnsubscribeNotificationTopicRequest.kt
@@ -2,7 +2,7 @@ package team.aliens.dms.android.network.notification.model
 
 import com.google.gson.annotations.SerializedName
 
-data class SubscribeNotificationTopicRequest(
+data class UnsubscribeNotificationTopicRequest(
     @SerializedName("device_token") val deviceToken: String,
     @SerializedName("topic") val topic: NotificationTopic,
 )
