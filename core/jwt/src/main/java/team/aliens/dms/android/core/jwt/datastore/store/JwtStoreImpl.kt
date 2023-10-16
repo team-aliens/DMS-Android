dcfcbf31ac876d95359f8d131eed5804746df0db
junsuPark <krpjs0508@gmail.com>
2023-10-17 08:08:10 +0900

    ♻️ :: jwtstore impl 함수 구현

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/datastore/store/JwtStoreImpl.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/datastore/store/JwtStoreImpl.kt
@@ -12,12 +12,14 @@ import team.aliens.dms.android.core.jwt.AccessToken
 import team.aliens.dms.android.core.jwt.AccessTokenExpiration
 import team.aliens.dms.android.core.jwt.RefreshToken
 import team.aliens.dms.android.core.jwt.RefreshTokenExpiration
+import team.aliens.dms.android.core.jwt.Tokens
 import team.aliens.dms.android.core.jwt.exception.AccessTokenExpirationNotFoundException
 import team.aliens.dms.android.core.jwt.exception.AccessTokenNotFoundException
 import team.aliens.dms.android.core.jwt.exception.CannotStoreAccessTokenException
 import team.aliens.dms.android.core.jwt.exception.CannotStoreAccessTokenExpirationException
 import team.aliens.dms.android.core.jwt.exception.CannotStoreRefreshTokenException
 import team.aliens.dms.android.core.jwt.exception.CannotStoreRefreshTokenExpirationException
+import team.aliens.dms.android.core.jwt.exception.CannotStoreTokensException
 import team.aliens.dms.android.core.jwt.exception.RefreshTokenExpirationNotFoundException
 import team.aliens.dms.android.core.jwt.exception.RefreshTokenNotFoundException
 import team.aliens.dms.android.shared.date.extension.toEpochMilli
@@ -27,6 +29,38 @@ import javax.inject.Inject
 internal class JwtStoreImpl @Inject constructor(
     private val preferencesDataStore: PreferencesDataStore,
 ) : JwtStore() {
+
+    override fun loadTokens(): Tokens = runBlocking {
+        preferencesDataStore.data.map { preferences ->
+            val accessToken = preferences[ACCESS_TOKEN] ?: throw AccessTokenNotFoundException()
+            val accessTokenExpiration = preferences[ACCESS_TOKEN_EXPIRATION]
+                ?: throw AccessTokenExpirationNotFoundException()
+            val refreshToken = preferences[REFRESH_TOKEN] ?: throw RefreshTokenNotFoundException()
+            val refreshTokenExpiration = preferences[REFRESH_TOKEN_EXPIRATION]
+                ?: throw RefreshTokenExpirationNotFoundException()
+
+            return@map Tokens(
+                accessToken = accessToken,
+                accessTokenExpiration = accessTokenExpiration.toLocalDateTime(),
+                refreshToken = refreshToken,
+                refreshTokenExpiration = refreshTokenExpiration.toLocalDateTime(),
+            )
+        }.first()
+    }
+
+    override suspend fun storeTokens(tokens: Tokens) {
+        transform(
+            onFailure = { throw CannotStoreTokensException() },
+        ) {
+            preferencesDataStore.edit { preferences ->
+                preferences[ACCESS_TOKEN] = tokens.accessToken
+                preferences[ACCESS_TOKEN_EXPIRATION] = tokens.accessTokenExpiration.toEpochMilli()
+                preferences[REFRESH_TOKEN] = tokens.refreshToken
+                preferences[REFRESH_TOKEN_EXPIRATION] = tokens.refreshTokenExpiration.toEpochMilli()
+            }
+        }
+    }
+
     override fun loadAccessToken(): AccessToken = runBlocking {
         preferencesDataStore.data.map { preferences ->
             preferences[ACCESS_TOKEN] ?: throw AccessTokenNotFoundException()
