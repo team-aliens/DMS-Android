0f9969c06a648faf84b5c405b11219c037bf9a06
junsuPark <krpjs0508@gmail.com>
2023-10-17 08:25:38 +0900

    ♻️ :: jwt store 함수 라인 수정

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/datastore/store/JwtStore.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/datastore/store/JwtStore.kt
@@ -7,14 +7,24 @@ import team.aliens.dms.android.core.jwt.RefreshTokenExpiration
 import team.aliens.dms.android.core.jwt.Tokens
 
 abstract class JwtStore {
+
     abstract fun loadTokens(): Tokens
+
     abstract suspend fun storeTokens(tokens: Tokens)
+
     abstract fun loadAccessToken(): AccessToken
+
     abstract suspend fun storeAccessToken(token: AccessToken)
+
     abstract fun loadAccessTokenExpiration(): AccessTokenExpiration
+
     abstract suspend fun storeAccessTokenExpiration(expiration: AccessTokenExpiration)
+
     abstract fun loadRefreshToken(): RefreshToken
+
     abstract suspend fun storeRefreshToken(token: RefreshToken)
+
     abstract fun loadRefreshTokenExpiration(): RefreshTokenExpiration
+
     abstract suspend fun storeRefreshTokenExpiration(expiration: RefreshTokenExpiration)
 }
