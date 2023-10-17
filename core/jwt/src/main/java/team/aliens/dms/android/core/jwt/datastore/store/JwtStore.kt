85c38799bee5616006cc993a4e05b6afc7b3697f
junsuPark <krpjs0508@gmail.com>
2023-10-17 17:08:20 +0900

    ♻️ :: jwt 접근제한자 수정

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/datastore/store/JwtStore.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/datastore/store/JwtStore.kt
@@ -6,7 +6,7 @@ import team.aliens.dms.android.core.jwt.RefreshToken
 import team.aliens.dms.android.core.jwt.RefreshTokenExpiration
 import team.aliens.dms.android.core.jwt.Tokens
 
-abstract class JwtStore {
+internal abstract class JwtStore {
 
     abstract fun loadTokens(): Tokens
 
