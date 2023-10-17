f963e12b0d9a3c12e0cef9f5221610087a1fcc2c
junsuPark <krpjs0508@gmail.com>
2023-10-17 20:25:51 +0900

    ♻️ :: token reissue manager lagacy 제거

--- data/src/main/java/team/aliens/dms/android/data/_legacy/manager/TokenReissueManager.kt
+++
@@ -1,7 +0,0 @@
-package team.aliens.dms.android.data._legacy.manager
-
-import team.aliens.dms.android.domain.model._common.AuthenticationOutput
-
-interface TokenReissueManager {
-    fun reissueToken(refreshToken: String): AuthenticationOutput
-}
