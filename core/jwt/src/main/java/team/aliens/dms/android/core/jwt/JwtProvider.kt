176d66e5c4b855419a73b97b8c8140d0ec786ae0
junsuPark <krpjs0508@gmail.com>
2023-10-17 19:26:18 +0900

    ♻️ :: JwtProvider 구현

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/JwtProvider.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/JwtProvider.kt
@@ -3,56 +3,63 @@ package team.aliens.dms.android.core.jwt
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import team.aliens.dms.android.core.jwt.datastore.store.JwtStore
+import team.aliens.dms.android.core.datastore.exception.LoadFailureException
+import team.aliens.dms.android.core.jwt.datastore.JwtDataStoreDataSource
+import team.aliens.dms.android.core.jwt.exception.CannotUseTokensException
 import team.aliens.dms.android.core.jwt.network.TokenReissueManager
 import team.aliens.dms.android.shared.date.util.now
 import javax.inject.Inject
 
+// TODO: JWT Repository 구현 고민
 object JwtProvider : JwtProviderInjectionDelegation() {
 
     private var _cachedAccessToken: AccessToken? = null
     val cachedAccessToken: AccessToken
-        get() = if (_cachedAccessToken == null) {
-            this.updateAccessToken()
+        get() = if (isCachedAccessTokenAvailable) {
+            _cachedAccessToken!!
         } else {
-            this.fetchAccessToken()
+            this.fetchTokens().accessToken
         }
 
-    // 재발급 및 저장 함수
-    // 토큰 유효 확인 함수 (access, refresh 추상화)
+    private var _cachedAccessTokenExpiration: AccessTokenExpiration? = null
+    val cachedAccessTokenExpiration: AccessTokenExpiration
+        get() = _cachedAccessTokenExpiration ?: this.fetchTokens().accessTokenExpiration
 
-    private var cachedAccessTokenExpiration: AccessTokenExpiration? = null
-
-    private var cachedRefreshToken: RefreshToken? = null
-
-    private var cachedRefreshTokenExpiration: RefreshTokenExpiration? = null
-
-    private fun updateAccessToken(): AccessToken {
-        val refreshToken = jwtStore.loadRefreshToken()
-        val fetchedTokens = tokenReissueManager(refreshToken = refreshToken).toModel()
-
-        CoroutineScope(Dispatchers.IO).launch {
-            jwtStore.storeTokens(fetchedTokens)
+    private val isCachedAccessTokenAvailable: Boolean
+        get() {
+            val con1 = _cachedAccessToken != null
+            val con2 = now.isBefore(cachedAccessTokenExpiration)
+            return con1 && con2
         }
 
-        return fetchedTokens.accessToken
+    init {
+        loadTokens()
     }
 
-    private fun fetchAccessToken(): AccessToken {
-        val accessTokenExpiration = jwtStore.loadAccessTokenExpiration()
+    private fun loadTokens(): Tokens = try {
+        jwtDataStoreDataSource.loadTokens().also(::updateTokens)
+    } catch (e: LoadFailureException) {
+        throw CannotUseTokensException()
+    }
 
-        return if (accessTokenExpiration > now) {
-            this.updateAccessToken()
-        } else {
-            jwtStore.loadAccessToken()
-        }
+    private fun fetchTokens(): Tokens = try {
+        val refreshToken = jwtDataStoreDataSource.loadRefreshToken()
+        tokenReissueManager(refreshToken).toModel().also(::updateTokens)
+    } catch (e: LoadFailureException) {
+        throw CannotUseTokensException()
+    }
+
+    private fun updateTokens(tokens: Tokens) {
+        this._cachedAccessToken = tokens.accessToken
+        this._cachedAccessTokenExpiration = tokens.accessTokenExpiration
+        CoroutineScope(Dispatchers.IO).launch { jwtDataStoreDataSource.storeTokens(tokens) }
     }
 }
 
 abstract class JwtProviderInjectionDelegation {
 
     @Inject
-    lateinit var jwtStore: JwtStore
+    lateinit var jwtDataStoreDataSource: JwtDataStoreDataSource
 
     @Inject
     lateinit var tokenReissueManager: TokenReissueManager
