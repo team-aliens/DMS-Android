2d103376d3b5bc28e365e942f93cd9c4897f0594
junsuPark <krpjs0508@gmail.com>
2023-10-18 22:52:23 +0900

    ♻️ :: Colors 추가

---
+++ design-system/src/main/java/team/aliens/dms/android/designsystem/color/Colors.kt
@@ -0,0 +1,64 @@
+package team.aliens.dms.android.designsystem.color
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+@Stable
+val PrimaryDefault = Color(0xff3d8aff)
+
+@Stable
+val PrimaryLighten1 = Color(0xffb1d0ff)
+
+@Stable
+val PrimaryLighten2 = Color(0xfff5f9ff)
+
+@Stable
+val PrimaryDarken1 = Color(0xff1070ff)
+
+@Stable
+val PrimaryDarken2 = Color(0xff005de8)
+
+@Stable
+val ErrorDefault = Color(0xffff4646)
+
+@Stable
+val ErrorLighten1 = Color(0xffffd3d3)
+
+@Stable
+val ErrorLighten2 = Color(0xfffff0f0)
+
+@Stable
+val ErrorDarken1 = Color(0xffc23535)
+
+@Stable
+val ErrorDarken2 = Color(0xff852424)
+
+@Stable
+val Gray1 = Color(0xffffffff)
+
+@Stable
+val Gray2 = Color(0xfff9f9f9)
+
+@Stable
+val Gray3 = Color(0xffeeeeee)
+
+@Stable
+val Gray4 = Color(0xffdddddd)
+
+@Stable
+val Gray5 = Color(0xff999999)
+
+@Stable
+val Gray6 = Color(0xff555555)
+
+@Stable
+val Gray7 = Color(0xff343434)
+
+@Stable
+val Gray8 = Color(0xff202020)
+
+@Stable
+val Gray9 = Color(0xff121212)
+
+@Stable
+val Gray10 = Color(0xff000000)
