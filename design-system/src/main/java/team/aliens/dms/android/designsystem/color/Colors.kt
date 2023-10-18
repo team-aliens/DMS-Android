08cc06f93e098a4e60fe5a06aaa2a452e8d47dd4
junsuPark <krpjs0508@gmail.com>
2023-10-19 07:15:50 +0900

    ♻️ :: colors 이동

--- design-system/src/main/java/team/aliens/dms/android/designsystem/color/Colors.kt
+++ design-system/src/main/java/team/aliens/dms/android/designsystem/color/Colors.kt
@@ -1,6 +1,10 @@
 package team.aliens.dms.android.designsystem.color
 
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.graphics.Color
 
 @Stable
@@ -62,3 +66,131 @@ val Gray9 = Color(0xff121212)
 
 @Stable
 val Gray10 = Color(0xff000000)
+
+@Stable
+class Colors(
+    primary: Color,
+    onPrimary: Color,
+    primaryContainer: Color,
+    onPrimaryContainer: Color,
+    error: Color,
+    onError: Color,
+    errorContainer: Color,
+    onErrorContainer: Color,
+    background: Color,
+    onBackground: Color,
+    backgroundVariant: Color,
+    onBackgroundVariant: Color,
+    surface: Color,
+    onSurface: Color,
+    surfaceVariant: Color,
+    onSurfaceVariant: Color,
+    icon: Color,
+    line: Color,
+    isLight: Boolean,
+) {
+    var primary by mutableStateOf(primary, structuralEqualityPolicy())
+        internal set
+    var onPrimary by mutableStateOf(onPrimary, structuralEqualityPolicy())
+        internal set
+    var primaryContainer by mutableStateOf(primaryContainer, structuralEqualityPolicy())
+        internal set
+    var onPrimaryContainer by mutableStateOf(onPrimaryContainer, structuralEqualityPolicy())
+        internal set
+
+    var error by mutableStateOf(error, structuralEqualityPolicy())
+        internal set
+    var onError by mutableStateOf(onError, structuralEqualityPolicy())
+        internal set
+    var errorContainer by mutableStateOf(errorContainer, structuralEqualityPolicy())
+        internal set
+    var onErrorContainer by mutableStateOf(onErrorContainer, structuralEqualityPolicy())
+        internal set
+
+    var background by mutableStateOf(background, structuralEqualityPolicy())
+        internal set
+    var onBackground by mutableStateOf(onBackground, structuralEqualityPolicy())
+        internal set
+    var backgroundVariant by mutableStateOf(backgroundVariant, structuralEqualityPolicy())
+        internal set
+    var onBackgroundVariant by mutableStateOf(onBackgroundVariant, structuralEqualityPolicy())
+        internal set
+
+    var surface by mutableStateOf(surface, structuralEqualityPolicy())
+        internal set
+    var onSurface by mutableStateOf(onSurface, structuralEqualityPolicy())
+        internal set
+    var surfaceVariant by mutableStateOf(surfaceVariant, structuralEqualityPolicy())
+        internal set
+    var onSurfaceVariant by mutableStateOf(onSurfaceVariant, structuralEqualityPolicy())
+        internal set
+
+    var icon by mutableStateOf(icon, structuralEqualityPolicy())
+        internal set
+    var line by mutableStateOf(line, structuralEqualityPolicy())
+        internal set
+
+    var isLight by mutableStateOf(isLight, structuralEqualityPolicy())
+        internal set
+
+    fun copy(
+        primary: Color = this.primary,
+        onPrimary: Color = this.onPrimary,
+        primaryContainer: Color = this.primaryContainer,
+        onPrimaryContainer: Color = this.onPrimaryContainer,
+        error: Color = this.error,
+        onError: Color = this.onError,
+        errorContainer: Color = this.onErrorContainer,
+        onErrorContainer: Color = this.onErrorContainer,
+        background: Color = this.background,
+        onBackground: Color = this.onBackground,
+        backgroundVariant: Color = this.backgroundVariant,
+        onBackgroundVariant: Color = this.onBackgroundVariant,
+        surface: Color = this.surface,
+        onSurface: Color = this.onSurface,
+        surfaceVariant: Color = this.surfaceVariant,
+        onSurfaceVariant: Color = this.onSurfaceVariant,
+        icon: Color = this.icon,
+        line: Color = this.line,
+        isLight: Boolean = this.isLight,
+    ): Colors = Colors(
+        primary = primary,
+        onPrimary = onPrimary,
+        primaryContainer = primaryContainer,
+        onPrimaryContainer = onPrimaryContainer,
+        error = error,
+        onError = onError,
+        errorContainer = errorContainer,
+        onErrorContainer = onErrorContainer,
+        background = background,
+        onBackground = onBackground,
+        backgroundVariant = backgroundVariant,
+        onBackgroundVariant = onBackgroundVariant,
+        surface = surface,
+        onSurface = onSurface,
+        surfaceVariant = surfaceVariant,
+        onSurfaceVariant = onSurfaceVariant,
+        icon = icon,
+        line = line,
+        isLight = isLight,
+    )
+
+    override fun toString(): String = "Colors(" +
+            "primary=$primary, " +
+            "onPrimary=$onPrimary, " +
+            "primaryContainer=$primaryContainer, " +
+            "onPrimaryContainer=$onPrimaryContainer, " +
+            "error=$error, " +
+            "onError=$onError, " +
+            "errorContainer=$errorContainer, " +
+            "onErrorContainer=$onErrorContainer, " +
+            "background=$background, " +
+            "onBackground=$onBackground, " +
+            "backgroundVariant=$backgroundVariant, " +
+            "onBackgroundVariant=$onBackgroundVariant, " +
+            "surface=$surface, " +
+            "onSurface=$onSurface, " +
+            "surfaceVariant=$surfaceVariant, " +
+            "onSurfaceVariant=$onSurfaceVariant, " +
+            ")"
+}
