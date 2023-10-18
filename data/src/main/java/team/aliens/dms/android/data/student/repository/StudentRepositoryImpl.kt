f475388fdf485fdbc341e07030f0ed5f563ecddb
junsuPark <krpjs0508@gmail.com>
2023-10-18 12:16:40 +0900

    ♻️ :: student repository impl 추가

---
+++ data/src/main/java/team/aliens/dms/android/data/student/repository/StudentRepositoryImpl.kt
@@ -0,0 +1,77 @@
+package team.aliens.dms.android.data.student.repository
+
+import team.aliens.dms.android.data.student.model.ExamineStudentNumberOutput
+import team.aliens.dms.android.data.student.model.FindIdOutput
+import team.aliens.dms.android.data.student.model.MyPage
+import team.aliens.dms.android.network.student.datasource.NetworkStudentDataSource
+import java.util.UUID
+import javax.inject.Inject
+
+internal class StudentRepositoryImpl @Inject constructor(
+    private val networkStudentDataSource: NetworkStudentDataSource,
+) : StudentRepository() {
+
+    override suspend fun signUp(
+        schoolVerificationCode: String,
+        schoolVerificationAnswer: String,
+        email: String,
+        emailVerificationCode: String,
+        grade: Int,
+        classRoom: Int,
+        number: Int,
+        accountId: String,
+        password: String,
+        profileImageUrl: String?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun examineStudentNumber(
+        schoolId: UUID,
+        grade: Int,
+        classRoom: Int,
+        number: Int,
+    ): ExamineStudentNumberOutput {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun findId(
+        schoolId: UUID,
+        studentName: String,
+        grade: Int,
+        classRoom: Int,
+        number: Int,
+    ): FindIdOutput {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun resetPassword(
+        accountId: String,
+        studentName: String,
+        email: String,
+        emailVerificationCode: String,
+        newPassword: String,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun checkIdDuplication(id: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun checkEmailDuplication(email: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchMyPage(): MyPage {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun editProfile(profileImageUrl: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun withdraw() {
+        TODO("Not yet implemented")
+    }
+}
