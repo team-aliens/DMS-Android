ce173e02f44437a2f381e6da932e5fc348ea1c02
junsuPark <krpjs0508@gmail.com>
2023-10-18 11:04:23 +0900

    ♻️ :: NotificationTopic subscription 추가

--- shared/model/src/main/java/team/aliens/dms/android/shared/model/NotificationTopic.kt
+++ shared/model/src/main/java/team/aliens/dms/android/shared/model/NotificationTopic.kt
@@ -3,4 +3,9 @@ package team.aliens.dms.android.shared.model
 enum class NotificationTopic {
     NOTICE, STUDY_ROOM,
     ;
+
+    data class Subscription(
+        val topic: NotificationTopic,
+        val subscribe: Boolean,
+    )
 }
