ea289cfc27d45728921016cef6bb1e80a6d3096f
junsuPark <krpjs0508@gmail.com>
2023-10-18 11:56:27 +0900

    ♻️ :: remains repo impl 추가

---
+++ data/src/main/java/team/aliens/dms/android/data/remains/repository/RemainsRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package team.aliens.dms.android.data.remains.repository
+
+import team.aliens.dms.android.data.remains.model.FetchAppliedRemainsOptionOutput
+import team.aliens.dms.android.data.remains.model.FetchRemainsApplicationTimeOutput
+import team.aliens.dms.android.data.remains.model.RemainsOption
+import team.aliens.dms.android.network.remains.datasource.NetworkRemainsDataSource
+import java.util.UUID
+import javax.inject.Inject
+
+internal class RemainsRepositoryImpl @Inject constructor(
+    private val networkRemainsDataSource: NetworkRemainsDataSource,
+) : RemainsRepository() {
+
+    override suspend fun updateRemainsOption(remainsOption: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchAppliedRemainsOption(): FetchAppliedRemainsOptionOutput {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchRemainsApplicationTime(): FetchRemainsApplicationTimeOutput {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchRemainsOptions(): List<RemainsOption> {
+        TODO("Not yet implemented")
+    }
+}
