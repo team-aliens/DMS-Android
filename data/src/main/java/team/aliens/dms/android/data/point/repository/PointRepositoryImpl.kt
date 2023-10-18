0020937213ce9e67b7c2725e7613ecba549b8437
junsuPark <krpjs0508@gmail.com>
2023-10-18 11:50:09 +0900

    ♻️ :: point repo impl 구현

---
+++ data/src/main/java/team/aliens/dms/android/data/point/repository/PointRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package team.aliens.dms.android.data.point.repository
+
+import team.aliens.dms.android.data.point.model.FetchPointsOutput
+import team.aliens.dms.android.network.point.datasource.NetworkPointDataSource
+import team.aliens.dms.android.shared.model.PointType
+import javax.inject.Inject
+
+internal class PointRepositoryImpl @Inject constructor(
+    private val networkPointDataSource: NetworkPointDataSource,
+) : PointRepository() {
+    override fun fetchPoints(
+        type: PointType,
+        page: Long?,
+        size: Long?,
+    ): FetchPointsOutput {
+        TODO("Not yet implemented")
+    }
+}
