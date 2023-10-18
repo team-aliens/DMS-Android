06eecd9f36ee7236cb355ea09eaeff533d399424
junsuPark <krpjs0508@gmail.com>
2023-10-18 11:26:45 +0900

    ♻️ :: NotificationRepository 추가

--- shared/model/src/main/java/team/aliens/dms/android/shared/model/NotificationTopicGroup.kt
+++ shared/model/src/main/java/team/aliens/dms/android/shared/model/NotificationTopicGroup.kt
@@ -4,12 +4,12 @@ enum class NotificationTopicGroup {
     NOTICE, STUDY_ROOM, STUDY_ROOM_APPLY,
     ;
 
-    data class Information(
+    data class Status(
         val topicGroup: NotificationTopicGroup,
         val groupTitle: String,
-        val topicSubscriptions: List<Subscription>,
+        val topicSubscriptions: List<TopicSubscription>,
     ) {
-        data class Subscription(
+        data class TopicSubscription(
             val topic: NotificationTopic,
             val title: String,
             val description: String,
