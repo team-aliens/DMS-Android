8ec573fd0cdeb6a056a3fa2ef9bf42dc2162cb45
junsuPark <krpjs0508@gmail.com>
2023-10-18 18:05:55 +0900

    ♻️ :: feature 의존성 수정

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 
 dependencies {
     implementation(project(ProjectPaths.designSystem))
-    implementation(project(ProjectPaths.domain))
+    implementation(project(ProjectPaths.data))
 
     implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
