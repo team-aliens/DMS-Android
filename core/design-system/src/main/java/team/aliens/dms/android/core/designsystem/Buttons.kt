9f07591cde9a6564405500ca7138af3a6edd5ecc
junsuPark <krpjs0508@gmail.com>
2023-10-19 17:19:08 +0900

    ♻️ :: buttons material3 이전

--- core/design-system/src/main/java/team/aliens/dms/android/core/designsystem/Buttons.kt
+++ core/design-system/src/main/java/team/aliens/dms/android/core/designsystem/Buttons.kt
@@ -1,7 +1,6 @@
 package team.aliens.dms.android.core.designsystem
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,26 +10,22 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.ProvideTextStyle
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.contentColorFor
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
@@ -38,6 +33,83 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+
+@Composable
+fun Button(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: Dp = 0.dp,
+    shape: Shape = DmsTheme.shapes.small,
+    border: BorderStroke? = null,
+    colors: ButtonColors = ButtonDefaults.containedButtonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    val containerColor = colors.containerColor(enabled).value
+    val contentColor = colors.contentColor(enabled).value
+    Surface(
+        onClick = onClick,
+        modifier = modifier.semantics { role = Role.Button },
+        enabled = enabled,
+        shape = shape,
+        color = containerColor,
+        contentColor = contentColor,
+        border = border,
+        interactionSource = interactionSource
+    ) {
+        CompositionLocalProvider(LocalContentColor provides contentColor) {
+            ProvideTextStyle(value = DmsTheme.typography.button) {
+                Row(
+                    Modifier
+                        .defaultMinSize(
+                            minWidth = ButtonDefaults.MinWidth, minHeight = ButtonDefaults.MinHeight
+                        )
+                        .padding(contentPadding),
+                    horizontalArrangement = Arrangement.spacedBy(
+                        space = 8.dp,
+                        alignment = Alignment.CenterHorizontally,
+                    ),
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = content,
+                )
+            }
+        }
+    }/*
+        Surface(
+            onClick = onClick,
+            modifier = modifier.semantics { role = Role.Button },
+            enabled = enabled,
+            shape = shape,
+            color = colors.backgroundColor(enabled).value,
+            contentColor = contentColor.copy(alpha = 1f),
+            border = border,
+            elevation = elevation,
+            interactionSource = interactionSource,
+        ) {
+            CompositionLocalProvider(LocalContentAlpha provides contentColor.alpha) {
+                ProvideTextStyle(
+                    value = textStyle,
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .defaultMinSize(
+                                minWidth = ButtonDefaults.MinWidth,
+                                minHeight = ButtonDefaults.MinHeight,
+                            )
+                            .padding(contentPadding),
+                        horizontalArrangement = Arrangement.spacedBy(
+                            space = 8.dp,
+                            alignment = Alignment.CenterHorizontally,
+                        ),
+                        verticalAlignment = Alignment.CenterVertically,
+                        content = content,
+                    )
+                }
+            }
+        }*/
+}
 
 @Preview(showSystemUi = true)
 @Composable
@@ -61,6 +133,10 @@ private fun ButtonPreview() {
             onClick = {},
         ) {
             Text(text = "Contained Button")
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back),
+                contentDescription = null,
+            )
         }
 
         OutlinedButton(
@@ -70,6 +146,10 @@ private fun ButtonPreview() {
             onClick = { },
         ) {
             Text(text = "Outlined Button")
+        }
+
+        androidx.compose.material3.Button(onClick = { /*TODO*/ }) {
+
         }
     }
 }
@@ -83,11 +163,8 @@ fun ContainedButton(
     elevation: Dp = 0.dp,
     shape: Shape = DmsTheme.shapes.small,
     border: BorderStroke? = null,
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    colors: ButtonColors = ButtonDefaults.containedButtonColors(),
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
-    textStyle: TextStyle = DmsTheme.typography.button.copy(
-        color = DmsTheme.colors.onPrimary,
-    ),
     content: @Composable RowScope.() -> Unit,
 ) = Button(
     onClick = onClick,
@@ -99,7 +176,6 @@ fun ContainedButton(
     border = border,
     colors = colors,
     contentPadding = contentPadding,
-    textStyle = textStyle,
     content = content,
 )
 
@@ -111,12 +187,9 @@ fun OutlinedButton(
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     elevation: Dp = 0.dp,
     shape: Shape = DmsTheme.shapes.small,
-    border: BorderStroke = ButtonDefaults.outlinedBorder,
+    border: BorderStroke? = null,
     colors: ButtonColors = ButtonDefaults.outlinedButtonColors(),
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
-    textStyle: TextStyle = DmsTheme.typography.button.copy(
-        color = DmsTheme.colors.primary,
-    ),
     content: @Composable RowScope.() -> Unit,
 ) = Button(
     onClick = onClick,
@@ -128,7 +201,6 @@ fun OutlinedButton(
     border = border,
     colors = colors,
     contentPadding = contentPadding,
-    textStyle = textStyle,
     content = content,
 )
 
@@ -142,72 +214,50 @@ fun RoundedButton() {
 
 }
 
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun Button(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    elevation: Dp = 0.dp,
-    shape: Shape = DmsTheme.shapes.small,
-    border: BorderStroke? = null,
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
-    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
-    textStyle: TextStyle = DmsTheme.typography.button,
-    content: @Composable RowScope.() -> Unit,
+@Stable
+class ButtonColors(
+    private val containerColor: Color,
+    private val contentColor: Color,
+    private val disabledContainerColor: Color,
+    private val disabledContentColor: Color,
 ) {
-    val contentColor by colors.contentColor(enabled)
+    @Composable
+    internal fun containerColor(enabled: Boolean): State<Color> = rememberUpdatedState(
+        if (enabled) {
+            containerColor
+        } else {
+            disabledContainerColor
+        },
+    )
 
-    Surface(
-        onClick = onClick,
-        modifier = modifier.semantics { role = Role.Button },
-        enabled = enabled,
-        shape = shape,
-        color = colors.backgroundColor(enabled).value,
-        contentColor = contentColor.copy(alpha = 1f),
-        border = border,
-        elevation = elevation,
-        interactionSource = interactionSource,
-    ) {
-        CompositionLocalProvider(LocalContentAlpha provides contentColor.alpha) {
-            ProvideTextStyle(
-                value = textStyle,
-            ) {
-                Row(
-                    modifier = Modifier
-                        .defaultMinSize(
-                            minWidth = ButtonDefaults.MinWidth,
-                            minHeight = ButtonDefaults.MinHeight,
-                        )
-                        .padding(contentPadding),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
-                    content = content,
-                )
-            }
-        }
+    @Composable
+    internal fun contentColor(enabled: Boolean): State<Color> = rememberUpdatedState(
+        if (enabled) {
+            contentColor
+        } else {
+            disabledContentColor
+        },
+    )
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || other !is ButtonColors) return false
+
+        if (containerColor != other.containerColor) return false
+        if (contentColor != other.contentColor) return false
+        if (disabledContainerColor != other.disabledContainerColor) return false
+        if (disabledContentColor != other.disabledContentColor) return false
+
+        return true
     }
-}
 
-@Stable
-interface ButtonElevation {
-
-    @Composable
-    fun elevation(
-        enabled: Boolean,
-        interactionSource: InteractionSource,
-    ): State<Dp>
-}
-
-@Stable
-interface ButtonColors {
-
-    @Composable
-    fun backgroundColor(enabled: Boolean): State<Color>
-
-    @Composable
-    fun contentColor(enabled: Boolean): State<Color>
+    override fun hashCode(): Int {
+        var result = containerColor.hashCode()
+        result = 31 * result + contentColor.hashCode()
+        result = 31 * result + disabledContainerColor.hashCode()
+        result = 31 * result + disabledContentColor.hashCode()
+        return result
+    }
 }
 
 object ButtonDefaults {
@@ -230,117 +280,73 @@ object ButtonDefaults {
 
     val IconSpacing = 8.dp
 
+    const val DisabledContainerOpacity = 0.12f
+
+    const val DisabledContentOpacity = 0.38f
+
+    const val DisabledTextOpacity = 0.38f
+
+    val containedShape: Shape
+        @Composable get() = DmsTheme.shapes.small
+
+    val outlinedShape: Shape
+        @Composable get() = DmsTheme.shapes.small
+
+    val textShape: Shape
+        @Composable get() = DmsTheme.shapes.small
+
+    val roundShape: Shape
+        @Composable get() = DmsTheme.shapes.circle
+
     @Composable
-    fun buttonColors(
-        backgroundColor: Color = DmsTheme.colors.primary,
-        contentColor: Color = contentColorFor(backgroundColor),
-        disabledBackgroundColor: Color = DmsTheme.colors.onSurface.copy(alpha = 0.12f)
-            .compositeOver(DmsTheme.colors.surface),
-        disabledContentColor: Color = DmsTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
-    ): ButtonColors = DefaultButtonColors(
-        backgroundColor = backgroundColor,
+    fun containedButtonColors(
+        containerColor: Color = DmsTheme.colors.primary,
+        contentColor: Color = DmsTheme.colors.onPrimary,
+        disabledContainerColor: Color = containerColor.copy(alpha = DisabledContainerOpacity),
+        disabledContentColor: Color = contentColor.copy(alpha = DisabledContentOpacity),
+    ): ButtonColors = ButtonColors(
+        containerColor = containerColor,
         contentColor = contentColor,
-        disabledBackgroundColor = disabledBackgroundColor,
+        disabledContainerColor = disabledContainerColor,
         disabledContentColor = disabledContentColor,
     )
 
     @Composable
     fun outlinedButtonColors(
-        backgroundColor: Color = Color.Transparent,
-        contentColor: Color = Color.Transparent,
-        disabledContentColor: Color = DmsTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
-    ): ButtonColors = DefaultButtonColors(
-        backgroundColor = backgroundColor,
+        containerColor: Color = Color.Transparent,
+        contentColor: Color = DmsTheme.colors.primary,
+        disabledContainerColor: Color = containerColor,
+        disabledContentColor: Color = contentColor.copy(alpha = DisabledTextOpacity),
+    ): ButtonColors = ButtonColors(
+        containerColor = containerColor,
         contentColor = contentColor,
-        disabledBackgroundColor = backgroundColor,
+        disabledContainerColor = disabledContainerColor,
         disabledContentColor = disabledContentColor,
     )
 
     @Composable
     fun textButtonColors(
-        backgroundColor: Color = Color.Transparent,
+        containerColor: Color = Color.Transparent,
         contentColor: Color = DmsTheme.colors.primary,
-        disabledContentColor: Color = DmsTheme.colors.onSurface
-            .copy(alpha = ContentAlpha.disabled),
-    ): ButtonColors = DefaultButtonColors(
-        backgroundColor = backgroundColor,
+        disabledContainerColor: Color = containerColor,
+        disabledContentColor: Color = contentColor.copy(alpha = DisabledTextOpacity),
+    ): ButtonColors = ButtonColors(
+        containerColor = containerColor,
         contentColor = contentColor,
-        disabledBackgroundColor = backgroundColor,
+        disabledContainerColor = disabledContainerColor,
         disabledContentColor = disabledContentColor,
     )
 
-    //TODO
     @Composable
     fun roundedButtonColors(
-        backgroundColor: Color = Color.Transparent,
-        contentColor: Color = DmsTheme.colors.primary,
-        disabledContentColor: Color = DmsTheme.colors.onSurface
-            .copy(alpha = ContentAlpha.disabled),
-    ): ButtonColors = DefaultButtonColors(
-        backgroundColor = backgroundColor,
+        containerColor: Color = DmsTheme.colors.primaryContainer,
+        contentColor: Color = DmsTheme.colors.onPrimaryContainer,
+        disabledContainerColor: Color = containerColor.copy(alpha = DisabledContainerOpacity),
+        disabledContentColor: Color = contentColor.copy(alpha = DisabledContentOpacity),
+    ): ButtonColors = ButtonColors(
+        containerColor = containerColor,
         contentColor = contentColor,
-        disabledBackgroundColor = backgroundColor,
-        disabledContentColor = disabledContentColor
+        disabledContainerColor = disabledContainerColor,
+        disabledContentColor = disabledContentColor,
     )
-
-    const val OutlinedBorderOpacity = 1f
-    val OutlinedBorderSize = 1.dp
-
-    val outlinedBorder: BorderStroke
-        @Composable
-        get() = BorderStroke(
-            width = OutlinedBorderSize,
-            color = DmsTheme.colors.primary.copy(alpha = OutlinedBorderOpacity),
-        )
-}
-
-@Immutable
-private class DefaultButtonColors(
-    private val backgroundColor: Color,
-    private val contentColor: Color,
-    private val disabledBackgroundColor: Color,
-    private val disabledContentColor: Color,
-) : ButtonColors {
-
-    @Composable
-    override fun backgroundColor(enabled: Boolean): State<Color> =
-        rememberUpdatedState(if (enabled) backgroundColor else disabledBackgroundColor)
-
-    @Composable
-    override fun contentColor(enabled: Boolean): State<Color> =
-        rememberUpdatedState(if (enabled) contentColor else disabledContentColor)
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-        if (other == null || this::class != other::class) {
-            return false
-        }
-
-        other as DefaultButtonColors
-
-        if (backgroundColor != other.backgroundColor) {
-            return false
-        }
-        if (contentColor != other.contentColor) {
-            return false
-        }
-        if (disabledBackgroundColor != other.disabledBackgroundColor) {
-            return false
-        }
-        if (disabledContentColor != other.disabledContentColor) {
-            return false
-        }
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = backgroundColor.hashCode()
-        result = 31 * result + contentColor.hashCode()
-        result = 31 * result + disabledBackgroundColor.hashCode()
-        result = 31 * result + disabledContentColor.hashCode()
-        return result
-    }
 }
