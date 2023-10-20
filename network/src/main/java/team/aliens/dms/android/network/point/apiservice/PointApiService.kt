12b9280715f638cc7a04c056228d7ca2b2e5f6c7
junsuPark <krpjs0508@gmail.com>
2023-10-20 20:24:31 +0900

    ♻️ :: point interface 변경

--- network/src/main/java/team/aliens/dms/android/network/point/apiservice/PointApiService.kt
+++ network/src/main/java/team/aliens/dms/android/network/point/apiservice/PointApiService.kt
@@ -5,11 +5,11 @@ import retrofit2.http.Query
 import team.aliens.dms.android.core.jwt.RequiresAccessToken
 import team.aliens.dms.android.network.point.model.FetchPointsResponse
 
-internal abstract class PointApiService {
+internal interface PointApiService {
 
     @GET("points")
     @RequiresAccessToken
-    abstract suspend fun fetchPoints(
+    suspend fun fetchPoints(
         @Query("type") type: String,
         @Query("page") page: Long?,
         @Query("size") size: Long?,
