e0f2f69a7b9cdff066af2b1453d8021db03629c5
junsuPark <krpjs0508@gmail.com>
2023-10-20 19:23:29 +0900

    ♻️ :: user inputs

--- feature/src/main/java/team/aliens/dms/android/feature/signin/SignInScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signin/SignInScreen.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.android.feature.signin
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,20 +11,27 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import team.aliens.dms.android.core.designsystem.Checkbox
 import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.core.designsystem.TextField
 import team.aliens.dms.android.core.designsystem.typography.Body2
+import team.aliens.dms.android.core.ui.DefaultVerticalSpace
 import team.aliens.dms.android.core.ui.composable.AppLogo
+import team.aliens.dms.android.core.ui.composable.PasswordTextField
+import team.aliens.dms.android.core.ui.horizontalPadding
 import team.aliens.dms.android.feature.R
 import team.aliens.dms.android.feature.signin.navigation.SignInNavigator
 
@@ -70,27 +78,62 @@ private fun Banner(
 @Composable
 private fun UserInformationInputs(
     modifier: Modifier = Modifier,
-    accountIdValue: String,
-    onAccountIdChange: (String) -> Unit,
-    passwordValue: String,
+    id: String,
+    idChange: (String) -> Unit,
+    password: String,
     onPasswordChange: (String) -> Unit,
     autoSignIn: Boolean,
     onAutoSignInChange: (Boolean) -> Unit,
     idError: Exception? = null,
     passwordError: Exception? = null,
 ) {
+    val (passwordShowing, onPasswordShowingChange) = remember { mutableStateOf(false) }
+
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(DefaultVerticalSpace),
     ) {
         TextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            value = accountIdValue,
-            onValueChange = onAccountIdChange,
+                .horizontalPadding(),
+            value = id,
+            onValueChange = idChange,
+            hint = { Text(text = "아이디") },
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
         )
+
+        PasswordTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalPadding(),
+            value = password,
+            onValueChange = onPasswordChange,
+            hintText = "비밀번호",
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done,
+            ),
+            passwordShowing = passwordShowing,
+            onPasswordShowingChange = onPasswordShowingChange,
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalPadding(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Checkbox(
+                checked = autoSignIn,
+                onCheckedChange = onAutoSignInChange,
+            )
+            Text(
+                text = "자동 로그인",
+                style = DmsTheme.typography.body2,
+                color = DmsTheme.colorScheme.onSurfaceVariant,
+            )
+        }
     }
 }
 
@@ -101,21 +144,26 @@ private fun SignInPreview() {
     val (password, onPasswordChange) = remember { mutableStateOf("") }
     val (autoSignIn, onAutoSignInChange) = remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier
-            .background(DmsTheme.colorScheme.background)
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        UserInformationInputs(
-            accountIdValue = id,
-            passwordValue = password,
-            onAccountIdChange = onIdChange,
-            onPasswordChange = onPasswordChange,
-            autoSignIn = autoSignIn,
-            onAutoSignInChange = onAutoSignInChange,
-        )
+    Scaffold { padValues ->
+        Column(
+            modifier = Modifier
+                .background(DmsTheme.colorScheme.background)
+                .fillMaxSize()
+                .padding(padValues)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(DefaultVerticalSpace),
+        ) {
+            Banner(Modifier.fillMaxWidth())
+            UserInformationInputs(
+                id = id,
+                password = password,
+                idChange = onIdChange,
+                onPasswordChange = onPasswordChange,
+                autoSignIn = autoSignIn,
+                onAutoSignInChange = onAutoSignInChange,
+            )
+            //UserInformationInputs(modifier = Modifier.fillMaxWidth())
+        }
     }
 }
 
