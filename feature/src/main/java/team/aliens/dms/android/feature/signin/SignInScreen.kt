97e45bd095b41a8da8dd6bd9d488f2cfcdd20110
junsuPark <krpjs0508@gmail.com>
2023-10-20 12:13:39 +0900

    ♻️ :: user input id

--- feature/src/main/java/team/aliens/dms/android/feature/signin/SignInScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signin/SignInScreen.kt
@@ -1,17 +1,27 @@
 package team.aliens.dms.android.feature.signin
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import team.aliens.dms.android.core.designsystem.DmsTheme
+import team.aliens.dms.android.core.designsystem.TextField
 import team.aliens.dms.android.core.designsystem.typography.Body2
 import team.aliens.dms.android.core.ui.composable.AppLogo
 import team.aliens.dms.android.feature.R
@@ -33,7 +43,8 @@ internal fun SignInScreen(
                 .fillMaxSize()
                 .padding(padValues),
         ) {
-            Banner(modifier.fillMaxWidth())
+            Banner(Modifier.fillMaxWidth())
+            //UserInformationInputs(modifier = Modifier.fillMaxWidth())
         }
     }
 }
@@ -43,16 +54,14 @@ private fun Banner(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         AppLogo(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
+            modifier = Modifier.padding(start = 16.dp),
         )
         Body2(
-            modifier = Modifier.padding(horizontal = 16.dp),
+            modifier = Modifier.padding(start = 16.dp),
             text = stringResource(R.string.app_description)
         )
     }
@@ -60,70 +69,57 @@ private fun Banner(
 
 @Composable
 private fun UserInformationInputs(
+    modifier: Modifier = Modifier,
     accountIdValue: String,
-    passwordValue: String,
-    autoSignInValue: Boolean,
     onAccountIdChange: (String) -> Unit,
+    passwordValue: String,
     onPasswordChange: (String) -> Unit,
-    onAutoSignInOptionChanged: (Boolean) -> Unit,
-    idError: Boolean,
-    passwordError: Boolean,
-) {/*
-    val focusManager = LocalFocusManager.current
+    autoSignIn: Boolean,
+    onAutoSignInChange: (Boolean) -> Unit,
+    idError: Exception? = null,
+    passwordError: Exception? = null,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            value = accountIdValue,
+            onValueChange = onAccountIdChange,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        )
+    }
+}
 
-    // 아이디
-    DormTextField(
-        modifier = Modifier.padding(
-            horizontal = 16.dp
-        ),
-        value = accountIdValue,
-        onValueChange = onAccountIdChange,
-        hint = stringResource(R.string.id),
-        keyboardOptions = KeyboardOptions(
-            imeAction = ImeAction.Next,
-        ),
-        keyboardActions = KeyboardActions(
-            onNext = {
-                focusManager.moveFocus(FocusDirection.Next)
-            },
-        ),
-        error = idError,
-        *//*
-                errorDescription = idErrorMessage,*//*
-    )
-    Spacer(Modifier.height(32.dp))
-    // 비밀번호
-    DormTextField(
-        modifier = Modifier.padding(
-            horizontal = 16.dp
-        ),
-        value = passwordValue,
-        onValueChange = onPasswordChange,
-        isPassword = true,
-        hint = stringResource(R.string.password),
-        keyboardOptions = KeyboardOptions(
-            imeAction = ImeAction.Done,
-        ),
-        keyboardActions = KeyboardActions(
-            onDone = {
-                focusManager.clearFocus()
-            },
-        ),
-        error = passwordError,
-        *//*
-                errorDescription = passwordErrorMessage,*//*
-    )
-    Spacer(Modifier.height(28.dp))
-    DormTextCheckBox(
-        modifier = Modifier.padding(
-            start = 12.dp,
-        ),
-        text = stringResource(R.string.sign_in_auto_sign_in),
-        checked = autoSignInValue,
-        onCheckedChange = onAutoSignInOptionChanged,
-    )
-    Spacer(Modifier.height(28.dp))*/
-}/*
+@Preview(showSystemUi = true)
+@Composable
+private fun SignInPreview() {
+    val (id, onIdChange) = remember { mutableStateOf("") }
+    val (password, onPasswordChange) = remember { mutableStateOf("") }
+    val (autoSignIn, onAutoSignInChange) = remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .background(DmsTheme.colorScheme.background)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        UserInformationInputs(
+            accountIdValue = id,
+            passwordValue = password,
+            onAccountIdChange = onIdChange,
+            onPasswordChange = onPasswordChange,
+            autoSignIn = autoSignIn,
+            onAutoSignInChange = onAutoSignInChange,
+        )
+    }
+}
+
+/*
 
 @Composable
 private fun AuthActions(
