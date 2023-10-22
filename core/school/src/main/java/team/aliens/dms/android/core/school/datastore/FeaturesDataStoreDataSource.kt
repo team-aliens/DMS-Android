ec15a19cb397a57a07c86b4443846bd2f44a75da
junsuPark <krpjs0508@gmail.com>
2023-10-23 08:40:43 +0900

    ♻️ :: features datasource 구현

--- core/school/src/main/java/team/aliens/dms/android/core/school/datastore/FeaturesDataStoreDataSource.kt
+++ core/school/src/main/java/team/aliens/dms/android/core/school/datastore/FeaturesDataStoreDataSource.kt
@@ -1,3 +1,10 @@
 package team.aliens.dms.android.core.school.datastore
 
-internal abstract class FeaturesDataStoreDataSource
+import team.aliens.dms.android.core.school.Features
+
+internal abstract class FeaturesDataStoreDataSource {
+
+    abstract fun loadFeatures(): Features
+
+    abstract suspend fun storeFeatures(features: Features)
+}
--- core/school/src/main/java/team/aliens/dms/android/core/school/datastore/FeaturesDataStoreDataSourceImpl.kt
+++ core/school/src/main/java/team/aliens/dms/android/core/school/datastore/FeaturesDataStoreDataSourceImpl.kt
@@ -1,3 +1,15 @@
 package team.aliens.dms.android.core.school.datastore
 
-internal class FeaturesDataStoreDataSourceImpl : FeaturesDataStoreDataSource()
+import team.aliens.dms.android.core.school.Features
+import team.aliens.dms.android.core.school.datastore.store.FeaturesStore
+import javax.inject.Inject
+
+internal class FeaturesDataStoreDataSourceImpl @Inject constructor(
+    private val featuresStore: FeaturesStore,
+) : FeaturesDataStoreDataSource() {
+    override fun loadFeatures(): Features = featuresStore.loadFeatures()
+
+    override suspend fun storeFeatures(features: Features) {
+        featuresStore.storeFeatures(features)
+    }
+}
