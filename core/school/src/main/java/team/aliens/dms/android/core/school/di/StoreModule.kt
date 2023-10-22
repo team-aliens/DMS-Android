d03e499cd32b3d14695808f8b82cd05521e1a35b
junsuPark <krpjs0508@gmail.com>
2023-10-23 08:28:21 +0900

    ♻️ :: store module 네이밍 변경

--- core/school/src/main/java/team/aliens/dms/android/core/school/di/StoreModule.kt
+++ core/school/src/main/java/team/aliens/dms/android/core/school/di/StoreModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal abstract class DataSourceModule {
+internal abstract class StoreModule {
 
     @Binds
     @Singleton
