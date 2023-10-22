4168e31c472ba31d1566232467aa49a9bcc37160
junsuPark <krpjs0508@gmail.com>
2023-10-23 07:42:21 +0900

    ♻️ :: auth mapper

--- data/src/main/java/team/aliens/dms/android/data/auth/mapper/AuthMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/auth/mapper/AuthMapper.kt
@@ -1,14 +1,21 @@
 package team.aliens.dms.android.data.auth.mapper
 
 import team.aliens.dms.android.core.jwt.Tokens
+import team.aliens.dms.android.core.school.Features
 import team.aliens.dms.android.network.auth.model.SignInResponse
 import team.aliens.dms.android.shared.date.toLocalDateTime
 
 fun SignInResponse.extractTokens(): Tokens = Tokens(
-    accessToken = accessToken,
-    accessTokenExpiration = accessTokenExpiration.toLocalDateTime(),
-    refreshToken = refreshToken,
-    refreshTokenExpiration = refreshTokenExpiration.toLocalDateTime(),
+    accessToken = this.accessToken,
+    accessTokenExpiration = this.accessTokenExpiration.toLocalDateTime(),
+    refreshToken = this.refreshToken,
+    refreshTokenExpiration = this.refreshTokenExpiration.toLocalDateTime(),
 )
 
-// TODO: fun SignInResponse.extractFeatures():Features
+fun SignInResponse.extractFeatures(): Features = Features(
+    mealService = this.features.mealService,
+    noticeService = this.features.noticeService,
+    pointService = this.features.pointService,
+    studyRoomService = this.features.studyRoomService,
+    remainsService = this.features.remainsService,
+)
