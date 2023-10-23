36cfe6d61d4b2889d3cfe3192414d7424c1dcb68
junsuPark <krpjs0508@gmail.com>
2023-10-23 13:00:51 +0900

    ♻️ :: authorized nav graph

--- app/src/main/java/team/aliens/dms/android/app/navigation/authorized/AuthorizedNavGraph.kt
+++ app/src/main/java/team/aliens/dms/android/app/navigation/authorized/AuthorizedNavGraph.kt
@@ -4,6 +4,7 @@ import com.ramcosta.composedestinations.dynamic.routedIn
 import com.ramcosta.composedestinations.spec.DestinationSpec
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import com.ramcosta.composedestinations.spec.Route
+import team.aliens.dms.android.feature.destinations.MainDestination
 import team.aliens.dms.android.feature.destinations.NoticeDetailsScreenDestination
 import team.aliens.dms.android.feature.destinations.NotificationBoxScreenDestination
 import team.aliens.dms.android.feature.destinations.PointHistoryScreenDestination
@@ -14,16 +15,16 @@ import team.aliens.dms.android.feature.studyroom.navigation.StudyRoomNavGraph
 
 object AuthorizedNavGraph : NavGraphSpec {
     override val route: String = "authorized"
-    override val startRoute: Route = MainNavGraph
+    override val startRoute: Route = MainDestination routedIn this
     override val nestedNavGraphs: List<NavGraphSpec> =
         listOf(
-            MainNavGraph,
             EditPasswordNavGraph,
             StudyRoomNavGraph,
         )
 
     override val destinationsByRoute: Map<String, DestinationSpec<*>> =
         listOf<DestinationSpec<*>>(
+            MainDestination,
             NoticeDetailsScreenDestination,
             NoticeDetailsScreenDestination,
             NotificationBoxScreenDestination,
