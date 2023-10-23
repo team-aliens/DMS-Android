381076ff227b6722397ccbaa378c9871085141fa
junsuPark <krpjs0508@gmail.com>
2023-10-23 19:50:32 +0900

    ♻️ :: dms nav animations

--- app/src/main/java/team/aliens/dms/android/app/navigation/DmsNavHost.kt
+++ app/src/main/java/team/aliens/dms/android/app/navigation/DmsNavHost.kt
@@ -1,16 +1,24 @@
 package team.aliens.dms.android.app.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.NestedNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.navigation.dependency
 import com.ramcosta.composedestinations.scope.DestinationScopeWithNoDependencies
 import com.ramcosta.composedestinations.spec.NavGraphSpec
+import team.aliens.dms.android.app.navigation.authorized.AuthorizedNavGraph
+import team.aliens.dms.android.app.navigation.unauthorized.UnauthorizedNavGraph
 
 @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class)
 @Composable
@@ -18,7 +26,33 @@ fun DmsNavHost(
     modifier: Modifier = Modifier,
     autoSignIn: Boolean,
 ) {
-    val engine = rememberAnimatedNavHostEngine()
+    val engine = rememberAnimatedNavHostEngine(
+        rootDefaultAnimations = RootNavGraphDefaultAnimations.ACCOMPANIST_FADING,
+        defaultAnimationsForNestedNavGraph = mapOf(
+            AuthorizedNavGraph to NestedNavGraphDefaultAnimations(
+                enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) },
+                exitTransition = { fadeOut(tween(delayMillis = 10)) },
+                popEnterTransition = { EnterTransition.None },
+                popExitTransition = {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End,
+                        animationSpec = tween(delayMillis = 10),
+                    )
+                },
+            ),
+            UnauthorizedNavGraph to NestedNavGraphDefaultAnimations(
+                enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) },
+                exitTransition = { fadeOut(tween(delayMillis = 10)) },
+                popEnterTransition = { EnterTransition.None },
+                popExitTransition = {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End,
+                        animationSpec = tween(delayMillis = 10),
+                    )
+                },
+            )
+        ),
+    )
     val navController = engine.rememberNavController()
 
     DestinationsNavHost(
