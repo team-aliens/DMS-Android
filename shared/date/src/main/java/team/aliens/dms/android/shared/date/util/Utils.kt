52008e5742aec590f42c5c20ca230489655cce1e
junsuPark <krpjs0508@gmail.com>
2023-10-23 21:35:00 +0900

    ♻️ :: date util

--- shared/date/src/main/java/team/aliens/dms/android/shared/date/util/Utils.kt
+++ shared/date/src/main/java/team/aliens/dms/android/shared/date/util/Utils.kt
@@ -2,9 +2,21 @@ package team.aliens.dms.android.shared.date.util
 
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 val today: LocalDate
     inline get() = LocalDate.now()
 
 val now: LocalDateTime
     inline get() = LocalDateTime.now()
+
+fun dateOf(
+    year: Int,
+    month: Int,
+    dayOfMonth: Int,
+): Date {
+    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    return sdf.parse("$year-${month + 1}-$dayOfMonth")!!
+}
