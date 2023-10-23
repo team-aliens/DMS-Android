0be8054835c31938cdd2d06d9a9ec79c55107d23
junsuPark <krpjs0508@gmail.com>
2023-10-23 22:44:04 +0900

    ♻️ :: floating notice

--- core/ui/src/main/java/team/aliens/dms/android/core/ui/composable/FloatingNotice.kt
+++ core/ui/src/main/java/team/aliens/dms/android/core/ui/composable/FloatingNotice.kt
@@ -1,59 +1,51 @@
 package team.aliens.dms.android.core.ui.composable
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import team.aliens.dms.android.core.designsystem.DmsTheme
+import team.aliens.dms.android.core.ui.PaddingDefaults
+import team.aliens.dms.android.core.ui.horizontalPadding
+import team.aliens.dms.android.core.ui.verticalPadding
 
 @Composable
 fun FloatingNotice(
     modifier: Modifier = Modifier,
     text: String,
 ) {
-    Box(
-        contentAlignment = Alignment.CenterEnd,
+    Row(
         modifier = modifier
             .fillMaxWidth()
-            .defaultMinSize(
-                minHeight = 50.dp,
-            )
-            .background(
-                color = DmsTheme.colorScheme.surface,
-                shape = RoundedCornerShape(100),
-            ),
+            .clip(DmsTheme.shapes.circle)
+            .background(DmsTheme.colorScheme.surface)
+            .verticalPadding()
+            .horizontalPadding(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
+        Icon(
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = team.aliens.dms.android.core.designsystem.R.drawable.ic_notice),
+            tint = DmsTheme.colorScheme.primary,
+            contentDescription = null,
+        )
+        Text(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    vertical = 4.dp,
-                    horizontal = 16.dp,
-                ),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Image(
-                modifier = Modifier.size(26.dp),
-                painter = painterResource(id = team.aliens.dms.android.core.designsystem.R.drawable.ic_notice),
-                contentDescription = "NoticeIcon",
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(text = text)
-        }
+                .weight(1f)
+                .verticalPadding(PaddingDefaults.ExtraSmall),
+            text = text,
+            style = DmsTheme.typography.body2,
+            color = DmsTheme.colorScheme.onSurface,
+        )
     }
 }
