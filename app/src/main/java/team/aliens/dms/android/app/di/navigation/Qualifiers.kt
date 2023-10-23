0028e6accf7c1c5d8f2b8ac2e8c1a62c9039e666
junsuPark <krpjs0508@gmail.com>
2023-10-23 12:51:08 +0900

    ♻️ :: nav graph module 제거

--- app/src/main/java/team/aliens/dms/android/app/di/navigation/Qualifiers.kt
+++
@@ -1,9 +0,0 @@
-package team.aliens.dms.android.app.di.navigation
-
-import javax.inject.Qualifier
-
-// TODO: 추후 확인 필요
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.BINARY)
-annotation class RootNavGraph
