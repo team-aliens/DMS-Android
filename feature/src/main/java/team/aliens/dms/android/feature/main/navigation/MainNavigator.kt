20f4b31ec90d282ac56e0901c09aa882fd4e6fed
junsuPark <krpjs0508@gmail.com>
2023-10-23 19:10:18 +0900

    ♻️ :: Main Navigator 추가

---
+++ feature/src/main/java/team/aliens/dms/android/feature/main/navigation/MainNavigator.kt
@@ -0,0 +1,12 @@
+package team.aliens.dms.android.feature.main.navigation
+
+interface MainNavigator {
+
+    fun openHome()
+
+    fun openApplication()
+
+    fun openAnnouncementList()
+
+    fun openMyPage()
+}
