9b278652a02fe5eef016f3b9f276518ad97c54f1
junsuPark <krpjs0508@gmail.com>
2023-10-23 13:18:56 +0900

    ♻️ :: application navigator

--- feature/src/main/java/team/aliens/dms/android/feature/main/announcement/navigation/AnnouncementNavigator.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/main/announcement/navigation/AnnouncementNavigator.kt
@@ -2,7 +2,7 @@ package team.aliens.dms.android.feature.main.announcement.navigation
 
 import java.util.UUID
 
-interface AnnouncementNavigator {
+internal interface AnnouncementNavigator {
 
     fun openNoticeDetails(noticeId: UUID)
 }
---
+++ feature/src/main/java/team/aliens/dms/android/feature/main/application/navigation/ApplicationNavigator.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms.android.feature.main.application.navigation
+
+internal interface ApplicationNavigator {
+
+    fun openStudyRoomList()
+
+    fun openRemainsApplication()
+}
