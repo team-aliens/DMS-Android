bb14772b763a4717000bf38a1addd5a6c0101851
junsuPark <krpjs0508@gmail.com>
2023-10-23 19:10:35 +0900

    ♻️ :: Main Navigator

--- app/src/main/java/team/aliens/dms/android/app/navigation/authorized/AuthorizedNavigator.kt
+++ app/src/main/java/team/aliens/dms/android/app/navigation/authorized/AuthorizedNavigator.kt
@@ -2,6 +2,7 @@ package team.aliens.dms.android.app.navigation.authorized
 
 import team.aliens.dms.android.feature.editpassword.navigation.EditPasswordNavigator
 import team.aliens.dms.android.feature.editprofile.navigation.EditProfileNavigator
+import team.aliens.dms.android.feature.main.navigation.MainNavigator
 import team.aliens.dms.android.feature.notice.navigation.NoticeNavigator
 import team.aliens.dms.android.feature.notification.navigation.NotificationNavigation
 import team.aliens.dms.android.feature.point.navigation.PointHistoryNavigator
@@ -9,6 +10,7 @@ import team.aliens.dms.android.feature.remains.navigator.RemainsNavigator
 import team.aliens.dms.android.feature.studyroom.navigation.StudyRoomNavigator
 
 interface AuthorizedNavigator :
+    MainNavigator,
     EditPasswordNavigator,
     EditProfileNavigator,
     NoticeNavigator,
