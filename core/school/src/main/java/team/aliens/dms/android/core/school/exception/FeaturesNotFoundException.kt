c2c45b38b28499ce2afa4423ee1dae4209bb8e9e
junsuPark <krpjs0508@gmail.com>
2023-10-23 11:08:27 +0900

    ♻️ :: features not found

--- core/school/src/main/java/team/aliens/dms/android/core/school/exception/FeaturesNotFoundException.kt
+++ core/school/src/main/java/team/aliens/dms/android/core/school/exception/FeaturesNotFoundException.kt
@@ -2,4 +2,4 @@ package team.aliens.dms.android.core.school.exception
 
 import team.aliens.dms.android.core.datastore.exception.LoadFailureException
 
-sealed class FeaturesNotFoundException(message: String?) : LoadFailureException(message)
+class FeaturesNotFoundException : LoadFailureException("Features not found")
