6679447d2b0af0ca209ef8655f2bdb28625fef83
junsuPark <krpjs0508@gmail.com>
2023-10-24 12:36:48 +0900

    ♻️ :: meal type

--- data/src/main/java/team/aliens/dms/android/data/meal/model/Meal.kt
+++ data/src/main/java/team/aliens/dms/android/data/meal/model/Meal.kt
@@ -5,9 +5,9 @@ import org.threeten.bp.LocalDate
 data class Meal(
     val date: LocalDate,
     val breakfast: List<String>,
-    val kcalOfBreakfast: String,
+    val kcalOfBreakfast: String?,
     val lunch: List<String>,
-    val kcalOfLunch: String,
+    val kcalOfLunch: String?,
     val dinner: List<String>,
-    val kcalOfDinner: String,
+    val kcalOfDinner: String?,
 )
