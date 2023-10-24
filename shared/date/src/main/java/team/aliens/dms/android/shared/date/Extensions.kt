1a58728ef56c2d29829eab9e7a2ab5e09a93b642
junsuPark <krpjs0508@gmail.com>
2023-10-24 12:24:47 +0900

    ♻️ :: date ext

--- shared/date/src/main/java/team/aliens/dms/android/shared/date/Extensions.kt
+++ shared/date/src/main/java/team/aliens/dms/android/shared/date/Extensions.kt
@@ -33,7 +33,7 @@ fun LocalDate.toDate(): Date = Calendar.getInstance().apply {
 fun LocalDateTime.toDate(): Date = Calendar.getInstance().apply {
     set(
         this@toDate.year,
-        this@toDate.monthValue - 1,
+        this@toDate.monthValue,
         this@toDate.dayOfMonth,
         this@toDate.hour,
         this@toDate.minute,
