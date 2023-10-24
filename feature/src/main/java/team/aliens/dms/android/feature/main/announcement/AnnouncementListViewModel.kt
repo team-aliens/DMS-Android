fe74ccedc88c54cafca04e1d14aef24254821d5a
junsuPark <krpjs0508@gmail.com>
2023-10-25 08:52:29 +0900

    ♻️ :: announcement viewmodel

--- feature/src/main/java/team/aliens/dms/android/feature/main/announcement/AnnouncementListViewModel.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/main/announcement/AnnouncementListViewModel.kt
@@ -1,4 +1,78 @@
 package team.aliens.dms.android.feature.main.announcement
+
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import team.aliens.dms.android.core.ui.mvi.BaseMviViewModel
+import team.aliens.dms.android.core.ui.mvi.Intent
+import team.aliens.dms.android.core.ui.mvi.SideEffect
+import team.aliens.dms.android.core.ui.mvi.UiState
+import team.aliens.dms.android.data.notice.model.Notice
+import team.aliens.dms.android.data.notice.repository.NoticeRepository
+import team.aliens.dms.android.shared.model.Order
+import javax.inject.Inject
+
+@HiltViewModel
+internal class AnnouncementListViewModel @Inject constructor(
+    private val noticeRepository: NoticeRepository,
+) : BaseMviViewModel<AnnouncementListUiState, AnnouncementIntent, AnnouncementSideEffect>(
+    initialState = AnnouncementListUiState.initial(),
+) {
+    private lateinit var noticesAscByDate: List<Notice>
+    private lateinit var noticesDescByDate: List<Notice>
+
+    init {
+        fetchNotices()
+    }
+
+    override fun processIntent(intent: AnnouncementIntent) {
+        when (intent) {
+            is AnnouncementIntent.UpdateOrder -> updateOrder(intent.order)
+        }
+    }
+
+    private fun fetchNotices() {
+        viewModelScope.launch(Dispatchers.IO) {
+            runCatching {
+                noticeRepository.fetchNotices(order = Order.NEW)
+            }.onSuccess { notices ->
+                noticesAscByDate = notices
+                noticesDescByDate = notices.reversed()
+                reduce(newState = stateFlow.value.copy(notices = notices))
+            }
+        }
+    }
+
+    private fun updateOrder(order: Order): Boolean = reduce(
+        newState = stateFlow.value.copy(
+            selectedOrder = order,
+            notices = when (order) {
+                Order.NEW -> this.noticesAscByDate
+                Order.OLD -> this.noticesDescByDate
+            },
+        ),
+    )
+}
+
+internal data class AnnouncementListUiState(
+    val selectedOrder: Order,
+    val notices: List<Notice>,
+) : UiState() {
+    companion object {
+        fun initial() = AnnouncementListUiState(
+            selectedOrder = Order.NEW,
+            notices = emptyList(),
+        )
+    }
+}
+
+internal sealed class AnnouncementIntent : Intent() {
+    class UpdateOrder(val order: Order) : AnnouncementIntent()
+}
+
+internal sealed class AnnouncementSideEffect : SideEffect()
+
 /*
 
 import androidx.lifecycle.viewModelScope
