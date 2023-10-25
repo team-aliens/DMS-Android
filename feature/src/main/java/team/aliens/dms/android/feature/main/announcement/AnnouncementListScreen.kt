4c95b165c5611c1fc936516b0ce16258ba270ace
junsuPark <krpjs0508@gmail.com>
2023-10-26 08:29:51 +0900

    ♻️ :: announcement list

--- feature/src/main/java/team/aliens/dms/android/feature/main/announcement/AnnouncementListScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/main/announcement/AnnouncementListScreen.kt
@@ -2,8 +2,10 @@ package team.aliens.dms.android.feature.main.announcement
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
@@ -123,6 +126,10 @@ private fun NoticeList(
                 notice = notice,
                 onClick = onNavigateToNoticeDetails,
             )
+        }
+        // TODO: measure navigation bar height
+        item {
+            Spacer(modifier = Modifier.height(84.dp))
         }
     }
 }
