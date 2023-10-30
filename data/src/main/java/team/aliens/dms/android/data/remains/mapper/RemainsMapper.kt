ff74e0db183f78a9b27670316b1b1590e002e2e9
junsuPark <krpjs0508@gmail.com>
2023-10-30 19:14:35 +0900

    ♻️ :: mapper operator

--- data/src/main/java/team/aliens/dms/android/data/remains/mapper/RemainsMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/remains/mapper/RemainsMapper.kt
@@ -3,7 +3,9 @@ package team.aliens.dms.android.data.remains.mapper
 import team.aliens.dms.android.data.remains.model.AppliedRemainsOption
 import team.aliens.dms.android.network.remains.model.FetchAppliedRemainsOptionResponse
 
-fun FetchAppliedRemainsOptionResponse.toModel(): AppliedRemainsOption = AppliedRemainsOption(
+internal fun FetchAppliedRemainsOptionResponse.toModel(): AppliedRemainsOption = AppliedRemainsOption(
     remainsOptionId = this.remainsOptionId,
     title = this.title,
 )
+
+
