8b6082e95af5312ad0b02dafc81e9001d83adc60
junsuPark <krpjs0508@gmail.com>
2023-10-30 17:23:41 +0900

    ♻️ :: notice viewmodel

--- feature/src/main/java/team/aliens/dms/android/feature/notice/NoticeDetailsViewModel.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/notice/NoticeDetailsViewModel.kt
@@ -1,4 +1,49 @@
 package team.aliens.dms.android.feature.notice
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.threeten.bp.LocalDate
+import team.aliens.dms.android.core.ui.mvi.BaseMviViewModel
+import team.aliens.dms.android.core.ui.mvi.Intent
+import team.aliens.dms.android.core.ui.mvi.SideEffect
+import team.aliens.dms.android.core.ui.mvi.UiState
+import team.aliens.dms.android.data.notice.repository.NoticeRepository
+import java.util.UUID
+import javax.inject.Inject
+
+@HiltViewModel
+internal class NoticeDetailsViewModel @Inject constructor(
+    private val noticeRepository: NoticeRepository,
+) : BaseMviViewModel<NoticeDetailsUiState, NoticeDetailsIntent, NoticeDetailsSideEffect>(
+    initialState = NoticeDetailsUiState.initial(),
+) {
+
+    override fun processIntent(intent: NoticeDetailsIntent) {
+        when (intent) {
+            is NoticeDetailsIntent.FetchNoticeDetails -> TODO()
+        }
+    }
+}
+
+internal data class NoticeDetailsUiState(
+    val title: String?,
+    val content: String?,
+    val createdAt: LocalDate?,
+) : UiState() {
+    companion object {
+        fun initial() = NoticeDetailsUiState(
+            title = null,
+            content = null,
+            createdAt = null,
+        )
+    }
+}
+
+internal sealed class NoticeDetailsIntent : Intent() {
+    class FetchNoticeDetails(val noticeId: UUID) : NoticeDetailsIntent()
+}
+
+internal sealed class NoticeDetailsSideEffect : SideEffect()
+
 /*
 
 import androidx.lifecycle.viewModelScope
