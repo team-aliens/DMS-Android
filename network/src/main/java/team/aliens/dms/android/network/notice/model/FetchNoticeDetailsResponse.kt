adf4e7aa74ae300b8a2d12c3b75438bf7abb7804
junsuPark <krpjs0508@gmail.com>
2023-10-30 17:33:17 +0900

    ♻️ :: notice response

--- network/src/main/java/team/aliens/dms/android/network/notice/model/FetchNoticeDetailsResponse.kt
+++ network/src/main/java/team/aliens/dms/android/network/notice/model/FetchNoticeDetailsResponse.kt
@@ -1,12 +1,11 @@
 package team.aliens.dms.android.network.notice.model
 
 import com.google.gson.annotations.SerializedName
-import org.threeten.bp.LocalDateTime
 import java.util.UUID
 
 data class FetchNoticeDetailsResponse(
     @SerializedName("id") val noticeId: UUID,
     @SerializedName("title") val title: String,
     @SerializedName("content") val content: String,
-    @SerializedName("created_at") val createdAt: LocalDateTime,
+    @SerializedName("created_at") val createdAt: String,
 )
