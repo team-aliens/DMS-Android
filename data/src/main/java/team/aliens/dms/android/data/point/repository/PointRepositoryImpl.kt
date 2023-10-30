63321504ebb08175bea68ad22cdfe9d058138dbf
junsuPark <krpjs0508@gmail.com>
2023-10-30 15:28:03 +0900

    ♻️ :: point repo

--- data/src/main/java/team/aliens/dms/android/data/point/repository/PointRepositoryImpl.kt
+++ data/src/main/java/team/aliens/dms/android/data/point/repository/PointRepositoryImpl.kt
@@ -1,18 +1,21 @@
 package team.aliens.dms.android.data.point.repository
 
+import team.aliens.dms.android.data.point.mapper.toModel
 import team.aliens.dms.android.data.point.model.PointStatus
-import team.aliens.dms.android.network.point.datasource.NetworkPointDataSource
 import team.aliens.dms.android.data.point.model.PointType
+import team.aliens.dms.android.network.point.datasource.NetworkPointDataSource
 import javax.inject.Inject
 
 internal class PointRepositoryImpl @Inject constructor(
     private val networkPointDataSource: NetworkPointDataSource,
 ) : PointRepository() {
-    override fun fetchPoints(
+    override suspend fun fetchPoints(
         type: PointType,
         page: Long?,
         size: Long?,
-    ): PointStatus {
-        TODO("Not yet implemented")
-    }
+    ): PointStatus = networkPointDataSource.fetchPoints(
+        type = type.name,
+        page = page,
+        size = size,
+    ).toModel()
 }
