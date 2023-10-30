bb9a309d2faa502b9dabe3e266c69db335603943
junsuPark <krpjs0508@gmail.com>
2023-10-30 17:12:46 +0900

    ♻️ :: notice top bar

--- feature/src/main/java/team/aliens/dms/android/feature/notice/NoticeDetailsScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/notice/NoticeDetailsScreen.kt
@@ -1,18 +1,47 @@
 package team.aliens.dms.android.feature.notice
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.ramcosta.composedestinations.annotation.Destination
+import team.aliens.dms.android.core.designsystem.DmsScaffold
+import team.aliens.dms.android.core.designsystem.DmsTopAppBar
+import team.aliens.dms.android.feature.R
 import team.aliens.dms.android.feature.notice.navigation.NoticeNavigator
 import java.util.UUID
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 internal fun NoticeDetailsScreen(
     modifier: Modifier = Modifier,
     navigator: NoticeNavigator,
     noticeId: UUID,
-) {/*
+) {
+    DmsScaffold(
+        modifier = modifier,
+        topBar = {
+            DmsTopAppBar(
+                title = { Text(text = stringResource(id = R.string.announcement)) },
+                navigationIcon = {
+                    IconButton(onClick = navigator::popBackStack) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
+                            contentDescription = stringResource(id = R.string.top_bar_back_button),
+                        )
+                    }
+                },
+            )
+        }
+    ) { padValues ->
+
+    }
+    /*
     val uiState by noticeDetailsViewModel.stateFlow.collectAsStateWithLifecycle()
 
     LaunchedEffect(Unit) {
