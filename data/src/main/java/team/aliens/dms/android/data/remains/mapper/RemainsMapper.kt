63c1a7d71a4aca8946332daafa8a3351cae750b3
junsuPark <krpjs0508@gmail.com>
2023-10-30 19:15:42 +0900

    ♻️ :: remains mapper

--- data/src/main/java/team/aliens/dms/android/data/remains/mapper/RemainsMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/remains/mapper/RemainsMapper.kt
@@ -1,11 +1,20 @@
 package team.aliens.dms.android.data.remains.mapper
 
 import team.aliens.dms.android.data.remains.model.AppliedRemainsOption
+import team.aliens.dms.android.data.remains.model.RemainsApplicationTime
 import team.aliens.dms.android.network.remains.model.FetchAppliedRemainsOptionResponse
+import team.aliens.dms.android.network.remains.model.FetchRemainsApplicationTimeResponse
 
-internal fun FetchAppliedRemainsOptionResponse.toModel(): AppliedRemainsOption = AppliedRemainsOption(
-    remainsOptionId = this.remainsOptionId,
-    title = this.title,
-)
+internal fun FetchAppliedRemainsOptionResponse.toModel(): AppliedRemainsOption =
+    AppliedRemainsOption(
+        remainsOptionId = this.remainsOptionId,
+        title = this.title,
+    )
 
-
+internal fun FetchRemainsApplicationTimeResponse.toModel(): RemainsApplicationTime =
+    RemainsApplicationTime(
+        startDayOfWeek = startDayOfWeek,
+        startTime = startTime,
+        endDayOfWeek = endDayOfWeek,
+        endTime = endTime,
+    )
