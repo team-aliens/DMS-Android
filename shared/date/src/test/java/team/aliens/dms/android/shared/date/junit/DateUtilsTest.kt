8ecd7248124f60d6fa8660e4b8a0c3d30c33faac
junsuPark <krpjs0508@gmail.com>
2024-01-08 22:31:38 +0900

    ♻️ :: date public property test

---
+++ shared/date/src/test/java/team/aliens/dms/android/shared/date/junit/DateUtilsTest.kt
@@ -0,0 +1,47 @@
+package team.aliens.dms.android.shared.date.junit
+
+import org.junit.Test
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import team.aliens.dms.android.shared.date.util.now
+import team.aliens.dms.android.shared.date.util.today
+
+class DateUtilsTest {
+
+    /*
+        private lateinit var mToday: LocalDate
+        private lateinit var mNow: LocalDateTime
+
+        @Before
+        fun setup() {
+            this.mToday = today
+            this.mNow = now
+        }
+    */
+
+    @Test
+    fun `Test Today`() {
+        val today = today
+        val testToday = LocalDate.now()
+
+        assert(today == testToday)
+        assert(today == testToday.plusDays(5).minusDays(5))
+        assert(today == testToday.minusDays(802).plusDays(401).plusDays(401))
+
+        assert(today != testToday.plusDays(5))
+        assert(today != testToday.minusDays(7))
+    }
+
+    @Test
+    fun `Test Now`() {
+        val now = now
+        val testNow = LocalDateTime.now()
+
+        assert(now == testNow)
+        assert(now == testNow.plusSeconds(5).minusSeconds(5))
+        assert(now == testNow.minusDays(504).plusDays(500).plusDays(4))
+
+        assert(now != testNow.plusNanos(1))
+        assert(now != testNow.minusNanos(1))
+    }
+}
