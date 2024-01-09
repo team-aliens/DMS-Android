2002e1668d6e76a3de41384c6b051d4de10fc569
junsuPark <krpjs0508@gmail.com>
2024-01-09 12:59:46 +0900

    ♻️ :: Extension Test 추가

--- shared/date/src/test/java/team/aliens/dms/android/shared/date/junit/ExtensionsTest.kt
+++ shared/date/src/test/java/team/aliens/dms/android/shared/date/junit/ExtensionsTest.kt
@@ -5,7 +5,10 @@ import org.junit.Test
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneOffset
+import team.aliens.dms.android.shared.date.toDate
 import team.aliens.dms.android.shared.date.toEpochMilli
+import team.aliens.dms.android.shared.date.toLocalDate
+import team.aliens.dms.android.shared.date.toLocalDateTime
 import java.util.Date
 
 class ExtensionsTest {
@@ -14,26 +17,65 @@ class ExtensionsTest {
     private lateinit var mLocalDateTime: LocalDateTime
     private lateinit var mLocalDate: LocalDate
     private lateinit var mDate: Date
+    private lateinit var mDateTime: Date
+    private lateinit var mZoneOffset: ZoneOffset
+    private lateinit var mIsoDateTimeFormat: String
+    private lateinit var mIsoDateFormat: String
 
     @Before
     fun setupDateMembers() {
-        this.mEpochMillis = 1147113418000L
-        this.mEpochSecond = 1147113418L
-        this.mLocalDateTime = LocalDateTime.of(2006, 5, 8, 18, 36, 58)
+        this.mEpochMillis = 1147113414000L
+        this.mEpochSecond = 1147046400L
+        this.mLocalDateTime = LocalDateTime.of(2006, 5, 8, 18, 36, 54)
         this.mLocalDate = LocalDate.of(2006, 5, 8)
         this.mDate = Date(2006, 5, 8)
+        this.mDateTime = Date(2006, 5, 8, 18, 36, 54)
+        this.mZoneOffset = ZoneOffset.UTC
+        this.mIsoDateTimeFormat = "2006-05-08T18:36:54"
+        this.mIsoDateFormat = "2006-05-08"
 
         check(mEpochMillis != -1L)
         check(mEpochSecond != -1L)
         check(::mLocalDateTime.isInitialized)
         check(::mLocalDate.isInitialized)
         check(::mDate.isInitialized)
+        check(::mZoneOffset.isInitialized)
+        check(::mIsoDateTimeFormat.isInitialized)
+        check(::mIsoDateFormat.isInitialized)
     }
 
     @Test
-    fun `Test LocalDateTime to Epoch Milliseconds`() {
-        val zone = ZoneOffset.UTC
+    fun `Test LocalDateTime to Epoch Millisecond`() {
+        assert(mLocalDateTime.toEpochMilli(mZoneOffset) == mEpochMillis)
+    }
 
-        assert(mLocalDateTime.toEpochMilli(zone) == mEpochMillis)
+    @Test
+    fun `Test EpochMilliseconds to LocalDateTime`() {
+        assert(mEpochMillis.toLocalDateTime(mZoneOffset) == mLocalDateTime)
+    }
+
+    @Test
+    fun `Test EpochDay to LocalDate`() {
+        assert(mEpochSecond.toLocalDate() == mLocalDate)
+    }
+
+    @Test
+    fun `Test String to LocalDateTime`() {
+        assert(mIsoDateTimeFormat.toLocalDateTime() == mLocalDateTime)
+    }
+
+    @Test
+    fun `Test String to LocalDate`() {
+        assert(mIsoDateFormat.toLocalDate() == mLocalDate)
+    }
+
+    @Test
+    fun `Test LocalDate to Date`() {
+        assert(mLocalDate.toDate() == mDate)
+    }
+
+    @Test
+    fun `Test LocalDateTime to Date`() {
+        assert(mLocalDateTime.toDate() == mDateTime)
     }
 }
