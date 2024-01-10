b1d0f5bc9eaad283d4eae3e1f8d26d651248860a
junsuPark <krpjs0508@gmail.com>
2024-01-10 23:31:34 +0900

    ♻️ :: sign in api service test 추가

--- network/src/androidTest/java/team/aliens/dms/android/network/auth/apiservice/AuthApiServiceTest.kt
+++ network/src/androidTest/java/team/aliens/dms/android/network/auth/apiservice/AuthApiServiceTest.kt
@@ -1,22 +1,25 @@
 package team.aliens.dms.android.network.auth.apiservice
 
 import android.content.Context
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.annotation.RawRes
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.gson.Gson
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import team.aliens.dms.android.network.auth.apiservice.AuthApiService
+import team.aliens.dms.android.network.auth.model.SignInRequest
 import team.aliens.dms.android.network.auth.model.SignInResponse
+import team.aliens.dms.android.network.test.R
 import java.io.BufferedReader
+import java.net.HttpURLConnection
 
 @HiltAndroidTest
 class AuthApiServiceTest {
@@ -29,33 +32,47 @@ class AuthApiServiceTest {
     private lateinit var authApiService: AuthApiService
     private lateinit var context: Context
 
+    val successResponse by lazy {
+        MockResponse().apply {
+            setResponseCode(HttpURLConnection.HTTP_OK)
+            setBody(context.readJsonFromRawResources(R.raw.sign_in_response))
+        }
+    }
+
     @Before
     fun setup() {
-/*        this.server = MockWebServer().also(MockWebServer::start)
-        this.retrofit = Retrofit.Builder()
-            .baseUrl(server.url("/"))
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        this.server = MockWebServer()
+        this.server.start()
+        this.retrofit = Retrofit.Builder().baseUrl(server.url("/"))
+            .addConverterFactory(GsonConverterFactory.create()).build()
         this.authApiService = retrofit.create(AuthApiService::class.java)
-        this.context = InstrumentationRegistry.getInstrumentation().context*/
+        this.context = InstrumentationRegistry.getInstrumentation().targetContext
     }
 
     @Test
-    fun testtest() {
-       /* Gson().fromJson(
-            context.readJsonFromAsset("sign_in_response.json"),
+    fun sign_in() {
+        this.server.enqueue(successResponse)
+
+        val request = SignInRequest(
+            accountId = "student",
+            password = "rhqmffls13!",
+        )
+        val response = Gson().fromJson(
+            context.readJsonFromRawResources(R.raw.sign_in_response),
             SignInResponse::class.java,
-        ).also { println(it) }
-*/
-        assert(true)
+        )
+        runBlocking {
+            val result = authApiService.signIn(request)
+            assert(result == response)
+        }
     }
 
     @After
     fun teardown() {
-      //    this.server.shutdown()
+        this.server.shutdown()
     }
 
     // TODO: read text
-    fun Context.readJsonFromAsset(filename: String): String =
-        this.assets.open(filename).bufferedReader().use(BufferedReader::readText)
+    fun Context.readJsonFromRawResources(@RawRes id: Int): String =
+        this.resources.openRawResource(id).bufferedReader().use(BufferedReader::readText)
 }
