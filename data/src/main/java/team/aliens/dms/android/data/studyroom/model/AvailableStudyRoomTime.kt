5c9b2c8a68ce617c45e8ed9ead964659b3802b72
junsuPark <krpjs0508@gmail.com>
2024-01-25 19:41:29 +0900

    ♻️ :: Available Study Room Time 이름 변경

--- data/src/main/java/team/aliens/dms/android/data/studyroom/model/AvailableStudyRoomTime.kt
+++ data/src/main/java/team/aliens/dms/android/data/studyroom/model/AvailableStudyRoomTime.kt
@@ -2,7 +2,7 @@ package team.aliens.dms.android.data.studyroom.model
 
 import java.util.UUID
 
-data class Timeslot(
+data class AvailableStudyRoomTime(
     val id: UUID,
     val startTime: String,
     val endTime: String,
