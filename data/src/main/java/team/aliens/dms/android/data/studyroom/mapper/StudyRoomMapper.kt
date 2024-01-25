2fa0ee07c5e4b0f42d4b5c82f3e68c58f2204c2f
junsuPark <krpjs0508@gmail.com>
2024-01-25 19:34:43 +0900

    ♻️ :: studyroom applied mapper 구현

--- data/src/main/java/team/aliens/dms/android/data/studyroom/mapper/StudyRoomMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/studyroom/mapper/StudyRoomMapper.kt
@@ -1,7 +1,14 @@
 package team.aliens.dms.android.data.studyroom.mapper
 
 import team.aliens.dms.android.data.studyroom.model.AppliedStudyRoom
+import team.aliens.dms.android.data.studyroom.model.StudyRoomApplicationTime
 import team.aliens.dms.android.network.studyroom.model.FetchAppliedStudyRoomResponse
+import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomApplicationTimeResponse
+
+fun FetchStudyRoomApplicationTimeResponse.toModel() : StudyRoomApplicationTime = StudyRoomApplicationTime(
+    startAt = this.startAt,
+    endAt = this.endAt,
+)
 
 fun FetchAppliedStudyRoomResponse.toModel(): AppliedStudyRoom = AppliedStudyRoom(
     floor = this.floor,
