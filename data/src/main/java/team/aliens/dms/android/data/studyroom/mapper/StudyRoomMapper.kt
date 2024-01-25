f253232abbc349209ce66f20a0d903096a74515a
junsuPark <krpjs0508@gmail.com>
2024-01-25 19:38:50 +0900

    ♻️ :: studyroomresponse mapper 구현

--- data/src/main/java/team/aliens/dms/android/data/studyroom/mapper/StudyRoomMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/studyroom/mapper/StudyRoomMapper.kt
@@ -1,13 +1,31 @@
 package team.aliens.dms.android.data.studyroom.mapper
 
 import team.aliens.dms.android.data.studyroom.model.AppliedStudyRoom
+import team.aliens.dms.android.data.studyroom.model.StudyRoom
 import team.aliens.dms.android.data.studyroom.model.StudyRoomApplicationTime
 import team.aliens.dms.android.network.studyroom.model.FetchAppliedStudyRoomResponse
 import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomApplicationTimeResponse
+import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomsResponse
+import team.aliens.dms.android.shared.model.Sex
 
-fun FetchStudyRoomApplicationTimeResponse.toModel() : StudyRoomApplicationTime = StudyRoomApplicationTime(
-    startAt = this.startAt,
-    endAt = this.endAt,
+fun FetchStudyRoomApplicationTimeResponse.toModel(): StudyRoomApplicationTime =
+    StudyRoomApplicationTime(
+        startAt = this.startAt,
+        endAt = this.endAt,
+    )
+
+fun FetchStudyRoomsResponse.toModel(): List<StudyRoom> =
+    this.studyRoomResponses.map(FetchStudyRoomsResponse.StudyRoomResponse::toModel)
+
+fun FetchStudyRoomsResponse.StudyRoomResponse.toModel(): StudyRoom = StudyRoom(
+    id = this.id,
+    floor = this.floor,
+    name = this.name,
+    availableGrade = this.availableGrade,
+    availableSex = Sex.valueOf(this.availableSex),
+    inUseHeadcount = this.inUseHeadcount,
+    totalAvailableSeat = this.totalAvailableSeat,
+    isMine = this.isMine,
 )
 
 fun FetchAppliedStudyRoomResponse.toModel(): AppliedStudyRoom = AppliedStudyRoom(
