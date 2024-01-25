62d029b7bd142f36223b036c4a7c1fefd08f82db
junsuPark <krpjs0508@gmail.com>
2024-01-25 19:43:44 +0900

    ♻️ :: study room time response

--- network/src/main/java/team/aliens/dms/android/network/studyroom/model/FetchAvailableStudyRoomTimesResponse.kt
+++ network/src/main/java/team/aliens/dms/android/network/studyroom/model/FetchAvailableStudyRoomTimesResponse.kt
@@ -4,9 +4,9 @@ import com.google.gson.annotations.SerializedName
 import java.util.UUID
 
 data class FetchAvailableStudyRoomTimesResponse(
-    @SerializedName("time_slots") val timeSlots: List<TimeSlotResponse>,
+    @SerializedName("time_slots") val availableStudyRoomTimes: List<AvailableStudyRoomTimeResponse>,
 ) {
-    data class TimeSlotResponse(
+    data class AvailableStudyRoomTimeResponse(
         @SerializedName("id") val id: UUID,
         @SerializedName("start_time") val startTime: String,
         @SerializedName("end_time") val endTime: String,
