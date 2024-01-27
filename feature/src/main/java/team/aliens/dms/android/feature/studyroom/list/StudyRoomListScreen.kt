46b7902da6136a9c39ce3ef5df267be640f21eba
junsuPark <krpjs0508@gmail.com>
2024-01-27 20:13:12 +0900

    ♻️ :: study room text

--- feature/src/main/java/team/aliens/dms/android/feature/studyroom/list/StudyRoomListScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/studyroom/list/StudyRoomListScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -168,6 +169,7 @@ private fun StudyRoomCard(
     }
 }
 
+@Stable
 private val Sex.text: String
     @Composable get() = stringResource(
         id = when (this) {
