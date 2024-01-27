7b758d3ca3b549dd6ff63aaf422252f386b2ab49
junsuPark <krpjs0508@gmail.com>
2024-01-27 21:24:44 +0900

    ♻️ :: study room list select

--- feature/src/main/java/team/aliens/dms/android/feature/studyroom/list/StudyRoomListScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/studyroom/list/StudyRoomListScreen.kt
@@ -3,35 +3,48 @@ package team.aliens.dms.android.feature.studyroom.list
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
-import team.aliens.dms.android.core.designsystem.DmsScaffold
+import kotlinx.coroutines.launch
+import team.aliens.dms.android.core.designsystem.Button
+import team.aliens.dms.android.core.designsystem.ButtonDefaults
+import team.aliens.dms.android.core.designsystem.DmsBottomSheetScaffold
 import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.core.designsystem.DmsTopAppBar
+import team.aliens.dms.android.core.designsystem.OutlinedButton
 import team.aliens.dms.android.core.designsystem.ShadowDefaults
 import team.aliens.dms.android.core.designsystem.layout.VerticallyFadedLazyColumn
 import team.aliens.dms.android.core.ui.DefaultHorizontalSpace
+import team.aliens.dms.android.core.ui.DefaultVerticalSpace
 import team.aliens.dms.android.core.ui.PaddingDefaults
 import team.aliens.dms.android.core.ui.composable.FloatingNotice
 import team.aliens.dms.android.core.ui.endPadding
@@ -56,16 +69,94 @@ internal fun StudyRoomListScreen(
     // TODO: val toastState = rememberToastState()
     val uiState by viewModel.stateFlow.collectAsStateWithLifecycle()
 
-    var shouldShowSelectedAvailableStudyRoomTimePickDialog by remember {
-        mutableStateOf(false)
+    val scope = rememberCoroutineScope()
+
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberStandardBottomSheetState(
+            skipHiddenState = false,
+            initialValue = SheetValue.Hidden,
+        ),
+    )
+
+    var newlySelectedAvailableStudyRoomTime by remember {
+        mutableStateOf<AvailableStudyRoomTime?>(null)
     }
 
-    if (shouldShowSelectedAvailableStudyRoomTimePickDialog) {
-
-    }
-
-    DmsScaffold(
+    DmsBottomSheetScaffold(
         modifier = modifier,
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(DefaultVerticalSpace),
+            ) {
+                Text(
+                    modifier = Modifier.startPadding(),
+                    text = stringResource(id = R.string.study_room_time),
+                    style = DmsTheme.typography.title2,
+                )
+                uiState.availableStudyRoomTimes?.let { availableStudyRoomTimes ->
+                    LazyRow(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(DefaultHorizontalSpace),
+                        contentPadding = PaddingDefaults.Horizontal,
+                    ) {
+                        items(availableStudyRoomTimes) { availableStudyRoomTime ->
+                            val selected =
+                                availableStudyRoomTime == uiState.selectedAvailableStudyRoomTime
+                            if (selected) {
+                                Button(
+                                    onClick = { /* explicit blank */ },
+                                ) {
+                                    Text(
+                                        text = stringResource(
+                                            id = R.string.format_study_room_available_study_room_time,
+                                            availableStudyRoomTime.startTime,
+                                            availableStudyRoomTime.endTime,
+                                        ),
+                                    )
+                                }
+                            } else {
+                                OutlinedButton(
+                                    onClick = {
+                                        newlySelectedAvailableStudyRoomTime = availableStudyRoomTime
+                                    },
+                                    colors = ButtonDefaults.outlinedGrayButtonColors(),
+                                ) {
+                                    Text(
+                                        text = stringResource(
+                                            id = R.string.format_study_room_available_study_room_time,
+                                            availableStudyRoomTime.startTime,
+                                            availableStudyRoomTime.endTime,
+                                        ),
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalPadding(),
+                    onClick = {
+                        if (newlySelectedAvailableStudyRoomTime != null) {
+                            if (newlySelectedAvailableStudyRoomTime != uiState.selectedAvailableStudyRoomTime) {
+                                viewModel.postIntent(
+                                    StudyRoomListIntent.UpdateSelectedAvailableStudyRoomTime(
+                                        availableStudyRoomTime = newlySelectedAvailableStudyRoomTime!!,
+                                    ),
+                                )
+                            }
+                        }
+                        scope.launch { scaffoldState.bottomSheetState.hide() }
+                    },
+                ) {
+                    Text(text = stringResource(id = R.string.accept))
+                }
+                Spacer(modifier = Modifier.height(52.dp))
+            }
+        },
         topBar = {
             DmsTopAppBar(
                 navigationIcon = {
@@ -105,13 +196,16 @@ internal fun StudyRoomListScreen(
                     .horizontalPadding(),
                 availableStudyRoomTime = uiState.selectedAvailableStudyRoomTime,
                 onFilterButtonClick = {
-
+                    scope.launch {
+                        scaffoldState.bottomSheetState.expand()
+                    }
                 },
             )
             uiState.studyRooms?.let { studyRooms ->
                 VerticallyFadedLazyColumn(
                     modifier = Modifier.fillMaxWidth(),
                 ) {
+                    // tODO: remove
                     repeat(5) {
                         items(studyRooms) { studyRoom ->
                             StudyRoomCard(
--- feature/src/main/java/team/aliens/dms/android/feature/studyroom/list/StudyRoomListViewModel.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/studyroom/list/StudyRoomListViewModel.kt
@@ -25,38 +25,58 @@ internal class StudyRoomListViewModel @Inject constructor(
         initStudyRooms()
     }
 
-    private fun fetchStudyRoomApplicationTime() {
-        viewModelScope.launch(Dispatchers.IO) {
-            runCatching {
-                studyRoomRepository.fetchStudyRoomApplicationTime()
-            }.onSuccess { fetchedStudyRoomApplicationTime ->
+    override fun processIntent(intent: StudyRoomListIntent) {
+        when (intent) {
+            is StudyRoomListIntent.UpdateSelectedAvailableStudyRoomTime -> updateSelectedAvailableStudyRoomTime(
+                availableStudyRoomTime = intent.availableStudyRoomTime
+            )
+        }
+    }
+
+    private fun fetchStudyRoomApplicationTime() = viewModelScope.launch(Dispatchers.IO) {
+        runCatching {
+            studyRoomRepository.fetchStudyRoomApplicationTime()
+        }.onSuccess { fetchedStudyRoomApplicationTime ->
+            reduce(
+                newState = stateFlow.value.copy(
+                    studyRoomApplicationTime = fetchedStudyRoomApplicationTime,
+                ),
+            )
+        }
+    }
+
+    // TODO 분리
+    private fun initStudyRooms() = viewModelScope.launch(Dispatchers.IO) {
+        runCatching {
+            studyRoomRepository.fetchAvailableStudyRoomTimes()
+        }.onSuccess { fetchedAvailableStudyRoomTimes ->
+            if (fetchedAvailableStudyRoomTimes.isNotEmpty()) {
+                val firstOfAvailableStudyRoomTime = fetchedAvailableStudyRoomTimes.firstOrNull()
+                val studyRooms =
+                    firstOfAvailableStudyRoomTime?.let { studyRoomRepository.fetchStudyRooms(it.id) }
                 reduce(
                     newState = stateFlow.value.copy(
-                        studyRoomApplicationTime = fetchedStudyRoomApplicationTime,
+                        availableStudyRoomTimes = fetchedAvailableStudyRoomTimes,
+                        selectedAvailableStudyRoomTime = firstOfAvailableStudyRoomTime,
+                        studyRooms = studyRooms,
                     ),
                 )
             }
         }
     }
 
-    private fun initStudyRooms() {
-        viewModelScope.launch(Dispatchers.IO) {
-            runCatching {
-                studyRoomRepository.fetchAvailableStudyRoomTimes()
-            }.onSuccess { fetchedAvailableStudyRoomTimes ->
-                if (fetchedAvailableStudyRoomTimes.isNotEmpty()) {
-                    val firstOfAvailableStudyRoomTime = fetchedAvailableStudyRoomTimes.firstOrNull()
-                    val studyRooms =
-                        firstOfAvailableStudyRoomTime?.let { studyRoomRepository.fetchStudyRooms(it.id) }
-                    reduce(
-                        newState = stateFlow.value.copy(
-                            availableStudyRoomTimes = fetchedAvailableStudyRoomTimes,
-                            selectedAvailableStudyRoomTime = firstOfAvailableStudyRoomTime,
-                            studyRooms = studyRooms,
-                        )
-                    )
-                }
-            }
+    private fun updateSelectedAvailableStudyRoomTime(
+        availableStudyRoomTime: AvailableStudyRoomTime,
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        runCatching {
+            studyRoomRepository.fetchStudyRooms(timeslot = availableStudyRoomTime.id)
+        }.onSuccess { fetchedStudyRooms ->
+            reduce(
+                newState = stateFlow.value.copy(
+                    studyRooms = fetchedStudyRooms,
+                    selectedAvailableStudyRoomTime = availableStudyRoomTime,
+                ),
+            )
         }
     }
 }
@@ -77,185 +97,9 @@ internal data class StudyRoomListUiState(
     }
 }
 
-internal sealed class StudyRoomListIntent : Intent()
+internal sealed class StudyRoomListIntent : Intent() {
+    class UpdateSelectedAvailableStudyRoomTime(val availableStudyRoomTime: AvailableStudyRoomTime) :
+        StudyRoomListIntent()
+}
 
 internal sealed class StudyRoomListSideEffect : SideEffect()
-
-/*
-
-import androidx.lifecycle.viewModelScope
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import team.aliens.dms.android.feature._legacy.base.BaseViewModel2
-import team.aliens.dms.android.feature._legacy.util.extractHourFromDate
-import team.aliens.dms.android.domain._legacy.exception.RemoteException
-import team.aliens.dms.android.domain.model.studyroom.FetchAvailableStudyRoomTimesOutput
-import team.aliens.dms.android.domain.model.studyroom.FetchStudyRoomsInput
-import team.aliens.dms.android.domain.model.studyroom.FetchStudyRoomsOutput
-import team.aliens.dms.android.domain.usecase.studyroom.FetchAvailableStudyRoomTimesUseCase
-import team.aliens.dms.android.domain.usecase.studyroom.FetchStudyRoomApplicationTimeUseCase
-import team.aliens.dms.android.domain.usecase.studyroom.FetchStudyRoomsUseCase
-import team.aliens.dms.android.feature._legacy.base.UiState
-import java.util.UUID
-import javax.inject.Inject
-
-@HiltViewModel
-class StudyRoomListViewModel @Inject constructor(
-    private val fetchStudyRoomsUseCase: FetchStudyRoomsUseCase,
-    private val studyRoomApplyTimeUseCase: FetchStudyRoomApplicationTimeUseCase,
-    private val studyRoomAvailableTimeListUseCase: FetchAvailableStudyRoomTimesUseCase,
-) : BaseViewModel2<StudyRoomListUiState, StudyRoomListViewModel.UiEvent>() {
-
-    init {
-        fetchApplyTime()
-    }
-
-    sealed class UiEvent : team.aliens.dms.android.feature._legacy.base.UiEvent {
-
-        data class FetchStudyRooms(
-            val timeSlot: UUID,
-        ) : UiEvent()
-
-        internal object FetchStudyRoomAvailableTimes : UiEvent()
-
-        internal class FilterStudyRoom(
-            studyRoomTime: Any?,
-        ) : UiEvent() // todo implement
-    }
-
-    override val _uiState = MutableStateFlow(StudyRoomListUiState())
-
-    override fun onEvent(
-        event: UiEvent,
-    ) {
-        when (event) {
-            is UiEvent.FetchStudyRooms -> {
-                fetchStudyRoomList(
-                    timeSlot = event.timeSlot,
-                )
-            }
-            is UiEvent.FetchStudyRoomAvailableTimes -> {
-                fetchStudyRoomAvailableTimeList()
-            }
-            is UiEvent.FilterStudyRoom -> {
-                // todo
-            }
-        }
-    }
-
-    private fun fetchApplyTime() {
-        viewModelScope.launch {
-
-            val result = kotlin.runCatching {
-                studyRoomApplyTimeUseCase()
-            }
-
-            if (result.isSuccess) {
-
-                val resultEntity = result.getOrThrow()
-
-                _uiState.value = _uiState.value.copy(
-                    startAt = resultEntity.startAt.extractHourFromDate(),
-                    endAt = resultEntity.endAt.extractHourFromDate(),
-                    hasApplyTime = true,
-                )
-            } else {
-                val exception = result.exceptionOrNull()
-
-                if (exception !is team.aliens.dms.android.domain._legacy.exception.RemoteException.NotFound) {
-
-                    emitErrorEventFromThrowable(exception)
-                }
-            }
-        }
-    }
-
-    private fun fetchStudyRoomList(
-        timeSlot: UUID,
-    ) {
-        viewModelScope.launch {
-
-            val result = kotlin.runCatching {
-                fetchStudyRoomsUseCase(
-                    fetchStudyRoomsInput = FetchStudyRoomsInput(
-                        timeSlot = timeSlot,
-                    ),
-                )
-            }
-
-            if (result.isSuccess) {
-
-                val resultEntity = result.getOrThrow()
-
-                _uiState.value = _uiState.value.copy(
-                    studyRooms = resultEntity.studyRooms.toInformation(),
-                )
-            } else {
-                if (result.exceptionOrNull() !is NullPointerException) {
-                    emitErrorEventFromThrowable(result.exceptionOrNull())
-                }
-            }
-        }
-    }
-
-    private fun fetchStudyRoomAvailableTimeList() {
-        viewModelScope.launch {
-            kotlin.runCatching {
-                studyRoomAvailableTimeListUseCase()
-            }.onSuccess { resultEntity ->
-                _uiState.value = _uiState.value.copy(
-                    studyRoomAvailableTime = resultEntity.timeSlots,
-                )
-            }.onFailure {
-                emitErrorEventFromThrowable(
-                    throwable = it
-                )
-            }
-        }
-    }
-}
-
-data class StudyRoomListUiState(
-    var startAt: String = "",
-    var endAt: String = "",
-    var studyRooms: List<StudyRoomInformation> = emptyList(),
-    var studyRoomAvailableTime: List<FetchAvailableStudyRoomTimesOutput.TimeSlotInformation> = emptyList(),
-    var timeSlot: UUID? = null,
-    var hasApplyTime: Boolean = false,
-) : UiState
-
-data class StudyRoomInformation(
-    val roomId: UUID,
-    val position: String,
-    val title: String,
-    val currentNumber: Int,
-    val isMine: Boolean,
-    val maxNumber: Int,
-    val condition: String,
-)
-
-internal fun FetchStudyRoomsOutput.StudyRoomInformation?.toInformation(): StudyRoomInformation {
-    requireNotNull(this)
-
-    val grade = if (availableGrade == 0) "모든" else availableGrade
-
-    return StudyRoomInformation(
-        roomId = id,
-        position = "${floor}층",
-        title = name,
-        currentNumber = inUseHeadcount,
-        isMine = isMine,
-        maxNumber = totalAvailableSeat,
-        condition = "${grade}학년 ${availableSex.koreanValue}",
-    )
-}
-
-internal fun List<FetchStudyRoomsOutput.StudyRoomInformation?>.toInformation(): List<StudyRoomInformation> {
-    return if (this.isNotEmpty()) {
-        this.map { it.toInformation() }
-    } else {
-        return emptyList()
-    }
-}
-*/
