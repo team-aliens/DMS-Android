bcef0bff8e0e89622f6c42130af9f1048059d418
Junsu Park <krpjs0508@gmail.com>
2024-02-03 20:49:31 +0900

    ♻️ :: studyroom mapper

--- data/src/main/java/team/aliens/dms/android/data/studyroom/mapper/StudyRoomMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/studyroom/mapper/StudyRoomMapper.kt
@@ -7,6 +7,7 @@ import team.aliens.dms.android.data.studyroom.model.StudyRoomApplicationTime
 import team.aliens.dms.android.network.studyroom.model.FetchAppliedStudyRoomResponse
 import team.aliens.dms.android.network.studyroom.model.FetchAvailableStudyRoomTimesResponse
 import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomApplicationTimeResponse
+import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomDetailsResponse
 import team.aliens.dms.android.network.studyroom.model.FetchStudyRoomsResponse
 import team.aliens.dms.android.shared.model.Sex
 
@@ -29,6 +30,52 @@ fun FetchStudyRoomsResponse.StudyRoomResponse.toModel(): StudyRoom = StudyRoom(
     totalAvailableSeat = this.totalAvailableSeat,
     isMine = this.isMine,
 )
+
+fun FetchStudyRoomDetailsResponse.toModel(): StudyRoom.Details = StudyRoom.Details(
+    id = id,
+    floor = floor,
+    name = name,
+    startTime = startTime,
+    endTime = endTime,
+    totalAvailableSeat = totalAvailableSeat,
+    inUseHeadcount = inUseHeadcount,
+    availableSex = Sex.valueOf(this.availableSex),
+    availableGrade = availableGrade,
+    eastDescription = eastDescription,
+    westDescription = westDescription,
+    southDescription = southDescription,
+    northDescription = northDescription,
+    totalWidthSize = totalWidthSize,
+    totalHeightSize = totalHeightSize,
+    seats = seats.toModel(),
+)
+
+fun List<FetchStudyRoomDetailsResponse.SeatResponse>.toModel(): List<StudyRoom.Seat> =
+    this.map(FetchStudyRoomDetailsResponse.SeatResponse::toModel)
+
+fun FetchStudyRoomDetailsResponse.SeatResponse.toModel(): StudyRoom.Seat = StudyRoom.Seat(
+    id = this.id,
+    row = this.row,
+    column = this.column,
+    number = this.number,
+    type = this.type?.toModel(),
+    status = StudyRoom.Seat.Status.valueOf(this.status),
+    isMine = this.isMine,
+    student = this.student?.toModel(),
+)
+
+fun FetchStudyRoomDetailsResponse.SeatResponse.SeatTypeResponse.toModel(): StudyRoom.Seat.Type =
+    StudyRoom.Seat.Type(
+        id = this.id,
+        name = this.name,
+        color = this.color,
+    )
+
+fun FetchStudyRoomDetailsResponse.SeatResponse.StudentResponse.toModel(): StudyRoom.Seat.Student =
+    StudyRoom.Seat.Student(
+        id = this.id,
+        name = this.name,
+    )
 
 fun FetchAppliedStudyRoomResponse.toModel(): AppliedStudyRoom = AppliedStudyRoom(
     floor = this.floor,
