c6de0181393167aee6e1303b2c92c946b5a4c8ff
Junsu Park <krpjs0508@gmail.com>
2024-02-09 03:28:24 +0900

    ♻️ :: database import

--- database/src/test/java/team/aliens/dms/android/database/meal/DatabaseMealDataSourceTest.kt
+++ database/src/test/java/team/aliens/dms/android/database/meal/DatabaseMealDataSourceTest.kt
@@ -1,6 +1,5 @@
 package team.aliens.dms.android.database.meal
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
@@ -8,7 +7,6 @@ import org.junit.Rule
 import org.junit.runner.RunWith
 
 @HiltAndroidTest
-@RunWith(AndroidJUnit4::class)
 class DatabaseMealDataSourceTest {
 
     /*@get:Rule
