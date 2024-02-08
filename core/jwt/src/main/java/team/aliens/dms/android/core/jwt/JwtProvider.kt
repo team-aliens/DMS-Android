35fd796778fc81756d2fcd6e457fab19f46410b2
Junsu Park <krpjs0508@gmail.com>
2024-02-09 00:37:32 +0900

    ♻️ :: jwt provider documents

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/JwtProvider.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/JwtProvider.kt
@@ -4,12 +4,34 @@ import kotlinx.coroutines.flow.StateFlow
 
 abstract class JwtProvider {
 
+    /**
+     * represents cached access token
+     */
     abstract val cachedAccessToken: AccessToken
+
+    /**
+     * represents if the cached access token is available
+     * @see cachedAccessToken
+     */
     abstract val isCachedAccessTokenAvailable: StateFlow<Boolean>
+
+    /**
+     * represents cached refresh token
+     */
     abstract val cachedRefreshToken: RefreshToken
+
+    /**
+     * represents if the cached refresh token is available
+     */
     abstract val isCachedRefreshTokenAvailable: StateFlow<Boolean>
 
+    /**
+     * updates tokens on [JwtProvider] itself and device's local storage
+     */
     abstract fun updateTokens(tokens: Tokens)
 
+    /**
+     * clears tokens on [JwtProvider] itself and device's local storage
+     */
     abstract fun clearCaches()
 }
