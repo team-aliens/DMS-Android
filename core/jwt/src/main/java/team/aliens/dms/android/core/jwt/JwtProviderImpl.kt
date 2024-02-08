4bc188b6dfe7fd48a24b5aa0ee30595fcd6ac3ae
Junsu Park <krpjs0508@gmail.com>
2024-02-09 01:27:11 +0900

    ♻️ :: jwt provider legacy

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/JwtProviderImpl.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/JwtProviderImpl.kt
@@ -26,7 +26,7 @@ internal class JwtProviderImpl @Inject constructor(
             if (this._cachedAccessToken!!.isExpired()) {
                 this.reissueTokens()
             }
-            return _cachedAccessToken!!
+            return _cachedAccessToken!!.also { println("LOGLOGLOG :$it") } // TODO: Remove
         }
 
     private val _isCachedAccessTokenAvailable: MutableStateFlow<Boolean> = MutableStateFlow(false)
@@ -42,7 +42,7 @@ internal class JwtProviderImpl @Inject constructor(
             if (_cachedRefreshToken!!.isExpired()) {
                 throw CannotUseRefreshTokenException()
             }
-            return _cachedRefreshToken!!
+            return _cachedRefreshToken!!.also { println("LOGLOGLOG :$it") } // TODO: Remove
         }
 
     private val _isCachedRefreshTokenAvailable: MutableStateFlow<Boolean> = MutableStateFlow(false)
@@ -114,116 +114,3 @@ internal class JwtProviderImpl @Inject constructor(
         this@JwtProviderImpl.updateTokensAbility()
     }
 }
-
-/*
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import team.aliens.dms.android.core.jwt.datastore.JwtDataStoreDataSource
-import team.aliens.dms.android.core.jwt.exception.CannotUseTokensException
-import team.aliens.dms.android.core.jwt.network.TokenReissueManager
-import team.aliens.dms.android.shared.date.util.now
-import javax.inject.Inject
-
-internal class JwtProviderImpl @Inject constructor(
-    private val jwtDataStoreDataSource: JwtDataStoreDataSource,
-    private val tokenReissueManager: TokenReissueManager,
-) : JwtProvider() {
-
-    private var _cachedAccessToken: AccessToken? = null
-    override val cachedAccessToken: AccessToken
-        get() = if (checkCachedAccessTokenAvailable().also(::updateCachedAccessTokenAvailable)) {
-            _cachedAccessToken!!
-        } else {
-            this.fetchTokens().accessToken
-        }
-
-    override lateinit var cachedAccessTokenExpiration: AccessTokenExpiration
-        private set
-
-    private val _isCachedAccessTokenAvailable: MutableStateFlow<Boolean> = MutableStateFlow(false)
-    override val isCachedAccessTokenAvailable: StateFlow<Boolean> =
-        _isCachedAccessTokenAvailable.asStateFlow()
-
-    private var _cachedRefreshToken: RefreshToken? = null
-    override val cachedRefreshToken: RefreshToken
-        get() = if (checkCachedRefreshTokenAvailable().also(::updateCachedRefreshTokenAvailable)) {
-            _cachedRefreshToken!!
-        } else {
-            throw CannotUseTokensException()
-        }
-
-    override lateinit var cachedRefreshTokenExpiration: RefreshTokenExpiration
-        private set
-
-    private val _isCachedRefreshTokenAvailable: MutableStateFlow<Boolean> = MutableStateFlow(false)
-    override val isCachedRefreshTokenAvailable: StateFlow<Boolean> =
-        _isCachedRefreshTokenAvailable.asStateFlow()
-
-    init {
-        try {
-            loadTokens()
-        } catch (_: Exception) {
-        }
-    }
-
-    private fun checkCachedAccessTokenAvailable(): Boolean {
-        val con1 = _cachedAccessToken != null
-        val con2 = now.isBefore(cachedAccessTokenExpiration)
-        return con1 && con2
-    }
-
-    private fun checkCachedRefreshTokenAvailable(): Boolean {
-        val con1 = _cachedRefreshToken != null
-        val con2 = now.isBefore(cachedRefreshTokenExpiration)
-        return con1 && con2
-    }
-
-    private fun updateCachedAccessTokenAvailable(available: Boolean) {
-        CoroutineScope(Dispatchers.Default).launch { _isCachedAccessTokenAvailable.emit(available) }
-    }
-
-    private fun updateCachedRefreshTokenAvailable(available: Boolean) {
-        CoroutineScope(Dispatchers.Default).launch { _isCachedRefreshTokenAvailable.emit(available) }
-    }
-
-    private fun loadTokens(): Tokens = jwtDataStoreDataSource.loadTokens().also(::updateTokens)
-
-    private fun fetchTokens(): Tokens =
-        tokenReissueManager(refreshToken = cachedRefreshToken).toModel()
-            .also(::updateTokens)
-
-    override fun updateTokens(tokens: Tokens) {
-        this._cachedAccessToken = tokens.accessToken
-        this.cachedAccessTokenExpiration = tokens.accessTokenExpiration
-        this._cachedRefreshToken = tokens.refreshToken
-        this.cachedRefreshTokenExpiration = tokens.refreshTokenExpiration
-
-        runBlocking {
-            jwtDataStoreDataSource.storeTokens(tokens)
-            withContext(Dispatchers.Default) {
-                updateCachedAccessTokenAvailable(true)
-                updateCachedRefreshTokenAvailable(true)
-            }
-        }
-    }
-
-    override fun clearCaches() {
-        runBlocking {
-            jwtDataStoreDataSource.clearTokens()
-            withContext(Dispatchers.Default) {
-                updateCachedAccessTokenAvailable(false)
-
-
-                updateCachedRefreshTokenAvailable(false)
-            }
-        }
-    }
-}
-*/
