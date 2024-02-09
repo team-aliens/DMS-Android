756c438227810f80bfb984e39425eec13984fd43
Junsu Park <krpjs0508@gmail.com>
2024-02-09 16:09:13 +0900

    ♻️ :: user repository

--- data/src/main/java/team/aliens/dms/android/data/user/repository/UserRepositoryImpl.kt
+++ data/src/main/java/team/aliens/dms/android/data/user/repository/UserRepositoryImpl.kt
@@ -1,20 +1,26 @@
 package team.aliens.dms.android.data.user.repository
 
 import team.aliens.dms.android.network.user.datasource.NetworkUserDataSource
+import team.aliens.dms.android.network.user.model.EditPasswordRequest
 import javax.inject.Inject
 
 internal class UserRepositoryImpl @Inject constructor(
-    private val userDataSource: NetworkUserDataSource,
+    private val networkUserDataSource: NetworkUserDataSource,
 ) : UserRepository() {
 
     override suspend fun editPassword(
         currentPassword: String,
         newPassword: String,
     ) {
-        TODO("Not yet implemented")
+        networkUserDataSource.editPassword(
+            request = EditPasswordRequest(
+                currentPassword = currentPassword,
+                newPassword = newPassword
+            ),
+        )
     }
 
     override suspend fun comparePassword(password: String) {
-        TODO("Not yet implemented")
+        networkUserDataSource.comparePassword(password = password)
     }
 }
