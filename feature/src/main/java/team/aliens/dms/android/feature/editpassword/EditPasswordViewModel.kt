89f8791c547f8862a5e591fc9fb52155ebe9058e
Junsu Park <krpjs0508@gmail.com>
2024-02-09 16:09:20 +0900

    ♻️ :: password viewmodel

--- feature/src/main/java/team/aliens/dms/android/feature/editpassword/EditPasswordViewModel.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/editpassword/EditPasswordViewModel.kt
@@ -1,6 +1,9 @@
 package team.aliens.dms.android.feature.editpassword
 
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import team.aliens.dms.android.core.ui.mvi.BaseMviViewModel
 import team.aliens.dms.android.core.ui.mvi.Intent
 import team.aliens.dms.android.core.ui.mvi.SideEffect
@@ -16,35 +19,55 @@ internal class EditPasswordViewModel @Inject constructor(
 ) {
     override fun processIntent(intent: EditPasswordIntent) {
         when (intent) {
-            is EditPasswordIntent.UpdateCurrentPassword -> TODO()
-            is EditPasswordIntent.UpdateNewPassword -> TODO()
-            is EditPasswordIntent.UpdateNewPasswordConfirmation -> TODO()
+            is EditPasswordIntent.UpdateCurrentPassword -> this.updateCurrentPassword(value = intent.value)
+            EditPasswordIntent.ConfirmPassword -> this.confirmPassword()
         }
     }
+
+    private fun updateCurrentPassword(value: String) = reduce(
+        newState = stateFlow.value.copy(
+            currentPassword = value,
+            confirmButtonAvailable = this.checkConfirmButtonAvailable(),
+        ),
+    )
+
+    private fun confirmPassword() = viewModelScope.launch(Dispatchers.IO) {
+        runCatching {
+            userRepository.comparePassword(password = stateFlow.value.currentPassword)
+        }.onSuccess {
+            postSideEffect(EditPasswordSideEffect.PasswordConfirmed)
+        }.onFailure {
+            when (it) {
+// TODO
+            }
+            postSideEffect(EditPasswordSideEffect.PasswordMismatch)
+        }
+    }
+
+    private fun checkConfirmButtonAvailable() = stateFlow.value.currentPassword.isNotBlank()
 }
 
 internal data class EditPasswordUiState(
     val currentPassword: String,
-    val newPassword: String,
-    val newPasswordConfirmation: String,
+    val confirmButtonAvailable: Boolean,
 ) : UiState() {
     companion object {
         fun initial() = EditPasswordUiState(
             currentPassword = "",
-            newPassword = "",
-            newPasswordConfirmation = "",
+            confirmButtonAvailable = false,
         )
     }
 }
 
 internal sealed class EditPasswordIntent : Intent() {
-    class UpdateCurrentPassword(val password: String) : EditPasswordIntent()
-    class UpdateNewPassword(val password: String) : EditPasswordIntent()
-    class UpdateNewPasswordConfirmation(val password: String) : EditPasswordIntent()
+    class UpdateCurrentPassword(val value: String) : EditPasswordIntent()
+
+    data object ConfirmPassword : EditPasswordIntent()
 }
 
 internal sealed class EditPasswordSideEffect : SideEffect() {
-
+    data object PasswordConfirmed : EditPasswordSideEffect()
+    data object PasswordMismatch : EditPasswordSideEffect()
 }
 
 /*
