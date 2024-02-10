dc71d74f6c52dc476322b30e811ea30c9c129eaa
Junsu Park <krpjs0508@gmail.com>
2024-02-10 22:08:51 +0900

    ♻️ :: edit pw nav

--- feature/src/main/java/team/aliens/dms/android/feature/editpassword/navigation/EditPasswordNavigator.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/editpassword/navigation/EditPasswordNavigator.kt
@@ -2,7 +2,5 @@ package team.aliens.dms.android.feature.editpassword.navigation
 
 interface EditPasswordNavigator {
     fun openEditPasswordSetPassword()
-    fun openSignIn()
-
     fun navigateUp()
 }
