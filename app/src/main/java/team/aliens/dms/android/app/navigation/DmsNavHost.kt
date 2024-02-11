1cec1384e72cd51c283834f7db74e1f7a0832d96
Junsu Park <krpjs0508@gmail.com>
2024-02-11 20:42:33 +0900

    ♻️ :: nav host

--- app/src/main/java/team/aliens/dms/android/app/navigation/DmsNavHost.kt
+++ app/src/main/java/team/aliens/dms/android/app/navigation/DmsNavHost.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.defaults.NestedNavGraphDefaultAnimations
@@ -17,6 +18,7 @@ import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.navigation.dependency
 import com.ramcosta.composedestinations.scope.DestinationScopeWithNoDependencies
 import com.ramcosta.composedestinations.spec.NavGraphSpec
+import com.ramcosta.composedestinations.spec.NavHostEngine
 import team.aliens.dms.android.app.navigation.authorized.AuthorizedNavGraph
 import team.aliens.dms.android.app.navigation.unauthorized.UnauthorizedNavGraph
 
@@ -24,9 +26,7 @@ import team.aliens.dms.android.app.navigation.unauthorized.UnauthorizedNavGraph
 @Composable
 fun DmsNavHost(
     modifier: Modifier = Modifier,
-    autoSignIn: Boolean,
-) {
-    val engine = rememberAnimatedNavHostEngine(
+    engine: NavHostEngine = rememberAnimatedNavHostEngine(
         rootDefaultAnimations = RootNavGraphDefaultAnimations.ACCOMPANIST_FADING,
         defaultAnimationsForNestedNavGraph = mapOf(
             AuthorizedNavGraph to NestedNavGraphDefaultAnimations(
@@ -52,9 +52,10 @@ fun DmsNavHost(
                 },
             )
         ),
-    )
-    val navController = engine.rememberNavController()
-
+    ),
+    autoSignIn: Boolean,
+    navController: NavHostController = engine.rememberNavController()
+) {
     DestinationsNavHost(
         modifier = modifier,
         engine = engine,
