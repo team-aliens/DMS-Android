dc6a8852fdfea4a460ff784ef7b03aa14d824be2
Junsu Park <krpjs0508@gmail.com>
2024-02-11 20:34:20 +0900

    ♻️ :: hide system nav

--- app/src/main/java/team/aliens/dms/android/app/MainActivity.kt
+++ app/src/main/java/team/aliens/dms/android/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package team.aliens.dms.android.app
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -13,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import team.aliens.dms.android.app.navigation.DmsNavHost
 import team.aliens.dms.android.core.designsystem.DmsTheme
 
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: MainActivityViewModel by viewModels()
@@ -20,6 +22,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        )
 
         setContent {
             // FIXME: app 전역 state로 관리하도록 변경
