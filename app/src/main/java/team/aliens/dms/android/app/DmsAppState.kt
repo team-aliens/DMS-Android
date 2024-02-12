15f15219b09221ef348643421516e27877292f0c
Junsu Park <krpjs0508@gmail.com>
2024-02-12 11:37:40 +0900

    ♻️ :: dms app state variables

--- app/src/main/java/team/aliens/dms/android/app/DmsAppState.kt
+++ app/src/main/java/team/aliens/dms/android/app/DmsAppState.kt
@@ -3,7 +3,11 @@ package team.aliens.dms.android.app
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,11 +16,46 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.ramcosta.composedestinations.animations.defaults.NestedNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
+import com.ramcosta.composedestinations.spec.NavHostEngine
+import team.aliens.dms.android.app.navigation.authorized.AuthorizedNavGraph
+import team.aliens.dms.android.app.navigation.unauthorized.UnauthorizedNavGraph
+import team.aliens.dms.android.core.jwt.JwtProvider
+import javax.inject.Inject
 
+@OptIn(ExperimentalMaterialNavigationApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun rememberDmsAppState(
-    navController: NavHostController = rememberNavController(),
+    engine: NavHostEngine = rememberAnimatedNavHostEngine(
+        rootDefaultAnimations = RootNavGraphDefaultAnimations.ACCOMPANIST_FADING,
+        defaultAnimationsForNestedNavGraph = mapOf(
+            AuthorizedNavGraph to NestedNavGraphDefaultAnimations(
+                enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) },
+                exitTransition = { fadeOut(tween(delayMillis = 10)) },
+                popEnterTransition = { EnterTransition.None },
+                popExitTransition = {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End,
+                        animationSpec = tween(delayMillis = 10),
+                    )
+                },
+            ), UnauthorizedNavGraph to NestedNavGraphDefaultAnimations(
+                enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) },
+                exitTransition = { fadeOut(tween(delayMillis = 10)) },
+                popEnterTransition = { EnterTransition.None },
+                popExitTransition = {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End,
+                        animationSpec = tween(delayMillis = 10),
+                    )
+                },
+            )
+        ),
+    ),
+    navController: NavHostController = engine.rememberNavController(),
     context: Context = LocalContext.current,
 ) = remember(navController, context) {
     DmsAppState(
@@ -32,20 +71,34 @@ class DmsAppState(
     var isOnline by mutableStateOf(checkIfOnline())
         private set
 
+    var isSignedIn by mutableStateOf(checkIfSignedIn())
+        private set
+
+    @Inject
+    lateinit var jwtManager: JwtProvider
+
     /**
      * copied from jetcaster, android compose samples
      * https://github.com/android/compose-samples/blob/main/Jetcaster/app/src/main/java/com/example/jetcaster/ui/JetcasterAppState.kt
      */
-    @Suppress("DEPRECATION")
     private fun checkIfOnline(): Boolean {
         val cm = ContextCompat.getSystemService(context, ConnectivityManager::class.java)
 
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val capabilities = cm?.getNetworkCapabilities(cm.activeNetwork) ?: return false
-            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
-                    capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-        } else {
-            cm?.activeNetworkInfo?.isConnectedOrConnecting == true
-        }
+        val capabilities = cm?.getNetworkCapabilities(cm.activeNetwork) ?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) && capabilities.hasCapability(
+            NetworkCapabilities.NET_CAPABILITY_VALIDATED
+        )
+    }
+
+    fun refreshOnline() {
+        this.isOnline = checkIfOnline()
+    }
+
+    private fun checkIfSignedIn(): Boolean = jwtManager.run {
+        isCachedAccessTokenAvailable.value && isCachedRefreshTokenAvailable.value
+    }
+
+    fun refreshSignedIn() {
+        this.isSignedIn = checkIfSignedIn()
     }
 }
