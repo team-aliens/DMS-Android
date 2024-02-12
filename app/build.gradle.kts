254627e7eae58628387a78a3e85bc3ef0de352af
Junsu Park <krpjs0508@gmail.com>
2024-02-12 10:53:17 +0900

    ♻️ :: window app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
 
     implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
-
+    implementation(libs.androidx.window)
     implementation(libs.androidx.activity.compose)
 
     implementation(libs.androidx.compose)
