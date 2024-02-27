c0106db3fb153fb15450cc680c9346a23e7597cd
junsuPark <krpjs0508@gmail.com>
2024-02-27 16:57:12 +0900

    ♻️ :: school verific

--- feature/src/main/java/team/aliens/dms/android/feature/signup/2_EnterSchoolVerificationQuestionScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signup/2_EnterSchoolVerificationQuestionScreen.kt
@@ -1,110 +1,108 @@
 package team.aliens.dms.android.feature.signup
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.ramcosta.composedestinations.annotation.Destination
+import team.aliens.dms.android.core.designsystem.ContainedButton
+import team.aliens.dms.android.core.designsystem.DmsScaffold
+import team.aliens.dms.android.core.designsystem.DmsTopAppBar
+import team.aliens.dms.android.core.designsystem.TextField
+import team.aliens.dms.android.core.ui.Banner
+import team.aliens.dms.android.core.ui.BannerDefaults
+import team.aliens.dms.android.core.ui.DefaultHorizontalSpace
+import team.aliens.dms.android.core.ui.bottomPadding
+import team.aliens.dms.android.core.ui.horizontalPadding
+import team.aliens.dms.android.core.ui.startPadding
+import team.aliens.dms.android.feature.R
 import team.aliens.dms.android.feature.signup.navigation.SignUpNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 internal fun EnterSchoolVerificationQuestionScreen(
     modifier: Modifier = Modifier,
     navigator: SignUpNavigator,
     // signUpViewModel: SignUpViewModel,
-) {/*
+) {
+    // val uiState by viewModel.stateFlow.collectAsStateWithLifecycle()
 
-    val uiState by signUpViewModel.stateFlow.collectAsStateWithLifecycle()
-
-    val focusManager = LocalFocusManager.current
-
-    val onAnswerChange = { schoolAnswer: String ->
-        signUpViewModel.postIntent(
-            SignUpIntent.SchoolQuestion.SetSchoolAnswer(
-                schoolAnswer = schoolAnswer,
+    DmsScaffold(
+        modifier = modifier,
+        topBar = {
+            DmsTopAppBar(
+                title = { Text(text = stringResource(id = R.string.announcement)) },
+                navigationIcon = {
+                    IconButton(onClick = navigator::navigateUp) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
+                            contentDescription = stringResource(id = R.string.top_bar_back_button),
+                        )
+                    }
+                },
             )
-        )
-    }
-
-    LaunchedEffect(Unit) {
-        signUpViewModel.postIntent(SignUpIntent.SchoolQuestion.FetchSchoolQuestion)
-        signUpViewModel.sideEffectFlow.collect {
-            when (it) {
-                is SignUpSideEffect.SchoolQuestion.SuccessVerifySchoolAnswer -> {
-                    navigator.openEnterEmail()
-                }
-
-                else -> {}
-            }
-        }
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(DormTheme.colors.surface)
-            .dormClickable(
-                rippleEnabled = false,
-            ) {
-                focusManager.clearFocus()
-            },
-    ) {
+        },
+    ) { padValues ->
         Column(
             modifier = Modifier
-                .fillMaxHeight()
-                .padding(horizontal = 16.dp),
+                .fillMaxSize()
+                .padding(padValues)
+                .imePadding(),
         ) {
-            Spacer(modifier = Modifier.height(108.dp))
-            AppLogo(
-                darkIcon = isSystemInDarkTheme(),
-            )
-            Space(space = 8.dp)
-            Body2(text = stringResource(id = R.string.sign_up_confirm_school_question))
-            Space(space = 60.dp)
-            Body2(text = uiState.schoolQuestion)
-            Space(space = 10.dp)
-            DormTextField(
-                value = uiState.schoolAnswer,
-                onValueChange = onAnswerChange,
-                hint = stringResource(id = R.string.Reply),
-                error = uiState.schoolAnswerMismatchError,
-                errorDescription = stringResource(id = R.string.sign_up_confirm_school_error_incorrect_answer),
-                keyboardActions = KeyboardActions {
-                    focusManager.clearFocus()
-                },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                ),
+            Spacer(modifier = Modifier.weight(1f))
+            Banner(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .startPadding(),
+                message = {
+                    BannerDefaults.DefaultText(
+                        text = stringResource(id = R.string.sign_up_confirm_school_please_enter_school_verification_code),
+                    )
+                }
             )
             Spacer(modifier = Modifier.weight(1f))
-            DormContainedLargeButton(
-                text = stringResource(id = R.string.accept),
-                color = DormButtonColor.Blue,
-                enabled = uiState.schoolAnswerButtonEnabled,
+            // FIXME
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .startPadding(),
+                text = "우리 학교 학생 수는?",
+            )
+            Spacer(modifier = Modifier.height(DefaultHorizontalSpace))
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalPadding(),
+                value = "asdf",
+                onValueChange = {},
+                supportingText = {
+                    Text(text = "오류오류")
+                },
+                isError = true,
+            )
+            Spacer(modifier = Modifier.weight(3f))
+            ContainedButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalPadding()
+                    .bottomPadding(),
+                // FIXME: 서버 연동
+                onClick = navigator::openEnterEmail,
             ) {
-                signUpViewModel.postIntent(SignUpIntent.SchoolQuestion.ExamineSchoolAnswer)
+                Text(text = stringResource(id = R.string.next))
             }
-            Space(space = 12.dp)
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Caption(
-                    text = stringResource(id = R.string.sign_up_confirm_school_do_you_have_account_already),
-                    color = DormTheme.colors.primaryVariant,
-                )
-                Space(space = 8.dp)
-                ButtonText(
-                    modifier = Modifier
-                        .padding(top = 1.dp)
-                        .dormClickable(
-                            rippleEnabled = false,
-                            onClick = navigator::openSignIn,
-                        ),
-                    text = stringResource(id = R.string.sign_in),
-                )
-            }
-            Spacer(modifier = Modifier.height(48.dp))
         }
-    }*/
+    }
 }
