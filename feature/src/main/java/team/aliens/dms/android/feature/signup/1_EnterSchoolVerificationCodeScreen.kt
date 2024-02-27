078fbc2208292891ca7b5fe8afb089ff0248b3e0
junsuPark <krpjs0508@gmail.com>
2024-02-27 16:04:09 +0900

    ♻️ :: school ver

--- feature/src/main/java/team/aliens/dms/android/feature/signup/1_EnterSchoolVerificationCodeScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signup/1_EnterSchoolVerificationCodeScreen.kt
@@ -1,7 +1,6 @@
 package team.aliens.dms.android.feature.signup
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,10 +29,12 @@ import com.ramcosta.composedestinations.annotation.Destination
 import team.aliens.dms.android.core.designsystem.ContainedButton
 import team.aliens.dms.android.core.designsystem.DmsScaffold
 import team.aliens.dms.android.core.designsystem.DmsTheme
+import team.aliens.dms.android.core.ui.Banner
+import team.aliens.dms.android.core.ui.BannerDefaults
 import team.aliens.dms.android.core.ui.bottomPadding
 import team.aliens.dms.android.core.ui.horizontalPadding
+import team.aliens.dms.android.core.ui.startPadding
 import team.aliens.dms.android.feature.R
-import team.aliens.dms.android.feature.editpassword.Banner
 import team.aliens.dms.android.feature.signup.navigation.SignUpNavigator
 
 @Destination
@@ -56,7 +56,16 @@ internal fun EnterSchoolVerificationCodeScreen(
                 .imePadding(),
         ) {
             Spacer(modifier = Modifier.weight(1f))
-            Banner(Modifier.fillMaxWidth())
+            Banner(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .startPadding(),
+                message = {
+                    BannerDefaults.DefaultText(
+                        text = stringResource(id = R.string.sign_up_confirm_school_please_enter_school_verification_code),
+                    )
+                }
+            )
             Spacer(modifier = Modifier.weight(1f))
             VerificationCodeInput(
                 modifier = Modifier
@@ -74,123 +83,13 @@ internal fun EnterSchoolVerificationCodeScreen(
                     .fillMaxWidth()
                     .horizontalPadding()
                     .bottomPadding(),
-                onClick = {
-
-                },
+                // FIXME: 서버 연동
+                onClick = navigator::openEnterSchoolVerificationQuestion,
             ) {
                 Text(text = stringResource(id = R.string.sign_up_authorize))
             }
         }
-    }/*
-
-    val uiState by signUpViewModel.stateFlow.collectAsState()
-
-    val schoolCodeMismatchError = uiState.schoolCodeMismatchError
-
-    val focusManager = LocalFocusManager.current
-
-    val focusRequester = remember { FocusRequester() }
-
-    val onVerificationCodeChange = { schoolCode: String ->
-        if(schoolCode.length <= 8) {
-            signUpViewModel.postIntent(
-                SignUpIntent.VerifySchool.SetSchoolVerificationCode(
-                    schoolCode = schoolCode,
-                )
-            )
-            if (schoolCode.length == 8) {
-                focusManager.clearFocus()
-                signUpViewModel.postIntent(SignUpIntent.VerifySchool.ExamineSchoolVerificationCode)
-            }
-        }
     }
-
-    LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
-        signUpViewModel.sideEffectFlow.collect {
-            when (it) {
-                is SignUpSideEffect.VerifySchool.SuccessVerifySchoolCode -> {
-                    navigator.openEnterSchoolVerificationQuestion()
-                }
-
-                else -> {}
-            }
-        }
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(DormTheme.colors.surface)
-            .dormClickable(rippleEnabled = false) {
-                focusManager.clearFocus()
-            },
-    ) {
-        Spacer(modifier = Modifier.height(108.dp))
-        Column(
-            modifier = Modifier.padding(start = 16.dp),
-            horizontalAlignment = Alignment.Start,
-        ) {
-            AppLogo(darkIcon = isSystemInDarkTheme())
-            Space(space = 8.dp)
-            Body2(text = stringResource(id = R.string.SchoolVerificationCode))
-        }
-        Space(space = 100.dp)
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            BasicTextField(
-                value = uiState.schoolCode,
-                modifier = Modifier.focusRequester(focusRequester),
-                onValueChange = onVerificationCodeChange,
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.NumberPassword,
-                    imeAction = ImeAction.Done,
-                ),
-                keyboardActions = KeyboardActions {
-                    focusManager.clearFocus()
-                },
-                decorationBox = {
-                    LazyRow(
-                        horizontalArrangement = Arrangement.spacedBy(24.dp)
-                    ) {
-                        items(8) { index ->
-                            Box(
-                                modifier = Modifier
-                                    .size(16.dp)
-                                    .clip(shape = CircleShape)
-                                    .background(
-                                        color = if (uiState.schoolCode.length - 1 >= index) DormTheme.colors.primaryVariant
-                                        else DormTheme.colors.secondaryVariant,
-                                    ),
-                            )
-                        }
-                    }
-                }
-            )
-            Space(space = 40.dp)
-            Body3(
-                text = stringResource(
-                    id = if (schoolCodeMismatchError) R.string.sign_up_confirm_school_error_incorrect_school_verification_code
-                    else R.string.sign_up_confirm_school_please_enter_school_verification_code,
-                ),
-                color = if (schoolCodeMismatchError) DormColor.Error
-                else DormColor.Gray500,
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            DormContainedLargeButton(
-                text = stringResource(id = R.string.Verification),
-                color = DormButtonColor.Blue,
-                enabled = uiState.schoolCodeButtonEnabled,
-            ) {
-                signUpViewModel.postIntent(intent = SignUpIntent.VerifySchool.ExamineSchoolVerificationCode)
-            }
-            Spacer(modifier = Modifier.height(82.dp))
-        }
-    }*/
 }
 
 @Composable
