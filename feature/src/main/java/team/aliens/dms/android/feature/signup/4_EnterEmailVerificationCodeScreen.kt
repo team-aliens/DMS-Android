ea907ed4c67e018c574d22ee4d8e367240cab7e5
junsuPark <krpjs0508@gmail.com>
2024-03-08 07:42:15 +0900

    ♻️ :: veri

--- feature/src/main/java/team/aliens/dms/android/feature/signup/4_EnterEmailVerificationCodeScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signup/4_EnterEmailVerificationCodeScreen.kt
@@ -41,6 +41,7 @@ import team.aliens.dms.android.core.ui.bottomPadding
 import team.aliens.dms.android.core.ui.collectInLaunchedEffectWithLifecycle
 import team.aliens.dms.android.core.ui.horizontalPadding
 import team.aliens.dms.android.core.ui.startPadding
+import team.aliens.dms.android.core.ui.topPadding
 import team.aliens.dms.android.feature.R
 import team.aliens.dms.android.feature.signup.navigation.SignUpNavigator
 
@@ -135,10 +136,10 @@ internal fun SignUpEnterEmailVerificationCodeScreen(
                 .padding(padValues)
                 .imePadding(),
         ) {
-            Spacer(modifier = Modifier.weight(1f))
             Banner(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .topPadding(BannerDefaults.DefaultTopSpace)
                     .startPadding(),
                 message = {
                     BannerDefaults.DefaultText(text = stringResource(id = R.string.sign_up_enter_email_verification_code))
