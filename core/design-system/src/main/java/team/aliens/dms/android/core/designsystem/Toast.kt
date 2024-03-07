3173b548da18cb1ea3672997da0db06004385c4c
junsuPark <krpjs0508@gmail.com>
2024-03-08 07:42:07 +0900

    ♻️ :: toast

--- core/design-system/src/main/java/team/aliens/dms/android/core/designsystem/Toast.kt
+++ core/design-system/src/main/java/team/aliens/dms/android/core/designsystem/Toast.kt
@@ -8,10 +8,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -179,10 +184,10 @@ private fun DormToast(
             Row(
                 modifier = Modifier
                     .padding(
-                        top = 92.dp,
                         start = 16.dp,
                         end = 16.dp,
                     )
+                    .windowInsetsPadding(WindowInsets.Companion.systemBars.only(WindowInsetsSides.Top))
                     .fillMaxWidth()
                     .shadow()
                     .background(
