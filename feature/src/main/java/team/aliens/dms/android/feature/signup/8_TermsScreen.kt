cefea1c6d4efc99b38905cfd2e3dc1475f7c5d2e
junsuPark <krpjs0508@gmail.com>
2024-03-07 09:58:22 +0900

    ♻️ :: terms

--- feature/src/main/java/team/aliens/dms/android/feature/signup/8_TermsScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signup/8_TermsScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import com.ramcosta.composedestinations.annotation.Destination
@@ -26,9 +27,11 @@ import team.aliens.dms.android.core.designsystem.ContainedButton
 import team.aliens.dms.android.core.designsystem.DmsScaffold
 import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.core.designsystem.DmsTopAppBar
+import team.aliens.dms.android.core.designsystem.LocalToast
 import team.aliens.dms.android.core.ui.DefaultHorizontalSpace
 import team.aliens.dms.android.core.ui.DefaultVerticalSpace
 import team.aliens.dms.android.core.ui.bottomPadding
+import team.aliens.dms.android.core.ui.collectInLaunchedEffectWithLifecycle
 import team.aliens.dms.android.core.ui.horizontalPadding
 import team.aliens.dms.android.feature.R
 import team.aliens.dms.android.feature.signup.navigation.SignUpNavigator
@@ -47,6 +50,22 @@ internal fun TermsScreen(
         "dark"
     } else {
         "light"
+    }
+
+    val toast = LocalToast.current
+    val context = LocalContext.current
+
+    viewModel.sideEffectFlow.collectInLaunchedEffectWithLifecycle { sideEffect ->
+        when (sideEffect) {
+            SignUpSideEffect.SignUpFailure -> toast.showErrorToast(
+                message = context.getString(R.string.sign_up_error_id_format)
+            )
+
+            // TODO: 모달 구현
+            SignUpSideEffect.SignedUp -> navigator.openSignIn()
+            else -> {/* explicit blank */
+            }
+        }
     }
 
     DmsScaffold(
@@ -68,8 +87,7 @@ internal fun TermsScreen(
                 factory = {
                     WebView(it).apply {
                         layoutParams = ViewGroup.LayoutParams(
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            ViewGroup.LayoutParams.MATCH_PARENT
+                            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
                         )
                         webViewClient = WebViewClient()
 
@@ -97,126 +115,11 @@ internal fun TermsScreen(
                     .fillMaxWidth()
                     .horizontalPadding()
                     .bottomPadding(),
-                onClick = {
-
-                },
+                onClick = { viewModel.postIntent(SignUpIntent.SignUp) },
                 enabled = agreeOnTerm,
             ) {
                 Text(text = stringResource(id = R.string.sign_up_terms_finish_sign_up))
             }
         }
     }
-    /*
-        val uiState by signUpViewModel.stateFlow.collectAsStateWithLifecycle()
-
-        val termsUrl by remember { mutableStateOf("https://webview.aliens-dms.com/policy/privacy") }
-
-        var signUpDialogState by remember { mutableStateOf(false) }
-
-        val onCheckChanged = { checked: Boolean ->
-            signUpViewModel.postIntent(SignUpIntent.Terms.SetCheckedPolicy(checked))
-        }
-
-        val toast = LocalToast.current
-
-        val alreadyExistsStudentMessage = stringResource(id = R.string.sign_up_error_conflict)
-        val emailNotVerified = stringResource(id = R.string.sign_up_error_unauthorized)
-
-        LaunchedEffect(Unit) {
-            signUpViewModel.sideEffectFlow.collect {
-                when (it) {
-                    is SignUpSideEffect.Terms.SuccessSignUp -> {
-                        signUpDialogState = true
-                    }
-
-                    is SignUpSideEffect.Terms.AlreadyExistsStudent -> {
-                        toast.showErrorToast(alreadyExistsStudentMessage)
-                    }
-
-                    is SignUpSideEffect.Terms.EmailNotVerified -> {
-                        toast.showErrorToast(emailNotVerified)
-                    }
-
-                    else -> {}
-                }
-            }
-        }
-
-        if (signUpDialogState) {
-            DormCustomDialog(
-                onDismissRequest = { signUpDialogState = false },
-            ) {
-                DormSingleButtonDialog(
-                    content = stringResource(id = R.string.CompleteRegister),
-                    mainBtnText = stringResource(id = R.string.GoLogin),
-                    onMainBtnClick = navigator::openSignIn,
-                    mainBtnTextColor = DormTheme.colors.primary,
-                )
-            }
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    DormTheme.colors.surface,
-                )
-                .padding(
-                    top = 108.dp,
-                    start = 16.dp,
-                    end = 16.dp,
-                ),
-        ) {
-            Column(
-                modifier = Modifier.fillMaxHeight(),
-            ) {
-                AppLogo(darkIcon = isSystemInDarkTheme())
-                Space(space = 8.dp)
-                Body2(text = stringResource(id = R.string.CheckRegisterPolicy))
-                Space(space = 36.dp)
-                Column(modifier = Modifier.height(374.dp)) {
-                    AndroidView(
-                        factory = {
-                            WebView(it).apply {
-                                layoutParams = ViewGroup.LayoutParams(
-                                    ViewGroup.LayoutParams.MATCH_PARENT,
-                                    ViewGroup.LayoutParams.MATCH_PARENT
-                                )
-                                webViewClient = WebViewClient()
-
-                                settings.javaScriptEnabled = true
-
-                                loadUrl(termsUrl)
-                            }
-                        },
-                        update = {
-                            it.loadUrl(termsUrl)
-                        },
-                    )
-                }
-                RatioSpace(height = 0.39f)
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    DormCheckBox(
-                        checked = uiState.checkedPolicy,
-                        onCheckedChange = onCheckChanged,
-                    )
-                    Space(space = 14.dp)
-                    Caption(
-                        modifier = Modifier.padding(bottom = 1.dp),
-                        text = stringResource(id = R.string.CheckAllPolicy),
-                    )
-                }
-                Space(space = 16.dp)
-                DormContainedLargeButton(
-                    text = stringResource(id = R.string.Check),
-                    color = DormButtonColor.Blue,
-                    enabled = uiState.checkedPolicy,
-                ) {
-                    signUpViewModel.postIntent(SignUpIntent.Terms.SignUp)
-                }
-            }
-        }*/
 }
