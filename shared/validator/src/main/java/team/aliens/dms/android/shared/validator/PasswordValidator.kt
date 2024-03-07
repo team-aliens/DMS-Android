09de2efe51ed57d4ac69a853e5b3202c816db50b
junsuPark <krpjs0508@gmail.com>
2024-03-07 13:20:28 +0900

    ♻️ :: email validator

--- shared/validator/src/main/java/team/aliens/dms/android/shared/validator/PasswordValidator.kt
+++ shared/validator/src/main/java/team/aliens/dms/android/shared/validator/PasswordValidator.kt
@@ -2,13 +2,12 @@ package team.aliens.dms.android.shared.validator
 
 object PasswordValidator : Validator<String>() {
 
-    override val regex: Regex =
-        Regex("^.*(?=^.{8,20}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$")
+    override val regex = Regex("^.*(?=^.{8,20}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$")
 
     override fun validate(value: String): Boolean = value.matches(regex)
 }
 
-fun checkIsPasswordValid(value: String): Boolean {
+fun checkIfPasswordValid(value: String): Boolean {
     if (value.isEmpty()) {
         return false
     }
