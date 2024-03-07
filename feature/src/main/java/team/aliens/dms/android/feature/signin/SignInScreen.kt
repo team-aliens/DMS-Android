a4b957712bef222bbe5b120869525f0585fc91a4
junsuPark <krpjs0508@gmail.com>
2024-03-07 22:44:02 +0900

    ♻️ :: padding

--- feature/src/main/java/team/aliens/dms/android/feature/signin/SignInScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signin/SignInScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -86,7 +87,8 @@ internal fun SignInScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padValues),
+                .padding(padValues)
+                .imePadding(),
         ) {
             Banner(
                 modifier = Modifier
