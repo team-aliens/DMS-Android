b33c1b9fabfd872ade2b13be1e5aa8dd737a1858
junsuPark <krpjs0508@gmail.com>
2024-03-13 11:16:59 +0900

    ♻️ :: id test

--- shared/validator/src/main/java/team/aliens/dms/android/shared/validator/IdValidator.kt
+++ shared/validator/src/main/java/team/aliens/dms/android/shared/validator/IdValidator.kt
@@ -1,7 +1,7 @@
 package team.aliens.dms.android.shared.validator
 
 object IdValidator : Validator<String>() {
-    override val regex = Regex("^[A-Za-z0-9]{4,20}\$")
+    override val regex = Regex("^[A-Za-z0-9._-]{4,20}\$")
 
     override fun validate(value: String): Boolean = value.matches(regex)
 }
