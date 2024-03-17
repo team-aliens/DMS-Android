fa43b1fa668244ddd2b857e2ea59e19cd7f2406b
junsuPark <krpjs0508@gmail.com>
2024-03-17 20:28:40 +0900

    ♻️ :: application time

--- data/src/main/java/team/aliens/dms/android/data/outing/model/OutingApplicationTime.kt
+++ data/src/main/java/team/aliens/dms/android/data/outing/model/OutingApplicationTime.kt
@@ -2,12 +2,28 @@ package team.aliens.dms.android.data.outing.model
 
 import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalDateTime
+import team.aliens.dms.android.network.outing.model.OutingAvailableTimeResponse
 import java.util.UUID
 
 data class OutingApplicationTime(
     val id: UUID,
     val startTime: LocalDateTime,
     val endTime: LocalDateTime,
-    val enabled: Boolean,
+    val available: Boolean,
     val dayOfWeek: DayOfWeek,
 )
+
+fun OutingAvailableTimeResponse.toModel(): List<OutingApplicationTime> =
+    this.availableTimes.map(OutingAvailableTimeResponse.AvailableTimeResponse::toModel)
+
+fun List<OutingAvailableTimeResponse.AvailableTimeResponse>.toModel(): List<OutingApplicationTime> =
+    this.map(OutingAvailableTimeResponse.AvailableTimeResponse::toModel)
+
+fun OutingAvailableTimeResponse.AvailableTimeResponse.toModel(): OutingApplicationTime =
+    OutingApplicationTime(
+        id = UUID.fromString(this.outingAvailableTimeId),
+        startTime = this.startTime,
+        endTime = this.endTime,
+        available = this.available,
+        dayOfWeek = DayOfWeek.valueOf(this.dayOfWeek),
+    )
