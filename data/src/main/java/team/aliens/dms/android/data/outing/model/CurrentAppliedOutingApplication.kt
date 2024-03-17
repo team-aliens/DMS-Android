a7622c53bc2b3aa27a06f3dd53c7bfb3a73d4245
junsuPark <krpjs0508@gmail.com>
2024-03-17 10:25:34 +0900

    ♻️ :: application

---
+++ data/src/main/java/team/aliens/dms/android/data/outing/model/CurrentAppliedOutingApplication.kt
@@ -0,0 +1,19 @@
+package team.aliens.dms.android.data.outing.model
+
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+
+data class CurrentAppliedOutingApplication(
+    val date: LocalDate,
+    val type: String,
+    val status: OutingStatus,
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime,
+    val reason: String?,
+    val companionNames: List<String>,
+) {
+    // TODO
+    enum class OutingStatus {
+        DONE, REJECTED, ;
+    }
+}
