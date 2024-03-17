98ff1105a918a617abf85e8abbe3a1c1a50bcff1
junsuPark <krpjs0508@gmail.com>
2024-03-17 20:31:59 +0900

    ♻️ :: repository impl

--- data/src/main/java/team/aliens/dms/android/data/outing/repository/OutingRepository.kt
+++ data/src/main/java/team/aliens/dms/android/data/outing/repository/OutingRepository.kt
@@ -1,5 +1,28 @@
 package team.aliens.dms.android.data.outing.repository
 
-abstract class OutingRepository {
+import org.threeten.bp.DayOfWeek
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import team.aliens.dms.android.data.outing.model.CurrentAppliedOutingApplication
+import team.aliens.dms.android.data.outing.model.OutingApplicationId
+import team.aliens.dms.android.data.outing.model.OutingApplicationTime
+import java.util.UUID
 
+abstract class OutingRepository {
+    abstract fun fetchOutingApplicationTimes(dayOfWeek: DayOfWeek): List<OutingApplicationTime>
+
+    abstract fun applyOuting(
+        date: LocalDate,
+        startTime: LocalDateTime,
+        endTime: LocalDateTime,
+        type: String,
+        reason: String?,
+        companionIds: List<UUID>,
+    ): OutingApplicationId
+
+    abstract fun fetchCurrentAppliedOutingApplication(): CurrentAppliedOutingApplication
+
+    abstract fun cancelOuting(applicationId: UUID)
+
+    abstract fun fetchOutingTypes(keyword: String): List<String>
 }
--- data/src/main/java/team/aliens/dms/android/data/outing/repository/OutingRepositoryImpl.kt
+++ data/src/main/java/team/aliens/dms/android/data/outing/repository/OutingRepositoryImpl.kt
@@ -1,9 +1,48 @@
 package team.aliens.dms.android.data.outing.repository
 
+import org.threeten.bp.DayOfWeek
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import team.aliens.dms.android.data.outing.model.CurrentAppliedOutingApplication
+import team.aliens.dms.android.data.outing.model.OutingApplicationId
+import team.aliens.dms.android.data.outing.model.OutingApplicationTime
+import team.aliens.dms.android.data.outing.model.toModel
+import team.aliens.dms.android.network.outing.datasource.OutingNetworkDataSource
+import team.aliens.dms.android.network.outing.model.ApplyOutingRequest
+import java.util.UUID
 import javax.inject.Inject
 
 class OutingRepositoryImpl @Inject constructor(
     private val outingNetworkDataSource: OutingNetworkDataSource,
 ) : OutingRepository() {
+    override fun fetchOutingApplicationTimes(dayOfWeek: DayOfWeek): List<OutingApplicationTime> =
+        outingNetworkDataSource.fetchOutingAvailableTime(dayOfWeek = dayOfWeek.name).toModel()
 
+    override fun applyOuting(
+        date: LocalDate,
+        startTime: LocalDateTime,
+        endTime: LocalDateTime,
+        type: String,
+        reason: String?,
+        companionIds: List<UUID>
+    ): OutingApplicationId = outingNetworkDataSource.applyOuting(
+        req = ApplyOutingRequest(
+            date = date,
+            startTime = startTime,
+            endTime = endTime,
+            type = type,
+            reason = reason,
+            companionIds = companionIds,
+        ),
+    ).applicationId
+
+    override fun fetchCurrentAppliedOutingApplication(): CurrentAppliedOutingApplication =
+        outingNetworkDataSource.fetchCurrentAppliedOutingApplication().toModel()
+
+    override fun cancelOuting(applicationId: UUID) {
+        outingNetworkDataSource.cancelOuting(applicationId = applicationId)
+    }
+
+    override fun fetchOutingTypes(keyword: String): List<String> =
+        outingNetworkDataSource.fetchOutingTypes(keyword = keyword).titles
 }
