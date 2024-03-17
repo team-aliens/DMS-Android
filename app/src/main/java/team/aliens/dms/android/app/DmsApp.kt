1b219e7e44ad6c154db200638f8ea0f692e4336b
junsuPark <krpjs0508@gmail.com>
2024-03-17 09:32:22 +0900

    ♻️ :: dms app

--- app/src/main/java/team/aliens/dms/android/app/DmsApp.kt
+++ app/src/main/java/team/aliens/dms/android/app/DmsApp.kt
@@ -1,10 +1,5 @@
 package team.aliens.dms.android.app
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeOut
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,20 +9,13 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.window.layout.DisplayFeature
-import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
-import com.ramcosta.composedestinations.animations.defaults.NestedNavGraphDefaultAnimations
-import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
-import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.navigation.dependency
 import com.ramcosta.composedestinations.scope.DestinationScopeWithNoDependencies
 import com.ramcosta.composedestinations.spec.NavGraphSpec
-import com.ramcosta.composedestinations.spec.NavHostEngine
 import kotlinx.coroutines.flow.StateFlow
 import team.aliens.dms.android.app.navigation.DmsNavGraph
 import team.aliens.dms.android.app.navigation.DmsNavigator
-import team.aliens.dms.android.app.navigation.authorized.AuthorizedNavGraph
-import team.aliens.dms.android.app.navigation.unauthorized.UnauthorizedNavGraph
 import team.aliens.dms.android.feature.destinations.TermsScreenDestination
 import team.aliens.dms.android.feature.editpassword.EditPasswordViewModel
 import team.aliens.dms.android.feature.editpassword.navigation.EditPasswordNavGraph
@@ -39,16 +27,14 @@ import team.aliens.dms.android.feature.signup.navigation.SignUpNavGraph
 fun DmsApp(
     windowSizeClass: WindowSizeClass,
     displayFeatures: List<DisplayFeature>,
-    engine: NavHostEngine = rememberDmsNavHostEngine(),
     isJwtAvailable: StateFlow<Boolean>,
     appState: DmsAppState = rememberDmsAppState(
-        navController = engine.rememberNavController(),
         isJwtAvailable = isJwtAvailable,
     ),
 ) {
     val autoSignIn by appState.isJwtAvailable.collectAsStateWithLifecycle()
     DestinationsNavHost(
-        engine = engine,
+        engine = appState.engine,
         navGraph = DmsNavGraph.root(autoSignIn = autoSignIn),
         navController = appState.navController,
         dependenciesContainerBuilder = {
@@ -72,35 +58,6 @@ fun DmsApp(
         },
     )
 }
-
-@OptIn(ExperimentalMaterialNavigationApi::class, ExperimentalAnimationApi::class)
-@Composable
-private fun rememberDmsNavHostEngine() = rememberAnimatedNavHostEngine(
-    rootDefaultAnimations = RootNavGraphDefaultAnimations.ACCOMPANIST_FADING,
-    defaultAnimationsForNestedNavGraph = mapOf(
-        AuthorizedNavGraph to NestedNavGraphDefaultAnimations(
-            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) },
-            exitTransition = { fadeOut(tween(delayMillis = 10)) },
-            popEnterTransition = { EnterTransition.None },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.End,
-                    animationSpec = tween(delayMillis = 10),
-                )
-            },
-        ), UnauthorizedNavGraph to NestedNavGraphDefaultAnimations(
-            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) },
-            exitTransition = { fadeOut(tween(delayMillis = 10)) },
-            popEnterTransition = { EnterTransition.None },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.End,
-                    animationSpec = tween(delayMillis = 10),
-                )
-            },
-        )
-    ),
-)
 
 private fun DestinationScopeWithNoDependencies<*>.currentNavigator(autoSignIn: Boolean): DmsNavigator =
     DmsNavigator(
