edfdfb710a71052050054ac54aa207497f69500a
junsuPark <krpjs0508@gmail.com>
2024-03-28 08:32:40 +0900

    ♻️ :: student model

--- data/src/main/java/team/aliens/dms/android/data/student/model/Student.kt
+++ data/src/main/java/team/aliens/dms/android/data/student/model/Student.kt
@@ -1,10 +1,8 @@
 package team.aliens.dms.android.data.student.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Student(
-    @SerializedName("id") val id: String,
-    @SerializedName("name") val name: String,
-    @SerializedName("gcn") val gradeClassNumber: String,
-    @SerializedName("profile_image_url") val profileImageUrl: String,
+    val id: String,
+    val name: String,
+    val gradeClassNumber: String,
+    val profileImageUrl: String,
 )
---
+++ network/src/main/java/team/aliens/dms/android/network/student/model/FetchStudentsResponse.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.android.network.student.model
+
+import com.google.gson.annotations.SerializedName
+
+data class FetchStudentsResponse(
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("gcn") val gradeClassNumber: String,
+    @SerializedName("profile_image_url") val profileImageUrl: String,
+)
