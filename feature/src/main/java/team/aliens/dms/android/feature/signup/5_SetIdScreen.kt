a09aafd5e40246af57d11f59ef3b2f590fcebe34
uielPark <parkuiery@gmail.com>
2024-04-22 14:01:50 +0900

    ♻️ :: icon

--- feature/src/main/java/team/aliens/dms/android/feature/signup/5_SetIdScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/signup/5_SetIdScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -33,6 +36,7 @@ import com.ramcosta.composedestinations.annotation.Destination
 import kotlinx.coroutines.delay
 import team.aliens.dms.android.core.designsystem.AlertDialog
 import team.aliens.dms.android.core.designsystem.ContainedButton
+import team.aliens.dms.android.core.designsystem.DmsIcon
 import team.aliens.dms.android.core.designsystem.DmsScaffold
 import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.core.designsystem.DmsTopAppBar
@@ -127,11 +131,16 @@ internal fun SetIdScreen(
     }
 
     DmsScaffold(
-        modifier = modifier,
         topBar = {
             DmsTopAppBar(
-                title = {},
-                navigationIcon = {},
+                navigationIcon = {
+                    IconButton(onClick = navigator::navigateUp) {
+                        Icon(
+                            painter = painterResource(id = DmsIcon.Back),
+                            contentDescription = stringResource(id = R.string.top_bar_back_button),
+                        )
+                    }
+                },
             )
         },
     ) { padValues ->
