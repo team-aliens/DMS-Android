8a9037e745c7a16333a434e422a414876a69541a
uielPark <parkuiery@gmail.com>
2024-04-22 14:58:02 +0900

    ♻️ :: binary 형식 변경

--- network/src/main/java/team/aliens/dms/android/network/file/datasource/NetworkFileDataSourceImpl.kt
+++ network/src/main/java/team/aliens/dms/android/network/file/datasource/NetworkFileDataSourceImpl.kt
@@ -1,9 +1,15 @@
 package team.aliens.dms.android.network.file.datasource
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import team.aliens.dms.android.core.network.util.RequestType
 import team.aliens.dms.android.core.network.util.handleNetworkRequest
 import team.aliens.dms.android.network.file.apiservice.FileApiService
 import team.aliens.dms.android.network.file.model.FetchPresignedUrlResponse
 import java.io.File
+import java.nio.file.Files
 import javax.inject.Inject
 
 internal class NetworkFileDataSourceImpl @Inject constructor(
@@ -12,11 +18,14 @@ internal class NetworkFileDataSourceImpl @Inject constructor(
     override suspend fun fetchPresignedUrl(fileName: String): FetchPresignedUrlResponse =
         handleNetworkRequest { fileApiService.fetchPresignedUrl(fileName) }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun uploadFile(presignedUrl: String, file: File) =
         handleNetworkRequest {
             fileApiService.uploadFile(
                 presignedUrl = presignedUrl,
-                file = file,
+                file = Files.readAllBytes(file.toPath()).toRequestBody(
+                    contentType = RequestType.Binary.toMediaTypeOrNull()
+                ),
             )
         }
 }
