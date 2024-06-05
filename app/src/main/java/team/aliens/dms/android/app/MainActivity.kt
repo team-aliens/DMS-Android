7d539fc2924941d51e92eea909037b6d7b76e680
uiel <parkuiery@gmail.com>
2024-06-05 10:06:12 +0900

    ⚡️ :: 인 앱 업데이트 기능 추가

--- app/src/main/java/team/aliens/dms/android/app/MainActivity.kt
+++ app/src/main/java/team/aliens/dms/android/app/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSiz
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.view.WindowCompat
 import com.google.accompanist.adaptive.calculateDisplayFeatures
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.StateFlow
 import team.aliens.dms.android.core.designsystem.DmsTheme
@@ -30,6 +33,7 @@ class MainActivity : ComponentActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
         )
 
+        checkAppUpdate()
         setContent {
             val windowSizeClass = calculateWindowSizeClass(activity = this)
             val displayFeatures = calculateDisplayFeatures(activity = this)
@@ -39,6 +43,25 @@ class MainActivity : ComponentActivity() {
                     windowSizeClass = windowSizeClass,
                     displayFeatures = displayFeatures,
                     isJwtAvailable = isJwtAvailable,
+                )
+            }
+        }
+    }
+
+    private fun checkAppUpdate() {
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            val isUpdateAvailable =
+                appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+
+            if (isUpdateAvailable && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    0,
                 )
             }
         }
