5f7da27d4c7825520f04c2a826de4dd307a05996
uiel <parkuiery@gmail.com>
2024-06-09 23:30:52 +0900

    ⚡️ :: 알림 권한 확인

--- core/notification/src/main/java/team/aliens/dms/android/core/notification/CheckNotificationPermission.kt
+++ core/notification/src/main/java/team/aliens/dms/android/core/notification/CheckNotificationPermission.kt
@@ -1,4 +1,15 @@
 package team.aliens.dms.android.core.notification
 
-class CheckNotificationPermission {
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+
+fun notificationPermissionGranted(context: Context): Boolean {
+    return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS,
+            ) == PackageManager.PERMISSION_GRANTED) || Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU
 }
