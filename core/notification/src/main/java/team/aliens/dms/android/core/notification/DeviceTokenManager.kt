91665174c7de1f26fde09558bb77f3686f43d059
uiel <parkuiery@gmail.com>
2024-07-05 12:27:00 +0900

    ⚡️ :: DeviceTokenManger 세팅

--- core/notification/src/main/java/team/aliens/dms/android/core/notification/DeviceTokenManager.kt
+++ core/notification/src/main/java/team/aliens/dms/android/core/notification/DeviceTokenManager.kt
@@ -1,12 +1,23 @@
 package team.aliens.dms.android.core.notification
 
+import com.google.firebase.messaging.FirebaseMessaging
 import team.aliens.dms.android.data.notification.repository.NotificationRepository
 import javax.inject.Inject
 
 class DeviceTokenManager @Inject constructor(
-    private val notificationRepository: NotificationRepository,
+    //디바이스 토큰 로컬 저장 provider
 ) {
-    suspend operator fun invoke(deviceToken: String) = runCatching {
-        notificationRepository.registerDeviceNotificationToken(deviceToken = deviceToken)
+    fun getDeviceToken(): String? {
+        //디바이스 토큰 리턴
+        return null
+    }
+
+    fun fetchDeviceToken(deviceToken: String) {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                //Handle error
+            }
+            // 디바이스 토큰 등록
+        }
     }
 }
