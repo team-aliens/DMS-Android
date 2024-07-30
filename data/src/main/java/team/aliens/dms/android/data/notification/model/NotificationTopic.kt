c88f982c07d675a828b51d083fa799414bf4e791
uiel <parkuiery@gmail.com>
2024-07-30 14:20:31 +0900

    ♻️ :: 매핑 함수 추가

--- data/src/main/java/team/aliens/dms/android/data/notification/model/NotificationTopic.kt
+++ data/src/main/java/team/aliens/dms/android/data/notification/model/NotificationTopic.kt
@@ -1,5 +1,7 @@
 package team.aliens.dms.android.data.notification.model
 
+import team.aliens.dms.android.network.notification.model.BatchUpdateNotificationTopicRequest
+
 enum class NotificationTopic {
     NOTICE, STUDY_ROOM_TIME_SLOT, STUDY_ROOM_APPLY, POINT, OUTING,
     ;
@@ -9,3 +11,12 @@ enum class NotificationTopic {
         val subscribe: Boolean,
     )
 }
+
+fun List<NotificationTopic.Subscription>.toModel(): List<BatchUpdateNotificationTopicRequest.NotificationTopicRequest> =
+    this.map(NotificationTopic.Subscription::toModel)
+
+private fun NotificationTopic.Subscription.toModel(): BatchUpdateNotificationTopicRequest.NotificationTopicRequest =
+    BatchUpdateNotificationTopicRequest.NotificationTopicRequest(
+        topic = this.topic.name,
+        subscribed = this.subscribe,
+    )
