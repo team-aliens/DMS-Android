27753c7033b1191c8eb31cecb92152543d3e51e5
uiel <parkuiery@gmail.com>
2024-07-31 21:44:22 +0900

    ♻️ :: request 변경

--- network/src/main/java/team/aliens/dms/android/network/notification/model/SubscribeNotificationTopicRequest.kt
+++ network/src/main/java/team/aliens/dms/android/network/notification/model/SubscribeNotificationTopicRequest.kt
@@ -3,6 +3,6 @@ package team.aliens.dms.android.network.notification.model
 import com.google.gson.annotations.SerializedName
 
 data class SubscribeNotificationTopicRequest(
-    @SerializedName("token") val deviceToken: String,
+    @SerializedName("device_token") val deviceToken: String,
     @SerializedName("topic") val topic: String,
 )
