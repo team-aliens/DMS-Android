53c45d1725bce881d6cac41c0c0c447fdd23e2f6
uiel <parkuiery@gmail.com>
2024-08-11 22:36:50 +0900

    ♻️ :: add firebase crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,4 +20,5 @@ plugins {
     alias(libs.plugins.googleServices) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
 }
