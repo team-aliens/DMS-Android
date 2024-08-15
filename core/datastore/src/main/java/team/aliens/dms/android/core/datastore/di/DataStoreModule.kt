23b4a039fc4c367647c1c15ffec979ec35e304f5
uiel <parkuiery@gmail.com>
2024-08-15 22:18:47 +0900

    ♻️ :: DataStore 분리

--- core/datastore/src/main/java/team/aliens/dms/android/core/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/team/aliens/dms/android/core/datastore/di/DataStoreModule.kt
@@ -6,8 +6,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import team.aliens.dms.android.core.datastore.DeviceDataStore
+import team.aliens.dms.android.core.datastore.FeaturesDataStore
+import team.aliens.dms.android.core.datastore.JwtDataStore
 import team.aliens.dms.android.core.datastore.PreferencesDataStore
-import team.aliens.dms.android.core.datastore.dataStore
+import team.aliens.dms.android.core.datastore.deviceStore
+import team.aliens.dms.android.core.datastore.featuresStore
+import team.aliens.dms.android.core.datastore.jwtStore
 import javax.inject.Singleton
 
 @Module
@@ -16,7 +21,22 @@ internal object DataStoreModule {
 
     @Provides
     @Singleton
+    @JwtDataStore
     fun providePreferenceDataStore(
         @ApplicationContext context: Context,
-    ): PreferencesDataStore = context.dataStore
+    ): PreferencesDataStore = context.jwtStore
+
+    @Provides
+    @Singleton
+    @FeaturesDataStore
+    fun provideFeaturesDataStore(
+        @ApplicationContext context: Context,
+    ): PreferencesDataStore = context.featuresStore
+
+    @Provides
+    @Singleton
+    @DeviceDataStore
+    fun provideDeviceDataStore(
+        @ApplicationContext context: Context,
+    ): PreferencesDataStore = context.deviceStore
 }
