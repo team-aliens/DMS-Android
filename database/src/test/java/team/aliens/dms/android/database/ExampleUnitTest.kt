09713265e060b22b7c670ab51ad68a00f0aafc1a
uiel <parkuiery@gmail.com>
2024-08-20 11:32:21 +0900

    ♻️ :: ktlint

--- database/src/test/java/team/aliens/dms/android/database/ExampleUnitTest.kt
+++ database/src/test/java/team/aliens/dms/android/database/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package team.aliens.dms.android.database
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
