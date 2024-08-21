5419736513a49ae2d1e478dd863eb79fa73b280c
uiel <parkuiery@gmail.com>
2024-08-21 23:18:39 +0900

    ♻️ :: 접근 제한자 변경

--- data/src/main/java/team/aliens/dms/android/data/student/mapper/StudentMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/student/mapper/StudentMapper.kt
@@ -4,7 +4,7 @@ import team.aliens.dms.android.data.student.model.MyPage
 import team.aliens.dms.android.network.student.model.FetchMyPageResponse
 import team.aliens.dms.android.shared.model.Sex
 
-fun FetchMyPageResponse.toModel(): MyPage = MyPage(
+internal fun FetchMyPageResponse.toModel(): MyPage = MyPage(
     schoolName = this.schoolName,
     studentName = this.studentName,
     gradeClassNumber = this.gradeClassNumber,
