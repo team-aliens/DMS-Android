51be67a9691b95b4e4e21de55f67cfcc2881c113
uiel <parkuiery@gmail.com>
2024-08-21 23:18:35 +0900

    ♻️ :: 접근 제한자 변경

--- data/src/main/java/team/aliens/dms/android/data/school/mapper/SchoolMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/school/mapper/SchoolMapper.kt
@@ -3,7 +3,7 @@ package team.aliens.dms.android.data.school.mapper
 import team.aliens.dms.android.data.school.model.School
 import team.aliens.dms.android.network.school.model.FetchSchoolsResponse
 
-fun FetchSchoolsResponse.toModel(): List<School> =
+internal fun FetchSchoolsResponse.toModel(): List<School> =
     this.schools.map(FetchSchoolsResponse.SchoolResponse::toModel)
 
 private fun FetchSchoolsResponse.SchoolResponse.toModel(): School = School(
