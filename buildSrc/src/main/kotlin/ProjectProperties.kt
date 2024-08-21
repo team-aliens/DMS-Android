fdc3023a8e9bdda0ebf12621c9fd56854957ee72
uiel <parkuiery@gmail.com>
2024-08-21 09:02:16 +0900

    ⚡️ :: ProjectProperties 추가

---
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -0,0 +1,7 @@
+object ProjectProperties {
+    const val COMPILE_SDK = 34
+    const val MIN_SDK = 23
+    const val TARGET_SDK = 34
+    const val VERSION_CODE = 17
+    const val VERSION_NAME = "1.3.8"
+}
