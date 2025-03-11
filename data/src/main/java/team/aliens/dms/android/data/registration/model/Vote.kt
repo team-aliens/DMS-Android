04b92f363982993c6fe8d8de45cbb1908469e5b2
유섭 <yoonbk2001027@gmail.com>
2025-03-11 22:05:53 +0900

    feat :: enum vote type 추가

---
+++ data/src/main/java/team/aliens/dms/android/data/registration/model/Vote.kt
@@ -0,0 +1,4 @@
+package team.aliens.dms.android.data.registration.model
+
+enum class Vote {
+}
---
+++ feature/src/main/java/team/aliens/dms/android/feature/registration/RegistrationScreen.kt
@@ -0,0 +1,15 @@
+package team.aliens.dms.android.feature.registration
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import team.aliens.dms.android.feature.point.navigation.PointHistoryNavigator
+
+
+@Composable
+internal fun RegistrationScreen(
+    modifier: Modifier = Modifier,
+    navigator: PointHistoryNavigator,
+
+    ) {
+
+}
