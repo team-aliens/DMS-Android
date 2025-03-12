dc43e98c65a41451a980bbb9c2dc1a34f60f99fc
유섭 <yoonbk2001027@gmail.com>
2025-03-12 19:32:22 +0900

    feat :: voting api 생성

---
+++ network/src/main/java/team/aliens/dms/android/network/voting/apiservice/VotingApiService.kt
@@ -0,0 +1,38 @@
+package team.aliens.dms.android.network.voting.apiservice
+
+import retrofit2.Response
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+import team.aliens.dms.android.core.jwt.RequiresAccessToken
+import team.aliens.dms.android.network.voting.model.FetchAllVoteSearchResponse
+import team.aliens.dms.android.network.voting.model.FetchCheckVotingItemResponse
+import java.util.UUID
+
+internal interface VotingApiService {
+
+    @GET("/")
+    @RequiresAccessToken
+    suspend fun fetchAllVoteSearchResponse(): List<FetchAllVoteSearchResponse>
+
+    @GET("/option/{voting-topic-id}")
+    @RequiresAccessToken
+    suspend fun fetchCheckVotingItem(
+        @Path("voting-topic-id") votingTopicId: UUID
+    ): List<FetchCheckVotingItemResponse>
+
+    @POST("/student/{voting-topic-id}")
+    @RequiresAccessToken
+    suspend fun fetchCreateVotingItem(
+        @Path("voting-topic-id") votingTopicId: UUID,
+        @Query("selected_id") selectedId: UUID
+    ): Response<Unit>
+
+    @DELETE("/student/{vote_id}")
+    @RequiresAccessToken
+    suspend fun fetchDeleteVotingItem(
+        @Path("vote_id") voteId: UUID
+    ): Response<Unit>
+}
