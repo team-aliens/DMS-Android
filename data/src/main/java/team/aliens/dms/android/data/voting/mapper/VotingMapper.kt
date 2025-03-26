807eec5cac372a72135d731881957213d1da72ca
유섭 <yoonbk2001027@gmail.com>
2025-03-26 23:47:09 +0900

    refactor :: voting api response type change

--- data/src/main/java/team/aliens/dms/android/data/voting/mapper/VotingMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/voting/mapper/VotingMapper.kt
@@ -5,8 +5,10 @@ import team.aliens.dms.android.shared.date.toLocalDate
 import team.aliens.dms.android.data.voting.model.Vote
 import team.aliens.dms.android.data.voting.model.AllVoteSearch
 import team.aliens.dms.android.data.voting.model.CheckVotingItem
+import team.aliens.dms.android.data.voting.model.ModelStudentCandidates
 import team.aliens.dms.android.network.voting.model.FetchAllVoteSearchResponse
 import team.aliens.dms.android.network.voting.model.FetchCheckVotingItemResponse
+import team.aliens.dms.android.network.voting.model.FetchModelStudentCandidates
 
 internal fun FetchAllVoteSearchResponse.toModel(): List<AllVoteSearch> =
     this.votingTopics.map(FetchAllVoteSearchResponse.VoteSearchResponse::toModel)
@@ -25,4 +27,12 @@ internal fun FetchCheckVotingItemResponse.toModel(): CheckVotingItem =
     CheckVotingItem(
         id = this.id,
         optionName = this.optionName,
+    )
+
+internal fun FetchModelStudentCandidates.toModel(): ModelStudentCandidates =
+    ModelStudentCandidates(
+        id = this.id,
+        studentGcn = this.studentGcn,
+        name = this.name,
+        profileImageUrl = this.profileImageUrl
     )
--- data/src/main/java/team/aliens/dms/android/data/voting/repository/VotingRepositoryImpl.kt
+++ data/src/main/java/team/aliens/dms/android/data/voting/repository/VotingRepositoryImpl.kt
@@ -26,7 +26,7 @@ internal class VotingRepositoryImpl @Inject constructor(
         networkVotingDataSource.fetchDeleteVotingItem(voteId)
     }
 
-    override suspend fun fetchModelStudentCandidates(requestDate: LocalDate): ModelStudentCandidates {
-        networkVotingDataSource.fetchModelStudentCandidates(requestDate)
-    }
+    override suspend fun fetchModelStudentCandidates(requestDate: LocalDate): ModelStudentCandidates =
+        networkVotingDataSource.fetchModelStudentCandidates(requestDate).toModel()
+
 }
