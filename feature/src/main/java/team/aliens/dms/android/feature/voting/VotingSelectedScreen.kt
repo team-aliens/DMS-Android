f08e5deed53292606d5f8f5703599a8c9e6ba0f4
유섭 <yoonbk2001027@gmail.com>
2025-03-27 11:15:58 +0900

    feat :: voting detail screen ui 값 추가

--- feature/src/main/java/team/aliens/dms/android/feature/voting/VotingSelectedScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/voting/VotingSelectedScreen.kt
@@ -1,11 +1,13 @@
 package team.aliens.dms.android.feature.voting
 
 import android.util.Log
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.feature.voting.navigation.VotingNavigator
 
 @Destination
@@ -15,5 +17,8 @@ internal fun VotingSelectedScreen(
     navigator: VotingNavigator,
     votingDetailViewModel: VotingViewModel = hiltViewModel(),
 ) {
-    Log.d("TEST", "${votingDetailViewModel.stateFlow.collectAsState().value.selectedVoteList}")
+    Text(
+        text = "선택 투표",
+        style = DmsTheme.typography.body3,
+    )
 }
--- feature/src/main/java/team/aliens/dms/android/feature/voting/VotingStudentScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/voting/VotingStudentScreen.kt
@@ -1,9 +1,11 @@
 package team.aliens.dms.android.feature.voting
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.feature.voting.navigation.VotingNavigator
 
 @Destination
@@ -13,4 +15,8 @@ internal fun VotingStudentScreen(
     navigator: VotingNavigator,
     votingDetailViewModel: VotingViewModel = hiltViewModel(),
 ) {
+    Text(
+        text = "학생 투표",
+        style = DmsTheme.typography.body3,
+    )
 }
