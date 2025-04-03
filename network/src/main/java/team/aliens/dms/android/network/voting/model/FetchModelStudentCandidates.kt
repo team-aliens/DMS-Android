92e80381cc3b33d7bc026d2542464fad59bf84ac
유섭 <yoonbk2001027@gmail.com>
2025-04-03 11:46:17 +0900

    refactor :: modelstudent gcn 으로 변경

--- network/src/main/java/team/aliens/dms/android/network/voting/model/FetchModelStudentCandidates.kt
+++ network/src/main/java/team/aliens/dms/android/network/voting/model/FetchModelStudentCandidates.kt
@@ -1,8 +1,6 @@
 package team.aliens.dms.android.network.voting.model
 
-import android.graphics.ColorSpace.Model
 import com.google.gson.annotations.SerializedName
-import java.io.Serial
 import java.util.UUID
 
 data class FetchModelStudentCandidatesResponse(
@@ -10,7 +8,7 @@ data class FetchModelStudentCandidatesResponse(
 ) {
     data class ModelStudentCandidatesResponse(
         @SerializedName("id") val id: UUID,
-        @SerializedName("student_gcn") val studentGcn: Long,
+        @SerializedName("gcn") val studentGcn: Long,
         @SerializedName("name") val name: String,
         @SerializedName("profile_image_url") val profileImageUrl: String,
     )
