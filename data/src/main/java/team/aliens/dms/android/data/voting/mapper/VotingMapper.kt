87387e8a8a82b2e9c829e02c7f2445e2a8918592
유섭 <yoonbk2001027@gmail.com>
2025-04-03 11:03:30 +0900

    feat :: modelstudentlist mapper 추가

--- data/src/main/java/team/aliens/dms/android/data/voting/mapper/VotingMapper.kt
+++ data/src/main/java/team/aliens/dms/android/data/voting/mapper/VotingMapper.kt
@@ -1,15 +1,14 @@
 package team.aliens.dms.android.data.voting.mapper
 
-import java.util.UUID
-import team.aliens.dms.android.shared.date.toLocalDate
-import team.aliens.dms.android.data.voting.model.Vote
 import team.aliens.dms.android.data.voting.model.AllVoteSearch
 import team.aliens.dms.android.data.voting.model.CheckVotingItem
 import team.aliens.dms.android.data.voting.model.ModelStudentCandidates
+import team.aliens.dms.android.data.voting.model.Vote
 import team.aliens.dms.android.network.voting.model.FetchAllVoteSearchResponse
 import team.aliens.dms.android.network.voting.model.FetchCheckVotingItemResponse
-import team.aliens.dms.android.network.voting.model.FetchModelStudentCandidates
+import team.aliens.dms.android.network.voting.model.FetchModelStudentCandidatesResponse
 import team.aliens.dms.android.shared.date.toLocalDateTime
+import java.util.UUID
 
 internal fun FetchAllVoteSearchResponse.toModel(): List<AllVoteSearch> =
     this.votingTopics.map(FetchAllVoteSearchResponse.VoteSearchResponse::toModel)
@@ -30,10 +29,13 @@ internal fun FetchCheckVotingItemResponse.toModel(): CheckVotingItem =
         optionName = this.optionName,
     )
 
-internal fun FetchModelStudentCandidates.toModel(): ModelStudentCandidates =
+internal fun FetchModelStudentCandidatesResponse.toModel(): List<ModelStudentCandidates> =
+    this.students.map(FetchModelStudentCandidatesResponse.ModelStudentCandidatesResponse::toModel)
+
+private fun FetchModelStudentCandidatesResponse.ModelStudentCandidatesResponse.toModel(): ModelStudentCandidates =
     ModelStudentCandidates(
         id = this.id,
         studentGcn = this.studentGcn,
         name = this.name,
-        profileImageUrl = this.profileImageUrl
+        profileImageUrl = this.profileImageUrl,
     )
