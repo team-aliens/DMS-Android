0683f0703fb9678e23292513262244a0eb3a7039
유섭 <yoonbk2001027@gmail.com>
2025-04-03 11:02:21 +0900

    fix :: modelStudent dto 데이터 변경

--- network/src/main/java/team/aliens/dms/android/network/voting/model/FetchModelStudentCandidates.kt
+++ network/src/main/java/team/aliens/dms/android/network/voting/model/FetchModelStudentCandidates.kt
@@ -1,11 +1,18 @@
 package team.aliens.dms.android.network.voting.model
 
+import android.graphics.ColorSpace.Model
 import com.google.gson.annotations.SerializedName
+import java.io.Serial
 import java.util.UUID
 
-data class FetchModelStudentCandidates(
-    @SerializedName("id") val id: UUID,
-    @SerializedName("student_gcn") val studentGcn: Long,
-    @SerializedName("name") val name: String,
-    @SerializedName("profile_image_url") val profileImageUrl: String,
-)
+data class FetchModelStudentCandidatesResponse(
+    @SerializedName("students") val students: List<ModelStudentCandidatesResponse>
+) {
+    data class ModelStudentCandidatesResponse(
+        @SerializedName("id") val id: UUID,
+        @SerializedName("student_gcn") val studentGcn: Long,
+        @SerializedName("name") val name: String,
+        @SerializedName("profile_image_url") val profileImageUrl: String,
+    )
+}
+
