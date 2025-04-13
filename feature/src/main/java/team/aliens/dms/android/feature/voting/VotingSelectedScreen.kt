75ef8dee12419f0c84310027db95b3f3ea40cde5
유섭 <yoonbk2001027@gmail.com>
2025-04-13 20:08:34 +0900

    feat :: selected screen topicProfile 생성

--- feature/src/main/java/team/aliens/dms/android/feature/voting/VotingSelectedScreen.kt
+++ feature/src/main/java/team/aliens/dms/android/feature/voting/VotingSelectedScreen.kt
@@ -1,8 +1,13 @@
 package team.aliens.dms.android.feature.voting
 
+import android.adservices.topics.Topic
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,24 +15,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
+import team.aliens.dms.android.core.designsystem.ButtonDefaults
 import team.aliens.dms.android.core.designsystem.ContainedButton
 import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.core.designsystem.DmsTopAppBar
 import team.aliens.dms.android.core.designsystem.Scaffold
+import team.aliens.dms.android.core.designsystem.TextButton
 import team.aliens.dms.android.core.ui.PaddingDefaults
 import team.aliens.dms.android.core.ui.bottomPadding
 import team.aliens.dms.android.core.ui.horizontalPadding
@@ -93,9 +107,15 @@ internal fun VotingSelectedScreen(
             LazyColumn(
                 modifier = modifier
                     .verticalPadding(PaddingDefaults.Small)
+                    .padding(top = PaddingDefaults.Small),
             ) {
                 items(uiState.votingTopicCheckList) {
-
+                    TopicProfile(
+                        topicOption = it.optionName,
+                        onClick = {
+                            // TODO : id 저장
+                        },
+                    )
                 }
             }
             Spacer(modifier = Modifier.weight(1f))
@@ -105,10 +125,54 @@ internal fun VotingSelectedScreen(
                     .fillMaxWidth()
                     .horizontalPadding()
                     .bottomPadding(),
-                onClick = navigator::navigateUp,
+                onClick = {
+                    // TODO : 표 생성
+                },
             ) {
                 Text(text = "투표하기")
             }
+        }
+    }
+}
+
+@Composable
+private fun TopicProfile(
+    topicOption: String,
+    onClick: () -> Unit,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    var isClicked by remember { mutableStateOf(false) }
+    val color = if (isPressed) DmsTheme.colorScheme.primary else Color.Unspecified
+
+    HorizontalDivider(
+        thickness = 1.dp,
+        color = DmsTheme.colorScheme.onSurfaceVariant,
+    )
+    TextButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = color),
+        interactionSource = interactionSource,
+        onClick = {
+            isClicked = !isClicked
+            onClick()
+        },
+        colors = if(isClicked) ButtonDefaults.containedButtonColors() else ButtonDefaults.buttonColors(
+            containerColor = Color.Unspecified,
+        ),
+        shape = RectangleShape,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = topicOption,
+                textAlign = TextAlign.End,
+                color = Color.Black,
+            )
         }
     }
 }
