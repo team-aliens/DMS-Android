cec9353713b2727adb4423420634b762270e9d43
유섭 <yoonbk2001027@gmail.com>
2025-06-12 10:59:38 +0900

    refactor :: 봉사신청 화면 파일 생성

---
+++ feature/src/main/java/team/aliens/dms/android/feature/volunteers/VolunteersScreen.kt
@@ -0,0 +1,72 @@
+package team.aliens.dms.android.feature.volunteers
+
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import team.aliens.dms.android.core.designsystem.DmsTheme
+import team.aliens.dms.android.core.designsystem.DmsTopAppBar
+import team.aliens.dms.android.core.designsystem.Scaffold
+import team.aliens.dms.android.feature.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VolunteerScreen(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            DmsTopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.sign_up_terms))
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
+                            contentDescription = stringResource(id = R.string.top_bar_back_button),
+                        )
+                    }
+                },
+            )
+        },
+    ) { padValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padValues)
+                .imePadding()
+        ) {
+            AndroidView(
+                modifier = Modifier.weight(1f),
+                factory = {
+                    WebView(it).apply {
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                        )
+                        webViewClient = WebViewClient()
+
+                        settings.javaScriptEnabled = true
+
+                        loadUrl( "https://webview.dms-dsm.com/volunteers/application")
+                    }
+                },
+            )
+        }
+    }
+}
