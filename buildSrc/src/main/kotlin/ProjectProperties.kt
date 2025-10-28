6b269a593bb36b7007adfcca43dd6c48e9eb0599
유섭 <yoonbk2001027@gmail.com>
2025-10-28 14:43:03 +0900

    chore :: ktlint Check

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -1,7 +1,7 @@
 object ProjectProperties {
-    const val COMPILE_SDK = 34
+    const val COMPILE_SDK = 35
     const val MIN_SDK = 23
-    const val TARGET_SDK = 34
-    const val VERSION_CODE = 24
-    const val VERSION_NAME = "1.5.0"
+    const val TARGET_SDK = 35
+    const val VERSION_CODE = 25
+    const val VERSION_NAME = "1.5.1"
 }
