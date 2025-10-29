b6ce4f3bc8c61483719f789cc8401ae3e68d3b6f
유섭 <yoonbk2001027@gmail.com>
2025-10-29 20:21:42 +0900

    refactor :: 점(.)만 허용되게 정규식 수정

--- shared/validator/src/main/java/team/aliens/dms/android/shared/validator/EmailValidator.kt
+++ shared/validator/src/main/java/team/aliens/dms/android/shared/validator/EmailValidator.kt
@@ -2,7 +2,7 @@ package team.aliens.dms.android.shared.validator
 
 object EmailValidator : Validator<String>() {
     override val regex =
-        Regex("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}\$")
+        Regex("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}\$")
 
     override fun validate(value: String): Boolean = value.matches(regex)
 }
