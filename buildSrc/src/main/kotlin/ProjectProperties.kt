7fedd20921753b18beff4ef3312af9ab1652b35d
유섭 <yoonbk2001027@gmail.com>
2025-11-06 23:41:19 +0900

    chore :: 버전 코드 업데이트

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -2,6 +2,6 @@ object ProjectProperties {
     const val COMPILE_SDK = 35
     const val MIN_SDK = 23
     const val TARGET_SDK = 35
-    const val VERSION_CODE = 26
+    const val VERSION_CODE = 27
     const val VERSION_NAME = "1.5.2"
 }
