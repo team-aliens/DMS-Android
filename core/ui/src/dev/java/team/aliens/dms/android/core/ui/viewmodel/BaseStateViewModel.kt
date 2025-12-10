9c18a5c742f3607864b68341870dd827c2383d11
유섭 <yoonbk2001027@gmail.com>
2025-12-10 19:33:36 +0900

    feat :: BaseStateViewModel 구현

---
+++ core/ui/src/dev/java/team/aliens/dms/android/core/ui/viewmodel/BaseStateViewModel.kt
@@ -0,0 +1,44 @@
+package team.aliens.dms.android.core.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+/**
+ * Intent 패턴이 제거된 간소화된 ViewModel
+ *
+ * BaseMviViewModel과 달리 Intent 없이 직접 메서드 호출 방식을 사용합니다.
+ *
+ * @param S UiState 타입
+ * @param E SideEffect 타입 (필요 없으면 Unit 사용)
+ */
+abstract class BaseStateViewModel<S, E>(initialState: S) : ViewModel() {
+
+    private val _stateFlow: MutableStateFlow<S> = MutableStateFlow(initialState)
+    val stateFlow: StateFlow<S> = _stateFlow.asStateFlow()
+
+    private val sideEffectChannel: Channel<E> = Channel()
+    val sideEffectFlow: Flow<E> = sideEffectChannel.receiveAsFlow()
+
+    protected fun setState(newState: () -> S) {
+        _stateFlow.update {
+            newState()
+        }
+    }
+
+    protected fun postSideEffect(sideEffect: E): Job =
+        viewModelScope.launch { sideEffectChannel.send(sideEffect) }
+
+    override fun onCleared() {
+        super.onCleared()
+        sideEffectChannel.close()
+    }
+}
