923e5f40eb7f714f231c6e68f56db29b70675663
유섭 <yoonbk2001027@gmail.com>
2025-12-16 16:00:26 +0900

    refactor :: 시스템 바 투명으로 적용

--- app/src/dev/kotlin/team/aliens/dms/android/app/EdgeToEdgeConfig.kt
+++ app/src/dev/kotlin/team/aliens/dms/android/app/EdgeToEdgeConfig.kt
@@ -1,11 +1,16 @@
 package team.aliens.dms.android.app
 
+import android.graphics.Color
 import android.os.Build
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 
 fun ComponentActivity.setEdgeToEdgeConfig() {
-    enableEdgeToEdge()
+    enableEdgeToEdge(
+        statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+        navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+    )
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
         // Force the 3-button navigation bar to be transparent
         // See: https://developer.android.com/develop/ui/views/layout/edge-to-edge#create-transparent
