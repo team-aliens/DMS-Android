38736258094caa97c17768ed2cdc4f737b7f780a
유섭 <yoonbk2001027@gmail.com>
2025-12-22 17:23:39 +0900

    feat :: 탑 app bar, 캘린더 디자인 시스템 구현

---
+++ core/design-system/src/dev/java/team/aliens/dms/android/core/designsystem/appbar/DmsTopAppBar.kt
@@ -0,0 +1,83 @@
+package team.aliens.dms.android.core.designsystem.appbar
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import team.aliens.dms.android.core.designsystem.DmsTheme
+import team.aliens.dms.android.core.designsystem.bodyB
+import team.aliens.dms.android.core.designsystem.button.DmsIconButton
+import team.aliens.dms.android.core.designsystem.foundation.DmsIcon
+
+@Composable
+fun DmsTopAppBar(
+    modifier: Modifier = Modifier,
+    showLogo: Boolean = false,
+    onBackPressed: (() -> Unit)? = null,
+    actions: (@Composable RowScope.() -> Unit)? = null,
+    title: String? = null,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Transparent)
+            .padding(
+                horizontal = 24.dp,
+                vertical = 12.dp,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = if (showLogo || onBackPressed != null) Arrangement.SpaceBetween else Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            if (showLogo) {
+                Image(
+                    painter = painterResource(
+                        if (isSystemInDarkTheme()) {
+                            DmsIcon.SymbolDark
+                        } else {
+                            DmsIcon.SymbolLight
+                        },
+                    ),
+                    contentDescription = null,
+                )
+            }
+            onBackPressed?.let {
+                DmsIconButton(
+                    resource = DmsIcon.Backward,
+                    tint = DmsTheme.colorScheme.scrim,
+                    onClick = it,
+                )
+            }
+            actions?.let {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = it,
+                )
+            }
+        }
+        title?.let {
+            Text(
+                text = it,
+                style = DmsTheme.typography.bodyB,
+                color = DmsTheme.colorScheme.onBackground,
+            )
+        }
+    }
+}
---
+++ core/design-system/src/dev/java/team/aliens/dms/android/core/designsystem/calendar/DmsCalendar.kt
@@ -0,0 +1,126 @@
+package team.aliens.dms.android.core.designsystem.calendar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import team.aliens.dms.android.core.designsystem.DmsTheme
+import team.aliens.dms.android.core.designsystem.bodyM
+import team.aliens.dms.android.core.designsystem.button.ButtonColor
+import team.aliens.dms.android.core.designsystem.button.ButtonType
+import team.aliens.dms.android.core.designsystem.button.DmsButton
+import team.aliens.dms.android.core.designsystem.util.clickable
+import team.aliens.dms.android.core.designsystem.horizontalPadding
+import team.aliens.dms.android.core.designsystem.verticalPadding
+import java.time.LocalDate
+import java.time.YearMonth
+import kotlin.time.ExperimentalTime
+
+@OptIn(ExperimentalTime::class)
+@Composable
+fun DmsCalendar(
+    modifier: Modifier = Modifier,
+    selectDate: LocalDate,
+    onSelectedDateChange: (newDate: LocalDate) -> Unit,
+) {
+    var updateDate by remember { mutableStateOf(selectDate) }
+    val calendarState = rememberCalendarState(
+        startMonth = YearMonth.now().minusYears(1),
+        endMonth = YearMonth.now().plusYears(1),
+        firstVisibleMonth = YearMonth.now(),
+    )
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(DmsTheme.colorScheme.surfaceTint)
+            .navigationBarsPadding(),
+    ) {
+        HorizontalCalendar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.4f),
+            state = calendarState,
+            dayContent = { day ->
+                Day(
+                    day = day,
+                    isSelected = day.date == updateDate,
+                    onDayClick = { updateDate = it },
+                )
+            },
+        )
+
+        DmsButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalPadding(24.dp)
+                .verticalPadding(12.dp),
+            text = "확인",
+            buttonType = ButtonType.Contained,
+            buttonColor = ButtonColor.Primary,
+            onClick = { onSelectedDateChange(updateDate) },
+        )
+    }
+}
+
+@Composable
+private fun Day(
+    modifier: Modifier = Modifier,
+    day: CalendarDay,
+    isSelected: Boolean,
+    onDayClick: (LocalDate) -> Unit,
+) {
+    val isOtherMonthDay = day.position != DayPosition.MonthDate
+    val textColor = when {
+        isSelected -> DmsTheme.colorScheme.surface
+        isOtherMonthDay -> DmsTheme.colorScheme.onSurfaceVariant
+        else -> DmsTheme.colorScheme.inverseSurface
+    }
+    val backgroundColor = when {
+        isSelected -> DmsTheme.colorScheme.onPrimaryContainer
+        else -> Color.Transparent
+    }
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(onClick = { onDayClick(day.date) })
+            .padding(vertical = 12.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(26.dp)
+                .background(
+                    color = backgroundColor,
+                    shape = CircleShape,
+                ),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = day.date.toString(),
+                style = DmsTheme.typography.bodyM,
+                color = textColor,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
