6c91e8c8bc5ed487370396334d92a88a4ebf9e20
유섭 <yoonbk2001027@gmail.com>
2025-12-24 19:02:02 +0900

    refactor :: dev에서 안 쓰이는 data 모듈 파일 제거

--- data/src/dev/kotlin/team.aliens.dms.android.data/file/mapper/FileMapper.kt
+++
@@ -1,9 +0,0 @@
-package team.aliens.dms.android.data.file.mapper
-
-import team.aliens.dms.android.data.file.model.PresignedFileUrl
-import team.aliens.dms.android.network.file.model.FetchPresignedUrlResponse
-
-internal fun FetchPresignedUrlResponse.toModel(): PresignedFileUrl = PresignedFileUrl(
-    fileUploadUrl = this.fileUploadUrl,
-    fileUrl = this.fileUrl,
-)
