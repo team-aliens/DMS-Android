5c57a632f15f8eb34ac810cd66b23cbfac127124
유섭 <yoonbk2001027@gmail.com>
2025-12-24 19:13:16 +0900

    feat :: home screen 토스트 구현

--- feature/src/dev/kotlin/team/aliens/dms/android/feature/main/home/navigation/HomeRoute.kt
+++ feature/src/dev/kotlin/team/aliens/dms/android/feature/main/home/navigation/HomeRoute.kt
@@ -2,17 +2,19 @@ package team.aliens.dms.android.feature.main.home.navigation
 
 import androidx.compose.runtime.Composable
 import kotlinx.serialization.Serializable
+import team.aliens.dms.android.core.designsystem.snackbar.DmsSnackBarType
 import team.aliens.dms.android.feature.main.home.ui.Home
 
 @Composable
 fun HomeRoute(
     onNavigateMeal: () -> Unit,
+    onShowSnackBar: (DmsSnackBarType, String) -> Unit,
 ) {
     Home(
         onNavigateNotice = { },
         onNavigateNoticeDetail = { },
         onNavigatePointHistory = { },
         onNavigateMeal = onNavigateMeal,
-        onShowSnackBar = { _, _ -> },
+        onShowSnackBar = onShowSnackBar,
     )
 }
