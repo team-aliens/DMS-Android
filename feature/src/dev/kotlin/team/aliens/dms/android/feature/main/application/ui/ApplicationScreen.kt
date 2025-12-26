489f18954f9e336590b317195f3f96d30bbf4f8c
유섭 <yoonbk2001027@gmail.com>
2025-12-26 10:06:45 +0900

    feat :: application screen navhost 연결

--- feature/src/dev/kotlin/team/aliens/dms/android/feature/main/application/ui/ApplicationScreen.kt
+++ feature/src/dev/kotlin/team/aliens/dms/android/feature/main/application/ui/ApplicationScreen.kt
@@ -8,28 +8,57 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.launch
 import team.aliens.dms.android.core.designsystem.DmsTheme
+import team.aliens.dms.android.core.designsystem.snackbar.DmsSnackBarType
 import team.aliens.dms.android.core.designsystem.tab.DmsTab
 import team.aliens.dms.android.core.designsystem.tab.DmsTabRow
+import team.aliens.dms.android.data.voting.model.AllVoteSearch
+import team.aliens.dms.android.feature.main.application.viewmodel.ApplicationState
+import team.aliens.dms.android.feature.main.application.viewmodel.ApplicationViewModel
 import team.aliens.dms.kmp.feature.application.ui.component.ApplicationContent
 import team.aliens.dms.kmp.feature.application.ui.component.VoteContent
 
 @Composable
-internal fun Application() {
-    ApplicationScreen()
+internal fun Application(
+    onNavigateRemainApplication: () -> Unit,
+    onNavigateOutingApplication: () -> Unit,
+    onNavigateVolunteerApplication: () -> Unit,
+    onNavigateVote: (AllVoteSearch) -> Unit,
+    onShowSnackBar: (DmsSnackBarType, String) -> Unit,
+) {
+    val viewModel: ApplicationViewModel = hiltViewModel()
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+
+
+    ApplicationScreen(
+        state = state,
+        onNavigateRemainApplication = onNavigateRemainApplication,
+        onNavigateOutingApplication = { onShowSnackBar(DmsSnackBarType.SUCCESS, "준비중인 기능이에요") },
+        onNavigateVolunteerApplication = { onShowSnackBar(DmsSnackBarType.SUCCESS, "준비중인 기능이에요") },
+        onNavigateVote = onNavigateVote,
+    )
 }
 
 @Composable
-private fun ApplicationScreen() {
+private fun ApplicationScreen(
+    state: ApplicationState,
+    onNavigateRemainApplication: () -> Unit,
+    onNavigateOutingApplication: () -> Unit,
+    onNavigateVolunteerApplication: () -> Unit,
+    onNavigateVote: (AllVoteSearch) -> Unit,
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(DmsTheme.colorScheme.background)
-            .statusBarsPadding(),
+            .background(DmsTheme.colorScheme.background),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         val tabData = listOf(
---
+++ feature/src/dev/kotlin/team/aliens/dms/android/feature/main/application/viewmodel/ApplicationViewModel.kt
@@ -0,0 +1,35 @@
+package team.aliens.dms.android.feature.main.application.viewmodel
+
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import team.aliens.dms.android.core.ui.viewmodel.BaseStateViewModel
+import team.aliens.dms.android.data.voting.model.AllVoteSearch
+import team.aliens.dms.android.data.voting.repository.VotingRepository
+import javax.inject.Inject
+
+@HiltViewModel
+internal class ApplicationViewModel @Inject constructor(
+    private val votingRepository: VotingRepository,
+) : BaseStateViewModel<ApplicationState, Unit>(ApplicationState()) {
+
+    init {
+        getAllVotes()
+    }
+
+    private fun getAllVotes() {
+        viewModelScope.launch(Dispatchers.IO) {
+            votingRepository.fetchAllVoteSearch()
+                .onSuccess {
+                    setState { it.copy(votes = it.votes) }
+                }.onFailure {
+//                    Logger.a(it) { it.message.toString() }
+                }
+        }
+    }
+}
+
+data class ApplicationState(
+    val votes: List<AllVoteSearch> = emptyList(),
+)
