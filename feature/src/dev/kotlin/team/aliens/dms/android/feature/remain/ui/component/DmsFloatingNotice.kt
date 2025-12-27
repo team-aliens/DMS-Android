60285c52e118c6bef3ceba15b9c41a3eb61b12ae
유섭 <yoonbk2001027@gmail.com>
2025-12-27 19:23:29 +0900

    refactor :: maxLine에 맞게 크기 지정하도록 변경

--- feature/src/dev/kotlin/team/aliens/dms/android/feature/remain/ui/component/DmsFloatingNotice.kt
+++ feature/src/dev/kotlin/team/aliens/dms/android/feature/remain/ui/component/DmsFloatingNotice.kt
@@ -1,5 +1,6 @@
 package team.aliens.dms.android.feature.remain.ui.component
 
+import android.R
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -7,13 +8,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.foundation.text.TextAutoSizeDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.widget.TextViewCompat
 import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.core.designsystem.foundation.DmsIcon
 import team.aliens.dms.android.core.designsystem.labelM
@@ -24,6 +32,7 @@ fun DmsFloatingNotice(
     text: String,
     @DrawableRes iconResource: Int = DmsIcon.Notification,
 ) {
+
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -41,10 +50,16 @@ fun DmsFloatingNotice(
             painter = painterResource(iconResource),
             contentDescription = null,
         )
-        Text(
+        BasicText(
             text = text,
-            style = DmsTheme.typography.labelM,
-            color = DmsTheme.colorScheme.onBackground,
+            style = DmsTheme.typography.labelM.copy(
+                color = DmsTheme.colorScheme.onBackground,
+            ),
+            autoSize = TextAutoSize.StepBased(
+                minFontSize = 1.sp,
+                maxFontSize = 50.sp,
+            ),
+            maxLines = 1,
         )
     }
 }
