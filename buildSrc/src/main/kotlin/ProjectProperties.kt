cff3a34ee1b6bda4a0b4a234af1350016fed52a8
유섭 <yoonbk2001027@gmail.com>
2025-12-29 21:58:00 +0900

    refactor :: minSdk 26으로 변경

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -1,6 +1,6 @@
 object ProjectProperties {
     const val COMPILE_SDK = 36
-    const val MIN_SDK = 24
+    const val MIN_SDK = 26
     const val TARGET_SDK = 36
     const val VERSION_CODE = 28
     const val VERSION_NAME = "1.5.3"
