585d95f67b759eb15cdf78477cbba651046b164c
유섭 <yoonbk2001027@gmail.com>
2026-01-08 14:17:08 +0900

    refactor :: file name notification -> notice 로 변경

--- feature/src/dev/kotlin/team/aliens/dms/android/feature/notification/navigation/NotifiacationRoute.kt
+++
@@ -1,9 +0,0 @@
-package team.aliens.dms.android.feature.notification.navigation
-
-import androidx.compose.runtime.Composable
-import team.aliens.dms.android.feature.notification.ui.Notification
-
-@Composable
-fun NotificationRoute() {
-    Notification()
-}
