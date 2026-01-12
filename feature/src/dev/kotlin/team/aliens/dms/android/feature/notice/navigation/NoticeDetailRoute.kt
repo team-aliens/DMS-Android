21718d7b5995bb05b351cdf0eefb3d270ade0d53
유섭 <yoonbk2001027@gmail.com>
2026-01-12 19:02:11 +0900

    refactor :: 공지사항 디테일 이름 및 파일 루트 변경

--- feature/src/dev/kotlin/team/aliens/dms/android/feature/notice/navigation/NoticeDetailRoute.kt
+++ feature/src/dev/kotlin/team/aliens/dms/android/feature/notice/navigation/NoticeDetailRoute.kt
@@ -1,9 +1,16 @@
 package team.aliens.dms.android.feature.notice.navigation
 
 import androidx.compose.runtime.Composable
+import team.aliens.dms.android.core.designsystem.snackbar.DmsSnackBarType
 import team.aliens.dms.android.feature.notice.ui.NoticeDetail
 
 @Composable
-fun NoticeDetailRoute() {
-    NoticeDetail()
+fun NoticeDetailRoute(
+    onNavigateBack: () -> Unit,
+    onShowSnackBar: (DmsSnackBarType, String) -> Unit
+) {
+    NoticeDetail(
+        onNavigateBack = onNavigateBack,
+        onShowSnackBar = onShowSnackBar,
+    )
 }
