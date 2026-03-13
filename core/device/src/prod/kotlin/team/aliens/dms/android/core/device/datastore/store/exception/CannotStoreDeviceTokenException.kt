303c298a69cfc463f7e81826e38297e008e921bd
유섭 <yoonbk2001027@gmail.com>
2026-03-13 14:15:42 +0900

    refactor :: core 모듈 빌드 플레이버 및 중복 소스 정리

--- core/device/src/prod/kotlin/team/aliens/dms/android/core/device/datastore/store/exception/CannotStoreDeviceTokenException.kt
+++
@@ -1,5 +0,0 @@
-package team.aliens.dms.android.core.device.datastore.store.exception
-
-import team.aliens.dms.android.core.datastore.exception.TransformFailureException
-
-class CannotStoreDeviceTokenException : TransformFailureException("Cannot store deviceToken")
--- core/widget/src/prod/kotlin/team/aliens/dms/android/core/widget/MealState.kt
+++
@@ -1,7 +0,0 @@
-package team.aliens.dms.android.core.widget.meal
-
-data class MealState(
-    val mealType: MealType = MealType.Breakfast,
-    val meal: List<String> = emptyList(),
-    val calories: String = "",
-)
