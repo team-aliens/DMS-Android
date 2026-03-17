bb43f1405dc52648c5d8269c982ae2baaeee422f
유섭 <yoonbk2001027@gmail.com>
2026-03-17 22:39:54 +0900

    feat :: pointerInput 적용

--- feature/src/main/kotlin/team/aliens/dms/android/feature/signup/ui/SetIdScreen.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/signup/ui/SetIdScreen.kt
@@ -1,6 +1,7 @@
 package team.aliens.dms.android.feature.signup.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import team.aliens.dms.android.core.designsystem.DmsTheme
@@ -38,6 +41,7 @@ internal fun SetIdScreen(
 ) {
     val viewModel: SetIdViewModel = hiltViewModel()
     val state by viewModel.uiState.collectAsState()
+    val focusManager = LocalFocusManager.current
 
     LaunchedEffect(Unit) {
         viewModel.initialize(signUpData)
@@ -60,6 +64,7 @@ internal fun SetIdScreen(
         onNextClick = viewModel::onNextClick,
         state = state,
         onIdChange = viewModel::setId,
+        onClearFocus = { focusManager.clearFocus() },
     )
 }
 
@@ -69,13 +74,17 @@ private fun SetIdContent(
     onNextClick: () -> Unit,
     state: SetIdState,
     onIdChange: (String) -> Unit,
+    onClearFocus: () -> Unit,
 ) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(DmsTheme.colorScheme.surfaceTint)
             .statusBarsPadding()
-            .navigationBarsPadding(),
+            .navigationBarsPadding()
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = { onClearFocus() })
+            },
     ) {
         DmsTopAppBar(
             title = "회원가입",
