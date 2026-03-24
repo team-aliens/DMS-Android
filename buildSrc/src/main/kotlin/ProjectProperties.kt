3644c8890e7c23206f752dd37a02d11622d90579
유섭 <yoonbk2001027@gmail.com>
2026-03-24 16:54:06 +0900

    build(version) :: 앱 버전 메타데이터 값 갱신

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -1,6 +1,6 @@
 object ProjectProperties {
     const val COMPILE_SDK = 36
-    const val MIN_SDK = 26
+    const val MIN_SDK = 28
     const val TARGET_SDK = 36
     const val VERSION_CODE = 30
     const val VERSION_NAME = "2.1.0"
