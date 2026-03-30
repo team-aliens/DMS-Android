ca356289d2e5c2cae4399ee444f7254ec8ad6acf
유섭 <yoonbk2001027@gmail.com>
2026-03-30 16:57:02 +0900

    chore :: unused import 제거

--- feature/src/main/kotlin/team/aliens/dms/android/feature/meal/component/MealContent.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/meal/component/MealContent.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
