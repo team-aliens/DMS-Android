dde675761b964703a85d020041d9accc84e7a839
ashxom <anseha090115@naver.com>
2026-04-07 19:49:36 +0900

    refactor :: accountId 입력 시 trim 처리 및 sign-in 로직 정리

--- feature/src/main/kotlin/team/aliens/dms/android/feature/signin/viewmodel/SignInViewModel.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/signin/viewmodel/SignInViewModel.kt
@@ -24,7 +24,7 @@ internal class SignInViewModel @Inject constructor(
     internal fun setAccountId(accountId: String) {
         setState {
             it.copy(
-                accountId = accountId,
+                accountId = accountId.trim(),
                 showAccountIdErrorDescription = false,
             )
         }
@@ -62,7 +62,7 @@ internal class SignInViewModel @Inject constructor(
             with(uiState.value) {
                 setState { this.copy(isLoading = true, buttonEnabled = false) }
                 authRepository.signIn(
-                    accountId = this.accountId.trim(),
+                    accountId = this.accountId,
                     password = this.password,
                     deviceToken = deviceToken,
                 ).onSuccess {
