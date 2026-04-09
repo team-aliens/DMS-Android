49a32b8d061af2919ddcfbd5b9f8cde087d7a4bc
유섭 <yoonbk2001027@gmail.com>
2026-04-10 01:26:41 +0900

    refactor :: 네트워크 인증 계열 datasource 예외 처리 통일

--- network/src/main/kotlin/team/aliens/dms/android/network/user/datasource/NetworkUserDataSourceImpl.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/user/datasource/NetworkUserDataSourceImpl.kt
@@ -2,19 +2,19 @@ package team.aliens.dms.android.network.user.datasource
 
 import team.aliens.dms.android.network.user.apiservice.UserApiService
 import team.aliens.dms.android.network.user.model.EditPasswordRequest
-import team.aliens.dms.android.shared.exception.util.suspendRunCatching
+import team.aliens.dms.android.shared.exception.util.runCatchingCancellable
 import javax.inject.Inject
 
 internal class NetworkUserDataSourceImpl @Inject constructor(
     private val userApiService: UserApiService,
 ) : NetworkUserDataSource() {
     override suspend fun editPassword(request: EditPasswordRequest): Result<Unit> =
-        suspendRunCatching {
+        runCatchingCancellable {
             userApiService.editPassword(request)
         }
 
     override suspend fun comparePassword(password: String): Result<Unit> =
-        suspendRunCatching {
+        runCatchingCancellable {
             userApiService.comparePassword(password)
         }
 }
