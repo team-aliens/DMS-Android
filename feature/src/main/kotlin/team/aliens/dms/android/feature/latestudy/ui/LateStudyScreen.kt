fc38dfe6f3d9cfccc05d564678e8d2553501513d
ashxom <anseha090115@naver.com>
2026-04-11 16:04:45 +0900

    feat :: 새벽 자습 신청 화면 레이아웃 추가

---
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/LateStudyScreen.kt
@@ -0,0 +1,11 @@
+package team.aliens.dms.android.feature.latestudy.ui
+
+import androidx.compose.runtime.Composable
+import team.aliens.dms.android.core.designsystem.snackbar.DmsSnackBarType
+
+@Composable
+fun LateStudyScreen (
+    onBack: () -> Unit,
+    onShowSnackBar: (DmsSnackBarType, String) -> Unit,
+) {
+}
