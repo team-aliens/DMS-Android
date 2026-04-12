d4f6a8681d8559deaa1881026575c8a976263e1f
ashxom <anseha090115@naver.com>
2026-04-12 21:04:30 +0900

    feat :: 새벽 자습 신청 카드 컴포넌트 추가

--- feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/LateStudyScreen.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/LateStudyScreen.kt
@@ -1,11 +1,29 @@
-package team.aliens.dms.android.feature.latestudy.ui
+package team.aliens.dms.android.feature.latestudy.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import team.aliens.dms.android.core.designsystem.snackbar.DmsSnackBarType
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import team.aliens.dms.android.core.designsystem.DmsTheme
 
 @Composable
-fun LateStudyScreen (
-    onBack: () -> Unit,
-    onShowSnackBar: (DmsSnackBarType, String) -> Unit,
+fun LateStudySectionCard(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit,
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = DmsTheme.colorScheme.surface,
+                shape = RoundedCornerShape(24.dp),
+            )
+            .padding(16.dp),
+        content = content,
+    )
 }
---
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/component/LateStudyTypeItem.kt
@@ -0,0 +1,4 @@
+package team.aliens.dms.android.feature.latestudy.ui.component
+
+class LateStudyTypeItem {
+}
