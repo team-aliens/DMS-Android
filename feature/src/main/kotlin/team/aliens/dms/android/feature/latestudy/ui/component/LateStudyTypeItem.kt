8d4b90976a30f46fa4cfabdd6ae789d34235c925
ashxom <anseha090115@naver.com>
2026-04-12 21:14:27 +0900

    feat :: 새벽 자습 신청 유형 선택 컴포넌트 추가

--- feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/component/LateStudyTypeItem.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/component/LateStudyTypeItem.kt
@@ -1,4 +1,42 @@
 package team.aliens.dms.android.feature.latestudy.ui.component
 
-class LateStudyTypeItem {
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import team.aliens.dms.android.core.designsystem.DmsTheme
+
+@Composable
+fun LateStudyTypeItem(
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(vertical = 6.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = text,
+            color = DmsTheme.colorScheme.onBackground,
+            style = DmsTheme.typography.body1,
+        )
+
+        RadioButton(
+            selected = selected,
+            onClick = onClick,
+        )
+    }
 }
