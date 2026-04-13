0fa950ec9cc97c39ef3d7a0d2d0f6adbf6e335cf
ashxom <anseha090115@naver.com>
2026-04-13 09:10:34 +0900

    style :: 유형 텍스트 색상 조정

--- feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/component/LateStudyTypeItem.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/component/LateStudyTypeItem.kt
@@ -1,16 +1,19 @@
 package team.aliens.dms.android.feature.latestudy.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import team.aliens.dms.android.core.designsystem.DmsTheme
 import team.aliens.dms.android.core.designsystem.bodyM
@@ -25,6 +28,9 @@ fun LateStudyTypeItem(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .background(
+                color = if (selected) DmsTheme.colorScheme.primary else Color.Transparent,
+            )
             .clickable(onClick = onClick)
             .padding(vertical = 2.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -39,7 +45,11 @@ fun LateStudyTypeItem(
         RadioButton(
             selected = selected,
             onClick = onClick,
-            modifier = Modifier.scale(0.8f)
+            modifier = Modifier.scale(0.8f),
+            colors = RadioButtonDefaults.colors(
+                selectedColor = DmsTheme.colorScheme.onPrimaryContainer,
+                unselectedColor = DmsTheme.colorScheme.onSurfaceVariant,
+            ),
         )
     }
 }
