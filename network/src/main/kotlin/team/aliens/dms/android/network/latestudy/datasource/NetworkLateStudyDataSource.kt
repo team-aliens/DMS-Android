b6b1b95d9860940a64a7cf46fba9e3679fb00379
ashxom <anseha090115@naver.com>
2026-04-20 22:07:28 +0900

    feat :: 새벽 자습 신청 DataSource 추가

--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSource.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSource.kt
@@ -2,8 +2,10 @@ package team.aliens.dms.android.network.latestudy.datasource
 
 import TeacherResponse
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
+import team.aliens.dms.android.network.latestudy.model.SubmitLateStudyRequest
 
 interface NetworkLateStudyDataSource {
     suspend fun fetchStudyTypes(): FetchStudyTypesResponse
     suspend fun fetchTeachers(): List<TeacherResponse>
+    suspend fun submitLateStudy(request: SubmitLateStudyRequest)
 }
--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSourceImpl.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSourceImpl.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.android.network.latestudy.datasource
 import TeacherResponse
 import team.aliens.dms.android.network.latestudy.apiservice.LateStudyApiService
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
+import team.aliens.dms.android.network.latestudy.model.SubmitLateStudyRequest
 import javax.inject.Inject
 
 class NetworkLateStudyDataSourceImpl @Inject constructor(
@@ -14,4 +15,8 @@ class NetworkLateStudyDataSourceImpl @Inject constructor(
 
     override suspend fun fetchTeachers(): List<TeacherResponse> =
         lateStudyApiService.fetchTeachers()
+
+    override suspend fun submitLateStudy(request: SubmitLateStudyRequest) {
+        lateStudyApiService.submitLateStudy(request)
+    }
 }
