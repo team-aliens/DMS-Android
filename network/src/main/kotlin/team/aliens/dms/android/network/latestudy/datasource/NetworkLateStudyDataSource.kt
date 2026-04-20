07a759aae551506cbde31f8721bd949cb8f084db
ashxom <anseha090115@naver.com>
2026-04-20 09:13:17 +0900

    feat :: 담당 선생님 조회 함수 추가

--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSource.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSource.kt
@@ -1,7 +1,9 @@
 package team.aliens.dms.android.network.latestudy.datasource
 
+import TeacherResponse
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
 
 interface NetworkLateStudyDataSource {
     suspend fun fetchStudyTypes(): FetchStudyTypesResponse
+    suspend fun fetchTeachers(): List<TeacherResponse>
 }
--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSourceImpl.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSourceImpl.kt
@@ -1,13 +1,17 @@
 package team.aliens.dms.android.network.latestudy.datasource
 
+import TeacherResponse
 import team.aliens.dms.android.network.latestudy.apiservice.LateStudyApiService
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
-
 import javax.inject.Inject
 
 class NetworkLateStudyDataSourceImpl @Inject constructor(
     private val lateStudyApiService: LateStudyApiService,
 ) : NetworkLateStudyDataSource {
+
     override suspend fun fetchStudyTypes(): FetchStudyTypesResponse =
         lateStudyApiService.fetchStudyTypes()
+
+    override suspend fun fetchTeachers(): List<TeacherResponse> =
+        lateStudyApiService.fetchTeachers()
 }
