54f99e679992cef87bccf944c91a555bae2edd85
ashxom <anseha090115@naver.com>
2026-04-20 22:06:25 +0900

    feat :: 새벽 자습 신청 API 추가

--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/apiservice/LateStudyApiService.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/apiservice/LateStudyApiService.kt
@@ -1,8 +1,11 @@
 package team.aliens.dms.android.network.latestudy.apiservice
 
 import TeacherResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
+import team.aliens.dms.android.network.latestudy.model.SubmitLateStudyRequest
 
 interface LateStudyApiService {
     @GET("/study-type")
@@ -10,4 +13,9 @@ interface LateStudyApiService {
 
     @GET("/teachers/general")
     suspend fun fetchTeachers(): List<TeacherResponse>
+
+    @POST("/study-application")
+    suspend fun submitLateStudy(
+        @Body request: SubmitLateStudyRequest
+    )
 }
