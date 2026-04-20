b2719bd494c4c78d540a93e303fa9a0e4b567754
ashxom <anseha090115@naver.com>
2026-04-20 09:27:05 +0900

    feat :: 담당 선생님 조회 레포지토리 로직 추가

--- data/src/main/kotlin/team/aliens/dms/android/data/latestudy/repository/LateStudyRepository.kt
+++ data/src/main/kotlin/team/aliens/dms/android/data/latestudy/repository/LateStudyRepository.kt
@@ -1,7 +1,11 @@
 package team.aliens.dms.android.data.latestudy.repository
 
 import team.aliens.dms.android.data.latestudy.model.StudyType
+import TeacherResponse
 
 interface LateStudyRepository {
+
     suspend fun fetchStudyTypes(): List<StudyType>
+
+    suspend fun fetchTeachers(): List<TeacherResponse>
 }
--- data/src/main/kotlin/team/aliens/dms/android/data/latestudy/repository/LateStudyRepositoryImpl.kt
+++ data/src/main/kotlin/team/aliens/dms/android/data/latestudy/repository/LateStudyRepositoryImpl.kt
@@ -2,13 +2,17 @@ package team.aliens.dms.android.data.latestudy.repository
 
 import team.aliens.dms.android.data.latestudy.mapper.toModel
 import team.aliens.dms.android.data.latestudy.model.StudyType
-import team.aliens.dms.android.data.latestudy.repository.LateStudyRepository
 import team.aliens.dms.android.network.latestudy.datasource.NetworkLateStudyDataSource
+import TeacherResponse
 import javax.inject.Inject
 
 class LateStudyRepositoryImpl @Inject constructor(
     private val networkLateStudyDataSource: NetworkLateStudyDataSource,
 ) : LateStudyRepository {
+
     override suspend fun fetchStudyTypes(): List<StudyType> =
         networkLateStudyDataSource.fetchStudyTypes().toModel()
+
+    override suspend fun fetchTeachers(): List<TeacherResponse> =
+        networkLateStudyDataSource.fetchTeachers()
 }
