11a76772b8739016012783e4a5612bcc0d722279
ashxom <anseha090115@naver.com>
2026-04-21 22:15:48 +0900

    feat :: 담당 선생님 조회 API 엔드포인트 및 응답 타입 수정

--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/apiservice/LateStudyApiService.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/apiservice/LateStudyApiService.kt
@@ -1,18 +1,18 @@
 package team.aliens.dms.android.network.latestudy.apiservice
 
-import TeacherResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
+import team.aliens.dms.android.network.latestudy.model.FetchTeachersResponse
 import team.aliens.dms.android.network.latestudy.model.SubmitLateStudyRequest
 
 interface LateStudyApiService {
     @GET("/study-type")
     suspend fun fetchStudyTypes(): FetchStudyTypesResponse
 
-    @GET("/teachers/general")
-    suspend fun fetchTeachers(): List<TeacherResponse>
+    @GET("/general")
+    suspend fun fetchTeachers(): FetchTeachersResponse
 
     @POST("/study-application")
     suspend fun submitLateStudy(
