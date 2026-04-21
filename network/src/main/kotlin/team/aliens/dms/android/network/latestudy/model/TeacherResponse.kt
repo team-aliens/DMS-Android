a427ca4b66fd43604a84affb4223e5eabf6e64ca
ashxom <anseha090115@naver.com>
2026-04-21 22:12:58 +0900

    refactor :: 담당 선생님 조회 API 응답 모델 구조 수정

--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/model/TeacherResponse.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/model/TeacherResponse.kt
@@ -1,9 +1,10 @@
-import com.google.gson.annotations.SerializedName
+package team.aliens.dms.android.network.latestudy.model
+
+data class FetchTeachersResponse(
+    val teachers: List<TeacherResponse>,
+)
 
 data class TeacherResponse(
-    @SerializedName("teacher_id")
-    val teacherId: String,
-
-    @SerializedName("teacher_name")
-    val teacherName: String,
+    val id: String,
+    val name: String,
 )
