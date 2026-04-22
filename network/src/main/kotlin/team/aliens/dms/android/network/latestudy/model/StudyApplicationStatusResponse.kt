3630590b6d97c5c6033fdeed7893a7684a5bd4e3
ashxom <anseha090115@naver.com>
2026-04-22 21:54:38 +0900

    feat :: 새벽 자습 신청 상태 response 모델 추가

---
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/model/StudyApplicationStatusResponse.kt
@@ -0,0 +1,7 @@
+package team.aliens.dms.android.network.latestudy.model
+
+data class StudyApplicationStatusResponse(
+    val status: String,
+    val startDate: String,
+    val endDate: String,
+)
