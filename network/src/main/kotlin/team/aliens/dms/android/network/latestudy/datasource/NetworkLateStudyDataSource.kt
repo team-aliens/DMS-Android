015270f4d4229a6d877a5eab2f8dd0eb966bece8
ashxom <anseha090115@naver.com>
2026-04-22 22:03:06 +0900

    feat :: 새벽 자습 신청 상태 조회 datasource 연결

--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSource.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSource.kt
@@ -2,10 +2,12 @@ package team.aliens.dms.android.network.latestudy.datasource
 
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
 import team.aliens.dms.android.network.latestudy.model.FetchTeachersResponse
+import team.aliens.dms.android.network.latestudy.model.StudyApplicationStatusResponse
 import team.aliens.dms.android.network.latestudy.model.SubmitLateStudyRequest
 
 interface NetworkLateStudyDataSource {
     suspend fun fetchStudyTypes(): FetchStudyTypesResponse
     suspend fun fetchTeachers(): FetchTeachersResponse
+    suspend fun fetchMyStudyApplicationStatus(): StudyApplicationStatusResponse
     suspend fun submitLateStudy(request: SubmitLateStudyRequest)
 }
--- network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSourceImpl.kt
+++ network/src/main/kotlin/team/aliens/dms/android/network/latestudy/datasource/NetworkLateStudyDataSourceImpl.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.android.network.latestudy.datasource
 import team.aliens.dms.android.network.latestudy.apiservice.LateStudyApiService
 import team.aliens.dms.android.network.latestudy.model.FetchStudyTypesResponse
 import team.aliens.dms.android.network.latestudy.model.FetchTeachersResponse
+import team.aliens.dms.android.network.latestudy.model.StudyApplicationStatusResponse
 import team.aliens.dms.android.network.latestudy.model.SubmitLateStudyRequest
 import javax.inject.Inject
 
@@ -15,6 +16,9 @@ class NetworkLateStudyDataSourceImpl @Inject constructor(
 
     override suspend fun fetchTeachers(): FetchTeachersResponse =
         lateStudyApiService.fetchTeachers()
+
+    override suspend fun fetchMyStudyApplicationStatus(): StudyApplicationStatusResponse =
+        lateStudyApiService.fetchMyStudyApplicationStatus()
 
     override suspend fun submitLateStudy(request: SubmitLateStudyRequest) {
         lateStudyApiService.submitLateStudy(request)
