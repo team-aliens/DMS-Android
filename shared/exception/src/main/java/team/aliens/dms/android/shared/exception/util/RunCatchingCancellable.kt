97451dea6b63f4a3057aee70e3a8271228e79fd1
유섭 <yoonbk2001027@gmail.com>
2026-04-29 14:05:40 +0900

    refactor :: 가독성 향상을 위한 runCatchingCancellable 코드 포맷팅 수정

--- shared/exception/src/main/java/team/aliens/dms/android/shared/exception/util/RunCatchingCancellable.kt
+++ shared/exception/src/main/java/team/aliens/dms/android/shared/exception/util/RunCatchingCancellable.kt
@@ -3,7 +3,9 @@ package team.aliens.dms.android.shared.exception.util
 import kotlin.coroutines.cancellation.CancellationException
 
 @Suppress("TooGenericExceptionCaught")
-suspend inline fun <T> runCatchingCancellable(crossinline block: suspend () -> T): Result<T> =
+suspend inline fun <T> runCatchingCancellable(
+    crossinline block: suspend () -> T,
+): Result<T> =
     try {
         Result.success(block())
     } catch (exception: CancellationException) {
