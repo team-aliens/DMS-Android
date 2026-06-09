9ad0dc928637ad84edc76a7e302a41642e7e7aef
ashxom <anseha090115@naver.com>
2026-06-09 23:03:25 +0900

    fix :: 키보드 노출 시 새벽자습 화면 패딩 적용

--- feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/LateStudyScreen.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/ui/LateStudyScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -101,6 +102,7 @@ fun LateStudyScreen(
             .fillMaxSize()
             .background(DmsTheme.colorScheme.background)
             .statusBarsPadding()
+            .imePadding()
             .verticalScroll(rememberScrollState())
             .padding(horizontal = 14.dp, vertical = 16.dp),
     ) {
