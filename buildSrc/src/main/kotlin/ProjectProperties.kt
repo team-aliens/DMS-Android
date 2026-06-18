14e3a7ca6f7d7aa09699a9b6d82dd5b34d54e0ad
ashxom <anseha090115@naver.com>
2026-06-18 14:48:01 +0900

    release :: version code 업데이트

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -2,6 +2,6 @@ object ProjectProperties {
     const val COMPILE_SDK = 36
     const val MIN_SDK = 28
     const val TARGET_SDK = 36
-    const val VERSION_CODE = 41
+    const val VERSION_CODE = 42
     const val VERSION_NAME = "2.1.11"
 }
