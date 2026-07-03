6b35cf22033043ac06f6340a3ed781fb847d180e
ashxom <anseha090115@naver.com>
2026-07-03 09:59:17 +0900

    feat :: 챗봇 네트워크 모듈 추가

---
+++ network/src/main/kotlin/team/aliens/dms/android/network/chatbot/di/ChatBotNetworkModule.kt
@@ -0,0 +1,32 @@
+package team.aliens.dms.android.network.chatbot.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import team.aliens.dms.android.network.chatbot.apiservice.ChatBotApiService
+import team.aliens.dms.android.network.chatbot.datasource.NetworkChatBotDataSource
+import team.aliens.dms.android.network.chatbot.datasource.NetworkChatBotDataSourceImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class ChatBotNetworkModule {
+    @Binds
+    @Singleton
+    abstract fun bindNetworkChatBotDataSource(
+        networkChatBotDataSourceImpl: NetworkChatBotDataSourceImpl,
+    ): NetworkChatBotDataSource
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideChatBotApiService(
+            retrofit: Retrofit,
+        ): ChatBotApiService {
+            return retrofit.create(ChatBotApiService::class.java)
+        }
+    }
+}
