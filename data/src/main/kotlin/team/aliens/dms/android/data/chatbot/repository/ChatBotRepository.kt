3c4377cbf4bc30cffcd6190b0762b3236a4aea0d
ashxom <anseha090115@naver.com>
2026-07-03 10:00:43 +0900

    feat :: Repository 구현체 추가

--- data/src/main/kotlin/team/aliens/dms/android/data/chatbot/repository/ChatBotRepository.kt
+++ data/src/main/kotlin/team/aliens/dms/android/data/chatbot/repository/ChatBotRepository.kt
@@ -1,14 +1,5 @@
 package team.aliens.dms.android.data.chatbot.repository
 
-import team.aliens.dms.android.network.chatbot.apiservice.ChatBotApiService
-import team.aliens.dms.android.network.chatbot.model.ChatBotQuestionRequest
-
-class ChatBotRepository(
-    private val chatBotApi: ChatBotApiService,
-) {
-    suspend fun askQuestion(question: String): String {
-        return chatBotApi.askQuestion(
-            request = ChatBotQuestionRequest(question = question),
-        ).answer
-    }
+interface ChatBotRepository {
+    suspend fun askQuestion(question: String): String
 }
