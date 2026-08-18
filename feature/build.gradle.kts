1787ceda22d6d67351600865cded36745ba7db02
ashxom <anseha090115@naver.com>
2026-08-18 14:46:54 +0900

    fix :: 잘못 주입된 테스트 의존성 삭제

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -96,7 +96,6 @@ dependencies {
     implementation(libs.threetenbp)
 
     testImplementation(libs.androidx.junit)
-    testImplementation(libs.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
 }
 
