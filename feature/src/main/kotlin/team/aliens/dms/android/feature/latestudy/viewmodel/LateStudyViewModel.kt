01762c85da8bd638136fc0cfb91eb7f8839a507c
ashxom <anseha090115@naver.com>
2026-08-18 21:13:21 +0900

    fix :: 리뷰 코멘트 반영

--- feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/viewmodel/LateStudyViewModel.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/latestudy/viewmodel/LateStudyViewModel.kt
@@ -1,5 +1,6 @@
 package team.aliens.dms.android.feature.latestudy.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,10 +12,12 @@ import javax.inject.Inject
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 import team.aliens.dms.android.core.network.exception.ConflictException
+import team.aliens.dms.android.core.network.exception.NetworkException
 import team.aliens.dms.android.data.latestudy.model.StudyType
 import team.aliens.dms.android.data.latestudy.repository.LateStudyRepository
 import team.aliens.dms.android.network.latestudy.model.SubmitLateStudyRequest
 import team.aliens.dms.android.network.latestudy.model.TeacherResponse
+import team.aliens.dms.android.shared.exception.UnknownException
 
 @HiltViewModel
 class LateStudyViewModel @Inject constructor(
@@ -90,18 +93,38 @@ class LateStudyViewModel @Inject constructor(
                         endDate = endDate,
                     ),
                 )
-                onSuccess()
             } catch (e: CancellationException) {
                 throw e
             } catch (e: ConflictException) {
+                Log.w(TAG, "이미 새벽 자습을 신청했습니다.", e)
                 onFailure("이미 새벽 자습을 신청했습니다.")
+                return@launch
             } catch (e: IOException) {
-                onFailure("새벽 자습 신청에 실패했습니다.")
-            } catch (e: Exception) {
-                onFailure("새벽 자습 신청에 실패했습니다.")
+                handleSubmitFailure(e, onFailure)
+                return@launch
+            } catch (e: NetworkException) {
+                handleSubmitFailure(e, onFailure)
+                return@launch
+            } catch (e: UnknownException) {
+                handleSubmitFailure(e, onFailure)
+                return@launch
             } finally {
                 isSubmitting = false
             }
+
+            onSuccess()
         }
+    }
+
+    private fun handleSubmitFailure(
+        throwable: Throwable,
+        onFailure: (String) -> Unit,
+    ) {
+        Log.e(TAG, "새벽 자습 신청에 실패했습니다.", throwable)
+        onFailure("새벽 자습 신청에 실패했습니다.")
+    }
+
+    private companion object {
+        const val TAG = "LateStudyViewModel"
     }
 }
