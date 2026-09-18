7b1ab13a405011d26da4c29be31ce88f98c49455
ashxom <anseha090115@naver.com>
2026-09-18 19:56:31 +0900

    fix :: 세션 종료 전 디바이스 토큰 해제 지원

--- core/jwt/src/main/java/team/aliens/dms/android/core/jwt/SessionCleaner.kt
+++ core/jwt/src/main/java/team/aliens/dms/android/core/jwt/SessionCleaner.kt
@@ -1,5 +1,10 @@
 package team.aliens.dms.android.core.jwt
 
 abstract class SessionCleaner {
-    abstract suspend fun clearSession(tokensAlreadyCleared: Boolean = false)
+    abstract suspend fun unregisterDeviceToken()
+
+    abstract suspend fun clearSession(
+        tokensAlreadyCleared: Boolean = false,
+        shouldUnregisterDeviceToken: Boolean = true,
+    )
 }
