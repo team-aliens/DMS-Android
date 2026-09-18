a29b82b87b0bb4e7d9fdddc88b064d9a3c1b11fb
ashxom <anseha090115@naver.com>
2026-09-18 17:29:11 +0900

    fix :: 로그인 상태에 따른 급식 위젯 작업 갱신

--- app/src/main/kotlin/team/aliens/dms/android/app/DmsApplication.kt
+++ app/src/main/kotlin/team/aliens/dms/android/app/DmsApplication.kt
@@ -4,6 +4,13 @@ import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import team.aliens.dms.android.core.jwt.JwtProvider
+import team.aliens.dms.android.core.widget.MealWorker
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -11,6 +18,24 @@ class DmsApplication : Application(), Configuration.Provider {
 
     @Inject
     lateinit var workFactory: HiltWorkerFactory
+
+    @Inject
+    lateinit var jwtProvider: JwtProvider
+
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    override fun onCreate() {
+        super.onCreate()
+        applicationScope.launch {
+            jwtProvider.isCachedRefreshTokenAvailable.collect { isAvailable ->
+                if (isAvailable) {
+                    MealWorker.enqueue(this@DmsApplication)
+                } else {
+                    MealWorker.clear(this@DmsApplication)
+                }
+            }
+        }
+    }
 
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
