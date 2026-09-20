64cc702aff5a541cf040ea623f82b72bc0dec3a5
ashxom <anseha090115@naver.com>
2026-09-20 23:04:59 +0900

    fix :: 잔류 상태 표기 오류 수정

--- feature/src/main/kotlin/team/aliens/dms/android/feature/remain/viewmodel/RemainApplicationViewModel.kt
+++ feature/src/main/kotlin/team/aliens/dms/android/feature/remain/viewmodel/RemainApplicationViewModel.kt
@@ -20,21 +20,26 @@ class RemainApplicationViewModel @Inject constructor(
 ): BaseStateViewModel<RemainApplicationState, Unit>(RemainApplicationState()) {
 
     init {
-        getRemainsOptions()
+        getRemainsApplication()
         getRemainsApplicationTime()
     }
 
-    private fun getRemainsOptions() {
+    private fun getRemainsApplication() {
         viewModelScope.launch {
-            remainsRepository.fetchRemainsOptions().onSuccess { remainsOptions ->
-                val selectRemainsOptionId =
-                    remainsOptions.find { it.applied }?.id
-                setState {
-                    it.copy(
-                        remainsOptions = remainsOptions,
-                        selectRemainsOptionId = selectRemainsOptionId,
-                    )
-                }
+            val remainsOptions = remainsRepository.fetchRemainsOptions().getOrNull()
+                ?: return@launch
+            val appliedRemainsOption = remainsRepository.fetchAppliedRemainsOption().getOrNull()
+            val appliedRemainsOptionId = appliedRemainsOption?.id
+            val appliedRemainsOptionTitle = appliedRemainsOption?.title
+
+            setState {
+                it.copy(
+                    remainsOptions = remainsOptions.map { remainsOption ->
+                        remainsOption.copy(applied = remainsOption.id == appliedRemainsOptionId)
+                    },
+                    selectRemainsOptionId = appliedRemainsOptionId,
+                    selectedRemainTitle = appliedRemainsOptionTitle,
+                )
             }
         }
     }
